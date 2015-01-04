d4099197a91e17824ace40afa177d645b21ca528
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-04 15:20:09 +0000

    Initial commit of the hello_world project, module1
    git-svn-id: http://go-java-1.googlecode.com/svn@6 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/JavaMasters/alexey/module1/src/org/kudryavtsev/gojava/basic/helloworld/Main.java
@@ -0,0 +1,17 @@
+/**
+ *  GoJava2
+ *  Project #1 "Hello world"
+ *  Date: 2014-12-28
+ */
+package org.kudryavtsev.gojava.basic.helloworld;
+
+/**
+ * @author Omicron
+ * @version 0.02
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
