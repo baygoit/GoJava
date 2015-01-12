cf60aa2fc2cb3398e60ee15e95d82db5219e6c10
Antonina.Cherednichenko@gmail.com <Antonina.Cherednichenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-12 18:28:31 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@53 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/JavaMasters/anna/module1/HelloWorld/src/am/java/Hello.java
+++ trunk/JavaMasters/anna/module1/HelloWorld/src/am/java/Hello.java
@@ -1,11 +1,11 @@
-package am.java; // посмотри code conventions по именованию пакетов (подсказка  - доменное имя организации в обратном порядке)
+package am.java; 
 
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println ("Hello World!");
-     System.out.println ("Hi, Antonina! Have a nice day :)"); // thanks, same to you :)
+     System.out.println ("Hi, Antonina! Have a nice day :)"); 
 	}
 
 }
