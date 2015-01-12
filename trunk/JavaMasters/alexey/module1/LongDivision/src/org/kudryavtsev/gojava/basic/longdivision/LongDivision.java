4c5e7a1845df821ed8c23818678c95ff0352937e
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-12 21:03:51 +0000

    Commit of the LongDivision project, module1, alexey
    git-svn-id: http://go-java-1.googlecode.com/svn@55 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/JavaMasters/alexey/module1/LongDivision/src/org/kudryavtsev/gojava/basic/longdivision/LongDivision.java
+++ trunk/JavaMasters/alexey/module1/LongDivision/src/org/kudryavtsev/gojava/basic/longdivision/LongDivision.java
@@ -10,76 +10,48 @@ import java.util.Scanner;
 
 /**
  * @author Omicron
- * @version 0.05
+ * @version 0.08
  */
 
 public class LongDivision {
 
     public static void main(String[] args) {
 	final int DEPTH = 6;
-	String inputString = "12/42";
-	// Scanner in = new Scanner(System.in);
-	// inputString = in.nextLine();
-	// in.close();
+	String inputString = "";
+	System.out.println("Input numerator/denominator (e.g. 12/42):");
+	Scanner in = new Scanner(System.in);
+	inputString = in.nextLine();
+	in.close();
 
 	analize(DEPTH, inputString);
     }
 
     private static void analize(final int DEPTH, String stringToAnalize) {
 	int positionOfDevider = stringToAnalize.indexOf("/");
-	int numerator = Integer.parseInt(stringToAnalize.substring(0,
-		positionOfDevider));
-	int denominator = Integer.parseInt(stringToAnalize.substring(
-		positionOfDevider + 1, stringToAnalize.length()));
+	int numerator = Integer.parseInt(stringToAnalize.substring(0, positionOfDevider));
+	int denominator = Integer.parseInt(stringToAnalize.substring(positionOfDevider + 1, stringToAnalize.length()));
 
 	printDivision(DEPTH, numerator, denominator);
     }
 
-    private static void printDivision(final int DEPTH, int numerator,
-	    int denominator) {
+    private static void printDivision(final int DEPTH, int numerator, int denominator) {
 	double result = (double) numerator / denominator;
-
 	System.out.println(" " + numerator + " |" + denominator);
+	System.out.print(" ");
+	
+	int tempNumber;
+	int tempDigit;
+	String adjust = "";
+	String suffix = "|" + result;
+	tempNumber = numerator * 10;
 
-	    int t;
-	    int tempDigit;
-	    String adjust = "";
-	    String suffix = "|" + result;
-	    t = numerator * 10;
-	    System.out.print(" ");
-	    
 	for (int i = 0; i < DEPTH; i++) {
-
 	    tempDigit = (int) (result * 10 * Math.pow(10, i)) % 10;
 	    System.out.println(adjust + "-" + tempDigit * denominator + suffix + "\n" + adjust + " ---");
-	    t = (t - tempDigit * denominator) * 10;
-	    adjust +=" ";
+	    tempNumber = (tempNumber - tempDigit * denominator) * 10;
+	    adjust += " ";
 	    suffix = "";
-	    System.out.println(" " + adjust + t);
-//	    i++;
-//	    
-//
-//	    tempDigit = (int) (result * 10 * Math.pow(10, i)) % 10;
-//	    System.out.println(adjust + "-" + tempDigit * denominator + suffix + "\n" + adjust + " ---");
-//	    t = (t - tempDigit * denominator) * 10;
-//	    adjust +=" ";
-//	    suffix = "";	    
-//	    System.out.println(" " + adjust + t);
-//	    i++;
-//
-//
-//	    tempDigit = (int) (result * 10 * Math.pow(10, i)) % 10;
-//	    System.out.println(adjust + "-" + tempDigit * denominator + suffix + "\n" + adjust + " ---");
-//	    t = (t  - tempDigit * denominator) * 10;
-//	    adjust +=" ";
-//	    suffix = "";	    
-//	    System.out.println(" " + adjust + t);
-//	    i++;
-
-
-	    
-	    
+	    System.out.println(" " + adjust + tempNumber);
 	}
     }
-
 }
