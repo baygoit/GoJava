e540b0ea76a8ba1aae670f0fb5c3c8dac65c06da
alex.sash.kh@gmail.com <alex.sash.kh@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-14 15:51:50 +0000

    Initial commit of unit1
    git-svn-id: http://go-java-1.googlecode.com/svn@100 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/oleksandr.holmov/unit1/hello_world/src/ua/com/goit/gojava/alex_kholmov/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ua.com.goit.gojava.alex_kholmov;
+
+/**
+ * @author Alex Kholmov
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
