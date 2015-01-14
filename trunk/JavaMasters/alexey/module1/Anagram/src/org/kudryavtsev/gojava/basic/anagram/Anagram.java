86ce215c4c8c82ccc9b528ee75dad5aaa91fef69
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-14 01:01:04 +0000

    Commit version1 of the "Anagram" project, module1, alexey
    git-svn-id: http://go-java-1.googlecode.com/svn@63 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/JavaMasters/alexey/module1/Anagram/src/org/kudryavtsev/gojava/basic/anagram/Anagram.java
+++ trunk/JavaMasters/alexey/module1/Anagram/src/org/kudryavtsev/gojava/basic/anagram/Anagram.java
@@ -9,50 +9,31 @@ import java.util.Scanner;
 
 /**
  * @author Omicron
- * @version 0.12
+ * @version 0.15
  */
 public class Anagram {
 
     public static void main(String[] args) {
-
 	String stringToAnalize = "";
 	System.out.println("Enter the string:");
-
 	Scanner in = new Scanner(System.in);
 	stringToAnalize = in.nextLine();
 	in.close();
-	System.out.println(reversedLine(stringToAnalize));
+	printReversedLine(stringToAnalize);
     }
 
-    static String reversedLine(String stringToReverse) {
-	stringToReverse = stringToReverse.concat(" ");
-	String output, word;
-	output = "";
-	word = "";
-	int stringToReverseLength = stringToReverse.length();
-
-	for (int i = 1; i <= stringToReverseLength; i++) {
-	    String letter;
-
-	    letter = stringToReverse.substring(i - 1, i);
-
-	    if (letter.contains(" ")) {
-		output = output.concat(reverseWord(word) + " ");
+    static void printReversedLine(String stringToReverse) {
+	String word = "", letter = "";
+	for (int i = 0; i < stringToReverse.length(); i++) {
+	    letter = stringToReverse.substring(i, i + 1);
+	    word = letter + word;
+	    if (letter.equals(" ")) {
+		System.out.print(word.substring(1, word.length()) + " ");
 		word = "";
 	    } else {
-		word = word.concat(letter);
+		if (i == stringToReverse.length() - 1)
+		    System.out.print(word);
 	    }
 	}
-	return output.substring(0, stringToReverseLength - 1);
     }
-
-    static String reverseWord(String stringToReverse) {
-	String resultString = "";
-	for (int i = stringToReverse.length(); i > 0; i--) {
-	    resultString = resultString.concat(stringToReverse.substring(i - 1,
-		    i));
-	}
-	return resultString;
-    }
-
 }
