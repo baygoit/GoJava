21eac4a360f5e2e0bd73ba596ff6d5d367dfcb3a
Tima.PeaceWorld@gmail.com <Tima.PeaceWorld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 09:17:03 +0000

    Commit Hello World
    git-svn-id: http://go-java-1.googlecode.com/svn@148 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/tymur.berezhnoy/module1/Hello World/src/ButtonsProperties.java
@@ -0,0 +1,23 @@
+public class ButtonsProperties {
+	private String titleOfButtons;
+	private String language;
+	private String sentence;
+
+	public ButtonsProperties(String titleOfButtons, String sentence, String language) {
+		this.titleOfButtons = titleOfButtons;
+		this.language = language;
+		this.sentence = sentence;
+	}
+	
+	public String getTitleOfButtons() {
+		return titleOfButtons;
+	}
+	
+	public String getLanguage() {
+		return language;
+	}
+	
+	public String getSentence() {
+		return sentence;
+	}
+}
---
+++ trunk/GoJava2/tymur.berezhnoy/module1/Hello World/src/RunHelloWorld.java
@@ -0,0 +1,7 @@
+public class RunHelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorldView helloWorldViewOb = new HelloWorldView();
+		helloWorldViewOb.startProcessing();
+	}
+}
