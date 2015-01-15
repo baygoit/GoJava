83c794abc78f1263dd439f8e8cb51361f1197e60
lslayer2005@gmail.com <lslayer2005@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 08:47:23 +0000

    Initial project import
    git-svn-id: http://go-java-1.googlecode.com/svn@140 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/viktor.gogilchyn/src/main/java/ua/lslayer/hackit/Account.java
@@ -0,0 +1,33 @@
+package ua.lslayer.hackit;
+
+public class Account {
+    private String login;
+    private String password;
+    private boolean logged = false;
+    private Hero hero = new Hero("Meg@P|h@r20125");
+    
+    public Account (String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+    
+    public boolean login (String login, String password) {
+         this.logged = ((login    == this.login) && 
+                        (password == this.password));
+         return this.logged;
+        
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void setHero(Hero hero) {
+        this.hero = hero;
+    }
+
+    public boolean loggedIn() {
+        // TODO Auto-generated method stub
+        return this.logged;
+    }
+}
