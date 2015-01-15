29ee0fdb7c1557a8cac74ea180e69524943a1b8a
iavorskyi.v.v@gmail.com <iavorskyi.v.v@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 19:33:54 +0000

    just formated (code convention)
    git-svn-id: http://go-java-1.googlecode.com/svn@223 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/volodymyr.iavorskiy/module1/src/ua/com/goit/gojava1/iavorskyi/petproject/Login.java
+++ trunk/GoJava1/volodymyr.iavorskiy/module1/src/ua/com/goit/gojava1/iavorskyi/petproject/Login.java
@@ -12,7 +12,7 @@ public class Login {
 		String login = null;
 		String password = null;
 		String repeatPassword = null;
-		
+
 		if (input.equals("1")) {
 			System.out.println("input login: ");
 			login = in.next();
@@ -23,17 +23,17 @@ public class Login {
 			System.out.println("input login: ");
 			login = in.next();
 			do {
-			System.out.println("input password: ");
-			password = in.next();
-			System.out.println("repeat password: ");
-			repeatPassword = in.next();
-			if (!password.equals(repeatPassword)) {
-				System.out.println("passwords does not match, please repeat.");
-			}
+				System.out.println("input password: ");
+				password = in.next();
+				System.out.println("repeat password: ");
+				repeatPassword = in.next();
+				if (!password.equals(repeatPassword)) {
+					System.out.println("passwords does not match, please repeat.");
+				}
 			} while (!password.equals(repeatPassword));
 			System.out.println("you are registered.");
 		}
-		
+
 		in.close();
 
 	}
