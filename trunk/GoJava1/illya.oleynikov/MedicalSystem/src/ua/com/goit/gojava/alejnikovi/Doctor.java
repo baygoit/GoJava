fec1fff1c8501244f0ef382d233e00ec109b94da
Alejnikov.I@gmail.com <Alejnikov.I@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 16:19:29 +0000

    Method input moved to main. Contructor for Doctor class changed.
    git-svn-id: http://go-java-1.googlecode.com/svn@204 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/illya.oleynikov/MedicalSystem/src/ua/com/goit/gojava/alejnikovi/Doctor.java
+++ trunk/GoJava1/illya.oleynikov/MedicalSystem/src/ua/com/goit/gojava/alejnikovi/Doctor.java
@@ -8,22 +8,12 @@ public class Doctor {
 	String surname;
 	String specialization;
 
-	Doctor (){
-		System.out.println("Внесите имя нового доктора");
-		name = input();
-		System.out.println("Внесите фамилию нового доктора");
-		surname = input();
-		System.out.println("Внесите специализацию нового доктора");
-		specialization = input();
-		System.out.println("Спасибо! Добавлен новый доктор: " + name + " " + surname + " " + specialization);
-		System.out.println();
-	}
-	
-	public static String input(){
-		Scanner sc = new Scanner(System.in);
-		return sc.nextLine();
-	}
-	
+	Doctor (String name, String surname, String specialisation){
+		this.name = name;
+		this.surname = surname;
+		this.specialization = specialization;
+	}  
+  
 	public String getSpec(){
 		return this.specialization;
 	}
