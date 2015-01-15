eecd0ea4258d2d1290c50285b5f48d6a2192a8bc
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 11:15:10 +0000

    Commit sources files of Hello World project.
    To run project:
    1) Before import all source files in your Eclipse work space, set in Eclipse UTF-8 encoding (Preferences -> General -> Workspace ->  Text file encoding (set UTF-8)).
    2) Add "jl1.0.1.jar" from JLayer1.0.1 path into your work space project in Eclipse (Right clic on your project -> Build Path -> Add External Archives and chose jl1.0.1.jar).
    
    git-svn-id: http://go-java-1.googlecode.com/svn@171 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/tymur.berezhnoy/module1/Hello World/src/home/timaPeaceWorld/GoJava2/basics/RunHelloWorld.java
@@ -0,0 +1,8 @@
+package home.timaPeaceWorld.GoJava2.basics;
+public class RunHelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorldView helloWorldViewOb = new HelloWorldView();
+		helloWorldViewOb.startProcessing();
+	}
+}
