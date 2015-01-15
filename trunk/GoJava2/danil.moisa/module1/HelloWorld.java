cc64800d6dc8f58f2e4271ffef855bee94eb8a53
d.moysa@gmail.com <d.moysa@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 22:45:27 +0000

    Hello
    git-svn-id: http://go-java-1.googlecode.com/svn@244 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/danil.moisa/module1/HelloWorld.java
@@ -0,0 +1,9 @@
+package ua.com.goit.gojava.Moisa.HelloWorld;
+
+//First program - "Hello world"
+//
+public class HelloWorld{
+	public static void main (String args[]){
+		System.out.println("Hello world!");
+	}
+}
