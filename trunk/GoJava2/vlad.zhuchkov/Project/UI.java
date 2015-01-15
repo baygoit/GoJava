8df563ed268c018679bc2738658261be01fe207c
zhuchkov.vlad@gmail.com <zhuchkov.vlad@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-15 01:09:47 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@130 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/vlad.zhuchkov/Project/UI.java
@@ -0,0 +1,18 @@
+package economicalEfficiency;
+
+public class UI {
+	
+	public static void main (String[] args){
+		while(true){
+			System.out.println("select option");
+			
+		/*
+		 *  
+		 * 
+		 * 
+		 */
+		System.out.println("do you whant to do something else?");
+		}
+	}
+
+}
