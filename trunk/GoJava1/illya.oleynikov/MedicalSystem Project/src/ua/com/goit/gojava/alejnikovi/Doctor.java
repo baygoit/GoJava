21edcf7227956cfbb29423c8358a22e9f88458d5
Alejnikov.I@gmail.com <Alejnikov.I@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-16 17:14:26 +0000

    Initial import v2 project. Created list for doctors specializations and method for choosing it. User story 2 completed. New method for check if any doctor got selected specialization.
    git-svn-id: http://go-java-1.googlecode.com/svn@328 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/illya.oleynikov/MedicalSystem Project/src/ua/com/goit/gojava/alejnikovi/Doctor.java
@@ -0,0 +1,19 @@
+package ua.com.goit.gojava.alejnikovi;
+
+public class Doctor {
+
+	String name;
+	String surname;
+	String specialization;
+
+	Doctor (String name, String surname, String specialization){
+		this.name = name;
+		this.surname = surname;
+		this.specialization = specialization;
+	}  
+  
+	public String getSpec(){
+		return this.specialization;
+	}
+
+}
---
+++ trunk/GoJava1/illya.oleynikov/MedicalSystem Project/src/ua/com/goit/gojava/alejnikovi/MedicalSystem.java
@@ -0,0 +1,75 @@
+package ua.com.goit.gojava.alejnikovi;
+
+import java.util.*;
+
+public class MedicalSystem {
+
+	public static void main(String[] args) {
+		
+        ArrayList<Doctor> doctors = new ArrayList<Doctor>();
+        ArrayList<String> specs = new ArrayList<String>();
+        specs.add("Терапевт");
+        specs.add("Хирург");
+        specs.add("Уростоматолог");
+        
+        int userSelection = 0;
+        String specSelection;
+        
+        while (userSelection != 3) try {
+            System.out.println();
+            userSelection = Integer.parseInt(input("Для создания нового врача введите '1'" + "\n" +
+                                            "Для просмотра списка врачей введите '2'" + "\n" +
+                                            "Для выхода введите '3'"));
+            System.out.println();
+            if (userSelection == 1){
+                doctors.add(createDoctor(specs));
+            } else if (userSelection == 2){
+            	specSelection = selectSpec(specs);
+            	if (isHaveSpec(doctors, specSelection)){
+            		System.out.println("Список врачей по выбранной Вами специализации:");
+            		for (Doctor doc: doctors){
+                		if (doc.specialization.equals(specSelection)){
+                			System.out.println(doc.name + " " + doc.surname);
+                		}
+                	}
+            	} else {
+            		System.out.println("Врачей по выбранной Вами специализации не найдено");
+            	}            	
+            }
+        } catch (NumberFormatException e){
+            System.err.println("Вы должны внести цифру 1, 2 или 3");
+        }        
+ 
+    }
+ 
+    public static String input(String prompt){
+    	System.out.println(prompt);     	
+        return (new Scanner(System.in)).nextLine();
+    }
+ 
+    private static Doctor createDoctor(ArrayList<String> specs){
+        String name = input("Введите имя врача");
+        String surname = input("Введите фамилию врача");
+        String spec = selectSpec(specs); 
+        System.out.println("Добавлен новый врач - " + name + " " + surname + ", " + spec);
+        return new Doctor(name, surname, spec);
+     }
+    
+    private static String selectSpec(ArrayList<String> specs){
+    	System.out.println('\n' + "Введите номер специализации врача, которую вы хотите выбрать:");
+    	for (int i = 0; i < specs.size(); i++){
+    		System.out.println((i + 1) + ". " + specs.get(i));
+    	}
+    	int specIndex = Integer.parseInt(input("")) - 1;
+    	return specs.get(specIndex);
+    	
+    }	
+    
+    private static boolean isHaveSpec(ArrayList<Doctor> doctors, String spec){
+    	for(Doctor doc: doctors){
+    		if(doc.specialization.equals(spec)) return true;
+    	}
+    	return false;
+    }
+
+}
