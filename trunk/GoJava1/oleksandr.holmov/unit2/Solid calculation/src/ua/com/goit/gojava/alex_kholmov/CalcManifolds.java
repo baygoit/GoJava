8945acfb09411f013daa0bdb7e4b14dfdeec8ab1
alex.sash.kh@gmail.com <alex.sash.kh@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-16 11:37:26 +0000

    Initial commit of unit2. First project version
    git-svn-id: http://go-java-1.googlecode.com/svn@291 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/oleksandr.holmov/unit2/Solid calculation/src/ua/com/goit/gojava/alex_kholmov/CalcManifolds.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ua.com.goit.gojava.alex_kholmov;
+
+import java.util.Map;
+import java.util.HashMap;
+/**
+ * @author Alex Kholmov
+ *
+ */
+public class CalcManifolds {
+    //stub method
+    public static void printCalcType(int mPoint) {
+        System.out.print(MainMenu.mMenu.get(mPoint));
+    }
+}
