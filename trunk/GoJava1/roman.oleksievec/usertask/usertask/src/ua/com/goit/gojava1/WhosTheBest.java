83a5ca7a1890c2fa467f0fa91d0325463962ae9e
Oleks.rome@gmail.com <Oleks.rome@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-16 12:01:16 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@295 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/roman.oleksievec/usertask/usertask/src/ua/com/goit/gojava1/WhosTheBest.java
@@ -0,0 +1,14 @@
+package ua.com.goit.gojava1;
+
+public class WhosTheBest extends Players{
+
+public static void main(String[] args) {
+	WhosTheBest run = new WhosTheBest();
+	run.addPlayers(bestPlayers);
+	run.findPlayer();
+	run.showList();
+	
+	    }
+
+}
+
