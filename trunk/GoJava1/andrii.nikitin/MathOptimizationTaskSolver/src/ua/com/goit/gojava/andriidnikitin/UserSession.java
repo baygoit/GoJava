98263efbd37515adf7dedc4ffdc49d92ef265a0b
andrii.d.nikitin@gmail.com <andrii.d.nikitin@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-17 10:53:23 +0000

    - deleted static method of generation for UserSession - new method goToSolution()
    git-svn-id: http://go-java-1.googlecode.com/svn@347 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/andrii.nikitin/MathOptimizationTaskSolver/src/ua/com/goit/gojava/andriidnikitin/UserSession.java
+++ trunk/GoJava1/andrii.nikitin/MathOptimizationTaskSolver/src/ua/com/goit/gojava/andriidnikitin/UserSession.java
@@ -27,7 +27,10 @@ public class UserSession {
 	
 	static public final int COMMAND_NEXT = -3;		
 
-	static public final int FLAG_X = 0;	
+	static public final int FLAG_X = 1;		
+
+	static public final int FLAG_XNB = 7;	
+	
 	
 	static {
 		setProblemsList();
@@ -46,67 +49,61 @@ public class UserSession {
 	}
 	
 	public static void main(String[] args) throws Exception{
-		UserSession actualSession = 
-				UserSession.newSession("default", System.out, System.in);
+		UserSession actualSession =  
+				new UserSession("default", System.in, System.out);
 	}
 	
-	public static UserSession newSession(String login, PrintStream outStreamArg,
-			InputStream inStreamArg){
-		return new UserSession(login, outStreamArg, inStreamArg);
-	}
 	
-	private UserSession (String login, PrintStream outStreamArg, 
-			InputStream inStreamArg){
+	public UserSession (String login,  
+			InputStream inStreamArg, PrintStream outStreamArg){
 		userName = login;
 		outStream = outStreamArg;
 		inputSource = new Scanner (inStreamArg);
 		addNewVisit();		
-		startSession();
+		startSession(login);
 	}
 	
 	private void addNewVisit(){
 		guestBook.add(userName + " has entered the system.");
 	}
 	
-	private void startSession(){
-		outStream.println("Your session is launched.");
+	private void startSession(String login){
+		outStream.println("Welcome, " + login);
 		launchMenu();
 	}
 	
+	private void launchMenu(){
+		printBlockSeparator();
+		printProblemList();
+		goFromMainMenu();
+		//TODO - farawell();
+	}			
+		
 	private void printProblemList(){
 		outStream.println("Choose a problem You want to solve:\n");
 		for (int i = 0; i < listOfProblems.size(); i++)
-			outStream.println(i + " - " + listOfProblems.get(i) );
+			outStream.println(i + 1 + " - " + listOfProblems.get(i) );
 		outStream.println();
 	}
-	
-	private void chooseProblem(int numberOfProblem){
-		switch (numberOfProblem){
-			case 0: solubleProblemUnit = SolubleProblem.LINEAR_PROGRAM;
-			break;
-			case 1: solubleProblemUnit = SolubleProblem.TRANSPORTATION_PROBLEM;
-			break;
-			default: solubleProblemUnit = null;
-			break;
-		}
-	}	
 	
 	private void printBlockSeparator(){
 		outStream.println("\n===========================================\n");
 	}	
 	
-	private void printCommands(int flags){
+	private void printCommands(int flags){//TODO- refactor
 		StringBuilder output = new StringBuilder();
-		if (flags / 2 == 0) output.append("x - exit; ");
-		if (flags / 2 == 1) output.append("n - next; ");
-		if (flags / 4 == 1) output.append("b - back; ");
-		if (flags / 8 == 1) output.append("v - vendetta; ");//TODO - delete
-		outStream.println(output);
+		if (flags % 2 == 1) output.append("x - exit; ");
+		flags = flags / 2;
+		if (flags % 2 == 1) output.append("n - next; ");
+		flags = flags / 2;
+		if (flags % 2 == 1) output.append("b - back; ");
+		flags = flags / 2;
+		if (flags % 2 == 1) output.append("s - skip; ");
+		outStream.println(output + "\n");
 	}
 	
-	private int getCommand(){
-		//String input =  getString(); TODO is proxy needed
-		String input =  inputSource.nextLine();
+	private int getCommand(){//TODO- refactor
+		String input =  getString();
 		try {
 			return Integer.parseInt(input);
 		} catch (NumberFormatException exception){
@@ -128,36 +125,69 @@ public class UserSession {
 		return COMMAND_UNKNOWN;
 	}	
 	
-	private String getString(){//is such proxy needed?
+	private String getString(){
 		return inputSource.nextLine();
 	}
-		
-	private void launchMenu(){
-		printBlockSeparator();
-		printProblemList();
-		goFromMainMenu();
-		//TODO - farawell();
-	}			
 	
-	private void goFromMainMenu(){
+	private void chooseProblem(int numberOfProblem){//TODO - redo with cycle 
+		switch (numberOfProblem){
+			case 0: solubleProblemUnit = SolubleProblem.LINEAR_PROGRAM;
+			break;
+			case 1: solubleProblemUnit = SolubleProblem.TRANSPORTATION_PROBLEM;
+			break;
+			default: solubleProblemUnit = null;
+			break;
+		}
+	}	
+			
+	private void goFromMainMenu(){//TODO- refactor
 		printCommands(FLAG_X);
 		int command = getCommand();
 		switch (command){
-			case COMMAND_EXIT: return;
-			case COMMAND_UNKNOWN :{
-				//throw new IllegalArgumentException(); TODO - make correct exception
-				return;//TODO - delete
-			}
-			//break; TODO - perform all this 3 todo's as one
-			case 1 : chooseProblem(1);
+			case COMMAND_EXIT: 
+				return;
+			case 1 : 
+				chooseProblem(0);
 				break;
-			case 2 : chooseProblem(2);
+			case 2 : 
+				chooseProblem(1);
 				break;
-			default: return;
+			default: command = getCommand();
+			break;
 		}
 		goToProblem();
 	}
-	private void goToProblem(){
+	
+	
+	private void goToProblem(){//TODO- refactor
 		solubleProblemUnit.describe(outStream);
+		printCommands(FLAG_XNB);
+		int command = getCommand();
+		switch (command){
+			case COMMAND_EXIT: 
+				return;
+			case COMMAND_BACK: 
+				launchMenu();	
+			case COMMAND_NEXT: 
+				goToSolution();
+			default: command = getCommand();
+			break;
+		}
+	}
+	
+	private void goToSolution(){//TODO- refactor
+		solubleProblemUnit.solveProblem(inputSource, outStream);
+		printCommands(FLAG_XNB);
+		int command = getCommand();
+		switch (command){
+			case COMMAND_EXIT: 
+				return;
+			case COMMAND_BACK: 
+				goToProblem();	
+			case COMMAND_NEXT: 
+				launchMenu();
+			default: command = getCommand();
+			break;
+		}
 	}
 }
