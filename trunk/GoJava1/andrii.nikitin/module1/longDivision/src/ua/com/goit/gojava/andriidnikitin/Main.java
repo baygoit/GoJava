9bf4108c47db900e853f2564db2458813e273299
andrii.d.nikitin@gmail.com <andrii.d.nikitin@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-18 15:18:55 +0000

    added new methods lengthOfNumber() and getIntNumber()
    git-svn-id: http://go-java-1.googlecode.com/svn@388 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/andrii.nikitin/module1/longDivision/src/ua/com/goit/gojava/andriidnikitin/Main.java
+++ trunk/GoJava1/andrii.nikitin/module1/longDivision/src/ua/com/goit/gojava/andriidnikitin/Main.java
@@ -13,28 +13,19 @@ public class Main {
 	static int steps = 0;
 	public static Vector<Integer> dividends = new Vector<Integer>();
 	public static void main(String[] args) throws IOException{
-		int dividend = 0;
-		int divider = 0;	
+		int dividend;
+		int divider;	
 		try(Scanner inputSource = new Scanner(System.in)){
-			System.out.println("Type input numbers:");
-			System.out.println("dividend = ");	
-			try{
-				dividend = Integer.parseInt(inputSource.nextLine());
-			}
-			catch(NumberFormatException e){
-				throw new IOException();
-			}
+			System.out.println ("Type input numbers:"
+					+ "");
+			System.out.println ("dividend = ");	
+			dividend = getIntNumber(inputSource);
 			System.out.println("divider = ");
-			try{
-				divider = Integer.parseInt(inputSource.nextLine());
-			}
-			catch(NumberFormatException e){
-				throw new IOException();
-			}		
+			divider = getIntNumber(inputSource);
 			longDivision(dividend, divider);
 			visualise();
 		}
-		catch (IOException e){
+		catch (IllegalArgumentException e){
 			System.out.println("Wrong input!");
 		}
 	}
@@ -44,7 +35,7 @@ public class Main {
 				log.add("0 : 0 = NaN");
 				return "NaN";
 			}
-			else{
+			else {
 				log.add("0 : " 
 						+ inputDivider + " = 0");
 				return "0"; 
@@ -172,6 +163,16 @@ public class Main {
 	public static int differenceOfLengths(int arg0, int arg1){
 		return Integer.valueOf(arg0).toString().length()
 				-Integer.valueOf(arg1).toString().length();
+	}
+	
+	public static int lengthOfNumber(int number){
+		return Integer.valueOf(number).toString().length();
+	}
+	
+	public static int getIntNumber(Scanner inputSource) 
+			throws NumberFormatException{
+		String input = inputSource.nextLine();
+		return Integer.parseInt(input);
 	}
 	
 }
