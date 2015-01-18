f0b1846757dcc1f5d0b8554f24a28b1094dc72c0
lslayer2005@gmail.com <lslayer2005@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-18 21:29:23 +0000

    Minor changes
    git-svn-id: http://go-java-1.googlecode.com/svn@402 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/viktor.gogilchyn/HackIt/src/main/java/ua/lslayer/hackit/computers/Copmutable.java
+++
@@ -1,9 +0,0 @@
-package ua.lslayer.hackit.computers;
-
-import ua.lslayer.hackit.Action;
-import ua.lslayer.hackit.computers.hardware.Hardware;
-
-public interface Copmutable {
-	public void execute(Action action);
-	public void placeHardware(Hardware item);
-}
