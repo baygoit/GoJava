4ff7bd1657861fcfd989615c9c9b544c78615518
andrii.d.nikitin@gmail.com <andrii.d.nikitin@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-19 23:13:50 +0000

    -translated user cases -added fields with getters/setters for Goods -added lists and methods for Store
    git-svn-id: http://go-java-1.googlecode.com/svn@439 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/andrii.nikitin/MyShop/src/ua/com/goit/gojava/andriidnikitin/Goods.java
+++ trunk/GoJava1/andrii.nikitin/MyShop/src/ua/com/goit/gojava/andriidnikitin/Goods.java
@@ -1,5 +1,43 @@
 package ua.com.goit.gojava.andriidnikitin;
 
 public class Goods {
+	
+	private String category;
+	
+	private String name;
+	
+	private int id;
+		
+	static private int maxID = 1;
+	
+	public Goods(String category, String name) {
+		super();
+		this.category = category;
+		this.name = name;
+		id = maxID;
+		maxID++;
+	}
 
+	public String toString(){
+		return getName();
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 }
--- trunk/GoJava1/andrii.nikitin/MyShop/src/ua/com/goit/gojava/andriidnikitin/Store.java
+++ trunk/GoJava1/andrii.nikitin/MyShop/src/ua/com/goit/gojava/andriidnikitin/Store.java
@@ -1,5 +1,49 @@
 package ua.com.goit.gojava.andriidnikitin;
 
-public class Store {
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Store {
+	
+	private List<Goods> goods = new ArrayList<Goods>();
+	
+	private List<String> categories = new ArrayList<String>();
+
+	PrintStream outStream;
+	
+	public Store (PrintStream outStreamArg ){
+		outStream = outStreamArg;
+		initGoods();
+		initCategories();
+	}
+	
+	private void initGoods(){
+		goods.add(new Goods(categories.get(0), "Fender Strat"));
+	}
+	
+	private void initCategories(){
+		categories.add("Guitars");
+		categories.add("Synths");
+		categories.add("Other");
+		
+	}
+	
+	public void showCategories(){
+		for (int i = 0; i < categories.size(); i++) 
+			System.out.println(i + 1 + " - " + categories.get(i));
+	}
+	
+	public void showGoodsInCategory(String chosenCategory){
+		int numberInListOfGoodsInSpecifiedCategory = 0;
+		for (Goods good : goods){
+			if (good.getCategory() == chosenCategory)
+				System.out.println(numberInListOfGoodsInSpecifiedCategory++ + 
+						" - " + good);
+		}
+	}	
 }
+
+
+
+
