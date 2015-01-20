10c45ac61c42f5a9d290169e95388ae2e5727cbf
lslayer2005@gmail.com <lslayer2005@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-20 20:38:53 +0000

    Added Commands class
    git-svn-id: http://go-java-1.googlecode.com/svn@498 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/viktor.gogilchyn/HackIt/src/main/java/ua/lslayer/hackit/commands/CommandRunner.java
@@ -0,0 +1,30 @@
+package ua.lslayer.hackit.commands;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandRunner {
+	private List<Command> registeredCommands = new ArrayList<Command> ();
+	public void register(Command registrant) {
+		this.registeredCommands.add(registrant);
+	}
+	public void unRegister(Command registrant) {
+		if (this.registeredCommands.contains(registrant)) {
+			this.registeredCommands.remove(registrant);
+		}
+	}
+	public List<String> getAllDescriptions() {
+		List<String> result = new ArrayList<String>();
+		for (Command command : this.registeredCommands) {
+			result.add(command.getDescription());
+		}
+		return result;
+	}
+	public String runCommand(String commandName, String args) {
+		for (Command command : this.registeredCommands) {
+			if (command.getClass().getSimpleName().equals(commandName + "Command")) return command.runCommand(args);
+		}
+		return "Unregistered command, try \"help\" to get all available commands!";
+	}
+
+}
