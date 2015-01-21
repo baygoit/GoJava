4b18e70b2a47b5893e311d8477561e2dae34f0fb
r.titkov@gmail.com <r.titkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-21 19:44:02 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@562 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/ruslan.titkov/module2/Kinopoisk/src/ua/kinopoisk/Menu.java
@@ -0,0 +1,18 @@
+package ua.kinopoisk;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	Scanner scanner = new Scanner (System.in);
+     
+	public static void Variant() // варианты добавления (то или другое)
+	
+	{
+		
+	}
+	
+	
+	}
+
+
