6ed304bc50b18200aeb7987a39f8fc3fddd97be0
beinlove.19@gmail.com <beinlove.19@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-21 00:09:27 +0000

    перші наброски
    git-svn-id: http://go-java-1.googlecode.com/svn@521 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/natalia.burtnyk/module2/MyProject/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	Production prod = new Production();
+	
+	prod.price = 54;
+	prod.name = "Varena";
+	prod.kilkist = 2;
+	
+	prod.calc();
+	
+	}
+
+}
