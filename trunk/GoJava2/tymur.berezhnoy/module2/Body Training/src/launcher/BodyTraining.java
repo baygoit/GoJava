f381929a7f63cb46a3498e0e7e1ad2730cb7d3de
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-22 15:06:12 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@622 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Body Training/src/launcher/BodyTraining.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Body Training/src/launcher/BodyTraining.java
@@ -3,15 +3,13 @@ import home.BodyTraining.diary.DiaryManager;
 import home.BodyTraining.diary.DiaryStorage;
 import home.BodyTraining.user.LoggedUser;
 
-import java.util.Scanner;
-
 public class BodyTraining {
 	
 	public static void main(String[] args) {
 		LoggedUser loggedUser = new LoggedUser(new DiaryManager(new DiaryStorage()));
-		loggedUser.createD(new Scanner(System.in));
-		loggedUser.createD(new Scanner(System.in));
-		loggedUser.createD(new Scanner(System.in));
+		loggedUser.createDiary("Fitnes");
+		loggedUser.createDiary("Body fitnes");
+		loggedUser.createDiary("Pilates");
 		loggedUser.myDiary();
 	}
 }
