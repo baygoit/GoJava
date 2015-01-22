8e9ea89d9e7b0eaee47fd3e76b94eeb46827132c
springedup@gmail.com <springedup@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-22 20:16:58 +0000

    added test class
    git-svn-id: http://go-java-1.googlecode.com/svn@644 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/sergey.poznyak/MyProject/FoodCalculations/trunk/src/ua/com/goit/gojava/poznyak/ListServiceHardcodedDataTest.java
@@ -0,0 +1,56 @@
+package ua.com.goit.gojava.poznyak;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Serge
+ *
+ */
+public class ListServiceHardcodedDataTest {
+	
+	private ListService service;
+	
+	@Before 
+	public void initialize() {
+		service = new ListServiceHardcodedData();
+	}
+
+	@Test
+	public void testGetDishListNotNull() {
+		assertNotNull(service.getDishList());
+	}
+	
+	@Test
+	public void testGetDishListNotEmpty() {
+		assertFalse(service.getDishList().isEmpty());
+	}
+
+	@Test
+	public void testGetIngredientListNotNull() {
+		assertNotNull(service.getIngredientList(77));
+	}
+	
+	@Test
+	public void testGetIngredientListZero() {
+		assertNotNull(service.getIngredientList(0));
+	}
+	
+	@Test
+	public void testGetIngredientListNotEmpty() {
+		assertFalse(service.getIngredientList(2).isEmpty());
+	}
+	
+	@Test
+	public void testGetIngredientMax() {
+		assertNotNull(service.getIngredientList(Integer.MAX_VALUE));
+	}
+	
+	@Test
+	public void testGetIngredientMin() {
+		assertNotNull(service.getIngredientList(Integer.MIN_VALUE));
+	}
+
+}
