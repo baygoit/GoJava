bf3a48835e24e8bf39ab6f559e3d148280c4b603
apofig@gmail.com <apofig@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-22 15:26:37 +0000

    Как гость я хочу видеть список категорий, с тем чтобы сфокусироваться на интересующей меня теме Сценарий: Захожу в приложение -> вижу список категорий и запрос на выбор категории Теперь мы можем выбрать категорию...
    git-svn-id: http://go-java-1.googlecode.com/svn@628 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Categories.java
+++ trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Categories.java
@@ -2,29 +2,26 @@ package ua.com.goit.gojava.kickstarter;
 
 public class Categories {
 
-	// Мы создали пока массив размером в 10 элементов и будем все хранить там. 
-	// конечно же у нас будет ошибка, если мы переполним массив, но пока нам так ок
-	// если что-то надо сделать дополнительное, то пишем TODO и потом исправляем (не слишком откладывая)
 	private Category[] categories = new Category[10];
 	
-	// так же нам понадобится индекс по массиву. Где есть массив - всегда есть индекс
-	// так как тут будет конфликт, то наверное надо назвать переменную как-то иначе
 	private int count = 0;
 	
 	public void add(Category category) {
 		categories[count] = category;
-		count++; // индекс увеличиваем, потому как следующая категория должна быть в новой позиции и не перетирать старую запись
+		count++; 
 	}
 	
-	// но немой список категорий наверное нам не пригодится. Надо как-то получать информацию о категориях. Как? 
 	public String[] getCategories() {
 		String[] result = new String[count];
-		// возвращать будем имена категорий, потому что их потом нам надо будет вывести на экран
 		for (int index = 0; index < count; index++) {
-			result[index] = categories[index].getName();
+			result[index] = String.valueOf(index) + " - " + categories[index].getName();
 		}
 		
 		return result;
+	}
+
+	public String getName(int index) {
+		return categories[index].getName();
 	}
 
 }
