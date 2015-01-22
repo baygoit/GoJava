5aadfd631b038bf8cedcb78da78ade7f49bba0d0
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-22 14:59:52 +0000

    Removed "private Diary diary";
    git-svn-id: http://go-java-1.googlecode.com/svn@619 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Body Training/src/home/BodyTraining/diary/DiaryManager.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Body Training/src/home/BodyTraining/diary/DiaryManager.java
@@ -2,7 +2,6 @@ package home.BodyTraining.diary;
 import java.util.List;
 public class DiaryManager implements DiaryManagment{
 	
-	private Diary diary;
 	private DiaryStorage diaryStorage;
 	
 	public DiaryManager(DiaryStorage diaryStorage) {
@@ -11,7 +10,7 @@ public class DiaryManager implements DiaryManagment{
 	
 	@Override
 	public void createDiary(String name) {
-		diary = new Diary(name);
+		Diary diary = new Diary(name);
 		diaryStorage.addDiaryToStorage(diary);
 	}
 	
