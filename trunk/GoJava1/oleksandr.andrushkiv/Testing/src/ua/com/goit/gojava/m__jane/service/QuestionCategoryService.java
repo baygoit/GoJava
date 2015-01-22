f08cff6fe141f253f408d6b32d7c42c69cdec1e5
AndrushkivAN@gmail.com <AndrushkivAN@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-22 09:38:55 +0000

    remove to update
    git-svn-id: http://go-java-1.googlecode.com/svn@584 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.andrushkiv/Testing/src/ua/com/goit/gojava/m__jane/service/QuestionCategoryService.java
+++
@@ -1,8 +0,0 @@
-package ua.com.goit.gojava.m__jane.service;
-
-import ua.com.goit.gojava.m__jane.model.QuestionCategory;
-
-public interface QuestionCategoryService {
-
-	public QuestionCategory getQuestionCategory(int questionCategoryId);
-}
