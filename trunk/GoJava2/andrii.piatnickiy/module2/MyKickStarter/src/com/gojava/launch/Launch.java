ec3a45e7a3abf650ce3cb6f204d0cddbacae5c65
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-23 15:17:25 +0000

    init display quote feature
    git-svn-id: http://go-java-1.googlecode.com/svn@672 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/launch/Launch.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/launch/Launch.java
@@ -3,8 +3,8 @@ package com.gojava.launch;
 public class Launch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Quote quote = new Quote();
+		quote.displayQuote();
 	}
-
+	
 }
---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/launch/Quote.java
@@ -0,0 +1,9 @@
+package com.gojava.launch;
+
+public class Quote {
+    String desc = "Motivating creativity quote!!";
+    public void displayQuote() {
+        System.out.println(desc);
+    }
+
+}
