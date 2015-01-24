aa90a7478c570eab60711b518871cbbf833f99bb
springedup@gmail.com <springedup@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-24 21:35:59 +0000

    exception fixed
    git-svn-id: http://go-java-1.googlecode.com/svn@727 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/sergey.poznyak/MyProject/FoodCalculations/trunk/src/ua/com/goit/gojava/poznyak/ListServiceHardcodedData.java
+++ trunk/GoJava1/sergey.poznyak/MyProject/FoodCalculations/trunk/src/ua/com/goit/gojava/poznyak/ListServiceHardcodedData.java
@@ -13,6 +13,7 @@ import java.util.Map;
  */
 public class ListServiceHardcodedData implements ListService {
 	
+	@SuppressWarnings("serial")
 	private List<Dish> dishesList = new ArrayList<Dish>() {{
 		this.add(new Dish(1, "Borshch"));
 		this.add(new Dish(2, "Buckwheat porridge"));
@@ -21,9 +22,10 @@ public class ListServiceHardcodedData implements ListService {
 		this.add(new Dish(5, "Pasta with meat"));
 	}};
 	
+	@SuppressWarnings("serial")
 	private Map<Integer, List<Ingredient>> ingredientsList = new HashMap<Integer, List<Ingredient>>() {{
 		List<Ingredient> currentDish = new ArrayList<Ingredient>();
-		ingredientsList.put(2, currentDish);
+		this.put(2, currentDish);
 		currentDish.add(new Ingredient("buckwheat", 0.08));
 		currentDish.add(new Ingredient("salt", 0.005));
 		currentDish.add(new Ingredient("stew", 0.125));
