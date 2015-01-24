87b4fd07127dab5bb0b64028b060c044e997392c
beinlove.19@gmail.com <beinlove.19@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-24 15:08:05 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@714 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Quote quote = new Quote();
+    	System.out.println(" Welcome to Kickstarter" + "\n" + "  *** *** *** *** *** " + 
+		                   "\n" +quote.printQuote() + "\n" + "  *** *** *** *** *** ");
+   	    
+	}
+
+}
