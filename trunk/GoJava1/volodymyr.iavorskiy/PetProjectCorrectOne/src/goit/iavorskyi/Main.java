611642667436311f68bb809fa88a251fc43317f8
iavorskyi.v.v@gmail.com <iavorskyi.v.v@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-24 07:21:39 +0000

    Autorizer user storie
    git-svn-id: http://go-java-1.googlecode.com/svn@699 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/volodymyr.iavorskiy/PetProjectCorrectOne/src/goit/iavorskyi/Main.java
+++ trunk/GoJava1/volodymyr.iavorskiy/PetProjectCorrectOne/src/goit/iavorskyi/Main.java
@@ -3,7 +3,9 @@ package goit.iavorskyi;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Autorizer autorizer = new Autorizer();
+        autorizer.autorize();
 
 	}
 
