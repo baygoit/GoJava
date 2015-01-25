3b55fd4874490f0293bfd1246e16c4513ee3aa50
beinlove.19@gmail.com <beinlove.19@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-25 22:33:38 +0000

    добавлено клас Engine
    git-svn-id: http://go-java-1.googlecode.com/svn@777 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/src/Main.java
+++ trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/src/Main.java
@@ -3,16 +3,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        Quote quote = new Quote();
-        System.out.println(" Welcome to Kickstarter" + "\n"
-                + "  *** *** *** *** *** " + "\n" + quote.printQuote() + "\n"
-                + "  *** *** *** *** *** ");
-
-        List<Сategory> a = new CategoryStorage().getCategoriesList();
-        int i = 1;
-        for (Сategory c : a) {
-            System.out.println(i + ". " + c.getName());
-            i++;
-		}
-	}
+        Engine engine = new Engine();
+        engine.run();
+    }
 }
