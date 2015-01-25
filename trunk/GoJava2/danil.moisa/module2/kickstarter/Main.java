1fdcc6821424cf2bd4f144bc3d5d571d5d08a878
d.moysa@gmail.com <d.moysa@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-25 02:42:50 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@746 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/danil.moisa/module2/kickstarter/Main.java
@@ -0,0 +1,14 @@
+package kickstarter;
+
+public class Main {
+	public static void main (String [] args){
+		 Output output = new Output();
+		 output.quatation();
+		 output.category();
+		 Engine eng = new Engine();
+		 eng.choiceCategory();
+
+		
+	}
+
+}
