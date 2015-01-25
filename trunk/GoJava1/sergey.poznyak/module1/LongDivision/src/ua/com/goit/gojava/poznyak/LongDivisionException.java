9c1888eed4e7ac53830ceee8aa95807042fa8697
springedup@gmail.com <springedup@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-25 22:53:59 +0000

    first commit
    git-svn-id: http://go-java-1.googlecode.com/svn@780 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/sergey.poznyak/module1/LongDivision/src/ua/com/goit/gojava/poznyak/LongDivisionException.java
@@ -0,0 +1,18 @@
+package ua.com.goit.gojava.poznyak;
+
+/**
+ * Exception class.
+ * 
+ * @version 1.0 25 Jan 2015
+ * @author Sergey Poznyak
+ */
+@SuppressWarnings("serial")
+public class LongDivisionException extends Exception {
+
+	public LongDivisionException() {}
+
+	public LongDivisionException(String message) {
+		super(message);
+	}
+
+}
