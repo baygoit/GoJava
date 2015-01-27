0918e1a3cde5122c891648a695a1cc7809e1a13f
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-27 13:56:59 +0000

    HashMap<Category, List<Project>> instead of ArrayList<Project>.
    git-svn-id: http://go-java-1.googlecode.com/svn@856 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/Projects.java
+++ trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/Projects.java
@@ -1,39 +1,29 @@
 package ua.home.kickstarter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Projects {
-	private ArrayList<Project> projects = new ArrayList<Project>();
+
+	private Map<Category, List<Project>> projects = new HashMap<Category, List<Project>>();
 
 	public void add(Project project) {
-		projects.add(project);
-	}
-
-	public ArrayList<Project> getProjects(Category category) {
-		ArrayList<Project> result = new ArrayList<Project>();
-
-		for (int index = 0; index < projects.size(); index++) {
-			Project project = projects.get(index);
-			if (projects.get(index).getCategory().equals(category)) {
-				result.add(project);
-
-			}
+		ArrayList<Project> projectsList = new ArrayList<Project>();
+		if (projects.containsKey(project.getCategory())) {
+			projects.get(project.getCategory()).add(project);
+		} else {
+			projectsList.add(project);
+			projects.put(project.getCategory(), projectsList);
 		}
-		return result;
 	}
 
-	public ArrayList<Project> getFullProject(int projectIndex, Category category) {
-		ArrayList<Project> result = new ArrayList<Project>();
-		result.add(getProjects(category).get(projectIndex - 1));
-		return result;
+	public List<Project> getProjects(Category category) {
+		return projects.get(category);
 	}
 
-	public Project getName(int index, Category category) {
-		ArrayList<Project> result = new ArrayList<Project>();
-		if (getProjects(category).get(index - 1).getCategory().equals(category)) {
-			result.add(getProjects(category).get(index - 1));
-		}
-		return result.get(0);
+	public Project getFullProject(int index, Category category) {
+		return projects.get(category).get(index - 1);
 	}
-
 }
