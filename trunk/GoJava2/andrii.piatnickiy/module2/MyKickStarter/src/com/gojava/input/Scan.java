9ed6597751f04ca4c0351a52dcd5ec2dc879d2d2
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-27 09:13:18 +0000

    replace scanner into scan package
    git-svn-id: http://go-java-1.googlecode.com/svn@838 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/input/Scan.java
@@ -0,0 +1,10 @@
+package com.gojava.input;
+
+import java.util.Scanner;
+
+public class Scan {
+    public int inputInt() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+}
