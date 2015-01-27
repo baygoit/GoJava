d0728ecee9c7db72572855d1599fc2da5af32b9f
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-27 08:56:17 +0000

    User story III model updated
    git-svn-id: http://go-java-1.googlecode.com/svn@836 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Model.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Model.java
@@ -1,10 +1,14 @@
 package ua.com.scread.kickstarter;
 
+import java.util.ArrayList;
+
 public class Model {
 	private Categories categories;
+	private Projects projects;
 	
 	public Model() {
 		categories = new Categories();
+		projects = new Projects();
 	}
 	
 	public void init() {
@@ -12,10 +16,18 @@ public class Model {
 		Category category2 = new Category("Science");
 		Category category3 = new Category("Virtual reality");
 		
-		
 		categories.add(category1);
 		categories.add(category2);
 		categories.add(category3);
+        
+        Project project1 = new Project("Name", "Description", 1000, 10);
+        Project project2 = new Project("Oculus rift", "Virtual reality glasses", 1000, 10);
+        
+        project1.setCategory(category3);
+        project2.setCategory(category3);
+
+        projects.add(project1);
+        projects.add(project2);
 	}
 
 	public String[] getStringCatigories() {
@@ -24,6 +36,10 @@ public class Model {
 	
 	public Categories getCategories() {
 		return categories;
+	}
+	
+	public ArrayList<Project> getProjects(Category category) {
+		return projects.getProjects(category);
 	}
 	
 }
