57285ab5d89dbb3730187f36baba5dbc045ae3f5
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-27 08:58:24 +0000

    User story III controller
    git-svn-id: http://go-java-1.googlecode.com/svn@837 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Controller.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Controller.java
@@ -1,5 +1,7 @@
 package ua.com.scread.kickstarter;
 
+import java.util.ArrayList;
+
 public class Controller {
 	private Model model;
     private View view;
@@ -10,10 +12,27 @@ public class Controller {
         this.view = view;
         this.scan = scan;
     }
+    
+//    UserStory III
+//    Как гость я хочу изучить список проектов определенной категории, чтобы понять во что я 
+//    хочу инвестировать
+//    Сценарий: захожу в приложение -> выбираю любую категорию (например, 1 - спорт) -> вижу 
+//    список проектов, каждый из которых содержит: название, краткое описание, сумму необходиму 
+//    для сбора, сколько собрали уже, сколько дней осталось
 
     public void start() {
         model.init();
         view.greed();
+
+        Categories categories = model.getCategories();
+        view.showCategoies(categories);	
+        
+        int answer = scan.getAnswer()-1;
+        Category category = categories.getCategory(answer);
+        view.showCategory(category);
+        
+        ArrayList<Project> foundProjects = model.getProjects(category);
+        view.showProjects(foundProjects);
     }
 
 }
