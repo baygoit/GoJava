f843f69d7f17edab9f8f5669a0e927d647a11ceb
iavorskyi.v.v@gmail.com <iavorskyi.v.v@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-28 07:24:22 +0000

    Some tests.
    git-svn-id: http://go-java-1.googlecode.com/svn@878 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/Autorizer.java
+++ trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/Autorizer.java
@@ -1,53 +1,19 @@
 package goit.iavorskyi;
 
-import java.util.Scanner;
-
 public class Autorizer {
- 
-        Scanner in = new Scanner(System.in);
-        private String login = null;
-        private String password = null;
-        private String repeatPassword = null;
-        private String choise = "";
- 
-        public void autorize() {
-                System.out.println("1. Login. \n2. Register.");
-                choise = in.next();
- 
-                if (choise.equals("1")) {
-                        login();
-                } else if (choise.equals("2")) {
-                        register();
-                } else {
-                        System.out.println("No such option.");
-                }
-        }
- 
-        private void login() {
-                System.out.println("input login: ");
-                login = in.next();
-                System.out.println("input password: ");
-                password = in.next();
- 
-                if (login.equals("user") && password.equals("pass")) {
-                        System.out.println("You are logined");
-                } else {
-                        System.out.println("User or password incorrect");
-                }
-        }
- 
-        private void register() {
-                System.out.println("input login: ");
-                login = in.next();
-                do {
-                        System.out.println("input password: ");
-                        password = in.next();
-                        System.out.println("repeat password: ");
-                        repeatPassword = in.next();
-                        if (!password.equals(repeatPassword)) {
-                                System.out.println("Passwords does not match. Repeat.");
-                        }
-                } while (!password.equals(repeatPassword));
-                System.out.println("You are registered.");
-        }
+
+	public boolean login(String login, String password) {
+		if (login.equals("user") && password.equals("pass")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean register(String login, String password,	String repeatPassword) {
+		if (!password.equals(repeatPassword) || (password.length() < 3) || (login.length() < 3)) {
+			return false;
+		}
+		return true;
+	}
 }
--- trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/Main.java
+++ trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/Main.java
@@ -4,16 +4,5 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Autorizer autorizer = new Autorizer();
-        autorizer.autorize();
-        
-        
-
 	}
-
-	
-	public int add(int a, int b) {
-    	return a + b;
-    }
-	
 }
