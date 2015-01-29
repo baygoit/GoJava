77eab04fa4f7eba8727c5050d6b712579f13d1e2
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-29 23:43:14 +0000

    deleted wrong qoute package and create new
    git-svn-id: http://go-java-1.googlecode.com/svn@956 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/quote/Quote.java
@@ -0,0 +1,10 @@
+package com.gojava.quote;
+
+public class Quote {
+    private String desc = "This is motivating creativity quote!!";
+
+    public void displayQuote() {
+        System.out.println(desc);
+    }
+
+}
