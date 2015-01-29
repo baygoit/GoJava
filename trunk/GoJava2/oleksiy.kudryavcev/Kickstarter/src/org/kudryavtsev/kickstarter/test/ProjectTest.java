91db34a7607407ed9a05a5fadcab5833abb0e164
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-01-29 16:31:01 +0000

    updated Project test (Kickstarter project, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@942 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ProjectTest.java
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ProjectTest.java
@@ -3,27 +3,23 @@ package org.kudryavtsev.kickstarter.test;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.kudryavtsev.kickstarter.Project;
 
 public class ProjectTest {
 
     @Test
-    public void testProjectStringStringIntIntInt() {
-        fail("Not yet implemented"); // TODO
+    public void ProjectToString() {
+        Project project = new Project("TestProject", "TestDescription", 100, 100, 100);
+        assertEquals("TestProject; TestDescription; funded: 100; pledged: 100; days to go: 100",
+                project.toString());
     }
 
     @Test
-    public void testProject() {
-        fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testToString() {
-        fail("Not yet implemented"); // TODO
-    }
-
-    @Test
-    public void testToStringFull() {
-        fail("Not yet implemented"); // TODO
+    public void ProjectToStringFull() {
+        Project project = new Project("TestProject", "TestDescription", 1000, 100, 101);
+        assertEquals(
+                "TestProject; TestDescription; funded: 1000; pledged: 100; days to go: 101\nFull description: Just long description\nHistory: History of the project\nVideo: Link to video with demo\nFAQ: Frequently Asked Questions",
+                project.toStringFull());
     }
 
 }
