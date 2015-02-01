d71c97fb7d600688e7c77387ba7f4ea56f02b6e7
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-01 12:32:36 +0000

    i've changed the way of projects data transmission  in "Project" class .
    git-svn-id: http://go-java-1.googlecode.com/svn@1013 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/DataManager.java
+++ trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/DataManager.java
@@ -10,7 +10,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class DataManager {
-	
+
 	public void dataInputToobjects() {
 		JSONParser parser = new JSONParser();
 		try {
@@ -25,34 +25,13 @@ public class DataManager {
 			categories.add(category1);
 			categories.add(category2);
 			categories.add(category3);
-			
-			JSONObject jsonObject1 = (JSONObject) a.get(0);
-			JSONObject jsonObject2 = (JSONObject) a.get(1);
-			JSONObject jsonObject3 = (JSONObject) a.get(2);
-			JSONObject jsonObject4 = (JSONObject) a.get(3);
-			JSONObject jsonObject5 = (JSONObject) a.get(4);
-			JSONObject jsonObject6 = (JSONObject) a.get(5);
 
-			Project project1 = new Project("" + jsonObject1.get("name"), "" + jsonObject1.get("description"),
-					Integer.parseInt("" + jsonObject1.get("goal")), Integer.parseInt("" + jsonObject1.get("daysLeft")),
-					"" + jsonObject1.get("history"), "" + jsonObject1.get("linksToVideo"));
-			Project project2 = new Project("" + jsonObject2.get("name"), "" + jsonObject2.get("description"),
-					Integer.parseInt("" + jsonObject2.get("goal")), Integer.parseInt("" + jsonObject2.get("daysLeft")),
-					"" + jsonObject2.get("history"), "" + jsonObject2.get("linksToVideo"));
-			Project project3 = new Project("" + jsonObject3.get("name"), "" + jsonObject3.get("description"),
-					Integer.parseInt("" + jsonObject3.get("goal")), Integer.parseInt("" + jsonObject3.get("daysLeft")),
-					"" + jsonObject3.get("history"), "" + jsonObject3.get("linksToVideo"));
-			Project project4 = new Project("" + jsonObject4.get("name"), "" + jsonObject4.get("description"),
-					Integer.parseInt("" + jsonObject4.get("goal")), Integer.parseInt("" + jsonObject4.get("daysLeft")),
-
-					"" + jsonObject4.get("history"), "" + jsonObject4.get("linksToVideo"));
-			Project project5 = new Project("" + jsonObject5.get("name"), "" + jsonObject5.get("description"),
-					Integer.parseInt("" + jsonObject5.get("goal")), Integer.parseInt("" + jsonObject5.get("daysLeft")),
-					"" + jsonObject5.get("history"), "" + jsonObject5.get("linksToVideo"));
-			Project project6 = new Project("" + jsonObject6.get("name"), "" + jsonObject6.get("description"),
-					Integer.parseInt("" + jsonObject6.get("goal")), Integer.parseInt("" + jsonObject6.get("daysLeft")),
-					"" + jsonObject6.get("history"), "" + jsonObject6.get("linksToVideo"));
-
+			Project project1 = new Project((JSONObject) a.get(0));
+			Project project2 = new Project((JSONObject) a.get(1));
+			Project project3 = new Project((JSONObject) a.get(2));
+			Project project4 = new Project((JSONObject) a.get(3));
+			Project project5 = new Project((JSONObject) a.get(4));
+			Project project6 = new Project((JSONObject) a.get(5));
 			project1.setCategory(category1);
 			project2.setCategory(category1);
 			project3.setCategory(category1);
