d6bdb25fbe600ffc503d4fd2ff99088b01f45638
apofig@gmail.com <apofig@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 21:21:52 +0000

    Убил комменты
    git-svn-id: http://go-java-1.googlecode.com/svn@1096 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunc/GoJava2/alex.baglay/Kickstarter/test/ua/com/goit/gojava/kickstarter/ProjectsTest.java
+++ trunc/GoJava2/alex.baglay/Kickstarter/test/ua/com/goit/gojava/kickstarter/ProjectsTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 public class ProjectsTest {
 
-	@Test // тест на то, что в пустом списке проектов ничего не найдем
+	@Test 
 	public void shouldEmptyProjects_whenNoProjects() {
 		// given 
 		Projects list = new Projects();
@@ -18,7 +18,7 @@ public class ProjectsTest {
 		assertEquals(0, found.length);
 	}
 	
-	@Test // тест на то, что в списке проектов ничего не найдем, если не та категория
+	@Test 
 	public void shouldEmptyProjects_whenNoProjectsWithSameCategory() {
 		// given 
 		Category category = new Category("category");
@@ -41,23 +41,22 @@ public class ProjectsTest {
 		assertEquals(0, found.length);
 	}
 	
-	@Test // хочу написать тест, в котором поиск получается, потому что категория совпадает
+	@Test 
 	public void shouldFoundProjects_whenExistsProjectWithCategory() {
 		// given 
 		Category category1 = new Category("category1");
 		Category category2 = new Category("category2");
 		
-		
 		Projects list = new Projects();
 		
 		Project project1 = new Project("name", 100, 10, "video", "description");
-		project1.setCategory(category1); // этот нет
+		project1.setCategory(category1); 
 		
 		Project project2 = new Project("name2", 200, 20, "video2", "description2");
-		project2.setCategory(category2); // этот найдем
+		project2.setCategory(category2); 
 		
 		Project project3 = new Project("name3", 300, 30, "video3", "description3");
-		project3.setCategory(category2); // этот найдем
+		project3.setCategory(category2); 
 		
 		list.add(project1);
 		list.add(project2);
@@ -67,14 +66,13 @@ public class ProjectsTest {
 		Project[] found = list.getProjects(category2);
 		
 		// then
-		// assertEquals вызывает у объектов actual.equals(expected)
 		assertEquals(2, found.length);
-		// assertSame говорит что это два идентичных объъекта
+		
 		assertSame(project2, found[0]);
 		assertSame(project3, found[1]);
 	}
 	
-	@Test // тест в котором я дергаю проекты по их порядковому номеру
+	@Test 
 	public void shouldGetProject_whenExistsSomeProjects() {
 		// given 
 		Projects list = new Projects();
