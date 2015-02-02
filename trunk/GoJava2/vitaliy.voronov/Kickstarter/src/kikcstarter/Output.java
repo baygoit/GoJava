35fa1b147f1f91fe70ed2dd6a54325225df5d82c
vitalik.voronov <vitalik.voronov@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 18:53:43 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1088 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/vitaliy.voronov/Kickstarter/src/kikcstarter/Output.java
+++ trunk/GoJava2/vitaliy.voronov/Kickstarter/src/kikcstarter/Output.java
@@ -27,15 +27,12 @@ public class Output {
 					project.getDaysBeforeEnd() + " days");
 		}
 	}
-	public void printSelectProject(ArrayList<Project> projects) {
-		int i = 0;
-		for (Project project : projects) {
-			i++;
-			System.out.println(i + ") " + project.getName() + "\n " + project.getDescription() + "\n We need - " 
-					+ project.getNeedMoney() + "$\n We have - " + project.getHaveMoney() + "$\n Time over - " + 
-					project.getDaysBeforeEnd() + " days" + project.getProjectHistory() + project.getLinkToDemoVideo() + 
-					project.getQuastionAnswer());
-		}
+	public void printSelectProject(Project project2) {
+			System.out.println(project2.getName() + "\n " + project2.getDescription() + "\n We need - " 
+					+ project2.getNeedMoney() + "$\n We have - " + project2.getHaveMoney() + "$\n Time over - " + 
+					project2.getDaysBeforeEnd() + " days" + project2.getProjectHistory() + "\n " + project2.getLinkToDemoVideo() + 
+					"\n " + project2.getQuastionAnswer());
+		
 	}
 
 	/*
