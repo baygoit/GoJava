5dff4520a1b3f0210a7fac9b1c621eaf1aa80082
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 08:42:45 +0000

    Unit test for class FAQ. Code coverage 47.3%.
    git-svn-id: http://go-java-1.googlecode.com/svn@1030 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/test/FAQTest.java
@@ -0,0 +1,21 @@
+package ua.com.scread.kickstarter.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import ua.com.scread.kickstarter.FAQ;
+
+public class FAQTest {
+	FAQ faq = new FAQ("question", "answer");
+	
+	@Test
+	public void shouldBeQuestion_whenGetQuestion() {
+		assertEquals("question", faq.getQuestion());
+	}
+	
+	@Test
+	public void shouldBeAnswer_whenGetAnswer() {
+		assertEquals("answer", faq.getAnswer());
+	}
+}
