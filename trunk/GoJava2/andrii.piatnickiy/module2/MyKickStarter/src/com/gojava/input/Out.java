222b525970806bc84f447522570709bff7cc632c
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 16:14:53 +0000

    refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@1069 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/input/Out.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/input/Out.java
@@ -7,36 +7,35 @@ public class Out {
     public String output(String in) {
         return in;
     }
-    
+
     public String output(String string, String string2) {
         return string + string2;
     }
-    
+
     public String output(String string, int i) {
         return string + i;
     }
-    
-//    public Category output(Category category) {
-//        return category;
-//    }
-    
+
+    // public Category output(Category category) {
+    // return category;
+    // }
+
     public String output(int i, String string) {
         return i + string;
     }
-    
-    
-    public void print(String  string ){
+
+    public void print(String string) {
         System.out.println(string);
     }
-    
+
     public void printProjectPreview(Project project)
     {
         StringBuffer sb = new StringBuffer();
         sb.append("Project Name: ").append(project.getName()).append("\n");
         sb.append("Description: ").append(project.getDescription()).append("\n");
-        // out.print(out.output("Need Sum: ", project.getNeedSum()));
-        // out.print(out.output("Current Sum: ", project.getCurrentSum()));
-        // out.print(out.output("Days Left: ", project.getDaysLeft()));
+        sb.append("Need Sum: ").append(project.getNeedSum()).append("\n");
+        sb.append("Current Sum: ").append(project.getCurrentSum()).append("\n");
+        sb.append("Days Left: ").append(project.getDaysLeft()).append("\n");
         System.out.println(sb.toString());
         
     }
