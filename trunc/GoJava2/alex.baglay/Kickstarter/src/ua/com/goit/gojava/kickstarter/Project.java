90cf55cabf750b808b65da48326eb6be0aca8120
apofig@gmail.com <apofig@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 15:41:43 +0000

    Выводится список меню проектов, каждый из которых можно выбрать и увидеть детальное описание (пока там захардкоджено) Есть пару багов
    git-svn-id: http://go-java-1.googlecode.com/svn@1066 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Project.java
+++ trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Project.java
@@ -45,4 +45,26 @@ public class Project {
 		return days;
 	}
 
+	public String getHistory() {
+		// пока я захардкоджу тут какое-то бла бла бла, чтобы просмотреть как оно работает впринципе
+		// текст рыба, обычно его используют для заполнения контента
+		// недоделано потому ставлю TODO
+		return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt "
+				+ "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco l"
+				+ "aboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in "
+				+ "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat "
+				+ "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	}
+
+	public String getDemoVideo() {
+		// недоделано потому ставлю TODO
+		return "http://youtube.com/tg67f347fg";
+	}
+
+	public String getQuestionAnswers() {
+		// недоделано потому ставлю TODO
+		return "Q: вопрос \n" +  // \n мисвол переноса строки
+	           "A: ответ";
+	}
+
 }
