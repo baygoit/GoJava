d63862bd26ddfdefd77402d4430537bb5d229181
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 08:38:47 +0000

    Unit test for Projects class. Code coverage 45%.
    git-svn-id: http://go-java-1.googlecode.com/svn@1029 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/test/ProjectsTest.java
@@ -0,0 +1,38 @@
+package ua.com.scread.kickstarter.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import ua.com.scread.kickstarter.Category;
+import ua.com.scread.kickstarter.Details;
+import ua.com.scread.kickstarter.FAQ;
+import ua.com.scread.kickstarter.Project;
+import ua.com.scread.kickstarter.Projects;
+
+
+public class ProjectsTest {
+	FAQ faq = new FAQ("question", "answer");
+	Details details = new Details("History", "video", faq);
+	Project project = new Project("Project", "Description", 1, 1, details);
+	Projects projects = new Projects();
+	
+	@Test
+	public void shouldAddProject_whenAddedProject() {
+		projects.add(project);
+		assertEquals(project, projects.getProjects().get(0));
+	}
+	
+	@Test
+	public void shouldBeProject_whenGetProjectByCategory() {
+		Category category = new Category("Vasya");
+		project.setCategory(category);
+		ArrayList<Project> expected = new ArrayList<Project>();
+		expected.add(project);
+		projects.add(project);
+		assertEquals(expected, projects.getProjects(category));
+	}
+
+}
