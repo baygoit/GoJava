1125d7f3263c9e796a63770fe42aff15ef6e8383
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-02 13:17:05 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1054 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/Quotations.java
+++ trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/Quotations.java
@@ -5,20 +5,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Random;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class Quotations {
 	private JSONObject jsonObject;
-    int jsonObjectSize;
-    
 	public Quotations() {
 		JSONParser parser = new JSONParser();
 		try {
-			Object obj = parser.parse(new FileReader("d:\\ss33.json"));
-			jsonObject = (JSONObject) obj;
-			jsonObjectSize = jsonObject.size();
+			JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("d:\\Quotations.json"));
+			jsonObject = (JSONObject) jsonArray.get(new Random().nextInt(jsonArray.size()));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -27,8 +25,9 @@ public class Quotations {
 			e.printStackTrace();
 		}
 	}
-
-	public String nextQuote(int index) {
-		return (String) jsonObject.get("" + index);
+	
+	public String randomQuote() {
+		return (String) jsonObject.get("quote");
 	}
+	
 }
