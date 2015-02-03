399aee5c306f599641a194cf9748ae5be9f9b63d
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-03 10:58:33 +0000

    rename package
    git-svn-id: http://go-java-1.googlecode.com/svn@1125 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/inputOutput/Out.java
@@ -0,0 +1,61 @@
+package com.gojava.inputOutput;
+
+import com.gojava.projects.Category;
+import com.gojava.projects.IProject;
+
+public class Out {
+    public String output(String in) {
+        return in;
+    }
+
+    public String output(String string, String string2) {
+        return string + string2;
+    }
+
+    public String output(String string, int i) {
+        return string + Integer.toString(i);
+    }
+
+
+    public String output(int i, String string) {
+        return i + string;
+    }
+
+    public void print(String string) {
+        System.out.println(string);
+    }
+    
+    public void printSb(Object object) {
+        System.out.println(object);
+    }
+
+    public String printProjectPreview(IProject project) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Project Name: ").append(project.getName()).append("\n");
+        sb.append("Description: ").append(project.getDescription())
+                .append("\n");
+        sb.append("Need Sum: ").append(project.getNeedSum()).append("\n");
+        sb.append("Current Sum: ").append(project.getCurrentSum()).append("\n");
+        sb.append("Days Left: ").append(project.getDaysLeft()).append("\n");
+        return sb.toString();
+
+    }
+
+    public String printAdditionalProjectFields(IProject project) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("ProjectHistory: ").append(project.getProjectHistory())
+        .append("\n");
+        sb.append("LinkOnvideo: ").append(project.getLinkOnvideo())
+        .append("\n");
+        sb.append("Questions and answers: ")
+        .append(project.getQuestionsAndAnswers()).append("\n");
+        return sb.toString();
+    }
+    
+    public void printCategory(Category category) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(category.toString());
+        System.out.println(sb);
+    }
+
+}
---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/inputOutput/Scan.java
@@ -0,0 +1,10 @@
+package com.gojava.inputOutput;
+
+import java.util.Scanner;
+
+public class Scan {
+    public int inputInt() {
+        Scanner in = new Scanner(System.in);
+        return in.nextInt();
+    }
+}
