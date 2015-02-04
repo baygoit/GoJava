fa3e85dba62c3b6b64eb7e28c44069ece788e9ce
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 09:17:25 +0000

    Generated equals and hashCode functions.
    git-svn-id: http://go-java-1.googlecode.com/svn@1158 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Categories.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Categories.java
@@ -32,4 +32,30 @@ public class Categories {
 		return categories.get(index);
 	}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((categories == null) ? 0 : categories.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Categories other = (Categories) obj;
+        if (categories == null) {
+            if (other.categories != null)
+                return false;
+        } else if (!categories.equals(other.categories))
+            return false;
+        return true;
+    }
+
 }
--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Details.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Details.java
@@ -23,4 +23,41 @@ public class Details {
 		return faq;
 	}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((faq == null) ? 0 : faq.hashCode());
+        result = prime * result + ((history == null) ? 0 : history.hashCode());
+        result = prime * result + ((video == null) ? 0 : video.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Details other = (Details) obj;
+        if (faq == null) {
+            if (other.faq != null)
+                return false;
+        } else if (!faq.equals(other.faq))
+            return false;
+        if (history == null) {
+            if (other.history != null)
+                return false;
+        } else if (!history.equals(other.history))
+            return false;
+        if (video == null) {
+            if (other.video != null)
+                return false;
+        } else if (!video.equals(other.video))
+            return false;
+        return true;
+    }
+
 }
