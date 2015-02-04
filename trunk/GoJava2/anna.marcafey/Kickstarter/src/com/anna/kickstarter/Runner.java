b8148cf1334b2c9e0af5c5d75cf40e6361dfd28e
anna.martsafey@gmail.com <anna.martsafey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 19:36:48 +0000

    User Story II
    git-svn-id: http://go-java-1.googlecode.com/svn@1178 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/anna.marcafey/Kickstarter/src/com/anna/kickstarter/Runner.java
+++ trunk/GoJava2/anna.marcafey/Kickstarter/src/com/anna/kickstarter/Runner.java
@@ -1,6 +1,7 @@
 package com.anna.kickstarter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,27 +9,35 @@ public class Runner {
 
 	public static void main(String[] args) {
 		List<String> qoutes = new ArrayList<String>();
-		qoutes.add("here will be nice words...");
-		qoutes.add("will");
-		qoutes.add("be");
-		qoutes.add("nice");
-		qoutes.add("words");
+		qoutes.add("Don't cry because it's over, smile because it happened.");
+		qoutes.add("It does not do to dwell on dreams and forget to live.");
+		qoutes.add("Everything you can imagine is real.");
+		qoutes.add("Some infinities are bigger than other infinities.");
+		qoutes.add("Reality continues to ruin my life.");
 		
 		int random = (int) (Math.random()*qoutes.size());
+		
 		System.out.println(qoutes.get(random));
+		
+		Category category1 = new Category("Category1");
+		Category category2 = new Category("Category2");
+		
+		Categories categories = new Categories();
+		
+		categories.add(category1);
+		categories.add(category2);
+		
+		System.out.println(Arrays.toString(categories.getCategories()));
+		System.out.print("Choose category: ");
+		
+		Scanner scanner = new Scanner(System.in);
+		int answer = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("Choosed category: " + categories.getCategory(answer - 1));
 		
 		System.out.println("Thanks for using my program!");
 
-//		System.out.println("1) " + Category.DESIGN);
-//		System.out.println("2) " + Category.MUSIC);
-//		System.out.println("3) " + Category.SPORT);
-//		
-//		StringBuilder text = new StringBuilder(); 
-//		Scanner sc = new Scanner(System.in);
-//		String temp;
-//		while ((temp = sc.nextLine()).length() > 0){
-//		    text.append(temp);
-//		}
 
 	}
 
