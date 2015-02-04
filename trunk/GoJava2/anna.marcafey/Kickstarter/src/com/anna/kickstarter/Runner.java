ff9a7aa55ee56975ce21021e4ef9b0adeb9245d4
anna.martsafey@gmail.com <anna.martsafey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 19:49:58 +0000

    Рефакторинг. Выделено цитаты в отдельный класс.
    git-svn-id: http://go-java-1.googlecode.com/svn@1179 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/anna.marcafey/Kickstarter/src/com/anna/kickstarter/Runner.java
+++ trunk/GoJava2/anna.marcafey/Kickstarter/src/com/anna/kickstarter/Runner.java
@@ -1,23 +1,12 @@
 package com.anna.kickstarter;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		List<String> qoutes = new ArrayList<String>();
-		qoutes.add("Don't cry because it's over, smile because it happened.");
-		qoutes.add("It does not do to dwell on dreams and forget to live.");
-		qoutes.add("Everything you can imagine is real.");
-		qoutes.add("Some infinities are bigger than other infinities.");
-		qoutes.add("Reality continues to ruin my life.");
-		
-		int random = (int) (Math.random()*qoutes.size());
-		
-		System.out.println(qoutes.get(random));
+		System.out.println(new QuoteGenerator().getQuote());
 		
 		Category category1 = new Category("Category1");
 		Category category2 = new Category("Category2");
@@ -40,5 +29,7 @@ public class Runner {
 
 
 	}
+
+	
 
 }
