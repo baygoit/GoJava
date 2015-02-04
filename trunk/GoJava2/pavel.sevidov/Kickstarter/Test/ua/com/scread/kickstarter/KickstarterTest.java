c0499f9d17551112b32511ee2e43d69b29d5e5c0
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 08:39:48 +0000

    Tests were added for user stories 1-3.
    git-svn-id: http://go-java-1.googlecode.com/svn@1155 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/Test/ua/com/scread/kickstarter/KickstarterTest.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/Test/ua/com/scread/kickstarter/KickstarterTest.java
@@ -7,20 +7,8 @@ import org.junit.Test;
 public class KickstarterTest {
 		
 	@Test
-	public void shouldBeQuestion_whenGetQuestion() {
-	    Categories categories = new Categories();
-	    Category category1 = new Category("category1");
-        Category category2 = new Category("category2");
-        categories.add(category1);
-        categories.add(category2);
-        
-        Projects projects = new Projects();
-        Project project = new Project("Project", "Description", 10, 10, 
-                new Details("Some history", "Video link", new FAQ("Question", "Answer")));
-        
-        projects.add(project);
-	    
-	    Model model = new Model(categories, projects);
+	public void shouldBeQuote_whenStartApplication() {
+	    Model model = new Model();
 	    IO io = mock(IO.class);
 	    QuoteGenerator quote = mock(QuoteGenerator.class);
 	    when(quote.getQuote()).thenReturn("quote");
@@ -30,10 +18,77 @@ public class KickstarterTest {
 		
 		kickstarter.run();
 		
-		verify(io).print("quote\n");
-		verify(io).print("\nChoose category: ");
-		verify(io).print("[1 - category1, 2 - category2]");
-		verify(io).print("Thanks for using my program!");
-        
+		verify(io).print("quote\n");        
 	}
+	
+	@Test
+    public void shouldBeCategories_whenStartApplication() {
+	    Categories categories = new Categories();
+	    Category category1 = new Category("category1");
+        Category category2 = new Category("category2");
+        categories.add(category1);
+        categories.add(category2);
+        
+        Projects projects = new Projects();
+  
+        Model model = new Model(categories, projects);
+        IO io = mock(IO.class);
+        QuoteGenerator quote = mock(QuoteGenerator.class);
+        when(quote.getQuote()).thenReturn("quote");
+        when(io.read()).thenReturn(1, 0);
+        
+        KickstarterRunner kickstarter = new KickstarterRunner(model, io, quote);
+        
+        kickstarter.run();
+        
+        verify(io).print("quote\n");
+        verify(io, times(2)).print("\nChoose category: ");
+        verify(io, times(2)).print("[1 - category1, 2 - category2]");
+        verify(io).print("You choosed: category1\n");
+        verify(io).print("Thanks for using my program!"); 
+    }
+	
+	@Test
+    public void shouldBeProjects_whenChoosedCategory() {
+        Categories categories = new Categories();
+        Category category1 = new Category("category1");
+        Category category2 = new Category("category2");
+        categories.add(category1);
+        categories.add(category2);
+        
+        Projects projects = new Projects();
+        Project project = new Project("Project", "Description", 10, 10, 
+                new Details("Some history", "Video link", new FAQ("Question", "Answer")));
+        project.setCategory(category1);
+        projects.add(project);
+      
+        Model model = new Model(categories, projects);
+        IO io = mock(IO.class);
+        QuoteGenerator quote = mock(QuoteGenerator.class);
+        when(quote.getQuote()).thenReturn("quote");
+        when(io.read()).thenReturn(1, 0, 0);
+        
+        KickstarterRunner kickstarter = new KickstarterRunner(model, io, quote);
+        
+        kickstarter.run();
+        
+        verify(io).print("Project\n");
+        verify(io).print("Description\n");
+    }
+	
+	@Test
+    public void shouldCloseMessage_whenClosedApp() {
+              
+        Model model = new Model();
+        IO io = mock(IO.class);
+        QuoteGenerator quote = mock(QuoteGenerator.class);
+        when(io.read()).thenReturn(0);
+        
+        KickstarterRunner kickstarter = new KickstarterRunner(model, io, quote);
+        
+        kickstarter.run();
+        
+        verify(io).print("Thanks for using my program!");
+    }
+	 
 }
