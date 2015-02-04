698aebaa6c0ed55cce3aa35362a7a6381911f258
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 09:20:16 +0000

    equals and hashCode for FAQ and Projects.
    git-svn-id: http://go-java-1.googlecode.com/svn@1160 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/FAQ.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/FAQ.java
@@ -17,4 +17,36 @@ public class FAQ {
 		return answer;
 	}
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((answer == null) ? 0 : answer.hashCode());
+        result = prime * result
+                + ((question == null) ? 0 : question.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        FAQ other = (FAQ) obj;
+        if (answer == null) {
+            if (other.answer != null)
+                return false;
+        } else if (!answer.equals(other.answer))
+            return false;
+        if (question == null) {
+            if (other.question != null)
+                return false;
+        } else if (!question.equals(other.question))
+            return false;
+        return true;
+    }
+
 }
--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Projects.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Projects.java
@@ -28,4 +28,30 @@ public class Projects {
 	public void add(Project project) {
 		projects.add(project);		
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((projects == null) ? 0 : projects.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Projects other = (Projects) obj;
+        if (projects == null) {
+            if (other.projects != null)
+                return false;
+        } else if (!projects.equals(other.projects))
+            return false;
+        return true;
+    }
 }
