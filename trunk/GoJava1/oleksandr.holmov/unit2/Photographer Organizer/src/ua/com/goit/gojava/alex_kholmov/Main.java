79b3e36dd7f2872367f0e3c011c0f2c80da02488
alex.sash.kh@gmail.com <alex.sash.kh@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-04 15:31:52 +0000

    Initial commit of unit2. with tests and UML diagram version 0.5
    git-svn-id: http://go-java-1.googlecode.com/svn@1167 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/src/ua/com/goit/gojava/alex_kholmov/Main.java
+++ trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/src/ua/com/goit/gojava/alex_kholmov/Main.java
@@ -16,12 +16,7 @@ public class Main {
         // TODO Auto-generated method stub
         FotoStudio myStudio = new FotoStudio("some studio", "333 street, room 12", "+38093 666 78 89", 2);
         myStudio.setReserveDate(2015, 2, 14, 14, 50);
-        myStudio.setReserveTime(3);
-        myStudio.printInfo();
-        myStudio.displayReserveDateTime();
-        System.out.println();
         Customer myCustomer = new Customer("vasya i petya", "34 stree, house 12", "+38096 888 89 65");
-        myCustomer.printInfo();
         
         PackageFotos pFotos1 = new PackageFotos(200, 10, 10);
         PackageFotos pFotos2 = new PackageFotos(35, 20, 15);
@@ -30,13 +25,16 @@ public class Main {
         service.addPackage(pFotos1);
         service.addPackage(pFotos2);
         service.daysEditFotosInService();
-        System.out.println("Amount work days: " + service.amountDays);
+        System.out.println("Количество дней дял работы: " + service.amountDays);
         
         Order myOrder = new Order();
+        myOrder.setCustomer(myCustomer);
+        myOrder.setFotoStudio(myStudio);
         myOrder.setDeadline(2015, 3, 22);
-        myOrder.setStartWork(2015, 3, 20);
+        myOrder.setStartWork(2015, 3, 15);
         myOrder.calcEndWork(service.amountDays);
         myOrder.displayOrder();
+        myOrder.checkWorkDays();
     }
 
 }
--- trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/src/ua/com/goit/gojava/alex_kholmov/Order.java
+++ trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/src/ua/com/goit/gojava/alex_kholmov/Order.java
@@ -14,12 +14,26 @@ import java.util.GregorianCalendar;
  *
  */
 public class Order {
-    private Calendar deadline;
-    private Calendar startWork;
+    private final String MSG_1 = "Дата окончания работы выходит за пределы даты, указанной в договоре";
+    private final String MSG_2 = "Исходя из выбранной дневной нагрузки,\nрекомендуется установить дату начала работ\nне позднее: ";
+    
+    Calendar deadline;
+    Calendar startWork;
     Calendar endWork;
     private Customer customer;
+    private FotoStudio fotoStudio;
     private SellService service;
     
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    
+    void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    void setFotoStudio(FotoStudio fotoStudio) {
+        this.fotoStudio = fotoStudio;
+    }
+
     void setDeadline(int year, int month, int date) {
         deadline = new GregorianCalendar();
         deadline.set(year, Calendar.MONTH, date);
@@ -31,7 +45,6 @@ public class Order {
     }
     
     void calcEndWork(int amountDays) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         String startWorkDate = dateFormat.format(startWork.getTime());
         try {
             Date dateParse = dateFormat.parse(startWorkDate);
@@ -44,10 +57,25 @@ public class Order {
         }
     }
     
+    void checkWorkDays() {  
+        if (endWork.after(deadline)) {
+            System.out.println(MSG_1);
+            int i = 1;
+            while (endWork.after(deadline)) {
+                endWork.add(Calendar.DAY_OF_MONTH, -i);
+                startWork.add(Calendar.DAY_OF_MONTH, -i);
+                i++;
+            }
+            System.out.println(MSG_2 + dateFormat.format(startWork.getTime()));
+        }
+    }
+    
     void displayOrder() {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        System.out.println(dateFormat.format(deadline.getTime()));
-        System.out.println(dateFormat.format(startWork.getTime()));
-        System.out.println(dateFormat.format(endWork.getTime()));
+        System.out.println("Дата окончания работ по договору: " + dateFormat.format(deadline.getTime()));
+        System.out.println("Дата начала работы: " + dateFormat.format(startWork.getTime()));
+        System.out.println("Посчитанная дата окончания работы: " + dateFormat.format(endWork.getTime()));
+        customer.printInfo();
+        fotoStudio.printInfo();
     }
 }
