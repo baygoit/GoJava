d779aef01326acf8eaabbda00c5ce2f0484f797a
apofig@gmail.com <apofig@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-05 15:56:19 +0000

    Реализовал меню проекта
    git-svn-id: http://go-java-1.googlecode.com/svn@1208 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Kickstarter.java
+++ trunc/GoJava2/alex.baglay/Kickstarter/src/ua/com/goit/gojava/kickstarter/Kickstarter.java
@@ -18,45 +18,99 @@ public class Kickstarter {
 		this.generator = generator;
 	}
 
+	// следующая история, которую надо реализовать - 
+	// Как гость я хочу инвестировать в понравившийся мне проект, чтобы поддержать его
+	// Сценарий: Находясь в конкретном проекте -> Вижу меню с вариантами, что могу сделать, один из пунктов - проинвестировать в проект ->
+	// Выбираю его -> Вижу вопрос от пеймент системы о вводе имени и номера карточки и суммы -> Ввожу их ->
+	// Перехожу на описание проекта, где вижу что инфа о количестве пожертвований поменялась
+	// опять же будем ее декомпозировать на части потому как все сразу реализовать будет сложно
+	// Какие части тут есть?
+	// TODO меню конкретного проекта - этого у меня еще нету. Сейчас проверим. Да сейчас выбирая проект я могу посмотреть его описание 
+	// и сразу же выбрать следуюший. Выберите проект: [1...2] или 0 для выхода . Можно сделать для начала так, чтобы у проекта было свое 
+	// меню с одним единственным пунктом - выход.
+	// TODO дальше можно добавить еще один пункт. Оплата, захоя в который программа запросит 
+	// последовательно имя, номер карточки и сумму, а после ввода возвращаться обратно в меню проекта
+	// TODO дальше можно сделать чтобы сумма списывалась из проекта как оплаченная. И на этом вроде как все. 
+	// Пока мне видятся такие етапы.
 	public void run() {		
 		println(generator.nextQuote());
 		
-		while (true) {
-			askCategory();
-			int menu = io.read();
-			if (menu == 0) { 
-				break; 
-			}
-			
-			Category category = chooseCategory(menu);
-			if (category == null) {
-				continue; 
-			}
-			
-			Project[] found = projects.getProjects(category);
-			printProjects(found);			
-			projectMenu(found);
-		}
+		categoryMenu();
 		println("Спасибо за использование нашей программы!");
 	}
 
-	private void projectMenu(Project[] found) {
-		while (true) { 
-			askProject(found);
-
-			int menu = io.read(); 
-			if (menu == 0) {
+	// это у нас меню категорий.
+	private void categoryMenu() {
+		while (true) { // цикл
+			askCategories();  // просим выбрать что-то
+			int menu = io.read(); // читаем номер меню
+			if (menu == 0) {  // если 0 - выходим
 				break; 
 			}
 			
-			Project project = chooseProject(menu, found);
-			if (project == null) {
+			Category category = chooseCategory(menu); // иначе выбираем
+			if (category == null) { // если нет ничего, начинаем с начала 
+				continue; 
+			}
+			
+			Project[] found = projects.getProjects(category); // печатаем детали по выбору
+			printProjects(found);			
+			projectsMenu(found); // переходим в подменю, в котором
+		}
+	}
+
+	// это меню проекты
+	private void projectsMenu(Project[] found) {
+		while (true) {  // тот же цикл
+			askProjects(found); // просим выбрать что-то
+
+			int menu = io.read(); // читаем номер меню
+			if (menu == 0) { // Если ноль то выходим из меню
+				break; 
+			}
+			
+			Project project = chooseProject(menu, found); // иначе выбираем
+			if (project == null) { // если не то выбрали то в начало
 				continue;
 			}
 			
-			chooseProject(project); 													
+			chooseProject(project); // печатаем детали по выбору 													
 			printProjectDetails(project);
+			// ну по аналогии получается что тут добавим
+			projectMenu(project); // заходим в подменю
 		}
+	}
+	// это все общий алгоритм работы меню, и он уже требует чтобы его выделили, потому как уже в третий раз прийдется его дублировать
+	// вопрос в том, как это сделать так, чтобы при этом в кикстартере осталась конкретика что делается в каждом меню, а 
+	// в класс Меню ушла абстрактная часть. Разберемся позже TODO. А сейчас пока что сделаем копипастом 
+
+	// пока это все у нас какого-то такого себе процедурного стиля и чем дальше в лес, 
+	// тем больше в Кикстартере ответственности за логику работы программы вместе с логикой хождения по меню. 
+	// думаю вскоре надо будет выделить класс Меню из Кикстартаера, поскольку я уже вижу его внутренности. 
+	// посмотри сам(а)
+	private void projectMenu(Project project) {
+		while (true) {  // тот же цикл
+			askProject(project); // просим выбрать что-то
+
+			int menu = io.read(); // читаем номер меню
+			if (menu == 0) { // Если ноль то выходим из меню
+				break; 
+			}
+			
+			// TODO тут логика работы с разными выборами меню
+			if (menu == 1) {
+				println("Спасибо, что хотите помочь проекту!");
+				// как по мне готово! Теперь осталось покрыть это все дело тестами
+				// скорее всего старые тесты поломаются, но ничего!
+				
+			}
+		}
+	}
+
+	private void askProject(Project project) {
+		// а нет, у нас жеж уже нельзя использовать консоль напрямую. Теперь для печати мы пользуемся 
+		println("Выберите что хотите сделать с проектом: \n"
+				+ "[0 - выйти к списку проектов, 1 - инвестировать в проект]");
 	}
 
 	private Project chooseProject(int menu, Project[] found) {
@@ -71,7 +125,7 @@ public class Kickstarter {
 		io.print(message + "\n");
 	}
 	
-	private void askProject(Project[] found) {
+	private void askProjects(Project[] found) {
 		if (found.length == 0) {
 			println("Проектов в категории нет. Нажмите 0 - для выхода.");
 		} else {
@@ -115,7 +169,7 @@ public class Kickstarter {
 		println("--------------------------------------");
 	}
 
-	private void askCategory() {
+	private void askCategories() {
 		println("Выберите категорию (или 0 для выхода):");
 		println(Arrays.toString(categories.getCategories()));
 	}
--- trunc/GoJava2/alex.baglay/Kickstarter/test/ua/com/goit/gojava/kickstarter/KickstarterTest.java
+++ trunc/GoJava2/alex.baglay/Kickstarter/test/ua/com/goit/gojava/kickstarter/KickstarterTest.java
@@ -117,10 +117,11 @@ public class KickstarterTest {
 		// проинитим fake - 
 		// 1- выбрали категорию 1, 
 		// 2 - выбрали второй проект, 
+		// 0 - вышли из проекта - это нам надо добавить, раньше такого небыло
 		// 0 - вышли из списка проектов, 
 		// 0 - вышли изсписка категорий, 
 		// 0 - вышли из программы 
-		FakeIO io = new FakeIO(1, 2, 0, 0, 0); 
+		FakeIO io = new FakeIO(1, 2, 0, 0, 0, 0); 
 		Kickstarter kickstarter = new Kickstarter(categories, projects, io, new StubQuoteGenerator()); 
 		
 		// when
@@ -155,11 +156,52 @@ public class KickstarterTest {
 			", video2\n" +
 			", QA\n" +
 			", --------------------------------------\n" +
+			", Выберите что хотите сделать с проектом: \n" +
+			"[0 - выйти к списку проектов, 1 - инвестировать в проект]\n" +
 			", Выберите проект: [1...2] или 0 для выхода\n" +
 			", Выберите категорию (или 0 для выхода):\n" +
 			", [1 - category1]\n" +
 			", Спасибо за использование нашей программы!\n" +
 			"]", io.getMessages().toString());
+	}
+	
+	// тест исправил, но мне хотелось бы проверить еще вход в меню оплаты, для этого напишу отдельный тест
+	@Test
+	public void shouldPrintProjectMenu_whenSelectIt() {
+  	    // given
+		Categories categories = new Categories();
+		Category category = new Category("category1");
+		categories.add(category);
+		
+		Projects projects = new Projects();
+		Project project = new Project("project1", 100, 1000, "video1", "description1");
+		projects.add(project);
+		project.setCategory(category);
+
+		IO io = mock(IO.class);
+		QuoteGenerator generator = mock(QuoteGenerator.class);
+		
+		Kickstarter kickstarter = new Kickstarter(categories, projects, io, generator); 
+		
+		// when
+		when(generator.nextQuote()).thenReturn("quote");
+		// проинитим мок
+		// 1 выбрали категорию
+		// 1 выбрали проект
+		// 1 выбрали оплату
+		// 0 вышли из проекта
+		// 0 вышли из проектов
+		// 0 вышли из категорий (программы)		
+		when(io.read()).thenReturn(1, 1, 1, 0, 0, 0);
+		
+		kickstarter.run();
+
+		// then
+		verify(io, times(2)).print("Выберите что хотите сделать с проектом: \n"
+				+ "[0 - выйти к списку проектов, 1 - инвестировать в проект]\n");
+		verify(io).print("Спасибо, что хотите помочь проекту!\n");
+		// иногда ошибешься в один символ и не поймешь в чем дело. Моки удобная штука, но в суппорте потом тесты сложнее держать
+		// в тонусе. Если моков слишком много может случиться mock hell
 	}
 	
 	@Test
