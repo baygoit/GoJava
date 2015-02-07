6b747756d24aad4b06267af1775edb8f12ed71c4
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-07 16:47:31 +0000

    Create path for test
    git-svn-id: http://go-java-1.googlecode.com/svn@1243 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/tymur.berezhnoy/module3/AnagramTDD/test/org/goJava2/anagram/AnagramTest.java
@@ -0,0 +1,61 @@
+package org.goJava2.anagram;
+
+import static org.junit.Assert.*;
+
+import org.goJava2.anagram.Anagram;
+import org.junit.Test;
+
+public class AnagramTest {
+	
+	private Anagram anagram = new Anagram();
+	
+	public void test(String expected, String actual) {
+		actual = anagram.getReversedSent(actual);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void shouldBe_Empty_WhenParamIs_Empty() {
+		test("", "");
+	}
+	
+	@Test
+	public void shouldBe_Space_WhenParamIs_Space() {
+		test(" ", " ");
+	}
+	
+	@Test
+	public void shouldBe_OneSymbol_WhenParamIs_OneSymbol() {
+		test("x", "x");
+	}
+	
+	@Test
+	public void souldBe_Reversed_WhenPramIs_XY() {
+		test("yx", "xy");
+	}
+	
+	@Test
+	public void shouldBe_Reversed_WhenParamIs_BySpace() {
+		test("yx yx", "xy xy");
+	}
+	
+	@Test
+	public void souldBe_SamePlace_WhenParamIs_Symbol() {
+		test("xy!", "yx!");
+	}
+	
+	@Test
+	public void shouldBe_Reversed_WhenParamIs_longSent() {
+		test("olleH dlrow! woH era uoy?", "Hello world! How are you?");
+	}
+	
+	@Test
+	public void shouldBe_SamePosition_WhenParamIs_SpecificSymbol() {
+		test(anagram.getNewDelimiters(), anagram.getNewDelimiters());
+	}
+	
+	@Test
+	public void exceptionsTest() {
+		assertEquals("", anagram.getReversedSent(null));
+	}
+}
