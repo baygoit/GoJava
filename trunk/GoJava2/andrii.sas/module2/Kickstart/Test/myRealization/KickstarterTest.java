669cfe780d248708ed6d5546d05f6855e3b1f46c
Sas.GoIT@gmail.com <Sas.GoIT@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-08 20:45:57 +0000

    All Tests
    git-svn-id: http://go-java-1.googlecode.com/svn@1286 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.sas/module2/Kickstart/Test/myRealization/KickstarterTest.java
+++ trunk/GoJava2/andrii.sas/module2/Kickstart/Test/myRealization/KickstarterTest.java
@@ -2,13 +2,12 @@ package myRealization;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 
 public class KickstarterTest {
 	@Test
@@ -174,28 +173,68 @@ public class KickstarterTest {
 		
 		//then
 		assertEquals("[quote\n" +
-", 1 - category1, 2 - category2\n" +
-"What are you interested in? Pleace, make your choice:\n" +
-", You chose - category1\n" +
-", 1) Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
-", If you want to return press \"0\"\n" +
-", --------------------------------------------------\n" +
-", You chose: Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
-", history1\n" +
-", videoLink1\n" +
-", questions\n" +
-", --------------------------------------------------\n" +
-", Return - \"0\"\n" +
-", Error!! You must enter 0 \n" +
-"Please, try again\n" +
-", 1) Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
-", If you want to return press \"0\"\n" +
-", Error!! There are no such project - Try again:\n" +
-", 1 - category1, 2 - category2\n" +
-"What are you interested in? Pleace, make your choice:\n" +
-", Error!! There are no such category - Try again:\n" +
-", Thanks for using our program, Goodbye!\n" +
-"]", storage);
+					", 1 - category1, 2 - category2\n" +
+					"What are you interested in? Pleace, make your choice:\n" +
+					", You chose - category1\n" +
+					", 1) Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
+					", If you want to return press \"0\"\n" +
+					", --------------------------------------------------\n" +
+					", You chose: Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
+					", history1\n" +
+					", videoLink1\n" +
+					", questions\n" +
+					", --------------------------------------------------\n" +
+					", Return - \"0\"\n" +
+					", Error!! You must enter 0 \n" +
+					"Please, try again\n" +
+					", 1) Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10\n" +
+					", If you want to return press \"0\"\n" +
+					", Error!! There are no such project - Try again:\n" +
+					", 1 - category1, 2 - category2\n" +
+					"What are you interested in? Pleace, make your choice:\n" +
+					", Error!! There are no such category - Try again:\n" +
+					", Thanks for using our program, Goodbye!\n" +
+					"]", storage);
+	}
+	
+	@Test
+	public void mockTest(){
+		//given
+		Output out = mock(Output.class);
+		Input in = mock(Input.class);
+		Categories categories = new Categories();
+		Projects projects = new Projects();
+		Quote quote = mock(Quote.class);
+		Category category1 = new Category("category1");
+		Category category2 = new Category("category2");
+		categories.addCategory(category1);
+		categories.addCategory(category2);
+		Project project1 = new Project(category1);
+		project1.setProject("name1", "description1", 1000, 200, 10, "history1", "videoLink1", "questions");
+		projects.addProject(project1);
+		Kickstart kickstart = new Kickstart(out, in, categories, projects, quote);
+		
+		//when
+		when(quote.generateQuote()).thenReturn("quote");
+		when(in.readChoice()).thenReturn(1, 1, 1, 0, 3, 0, 3, 0);
+		kickstart.buildMenu();
+		//then
+		verify(out).println("quote");
+		verify(out, times(2)).println("1 - category1, 2 - category2\nWhat are you interested in? Pleace, make your choice:");
+		verify(out, times(2)).println("1) Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10");
+		verify(out).println("Thanks for using our program, Goodbye!");
+		verify(out, times(2)).println("--------------------------------------------------");
+		verify(out).println("You chose: Name - name1, Description - description1, Money we need - 1000, Money we have - 200, Days left - 10");
+		verify(out, times(2)).println("If you want to return press \"0\"");
+		verify(out).println("You chose - category1");
+		verify(out).println("history1");
+		verify(out).println("videoLink1");
+		verify(out).println("questions");
+		verify(out).println("Return - \"0\"");
+		verify(out).println("Error!! There are no such project - Try again:");
+		verify(out).println("Error!! You must enter 0 \nPlease, try again");
+		verify(out).println("Error!! There are no such category - Try again:");
+		
 	}
 	
 }
