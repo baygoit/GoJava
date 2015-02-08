ec203de5a03b48da3d68b64c442754bc95525af6
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-08 10:48:52 +0000

    Added new test case.
    git-svn-id: http://go-java-1.googlecode.com/svn@1256 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/test/org/goJava2/kickstarter/model/QuoteStorageTest.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/test/org/goJava2/kickstarter/model/QuoteStorageTest.java
@@ -20,6 +20,11 @@ public class QuoteStorageTest {
 	private QuoteStorage quoteStorage;
 	
 	@Test
+	public void shouldRandomQuote() {
+		// TODO
+	}
+	
+	@Test
 	public void shouldListSizeNot0_whenNewQuoteStorage() {
 		quoteStorage = new QuoteStorage();
 		assertFalse("Expected that constructor for Hard-coded quotes init at least 1 quote.",
@@ -27,7 +32,7 @@ public class QuoteStorageTest {
 	}
 	
 	@Test
-	public void shouldListNotBeNull() {
+	public void shouldListNotBeNull_whenNewQuoteStorage() {
 		quoteStorage = new QuoteStorage();
 		assertFalse("Expected that list is not null.",
 					quoteStorage.getContent() == null);
@@ -36,14 +41,18 @@ public class QuoteStorageTest {
 	@Test
 	public void shouldListContainsNewQuote_whenAddNewQuote() {
 		quoteStorage = new QuoteStorage();
+		
 		Quote quote = new Quote("New quote", "author");
 		quoteStorage.addContent(quote);
-		assertTrue("Expected that new created cuote is at storage.",
+		
+		assertTrue("Expected that new created quote is at storage.",
 					quoteStorage.getContent().contains(quote));
 	}
 	
 	@Test
-	public void shouldRandomQuote() {
-	// TODO
+	public void listShouldNotBeNull_whenNewCustomQuoteStorage() {
+		quoteStorage = new QuoteStorage(new ArrayList<Quote>());
+		assertFalse("Expected that list is not null.",
+					quoteStorage.getContent() == null);
 	}
 }
