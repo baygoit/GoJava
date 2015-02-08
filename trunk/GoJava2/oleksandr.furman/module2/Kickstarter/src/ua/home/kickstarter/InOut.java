3c4094c3ab50ab197601c3175b70c1f67c76422b
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-08 13:48:23 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1260 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/InOut.java
+++
@@ -1,7 +0,0 @@
-package ua.home.kickstarter;
-
-public interface InOut {
-	public int nextIntIndex();
-
-	public void output(String s);
-}
--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/ua/home/kickstarter/Kickstarter.java
+++
@@ -1,9 +0,0 @@
-package ua.home.kickstarter;
-
-public class Kickstarter {
-
-	public static void main(String[] args) {
-		Processor processor = new Processor();
-		processor.run();
-	}	
-}
