e0f220c5528f00442a71d746c756ce2925787c30
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-09 18:20:20 +0000

    refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@1322 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/inputOutput/Out.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/inputOutput/Out.java
@@ -1,54 +1,8 @@
 package com.gojava.inputOutput;
-import com.gojava.projects.Project;
 
 public class Out {
-    public String output(String in) {
-        return in; 
-    }
-
-    public String output(String string, String string2) {
-        return string + string2;
-    }
-
-    public String output(String string, int i) {
-        return string + Integer.toString(i);
-    }
-
-    public String output(int i, String string) {
-        return i + string;
-    }
 
     public void print(String string) {
         System.out.println(string);
     }
-
-    public String printProjectPreview(Project project) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("Project Name: ").append(project.getName()).append("\n");
-        sb.append("Description: ").append(project.getDescription())
-                .append("\n");
-        sb.append("Need Sum: ").append(project.getNeedSum()).append("\n");
-        sb.append("Current Sum: ").append(project.getCurrentSum()).append("\n");
-        sb.append("Days Left: ").append(project.getDaysLeft()).append("\n");
-        return sb.toString();
-    }
-
-    public String printAdditionalProjectFields(Project project) {
-        StringBuffer sb = new StringBuffer();
-        sb.append("ProjectHistory: ").append(project.getProjectHistory())
-                .append("\n");
-        sb.append("LinkOnvideo: ").append(project.getLinkOnvideo())
-                .append("\n");
-        sb.append("Questions and answers: ")
-                .append(project.getQuestionsAndAnswers()).append("\n");
-        return sb.toString();
-    }
- 
-//    public String printCategory(Category category) {
-//        StringBuffer sb = new StringBuffer();
-//        sb.append(category.toString());
-//        return sb.toString();
-//    }
-
-
 }
