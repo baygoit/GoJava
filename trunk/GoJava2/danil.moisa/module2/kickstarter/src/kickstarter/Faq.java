55ef4b1df7ba6528b22990699d6c7c89d4a689d9
d.moysa@gmail.com <d.moysa@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-09 23:22:44 +0000

    new version +test - 40%
    git-svn-id: http://go-java-1.googlecode.com/svn@1343 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/danil.moisa/module2/kickstarter/src/kickstarter/Faq.java
@@ -0,0 +1,11 @@
+package kickstarter;
+
+public class Faq {
+	public String faq;
+
+	public Faq() {
+		faq = "Еще не было вопросов по данному проекту! \n Вы можете задать вопросы по проекту! \n Мы ответим на них в кратчайшие сроки";
+
+	}
+
+}
---
+++ trunk/GoJava2/danil.moisa/module2/kickstarter/src/kickstarter/Welcome.java
@@ -0,0 +1,11 @@
+package kickstarter;
+
+public class Welcome {
+	public Welcome() {
+
+			}
+
+	public void welcome() {
+		System.out.println("\n••••• Welcome to Kickstarter! •••••\n");
+	}
+}
