ec9b0a87b7efc3b855ccc0ec74ad19d3e116a2c9
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-09 20:59:24 +0000

    tests
    git-svn-id: http://go-java-1.googlecode.com/svn@1336 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/TestMenu.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/TestMenu.java
@@ -2,11 +2,18 @@ package com.gojava.view;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
 import org.junit.Test;
 
-import com.gojava.projects.ProjectManager;
+import com.gojava.projects.CategoryStorage;
+import com.gojava.projects.ProjectStorage;
 
 public class TestMenu {
+    Menu menu = new Menu(new CategoryStorage(), new ProjectStorage());
+    @Test
+    public void should(){
+        int currentLevelPosition = 1;
+        Level actual = menu.getCurrentLevel();
+        Level1 level1 = new Level1(null, null);
+        assertTrue(level1.getPosition() == actual.getPosition());;
+    }
 }
