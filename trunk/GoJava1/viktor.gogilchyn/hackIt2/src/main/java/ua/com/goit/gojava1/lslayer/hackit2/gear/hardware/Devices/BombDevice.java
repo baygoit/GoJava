7f17b04d0bd24ce4f72783cb0c77b7af890a1a4a
Lslayer2005@gmail.com <Lslayer2005@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-09 09:10:02 +0000

    WTF, Win7 ruleez!
    git-svn-id: http://go-java-1.googlecode.com/svn@1293 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/viktor.gogilchyn/hackIt2/src/main/java/ua/com/goit/gojava1/lslayer/hackit2/gear/hardware/Devices/BombDevice.java
+++ trunk/GoJava1/viktor.gogilchyn/hackIt2/src/main/java/ua/com/goit/gojava1/lslayer/hackit2/gear/hardware/Devices/BombDevice.java
@@ -1,4 +1,4 @@
-package ua.com.goit.gojava1.lslayer.hackit2.gear.hardware.Devices;
+package ua.com.goit.gojava1.lslayer.hackit2.gear.hardware.devices;
 
 import ua.com.goit.gojava1.lslayer.hackit2.gear.hardware.AbstractHardware;
 import ua.com.goit.gojava1.lslayer.hackit2.gear.hardware.Hardware;
