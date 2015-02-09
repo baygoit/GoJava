e7be9fdb93ac8f517a2b25431067e629b5d779d0
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-09 15:31:30 +0000

    shouldAllProgects_WhenGetAll
    git-svn-id: http://go-java-1.googlecode.com/svn@1313 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/projects/TestProjectStorage.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/projects/TestProjectStorage.java
@@ -6,14 +6,12 @@ import org.junit.Test;
 
 public class TestProjectStorage {
     ProjectStorage projectStorage = new ProjectStorage();
-
     @Test
-    public void shouldProgecttList_WhenAddProgectList() {
-        ProjectStorage projectStorage = new ProjectStorage();
+    public void shouldProgectList_WhenAddProgectList() {
         projectStorage.add("name", "desc", 1, 1, 1, "categoryId",
                 "linkOnvideo", "questionsAndAnswers", 1);
-        Project project = projectStorage.getProject(0);
-        assertEquals("name desc 1 1 1 categoryId linkOnvideo questionsAndAnswers 1", project.toString());
+        Project actual = projectStorage.getProject(0);
+        assertEquals("name desc 1 1 1 categoryId linkOnvideo questionsAndAnswers 1", actual.toString());
     }
     
 //    @Test TODO спросить как такое делать.
@@ -21,4 +19,14 @@ public class TestProjectStorage {
 //        ProjectStorage projectStorage = new ProjectStorage();
 //        assertNull(projectStorage.getProject(0));
 //    }
+    
+    @Test
+    public void shouldAllProgects_WhenGetAll() {
+        projectStorage.add("name", "desc", 1, 1, 1, "categoryId",
+                "linkOnvideo", "questionsAndAnswers", 1);
+        projectStorage.add("name2", "desc2", 2, 2, 2, "categoryId2",
+                "linkOnvideo2", "questionsAndAnswers2", 2);
+        String actual = projectStorage.getAll(1);
+        assertEquals("1) Project Name: name\nDescription: desc\nNeed Sum: 1\nCurrent Sum: 1\nDays Left: 1\n\n", actual);
+    }  
 }
