ec1ea0b50ed1109bc6585f88153a20409f4e69c4
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-10 13:39:07 +0000

    shouldNull_WhenDisplayMySelf()
    git-svn-id: http://go-java-1.googlecode.com/svn@1370 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/Level3Test.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/Level3Test.java
@@ -18,15 +18,21 @@ public class Level3Test {
      assertEquals("", actual);
      }
 
+     @Test
+     public void shouldGetOneProjects_WhenDisplayMySelf() {
+         Level3 level3 = new Level3(projectStorage);
+         projectStorage.add("test1", "description1", 1, 1, 1, "projectHistory1",
+                 "linkOnvideo1", "questionsAndAnswers1", 1);
+         String actual = level3.displayMySelf(1, 1);
+         assertEquals(
+                 "Project Name: test1\nDescription: description1\nNeed Sum: 1\nCurrent Sum: 1\nDays Left: 1\nProjectHistory: projectHistory1\nLinkOnvideo: linkOnvideo1\nQuestions and answers: questionsAndAnswers1\n\n",
+                 actual);
+     }
     @Test
-    public void shouldGetOneProjects_WhenDisplayMySelf() {
-        int a = 5;
+    public void shouldNull_WhenDisplayMySelf() {
         Level3 level3 = new Level3(projectStorage);
-        projectStorage.add("test1", "description1", 1, 1, 1, "projectHistory1",
-                "linkOnvideo1", "questionsAndAnswers1", 1);
-        String actual = level3.displayMySelf(1, 1);
-        assertEquals(
-                "Project Name: test1\nDescription: description1\nNeed Sum: 1\nCurrent Sum: 1\nDays Left: 1\nProjectHistory: projectHistory1\nLinkOnvideo: linkOnvideo1\nQuestions and answers: questionsAndAnswers1\n\n",
-                actual);
+        String actual = level3.displayMySelf(1);
+        assertNull(actual);
+               
     }
 }
