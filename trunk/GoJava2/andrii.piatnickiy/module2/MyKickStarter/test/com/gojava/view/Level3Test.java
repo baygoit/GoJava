d57dfcbb55ab089fce0342784a45904b73fc9e72
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-10 13:35:31 +0000

    shouldGetOneProjects_WhenDisplayMySelf()
    git-svn-id: http://go-java-1.googlecode.com/svn@1367 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/Level3Test.java
@@ -0,0 +1,33 @@
+package com.gojava.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.gojava.projects.ProjectStorage;
+import com.gojava.view.Level3;
+
+public class Level3Test {
+
+    ProjectStorage projectStorage = new ProjectStorage();
+
+    // @Test
+    // public void shouldGetNoProjects_WhenDisplayMySelf() {
+    //
+    // Level3 level3 = new Level3(projectStorage);
+    // String actual = level3.displayMySelf(1);
+    // assertEquals("", actual);
+    // }
+
+    @Test
+    public void shouldGetOneProjects_WhenDisplayMySelf() {
+        int a = 5;
+        Level3 level3 = new Level3(projectStorage);
+        projectStorage.add("test1", "description1", 1, 1, 1, "projectHistory1",
+                "linkOnvideo1", "questionsAndAnswers1", 1);
+        String actual = level3.displayMySelf(1, 1);
+        assertEquals(
+                "Project Name: test1\nDescription: description1\nNeed Sum: 1\nCurrent Sum: 1\nDays Left: 1\nProjectHistory: projectHistory1\nLinkOnvideo: linkOnvideo1\nQuestions and answers: questionsAndAnswers1\n\n",
+                actual);
+    }
+}
