04a0dd54fc990047050a9c2ab1375b822fbcd767
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-10 11:16:48 +0000

    shouldGetOneProjects_WhenDisplayMySelf()
    git-svn-id: http://go-java-1.googlecode.com/svn@1357 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/Level2Test.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/Level2Test.java
@@ -13,4 +13,11 @@ public class Level2Test {
         String actual = level2.displayMySelf(0);
         assertEquals("", actual);
     }
+    @Test
+    public void shouldGetOneProjects_WhenDisplayMySelf() {
+        Level2 level2 = new Level2(projectStorage);
+        projectStorage.add("test1", "description1", 1, 1, 1, "projectHistory1", "linkOnvideo1", "questionsAndAnswers1", 1);
+        String actual = level2.displayMySelf(0);
+        assertEquals("", actual);
+    }
 }
