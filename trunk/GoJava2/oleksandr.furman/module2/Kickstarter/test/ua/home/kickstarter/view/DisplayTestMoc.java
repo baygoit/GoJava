4e39370f7b7d07df1be4b3ce8515880123d2e165
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-11 13:26:26 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1412 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/test/ua/home/kickstarter/view/DisplayTestMoc.java
+++ trunk/GoJava2/oleksandr.furman/module2/Kickstarter/test/ua/home/kickstarter/view/DisplayTestMoc.java
@@ -1,5 +1,6 @@
 package ua.home.kickstarter.view;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -30,7 +31,7 @@ public class DisplayTestMoc {
 		StringBuilder categoriesContent = new StringBuilder();
 		for (Map.Entry<Integer, Category> pair : categories.entrySet()) {
 			categoriesContent.append(pair.getKey()).append(" - ").append(pair.getValue().getName()).append("\n");
-		}
+		} 
 		Display display = new Display(quotationsController, categoriesController, projectsController, consoleOutput);
 
 		when(categoriesController.passContentToView()).thenReturn(categoriesContent.toString());
@@ -68,19 +69,29 @@ public class DisplayTestMoc {
 
 		Display display = new Display(quotationsController, categoriesController, projectsController, consoleOutput);
 		
-		Map<Category, List<Project>> projects = new HashMap<Category, List<Project>>();
+		Map<Integer, Category> categories = new HashMap<Integer, Category>();
+		categories.put(1, new Category("FakeGames"));
+		categories.put(2, new Category("FakeTechnology"));
+		categories.put(3, new Category("FakeDesign"));
+		
 		Project project1 = new Project("FakeName1", "Fakedescription1", 34534, 34, "linksToVideo1");
 		Project project2 = new Project("FakeName2", "Fakedescription1", 23123, 345, "linksToVideo1");
 		Project project3 = new Project("FakeName3", "Fakedescription1", 34534, 67, "linksToVideo1");
 		Project project4 = new Project("FakeName4", "Fakedescription1", 6765, 78, "linksToVideo1");
 		Project project5 = new Project("FakeName5", "Fakedescription1", 345, 345, "linksToVideo1");
 		Project project6 = new Project("FakeName6", "Fakedescription1", 7645, 676, "linksToVideo1");
-
-		Map<Integer, Category> categories = new HashMap<Integer, Category>();
-		categories.put(1, new Category("FakeGames"));
-		categories.put(2, new Category("FakeTechnology"));
-		categories.put(3, new Category("FakeDesign"));
-
+		List<Project> pr1 = new ArrayList<Project>();
+		pr1.add(project1);
+		pr1.add(project2);
+		pr1.add(project3);
+		List<Project> pr2 = new ArrayList<Project>();
+		pr2.add(project4);
+		pr2.add(project5);
+		pr2.add(project6);
+		Map<Category, List<Project>> projects = new HashMap<Category, List<Project>>();
+		projects.put(categories.get(1), pr1);
+		projects.put(categories.get(2), pr2);
+		
 		project1.setCategory(categories.get(1));
 		project2.setCategory(categories.get(1));
 		project3.setCategory(categories.get(1));
@@ -88,11 +99,34 @@ public class DisplayTestMoc {
 		project5.setCategory(categories.get(2));
 		project6.setCategory(categories.get(2));
 		// when
+		int projectNumber = 1;
+		StringBuilder projectsContent = new StringBuilder();
+		for (Project project : projects.get(categories.get(1))) {
+			projectsContent.append(projectNumber).append(project.getShortInfo()).append("\n");
+			projectNumber++;
+		}
 
+		when(projectsController.passSpecificContentToView(categories.get(1))).thenReturn(projectsContent.toString());
 
-		when(projectsController.passContentToView()).thenReturn(null);
-
-		display.displayCategories();
-		verify(consoleOutput).output("Выберите категорию :\n 1 - FakeGames\n 2 - FakeTechnology\n 3 - FakeDesign");
+		display.displayProjects(categories.get(1));;
+		verify(consoleOutput).output("1 - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓"
++"\nНазвание проекта: FakeName1"
++"\nОписание проекта: Fakedescription1"
++"\nНеобходимая сумма: 34534$"
++"\nСобранная сумма: 0$"
++"\nДо окончания сбора средств: 34 дней"
++"\n2 - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓"
++"\nНазвание проекта: FakeName2"
++"\nОписание проекта: Fakedescription1"
++"\nНеобходимая сумма: 23123$"
++"\nСобранная сумма: 0$"
++"\nДо окончания сбора средств: 345 дней"
++"\n3 - ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓"
++"\nНазвание проекта: FakeName3"
++"\nОписание проекта: Fakedescription1"
++"\nНеобходимая сумма: 34534$"
++"\nСобранная сумма: 0$"
++"\nДо окончания сбора средств: 67 дней"
++"\n[Выберите проект от 1 до 0 или нажмите 0 для возврата к выбору категорий]\n ");
 	}
 }
