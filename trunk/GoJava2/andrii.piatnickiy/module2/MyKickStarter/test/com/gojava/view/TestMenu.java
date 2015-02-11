e691483a557b08a5364ee15086b5d02b85b43e33
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-11 14:27:45 +0000

    shoulGetStringFromCurrentLevel_FromLevel2ToLevel3() and rename shoulGetStringFromCurrentLevel_FromLevel1ToLevel2()
    git-svn-id: http://go-java-1.googlecode.com/svn@1419 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/TestMenu.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/test/com/gojava/view/TestMenu.java
@@ -9,10 +9,11 @@ import com.gojava.projects.CategoryStorage;
 import com.gojava.projects.ProjectStorage;
 
 public class TestMenu {
-    
+
     CategoryStorage categoryStorage = new CategoryStorage();
     ProjectStorage projectStorage = new ProjectStorage();
     Menu menu = new Menu(categoryStorage, projectStorage, null);
+
     private void initCategories() {
         categoryStorage.add("Sport", 1);
         categoryStorage.add("Car", 2);
@@ -24,47 +25,51 @@ public class TestMenu {
                 "History", "Link on video", "Questions and answers", 1);
         projectStorage.add("BMW X3", "BMW X3 description", 30000, 3000, 300,
                 "History", "Link on video", "Questions and answers", 2);
-        projectStorage.add("Audi Q5", "Audi Q5 description", 40000, 400, 40,
-                "History", "Link on video", "Questions and answers", 2);
         projectStorage.add("Laptop", "Laptop description", 500, 50, 50,
                 "History", "Link on video", "Questions and answers", 3);
-        projectStorage.add("Mobile phone", "Mobile phone description", 60, 60,
-                6, "History", "Link on video", "Questions and answers", 3);
     }
-
-    private static void initCategories(CategoryStorage categoryStorage) {
-
-    }
-
-     @Test
-     public void shouldLevel_WhenGetCurrentLevel() {
-     menu.setCurrentLevelPosition(1);
-     Level actual = menu.getCurrentLevel();
-     Level1 level1 = new Level1(null);
-     assertTrue(level1.getPosition() == actual.getPosition());
-     }
-    
-     @Test
-     public void shoulNotAlLowed_WhenTryGoUpOutOfMenu() {
-     menu.setCurrentLevelPosition(1);
-     String actual = menu.nextLevel(0);
-     assertEquals("not allowed to go below this level", actual);
-     }
-    
-     @Test
-     public void shoulNotAlLowed_WhenTryGoDownOutOfMenu() {
-     menu.setCurrentLevelPosition(3);
-     String actual = menu.nextLevel(1);
-     assertEquals("not allowed to go below this level", actual);
-     }
 
     @Test
-    public void shoulGetStringFromCurrentLevel_From1LevelTo2Level() {
+    public void shouldLevel_WhenGetCurrentLevel() {
+        menu.setCurrentLevelPosition(1);
+        Level actual = menu.getCurrentLevel();
+        Level1 level1 = new Level1(null);
+        assertTrue(level1.getPosition() == actual.getPosition());
+    }
+
+    @Test
+    public void shoulNotAlLowed_WhenTryGoUpOutOfMenu() {
+        menu.setCurrentLevelPosition(1);
+        String actual = menu.nextLevel(0);
+        assertEquals("not allowed to go below this level", actual);
+    }
+
+    @Test
+    public void shoulNotAlLowed_WhenTryGoDownOutOfMenu() {
+        menu.setCurrentLevelPosition(3);
+        String actual = menu.nextLevel(1);
+        assertEquals("not allowed to go below this level", actual);
+    }
+
+    @Test
+    public void shoulGetStringFromCurrentLevel_FromLevel1ToLevel2() {
         initCategories();
         initProjects(projectStorage);
         menu.setCurrentLevelPosition(1);
         String actual = menu.nextLevel(1);
-        assertEquals("1) Project Name: Bicycle\nDescription: Bicycle description\nNeed Sum: 10000\nCurrent Sum: 100\nDays Left: 10\n\n", actual);
+        assertEquals(
+                "1) Project Name: Bicycle\nDescription: Bicycle description\nNeed Sum: 10000\nCurrent Sum: 100\nDays Left: 10\n\n",
+                actual);
     }
 
+    @Test
+    public void shoulGetStringFromCurrentLevel_FromLevel2ToLevel3() {
+        initCategories();
+        initProjects(projectStorage);
+        menu.setCurrentLevelPosition(2);
+        menu.setCategoryPosition(1);
+        String actual = menu.nextLevel(1);
+        assertEquals("Project Name: Bicycle\nDescription: Bicycle description\nNeed Sum: 10000\nCurrent Sum: 100\nDays Left: 10\nProjectHistory: History\nLinkOnvideo: Link on video\nQuestions and answers: Questions and answers\n\n",
+                actual);
+    }
 }
