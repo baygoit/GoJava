367432866e7ac82f205fa5f4b758beef7adf06a3
iavorskyi.v.v@gmail.com <iavorskyi.v.v@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-11 07:35:00 +0000

    Test added.
    git-svn-id: http://go-java-1.googlecode.com/svn@1405 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/LearningUnitManager.java
+++ trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.java/goit/iavorskyi/LearningUnitManager.java
@@ -1,25 +1,14 @@
 package goit.iavorskyi;
 
-import goit.iavorskyi.io.Streamer;
-
 import java.util.LinkedList;
 import java.util.List;
 
 public class LearningUnitManager {
 
-//	Testing IO
-	public static void main(String[] args) {
-		Streamer stream = new Streamer();
-		String tmp = stream.read("D:\\1.txt");
-		stream.write(tmp, "D:\\2.txt");
-	}
-		
 	public List<String> search(String whatToSearch) {
-		List <String> result = new LinkedList<String>();
+		List<String> result = new LinkedList<String>();
 		// TODO make some search engine here.
 		return result;
 	}
-	
-	
-	
+
 }
--- trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.test/goit/iavorskyi/LearningUnitTest.java
+++ trunk/GoJava1/volodymyr.iavorskiy/JavaHub/src.test/goit/iavorskyi/LearningUnitTest.java
@@ -1,20 +1,64 @@
 package goit.iavorskyi;
 
 import static org.junit.Assert.*;
+import goit.iavorskyi.learningUnit.Comment;
+import goit.iavorskyi.learningUnit.LearningUnit;
+import goit.iavorskyi.learningUnit.Rating;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class LearningUnitTest {
 
-	@Test
-	public void testLogin() {
-		
+	private LearningUnit lu = new LearningUnit();
+	private Comment comment1 = new Comment();
+	private Rating rating1 = new Rating();
+	private Comment comment2 = new Comment();
+	private Rating rating2 = new Rating();
+	
+	@Before
+	public void init() {
+		comment1.setAuthor("Vova");
+		comment1.setComment("first comment");
+		comment1.setId(1);
+		lu.getComments().add(comment1);
+		comment2.setAuthor("Vanya");
+		comment2.setComment("second comment");
+		comment2.setId(2);
+		lu.getComments().add(comment2);
+		rating1.setAuthor("Vova");
+		rating1.setRating(10);
+		lu.getRating().add(rating1);
+		rating2.setAuthor("Vanya");
+		rating2.setRating(15);
+		lu.getRating().add(rating2);
 	}
 	
 	@Test
-	public void testRegister() {
-		
-		
+	public void testCalculatePopularity() {
+		assertEquals(4, lu.calculatePopularity());
+	}
+
+	@Test
+	public void testDeleteComment() {
+		assertTrue(lu.deleteComment(2));
+	}
+
+	@Test
+	public void testDeleteRate() {
+		assertTrue(lu.deleteRate("Vanya"));
+	}
+
+	@Test
+	public void testDeleteAllRates() {
+		lu.deleteAllComments();
+		assertEquals(0, lu.getComments().size());
+	}
+
+	@Test
+	public void testDeleteAllComments() {
+		lu.deleteAllRates();
+		assertEquals(0, lu.getRating().size());
 	}
 
 }
