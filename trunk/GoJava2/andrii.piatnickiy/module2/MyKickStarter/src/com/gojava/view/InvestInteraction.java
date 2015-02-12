2c8e8274f94caef1ba88851b4862e240cd1316c8
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-12 11:37:57 +0000

    display ammounts type from array list of ammounts
    git-svn-id: http://go-java-1.googlecode.com/svn@1449 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/view/InvestInteraction.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/view/InvestInteraction.java
@@ -1,5 +1,6 @@
 package com.gojava.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.gojava.projects.Project;
@@ -8,30 +9,54 @@ import com.gojava.projects.ProjectStorage;
 public class InvestInteraction implements Interactionable {
     Menu menu;
     ProjectStorage projectStorage;
+
     public InvestInteraction(Menu menu, ProjectStorage projectStorage) {
         this.menu = menu;
-        this.projectStorage = projectStorage; 
-        
-    }
+        this.projectStorage = projectStorage;
 
+    }
 
     @Override
     public String description() {
         return "Invest in the project";
     }
+
     public void displayInteractionSet() {
-        name();
-        bankAccoun();
-        ammount();
+        // name();
+        // bankAccoun();
+        chooseAmmountType();
+        // ammount();
     }
-    
+
+    ArrayList<Ammount> ammounts = new ArrayList<>();
+
+    private void initAmmounts() {
+        ammounts.add(new Ammount("Own payment method"));
+        ammounts.add(new Ammount("1"));
+        ammounts.add(new Ammount("10"));
+        ammounts.add(new Ammount("40"));
+    }
+
+    private void chooseAmmountType(){
+        initAmmounts(); 
+        String ammountMessage = "";
+        if(!ammounts.isEmpty()){
+            System.out.println("You can choose automatic or your own payment method");
+            for(int i = 0; i < ammounts.size(); i++){
+                ammountMessage += i + ") "+ ammounts.get(i).description + "\n";
+            }
+        }
+        System.out.println(ammountMessage);
+    }
+
     private void ammount() {
         String ammountMessage = "Please, enter your ammount";
         System.out.println(ammountMessage);
         Scanner scanner2 = new Scanner(System.in);
         int sum = scanner2.nextInt();
-        Project project = projectStorage.getSpecificProject(menu.currentCategory, menu.currentProject);
-        project.setCurrentSum(sum +  project.getCurrentSum());
+        Project project = projectStorage.getSpecificProject(
+                menu.currentCategory, menu.currentProject);
+        project.setCurrentSum(sum + project.getCurrentSum());
     }
 
     private void bankAccoun() {
