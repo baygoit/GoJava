eeaf16771258c09ae71de1219fc66aaf438f8acd
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-15 13:29:23 +0000

    MVC and Model tests added  (Kickstarter project, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@1504 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/MVCTest.java
@@ -0,0 +1,14 @@
+package org.kudryavtsev.kickstarter.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.kudryavtsev.kickstarter.MVC;
+
+public class MVCTest {
+
+    @Test
+    public void testMVC() {
+        MVC mvc = new MVC();
+    }
+}
--- trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ModelTest.java
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ModelTest.java
@@ -1,5 +1,8 @@
 package org.kudryavtsev.kickstarter.test;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.kudryavtsev.kickstarter.data.Model;
 
@@ -8,19 +11,28 @@ public class ModelTest {
 
     @Test
     public void testModelInit() {
-        model.init();
+
     }
 
     @Test
     public void testGetCategoriesList() {
-        // assertEquals("",
-        // model.getCategoriesList().toString());model.getCategoriesList()
-        model.getCategoriesList();
+        model.init();
+        assertEquals(
+                "[Sport - Sport projects, Technology - Technology projects, Science - Science projects]",
+                model.getCategoriesList().toString());
     }
 
     @Test
     public void testGetProjectsList() {
-        // assertEquals("", model.getProjectsList().toString());
-        model.getProjectsList();
+        model.init();
+        assertEquals(
+                "[FutureForward; FutureForward is an innovative training project; funded: 4; pledged: 200; days to go: 45, U23; Support Team Canada U23 in their quest for Gold!; funded: 5500; pledged: 5000; days to go: 5]",
+                model.getCategoriesList().get(0).getProjectsList().toString());
+        assertEquals(
+                "[APEX Wallet; The Apex is a slim aluminum wallet with RFID protection; funded: 56400; pledged: 4000; days to go: 12, Arduino; We love Arduino and we love space exploration. So we decided to combine them and let people run their own space experiments!; funded: 10600; pledged: 35000; days to go: 4]",
+                model.getCategoriesList().get(1).getProjectsList().toString());
+        assertEquals(
+                "[EVB; EVB: Replace the Brain and Sensors in your LEGO® MINDSTORMS EV3 with a BeagleBone Black; funded: 36000; pledged: 8000; days to go: 1, Makeblock; Makeblock is an aluminum extrusion based construct platform that can be used to build robots; funded: 186000; pledged: 30000; days to go: 2]",
+                model.getCategoriesList().get(2).getProjectsList().toString());
     }
 }
