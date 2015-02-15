324369b51f1d1c2322ee3c0c857a0432b7dc41d2
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-15 23:08:59 +0000

    Updated MVC test (Kickstarter project, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@1518 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/MVCTest.java
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/MVCTest.java
@@ -1,14 +1,15 @@
 package org.kudryavtsev.kickstarter.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 import org.kudryavtsev.kickstarter.MVC;
+import static org.mockito.Mockito.*;
 
 public class MVCTest {
 
     @Test
     public void testMVC() {
         MVC mvc = new MVC();
+        MVC mockedMVC = mock(MVC.class);
+        verify(mockedMVC);
     }
 }
