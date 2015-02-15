d03611c60302de05cd27519bcdea8090fd31497a
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-15 18:39:32 +0000

    getCategoryToString() in FileCategoryStorage class
    git-svn-id: http://go-java-1.googlecode.com/svn@1514 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/projects/FileCategoryStorage.java
+++ trunk/GoJava2/andrii.piatnickiy/module2/MyKickStarter/src/com/gojava/projects/FileCategoryStorage.java
@@ -1,6 +1,79 @@
 package com.gojava.projects;
 
-public class FileCategoryStorage {
-   
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+public class FileCategoryStorage implements CategoryStorage {
+
+    @Override
+    public void add(String name, int categoryId) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getCategoryToString() {
+        File file = new File("categories.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.out.println("Не смогли создать файл контейнер!");
+            }
+        }
+        
+        BufferedReader in = null;
+        String result = "";
+        try {
+            in = new BufferedReader(new FileReader(file));
+            try {
+                String firsrRead = "";
+                while(firsrRead != null){
+                    firsrRead = in.readLine();
+                    if(firsrRead == null){
+                        result += "";
+                    }else{
+                        result += firsrRead + "\n";
+                    }
+                }
+            } catch (IOException e) {
+                System.out.println("Не могу прочитать строку!");
+            }
+            return result;
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            try {
+                file.createNewFile();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                System.out.println("Не нашли файл!");
+                ;
+            }
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    System.out.println("Не можем закрыть файл!");
+                }
+            }
+        }
+        return result;
+
+
+    }
+
+    @Override
+    public Category getCategory(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
