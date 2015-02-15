cba73bc4ce26ad4faa9fdd74e8a7edf45bbfe332
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-15 10:19:37 +0000

    Project test updated  (Kickstarter project, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@1496 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ProjectTest.java
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/test/ProjectTest.java
@@ -7,19 +7,26 @@ import org.kudryavtsev.kickstarter.data.Project;
 
 public class ProjectTest {
 
+    private static final String PROJECT_DESC = "TestProject; TestDescription; funded: 100; pledged: 100; days to go: 100";
+
     @Test
     public void ProjectToString() {
-        Project project = new Project("TestProject", "TestDescription", 100, 100, 100);
-        assertEquals("TestProject; TestDescription; funded: 100; pledged: 100; days to go: 100",
-                project.toString());
+        Project project = creatingProject();
+        assertEquals(PROJECT_DESC, project.toString());
     }
 
     @Test
     public void ProjectToStringFull() {
-        Project project = new Project("TestProject", "TestDescription", 1000, 100, 101);
+        Project project = creatingProject();
         assertEquals(
-                "TestProject; TestDescription; funded: 1000; pledged: 100; days to go: 101\nFull description: Just long description\nHistory: History of the project\nVideo: Link to video with demo\nFAQ: Frequently Asked Questions",
+                PROJECT_DESC
+                        + "\nFull description: Just long description\nHistory: History of the project\nVideo: Link to video with demo\nFAQ: Frequently Asked Questions",
                 project.toStringFull());
+    }
+
+    private Project creatingProject() {
+        Project project = new Project("TestProject", "TestDescription", 100, 100, 100);
+        return project;
     }
 
 }
