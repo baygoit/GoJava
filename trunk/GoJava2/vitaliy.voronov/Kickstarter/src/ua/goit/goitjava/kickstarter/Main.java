278b14008d32e90c4c23911df33680d399060964
vitalik.voronov <vitalik.voronov@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-17 00:42:43 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1542 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/vitaliy.voronov/Kickstarter/src/ua/goit/goitjava/kickstarter/Main.java
+++ trunk/GoJava2/vitaliy.voronov/Kickstarter/src/ua/goit/goitjava/kickstarter/Main.java
@@ -72,18 +72,48 @@ public class Main {
 						category = cats1.getCategoryByIndex(x-1);
 						out.printSelectProject(projectsByCategory.get(x - 1));
 						out.printZero();
-						System.out.println("1) Ask a quation\n2) Invest money");
-						x = in.scanInt();
-						switch (x){
+						System.out.println("1) Ask a quation\n2) Invest money\n3) Choose a prize for investing");
+						int y = in.scanInt();
+						switch (y){
 						case 1:
 							System.out.println("¬ведите вопрос");
 							String s = in.scanLine();
 							projectsByCategory.get(x - 1).addFAQ(new FAQ(s,null));
 							break;
 						case 2:
+							System.out.println("¬ведите »м€");
+							String sOs = in.scanLine();
+							System.out.println("¬ведите є карты");
+							String gyGY = in.scanLine();
 							System.out.println("¬ведите сумму");
 							int sum = in.scanInt();
 							projectsByCategory.get(x - 1).addMoney(sum);
+							break;
+						case 3:
+							System.out.println("1 - 10$(you will get a thank)\n2 - 100$(you get a big thank)"
+									+ "\n3 - 1000$(you get a very big thank)\n4 - 100000$(you get a Chupa Chups :) ");
+							int choise = in.scanInt();
+							switch(choise){
+							case 1:
+								projectsByCategory.get(x - 1).addMoney(10);
+								System.out.println("Thank you");
+								break;
+							case 2:
+								projectsByCategory.get(x - 1).addMoney(100);
+								System.out.println("Thank you very mach");
+								break;
+							case 3:
+								projectsByCategory.get(x - 1).addMoney(1000);
+								System.out.println("Thank you very very mach");
+								break;
+							case 4:
+								System.out.println("Your Chupa Chups, we will send by Ukrpochta :)");
+								projectsByCategory.get(x - 1).addMoney(100000);
+								
+								break;
+							}
+						break;
+							
 						}
 					} 
 				}
