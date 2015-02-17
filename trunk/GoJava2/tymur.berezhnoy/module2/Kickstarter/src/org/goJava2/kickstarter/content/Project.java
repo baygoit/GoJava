0b9ef889465543c76e61615af91bdb77e9386f6a
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-17 12:14:52 +0000

    Changes: Remove equals and hachCode from class Quote. Reafctored equals and hashCode in class Project.
    git-svn-id: http://go-java-1.googlecode.com/svn@1561 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/org/goJava2/kickstarter/content/Project.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/org/goJava2/kickstarter/content/Project.java
@@ -3,6 +3,7 @@ package org.goJava2.kickstarter.content;
 public class Project {
 	
 	private final char symbolDollar = 36;
+	
 	private String name;
 	private String description;
 	private String story;
@@ -25,6 +26,50 @@ public class Project {
 		this.link = link;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((link == null) ? 0 : link.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((story == null) ? 0 : story.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (link == null) {
+			if (other.link != null)
+				return false;
+		} else if (!link.equals(other.link))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (story == null) {
+			if (other.story != null)
+				return false;
+		} else if (!story.equals(other.story))
+			return false;
+		return true;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -59,63 +104,6 @@ public class Project {
 			.append("\nLink to video: ").append(link);
 		return builder.toString();
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + backers;
-		result = prime * result + days;
-		result = prime * result
-				+ ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((link == null) ? 0 : link.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + requiredAmount;
-		result = prime * result + ((story == null) ? 0 : story.hashCode());
-		result = prime * result + symbolDollar;
-		result = prime * result + total;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Project other = (Project) obj;
-		if (backers != other.backers)
-			return false;
-		if (days != other.days)
-			return false;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (link == null) {
-			if (other.link != null)
-				return false;
-		} else if (!link.equals(other.link))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (requiredAmount != other.requiredAmount)
-			return false;
-		if (story == null) {
-			if (other.story != null)
-				return false;
-		} else if (!story.equals(other.story))
-			return false;
-		if (symbolDollar != other.symbolDollar)
-			return false;
-		if (total != other.total)
-			return false;
-		return true;
-	}
+	
+	
 }
--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/org/goJava2/kickstarter/content/Quote.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/org/goJava2/kickstarter/content/Quote.java
@@ -17,38 +17,4 @@ public class Quote {
 			.append(copyrightSymbol).append(" ").append(author);
 		return builder.toString(); 
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((author == null) ? 0 : author.hashCode());
-		result = prime * result + ((content == null) ? 0 : content.hashCode());
-		result = prime * result + copyrightSymbol;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Quote other = (Quote) obj;
-		if (author == null) {
-			if (other.author != null)
-				return false;
-		} else if (!author.equals(other.author))
-			return false;
-		if (content == null) {
-			if (other.content != null)
-				return false;
-		} else if (!content.equals(other.content))
-			return false;
-		if (copyrightSymbol != other.copyrightSymbol)
-			return false;
-		return true;
-	}
 }
