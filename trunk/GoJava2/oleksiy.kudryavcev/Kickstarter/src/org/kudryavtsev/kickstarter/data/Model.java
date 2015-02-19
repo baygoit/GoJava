04c8a0e129a10ab5c98fb3746ac3df1c9f93d070
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-19 13:33:24 +0000

    Class Model updated (Kickstarter project, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@1590 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/data/Model.java
+++ trunk/GoJava2/oleksiy.kudryavcev/Kickstarter/src/org/kudryavtsev/kickstarter/data/Model.java
@@ -9,27 +9,65 @@ public class Model {
 
     public Model() {
         setProjectsList(new ArrayList<Project>());
-        categoriesList = new ArrayList<Category>();
+        setCategoriesList(new ArrayList<Category>());
     }
 
     public void init() {
-        categoriesList.add(new Category("Sport", "Sport projects"));
-        categoriesList.add(new Category("Technology", "Technology projects"));
-        categoriesList.add(new Category("Science", "Science projects"));
-        
-        categoriesList.get(0).getProjectsList().add(new Project("FutureForward", "FutureForward is an innovative training project", 4, 200, 45));
-        categoriesList.get(1).getProjectsList().add(new Project("APEX Wallet", "The Apex is a slim aluminum wallet with RFID protection", 56400, 4000, 12));
-        categoriesList.get(2).getProjectsList().add(new Project("EVB", "EVB: Replace the Brain and Sensors in your LEGO® MINDSTORMS EV3 with a BeagleBone Black", 36000, 8000, 1));
-        categoriesList.get(0).getProjectsList().add(new Project("U23", "Support Team Canada U23 in their quest for Gold!", 5500, 5000, 5));
-        categoriesList.get(1).getProjectsList().add(new Project("Arduino", "We love Arduino and we love space exploration. So we decided to combine them and let people run their own space experiments!", 10600, 35000, 4));
-        categoriesList.get(2).getProjectsList().add(new Project("Makeblock", "Makeblock is an aluminum extrusion based construct platform that can be used to build robots", 186000, 30000, 2));
+        getCategoriesList().add(new Category("Sport", "Sport projects"));
+        getCategoriesList().add(new Category("Technology", "Technology projects"));
+        getCategoriesList().add(new Category("Science", "Science projects"));
+
+        getCategoriesList()
+                .get(0)
+                .getProjectsList()
+                .add(new Project("FutureForward",
+                        "FutureForward is an innovative training project", 4, 200, 45));
+        getCategoriesList()
+                .get(1)
+                .getProjectsList()
+                .add(new Project("APEX Wallet",
+                        "The Apex is a slim aluminum wallet with RFID protection", 56400, 4000, 12));
+        getCategoriesList()
+                .get(2)
+                .getProjectsList()
+                .add(new Project(
+                        "EVB",
+                        "EVB: Replace the Brain and Sensors in your LEGO® MINDSTORMS EV3 with a BeagleBone Black",
+                        36000, 8000, 1));
+        getCategoriesList()
+                .get(0)
+                .getProjectsList()
+                .add(new Project("U23", "Support Team Canada U23 in their quest for Gold!", 5500,
+                        5000, 5));
+        getCategoriesList()
+                .get(1)
+                .getProjectsList()
+                .add(new Project(
+                        "Arduino",
+                        "We love Arduino and we love space exploration. So we decided to combine them and let people run their own space experiments!",
+                        10600, 35000, 4));
+        getCategoriesList()
+                .get(2)
+                .getProjectsList()
+                .add(new Project(
+                        "Makeblock",
+                        "Makeblock is an aluminum extrusion based construct platform that can be used to build robots",
+                        186000, 30000, 2));
     }
 
     public List<Category> getCategoriesList() {
         return categoriesList;
     }
-    
-    private void setProjectsList(ArrayList<Project> arrayList) {
+
+    public List<Project> getProjectsList() {
+        return projectsList;
+    }
+
+    public void setProjectsList(ArrayList<Project> arrayList) {
         this.projectsList = arrayList;
+    }
+
+    public void setCategoriesList(List<Category> categoriesList) {
+        this.categoriesList = categoriesList;
     }
 }
