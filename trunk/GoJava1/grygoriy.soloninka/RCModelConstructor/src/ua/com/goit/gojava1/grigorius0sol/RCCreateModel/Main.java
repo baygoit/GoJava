ceb0b798fd0c5efbbc34cfa64313d8bc41c670ca
grigorius0sol@gmail.com <grigorius0sol@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-20 16:19:32 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1617 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/grygoriy.soloninka/RCModelConstructor/src/ua/com/goit/gojava1/grigorius0sol/RCCreateModel/Main.java
@@ -0,0 +1,11 @@
+package ua.com.goit.gojava1.grigorius0sol.RCCreateModel;
+
+public class Main {
+	
+	public static void main(String [] args){
+		
+		Model driftCar = new DriftCar();
+		ViewParts parts = new ViewParts();
+		//parts.showDetailsCat();
+	}
+}
