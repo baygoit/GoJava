f1228f17804439adcd7a5e7d721b2af174ec7e12
pavlosevidov@gmail.com <pavlosevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-20 14:08:11 +0000

    Refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@1603 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Project.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Project.java
@@ -1,129 +1,111 @@
-package ua.com.scread.kickstarter;
+package ua.com.scread.kickstarter.data;
+
+import ua.com.scread.kickstarter.storage.Bonuses;
 
 public class Project {
-	private String name;
-	private String description;
-	private double collected;
-	private double amount;
-	private int days;
-	private Category category;
-	private AdditionalInfo details;
+    private String name;
+    private String description;
+    private double collected;
+    private double amount;
+    private int days;
+    private Category category;
+    private AdditionalInfo details;
 
-	public Project(String name, String description, double amount, int days, AdditionalInfo delails) {
-		this.name = name;
-		this.description = description;
-		this.amount = amount;
-		this.days = days;
-		this.details = delails;
-		collected = 0;
-	}
+    public Project(String name, String description, double amount, int days, AdditionalInfo delails) {
+        this.name = name;
+        this.description = description;
+        this.amount = amount;
+        this.days = days;
+        this.details = delails;
+        collected = 0;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public double getCollected() {
-		return collected;
-	}
+    public double getCollected() {
+        return collected;
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    public double getAmount() {
+        return amount;
+    }
 
-	public int getDays() {
-		return days;
-	}
+    public int getDays() {
+        return days;
+    }
 
-	public Category getCategory() {
-		return category;
-	}
-	
-	public void addMoney(double amount) {
-	    collected += amount;
-	}
+    public Category getCategory() {
+        return category;
+    }
+    
+    public void addMoney(double amount) {
+        collected += amount;
+    }
 
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-	
-	public AdditionalInfo getDetails() {
-		return details;
-	}
-	
-	public void addFAQ(FAQ faq) {
-	    details.addFAQ(faq);
-	}
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    
+    public AdditionalInfo getDetails() {
+        return details;
+    }
+    
+    public void addFAQ(FAQ faq) {
+        details.addFAQ(faq);
+    }
 
-	public Bonus getBonus(int index) {
-	    return details.getBonus(index);
-	}
-	
-	public Bonuses getBonuses() {
-	    return details.getBonuses();
-	}
+    public Bonus getBonus(int index) {
+        return details.getBonus(index);
+    }
+    
+    public Bonuses getBonuses() {
+        return details.getBonuses();
+    }
 
-	@Override
+    @Override
+    public String toString() {
+        return name + ";" + description + ";" + collected + ";" + amount + ";" 
+                    + days + ";" + details.toString() + "\n";
+    }
+
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        long temp;
-        temp = Double.doubleToLongBits(amount);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        result = prime * result
-                + ((category == null) ? 0 : category.hashCode());
-        temp = Double.doubleToLongBits(collected);
-        result = prime * result + (int) (temp ^ (temp >>> 32));
-        result = prime * result + days;
-        result = prime * result
-                + ((description == null) ? 0 : description.hashCode());
-        result = prime * result + ((details == null) ? 0 : details.hashCode());
         result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) {
+            return true;            
+        }
+        
+        if (obj == null) {
+            return false;            
+        }
+        
+        if (getClass() != obj.getClass()) {
+            return false;            
+        }
+        
         Project other = (Project) obj;
-        if (Double.doubleToLongBits(amount) != Double
-                .doubleToLongBits(other.amount))
-            return false;
-        if (category == null) {
-            if (other.category != null)
-                return false;
-        } else if (!category.equals(other.category))
-            return false;
-        if (Double.doubleToLongBits(collected) != Double
-                .doubleToLongBits(other.collected))
-            return false;
-        if (days != other.days)
-            return false;
-        if (description == null) {
-            if (other.description != null)
-                return false;
-        } else if (!description.equals(other.description))
-            return false;
-        if (details == null) {
-            if (other.details != null)
-                return false;
-        } else if (!details.equals(other.details))
-            return false;
+        
         if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
+            if (other.name != null) {
+                return false;                
+            }
+        } else if (!name.equals(other.name)) {
+            return false;            
+        }
+        
         return true;
     }
-
-
 }
--- trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Projects.java
+++ trunk/GoJava2/pavel.sevidov/Kickstarter/src/ua/com/scread/kickstarter/Projects.java
@@ -1,61 +1,18 @@
-package ua.com.scread.kickstarter;
+package ua.com.scread.kickstarter.storage;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Projects {
-	private List<Project> projects;
-	
-	public Projects() {
-		projects = new ArrayList<Project>();
-	}
+import ua.com.scread.kickstarter.data.Category;
+import ua.com.scread.kickstarter.data.Project;
 
-	public List<Project> getProjects() {
-		return projects;
-	}
-	
-	public List<Project> getProjects(Category category) {
-		List<Project> result = new ArrayList<Project>();
-        for (int index = 0; index < projects.size(); index ++) {
-                Project project = projects.get(index);
-                if (project.getCategory().equals(category)) {
-                        result.add(project);
-                }
-        }
-        return result;
-	}
+public interface Projects {
 
-	public void add(Project project) {
-		projects.add(project);		
-	}
-	
-	public int size() {
-	    return projects.size();
-	}
+    List<Project> getProjects();
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result
-                + ((projects == null) ? 0 : projects.hashCode());
-        return result;
-    }
+    List<Project> getProjects(Category category);
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Projects other = (Projects) obj;
-        if (projects == null) {
-            if (other.projects != null)
-                return false;
-        } else if (!projects.equals(other.projects))
-            return false;
-        return true;
-    }
+    void add(Project project);
+
+    int size();
+
 }
