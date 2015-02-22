78ba917978c39e5f56354d7b7e9e0317f2d50057
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-22 17:47:11 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1659 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/com/gojava2/kickstarter/behavior/ControllerBehavior.java
+++
@@ -1,6 +0,0 @@
-package com.gojava2.kickstarter.behavior;
-
-public interface ControllerBehavior<T> {
-	Object getContent();
-	Object getSpecificContent(T t);
-}
--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/com/gojava2/kickstarter/behavior/StorageBehavior.java
+++
@@ -1,5 +0,0 @@
-package com.gojava2.kickstarter.behavior;
-
-public interface StorageBehavior<T> {
-	Object getContent();
-}
