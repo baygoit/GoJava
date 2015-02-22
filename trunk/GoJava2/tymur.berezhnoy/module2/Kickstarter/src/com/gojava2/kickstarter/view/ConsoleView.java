6c8f170f23a5171221c0fd5dcf2f9f51160d3609
tima.peaceworld@gmail.com <tima.peaceworld@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-22 15:20:30 +0000

    System.out,print(ln) changed by "static import".
    git-svn-id: http://go-java-1.googlecode.com/svn@1652 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/com/gojava2/kickstarter/view/ConsoleView.java
+++ trunk/GoJava2/tymur.berezhnoy/module2/Kickstarter/src/com/gojava2/kickstarter/view/ConsoleView.java
@@ -5,6 +5,7 @@ import com.gojava2.kickstarter.content.Project;
 import com.gojava2.kickstarter.controller.CategoryController;
 import com.gojava2.kickstarter.controller.ProjectController;
 import com.gojava2.kickstarter.controller.QuoteController;
+import static java.lang.System.out;
 
 public class ConsoleView {
 	
@@ -25,18 +26,18 @@ public class ConsoleView {
 	}
 	
 	public void displayHead() {
-		System.out.println("\t" + title + "\n" + quoteController.getRandomQuote().getQuoteContent());
+		out.println("\t" + title + "\n" + quoteController.getRandomQuote().getQuoteContent());
 	}
 	
 	public void displayCategories() {
 		path = "~ CATEGORIES/";
-		System.out.println("\n" + path + "\n------------------------------------\nID Name");
+		out.println("\n" + path + "\n------------------------------------\nID Name");
 		int i = 1;
 		for(Category category: categoryController.getContent()) {
-			System.out.println(i + ") " + category.getName());
+			out.println(i + ") " + category.getName());
 			i++;
 		}
-		System.out.println("------------------------------------");
+		out.println("------------------------------------");
 	}
 	
 	public void setCategoryPath(int i) { //TODO Name and logic
@@ -48,7 +49,7 @@ public class ConsoleView {
 		int projectNumb = 1;
 		StringBuilder shortInfo;
 		
-		System.out.println(path + "\n------------------------------------");
+		out.println(path + "\n------------------------------------");
 		for(Project project: projectController.getSpecificContent(category)) {
 			shortInfo = new StringBuilder();
 			shortInfo.append(projectNumb).append(") ").append(project.getName())
@@ -56,7 +57,7 @@ public class ConsoleView {
 					 .append("\nRequired amount: ").append(project.getRequiredAmount()).append(project.getSymbolDollar())
 					 .append("\nTotal: ").append(project.getTotal()).append(project.getSymbolDollar())
 					 .append("\nDays left: ").append(project.getDays());
-			System.out.println(shortInfo + "\n------------------------------------");
+			out.println(shortInfo + "\n------------------------------------");
 			projectNumb++;
 		}
 	}
@@ -66,13 +67,13 @@ public class ConsoleView {
 		
 		int size = projectController.getSpecificContent(category).size();
 		if(i > size) {
-			System.out.println("- There are no projet at number: " + i);
+			out.println("- There are no projet at number: " + i);
 			return;
 		} else {
 			Project project = projectController.getSpecificContent(category).get(i - 1);
 			StringBuilder fullInfo = new StringBuilder();
 			
-			System.out.print(newPath += project.getName().toUpperCase() + "/"
+			out.print(newPath += project.getName().toUpperCase() + "/"
 						+ "\n------------------------------------");
 			
 			fullInfo.append("\nName: ").append(project.getName())
@@ -84,7 +85,7 @@ public class ConsoleView {
 					.append("\nStory: ").append(project.getStory())
 					.append("\nLink to video: ").append(project.getLink());
 			
-				System.out.println(fullInfo + "\n------------------------------------");
+				out.println(fullInfo + "\n------------------------------------");
 		}
 	}
 }
