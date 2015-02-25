22fa9f539fa72758e43b48622eb2cf5432b105ec
aleksej.kudryavtsev@gmail.com <aleksej.kudryavtsev@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-25 19:50:46 +0000

    Initial import. (ServletHelloWorld, GoJava2, Oleksiy Kudryavcev)
    git-svn-id: http://go-java-1.googlecode.com/svn@1725 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/oleksiy.kudryavcev/ServletHelloWorld/src/com/example/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello World (or some another simple text)";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+        out.println("<html>\n");
+        out.println("<body bgcolor=\"#adfde6\">\n");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
