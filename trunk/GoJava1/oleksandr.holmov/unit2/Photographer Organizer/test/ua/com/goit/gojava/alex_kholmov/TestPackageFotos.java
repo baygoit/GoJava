45fc3d74846e5582a474230c3d5de40f9c8c8e21
alex.sash.kh@gmail.com <alex.sash.kh@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-02-28 20:40:20 +0000

    Initial commit Test 0.3
    git-svn-id: http://go-java-1.googlecode.com/svn@1749 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/test/ua/com/goit/gojava/alex_kholmov/TestPackageFotos.java
+++ trunk/GoJava1/oleksandr.holmov/unit2/Photographer Organizer/test/ua/com/goit/gojava/alex_kholmov/TestPackageFotos.java
@@ -15,11 +15,11 @@ public class TestPackageFotos {
     private PackageFotos pFotos = new PackageFotos("pack1", 250, 15, 10, 1);
 
     /**
-     * Test method for {@link ua.com.goit.gojava.alex_kholmov.PackageFotos#timeEditAllFoto()}.
+     * Test method for {@link ua.com.goit.gojava.alex_kholmov.PackageFotos#timeEditAllFotos()}.
      */
     @Test
     public void testTimeEditAllFoto() {
-        int res = pFotos.timeEditAllFoto();
+        int res = pFotos.timeEditAllFotos();
         assertEquals("Error", 4125, res);
     }
 
