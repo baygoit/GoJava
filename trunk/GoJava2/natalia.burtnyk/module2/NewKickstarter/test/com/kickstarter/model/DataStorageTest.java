a4c6db990d6e69f9325d714cd4146b0c44f2867b
beinlove.19@gmail.com <beinlove.19@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-02 14:54:49 +0000

    добавлено ще 3 тести
    git-svn-id: http://go-java-1.googlecode.com/svn@1771 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/test/com/kickstarter/model/DataStorageTest.java
+++ trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/test/com/kickstarter/model/DataStorageTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -41,4 +43,40 @@ public class DataStorageTest {
 		// then
 		assertSame(category1, storage.getSpecificCategory(1));
 	}	
+	
+	@Test
+	public void shouldSizeTwo_whenCategorytwo() {
+		// when
+		int size = 2;
+		storage.addCategory(new Сategory("Music"));
+		storage.addCategory(new Сategory("Sport"));		
+		// then
+		assertEquals(size, storage.getSizeCategories());
+	}
+	
+	@Test
+	public void shouldEmptyProjectsList_whenNoProjects() {
+		Сategory category1 = new Сategory("Sport");
+		storage.addCategory(category1);
+		// when
+		int size = 0;
+		// then
+		assertEquals(size, storage.getSpecificProjects(category1).size());
+	}
+	
+	@Test
+	public void shouldistContainsCategory_whenAddCategory() {
+		// when
+		Сategory category = new Сategory("Music");
+		Project project = new Project("Food","description","history","url",12,23,4);
+		project.setСategory(category);
+		storage.addProject(project);
+		
+		List<Project> projects = storage.getSpecificProjects(category);
+		Project projectResult = storage.getProject(1);
+		
+		// then
+		assertSame(project, projectResult);
+	}
+	
 }
