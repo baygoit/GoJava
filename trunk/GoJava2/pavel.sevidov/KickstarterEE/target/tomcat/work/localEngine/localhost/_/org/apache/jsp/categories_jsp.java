986126e8893bab5fe6ca68c4460dda60d7f0c489
PavloSevidov@gmail.com <PavloSevidov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-04 21:40:30 +0000

    Initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@1820 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/pavel.sevidov/KickstarterEE/target/tomcat/work/localEngine/localhost/_/org/apache/jsp/categories_jsp.java
@@ -0,0 +1,299 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class categories_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems;
+  private org.apache.jasper.runtime.TagHandlerPool _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+    _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems.release();
+    _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.release();
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html lang=\"en\">\r\n");
+      out.write("\t<head>\r\n");
+      out.write("\t\t<meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("\t\t<meta charset=\"utf-8\">\r\n");
+      out.write("\t\t<title>Kickstarter categories</title>\r\n");
+      out.write("\t\t<meta name=\"generator\" content=\"Bootply\" />\r\n");
+      out.write("\t\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1\">\r\n");
+      out.write("\t\t<link href=\"./css/bootstrap.min.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("\t\t<link href=\"//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("\t\t<!--[if lt IE 9]>\r\n");
+      out.write("\t\t\t<script src=\"//html5shim.googlecode.com/svn/trunk/html5.js\"></script>\r\n");
+      out.write("\t\t<![endif]-->\r\n");
+      out.write("\t\t<link href=\"./css/styles.css\" rel=\"stylesheet\" type=\"text/css\">\r\n");
+      out.write("\t</head>\r\n");
+      out.write("\t<body>\r\n");
+      out.write("<header class=\"navbar navbar-bright navbar-fixed-top\" role=\"banner\">\r\n");
+      out.write("  <div class=\"container\">\r\n");
+      out.write("    <div class=\"navbar-header\">\r\n");
+      out.write("      <button class=\"navbar-toggle\" type=\"button\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">\r\n");
+      out.write("        <span class=\"sr-only\">Toggle navigation</span>\r\n");
+      out.write("        <span class=\"icon-bar\"></span>\r\n");
+      out.write("        <span class=\"icon-bar\"></span>\r\n");
+      out.write("        <span class=\"icon-bar\"></span>\r\n");
+      out.write("      </button>\r\n");
+      out.write("    </div>\r\n");
+      out.write("    <nav class=\"collapse navbar-collapse\" role=\"navigation\">\r\n");
+      out.write("      <ul class=\"nav navbar-nav\">\r\n");
+      out.write("        <li>\r\n");
+      out.write("          <a href=\"http://localhost:8080/categories\" class=\"navbar-brand\">Home</a>\r\n");
+      out.write("        </li>\r\n");
+      out.write("        <li>\r\n");
+      out.write("          <a href=\"#\">Category</a>\r\n");
+      out.write("        </li>\r\n");
+      out.write("        <li>\r\n");
+      out.write("          <a href=\"#\">Category</a>\r\n");
+      out.write("        </li>\r\n");
+      out.write("        <li>\r\n");
+      out.write("          <a href=\"#\">Category</a>\r\n");
+      out.write("        </li>\r\n");
+      out.write("        <li>\r\n");
+      out.write("          <a href=\"#\">About</a>\r\n");
+      out.write("        </li>\r\n");
+      out.write("      </ul>\r\n");
+      out.write("      <ul class=\"nav navbar-right navbar-nav\">\r\n");
+      out.write("        <li class=\"dropdown\">\r\n");
+      out.write("          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\"><i class=\"glyphicon glyphicon-search\"></i></a>\r\n");
+      out.write("          <ul class=\"dropdown-menu\" style=\"padding:12px;\">\r\n");
+      out.write("            <form class=\"form-inline\">\r\n");
+      out.write("              <button type=\"submit\" class=\"btn btn-default pull-right\"><i class=\"glyphicon glyphicon-search\"></i></button><input type=\"text\" class=\"form-control pull-left\" placeholder=\"Search\">\r\n");
+      out.write("            </form>\r\n");
+      out.write("          </ul>\r\n");
+      out.write("        </li>\r\n");
+      out.write("      </ul>\r\n");
+      out.write("    </nav>\r\n");
+      out.write("  </div>\r\n");
+      out.write("</header>\r\n");
+      out.write("\r\n");
+      out.write("<div id=\"masthead\">  \r\n");
+      out.write("  <div class=\"container\">\r\n");
+      out.write("    <div class=\"row\">\r\n");
+      out.write("      <div class=\"col-md-7\">\r\n");
+      out.write("        <h1>Kickstarter\r\n");
+      out.write("          <p class=\"lead\"></p>\r\n");
+      out.write("        </h1>\r\n");
+      out.write("      </div>\r\n");
+      out.write("      <div class=\"col-md-5 sasha-logo\">\r\n");
+      out.write("       </div>\r\n");
+      out.write("    </div> \r\n");
+      out.write("  </div><!-- /cont -->\r\n");
+      out.write("  \r\n");
+      out.write("  <div class=\"container\">\r\n");
+      out.write("    <div class=\"row\">\r\n");
+      out.write("      <div class=\"col-md-12\">\r\n");
+      out.write("        <!-- <div class=\"top-spacer\"> </div> -->\r\n");
+      out.write("      </div>\r\n");
+      out.write("    </div> \r\n");
+      out.write("  </div><!-- /cont -->\r\n");
+      out.write("  \r\n");
+      out.write("</div>\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<div class=\"container\">\r\n");
+      out.write("  <div class=\"row\">\r\n");
+      out.write("    \r\n");
+      out.write("    <div class=\"col-md-12\"> \r\n");
+      out.write("      \r\n");
+      out.write("      <div class=\"panel\">\r\n");
+      out.write("        <div class=\"panel-body\">\r\n");
+      out.write("          \r\n");
+      out.write("          \r\n");
+      out.write("          \r\n");
+      out.write("          <!--/stories-->\r\n");
+      out.write("          ");
+      if (_jspx_meth_c_005fforEach_005f0(_jspx_page_context))
+        return;
+      out.write("\r\n");
+      out.write("          \r\n");
+      out.write("          <!--/stories-->\r\n");
+      out.write("          \r\n");
+      out.write("        </div>\r\n");
+      out.write("      </div>\r\n");
+      out.write("                                                                                       \r\n");
+      out.write("\t                                                \r\n");
+      out.write("                                                      \r\n");
+      out.write("   \t</div><!--/col-12-->\r\n");
+      out.write("  </div>\r\n");
+      out.write("</div>\r\n");
+      out.write("                                                \r\n");
+      out.write("                                                                                \r\n");
+      out.write("<!-- <hr> -->\r\n");
+      out.write("\r\n");
+      out.write("<div class=\"container\" id=\"footer\">\r\n");
+      out.write("  <div class=\"row\">\r\n");
+      out.write("    <div class=\"col col-sm-12\">\r\n");
+      out.write("      \r\n");
+      out.write("      <h1>Follow Us</h1>\r\n");
+      out.write("      <div class=\"btn-group\">\r\n");
+      out.write("       <a class=\"btn btn-twitter btn-lg\" href=\"#\"><i class=\"icon-twitter icon-large\"></i> Twitter</a>\r\n");
+      out.write("\t   <a class=\"btn btn-facebook btn-lg\" href=\"#\"><i class=\"icon-facebook icon-large\"></i> Facebook</a>\r\n");
+      out.write("\t   <a class=\"btn btn-google-plus btn-lg\" href=\"#\"><i class=\"icon-google-plus icon-large\"></i> Google+</a>\r\n");
+      out.write("      </div>\r\n");
+      out.write("      \r\n");
+      out.write("    </div>\r\n");
+      out.write("  </div>\r\n");
+      out.write("</div>\r\n");
+      out.write("\r\n");
+      out.write("<!-- <hr> -->\r\n");
+      out.write("\r\n");
+      out.write("<!-- <hr> -->\r\n");
+      out.write("\r\n");
+      out.write("<footer>\r\n");
+      out.write("  <div class=\"container\">\r\n");
+      out.write("    <div class=\"row\">\r\n");
+      out.write("      <div class=\"col-sm-6\">\r\n");
+      out.write("          <h3 class=\"pull-center foot\">&copy; Deuces Inc. 2014. All Rights Reserved.</a></p>      \r\n");
+      out.write("      </div>\r\n");
+      out.write("    </div>\r\n");
+      out.write("  </div>\r\n");
+      out.write("</footer>\r\n");
+      out.write("\t<!-- script references -->\r\n");
+      out.write("\t\t<script src=\"//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js\"></script>\r\n");
+      out.write("\t\t<script src=\"js/bootstrap.min.js\"></script>\r\n");
+      out.write("\t</body>\r\n");
+      out.write("</html>\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+
+  private boolean _jspx_meth_c_005fforEach_005f0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:forEach
+    org.apache.taglibs.standard.tag.rt.core.ForEachTag _jspx_th_c_005fforEach_005f0 = (org.apache.taglibs.standard.tag.rt.core.ForEachTag) _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems.get(org.apache.taglibs.standard.tag.rt.core.ForEachTag.class);
+    _jspx_th_c_005fforEach_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_c_005fforEach_005f0.setParent(null);
+    // /categories.jsp(95,10) name = items type = javax.el.ValueExpression reqTime = true required = false fragment = false deferredValue = true expectedTypeName = java.lang.Object deferredMethod = false methodSignature = null
+    _jspx_th_c_005fforEach_005f0.setItems(new org.apache.jasper.el.JspValueExpression("/categories.jsp(95,10) '${categories}'",_el_expressionfactory.createValueExpression(_jspx_page_context.getELContext(),"${categories}",java.lang.Object.class)).getValue(_jspx_page_context.getELContext()));
+    // /categories.jsp(95,10) name = var type = java.lang.String reqTime = false required = false fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_c_005fforEach_005f0.setVar("category");
+    int[] _jspx_push_body_count_c_005fforEach_005f0 = new int[] { 0 };
+    try {
+      int _jspx_eval_c_005fforEach_005f0 = _jspx_th_c_005fforEach_005f0.doStartTag();
+      if (_jspx_eval_c_005fforEach_005f0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+        do {
+          out.write(" \r\n");
+          out.write("          <div class=\"row\">    \r\n");
+          out.write("            <br>\r\n");
+          out.write("            <div class=\"col-md-2 col-sm-3 text-center\">\r\n");
+          out.write("              <a class=\"story-title\" href=\"/projects?category=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${category.id}", java.lang.String.class, (PageContext)_jspx_page_context, null, false));
+          out.write("\"><img alt=\"\" src=\"https://www.kickstarter.com/download/kickstarter-badge-funded.png\" style=\"width:100px;height:100px\" class=\"img-circle\"></a>\r\n");
+          out.write("            </div>\r\n");
+          out.write("            <div class=\"col-md-10 col-sm-9\">\r\n");
+          out.write("              <a href=\"/projects?category=");
+          out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${category.id}", java.lang.String.class, (PageContext)_jspx_page_context, null, false));
+          out.write("\"><h3>");
+          if (_jspx_meth_c_005fout_005f0(_jspx_th_c_005fforEach_005f0, _jspx_page_context, _jspx_push_body_count_c_005fforEach_005f0))
+            return true;
+          out.write("</h3></a>\r\n");
+          out.write("              <div class=\"row\">\r\n");
+          out.write("                <div class=\"col-xs-9\">\r\n");
+          out.write("                  <h4>More info about categories </h4><h4>\r\n");
+          out.write("                  <small style=\"font-family:courier,'new courier';\" class=\"text-muted\">Date last update - <a href=\"theme_1.html\" class=\"text-muted\">Read More</a></small>\r\n");
+          out.write("                  </h4></div>\r\n");
+          out.write("                <div class=\"col-xs-3\"></div>\r\n");
+          out.write("              </div>\r\n");
+          out.write("              <br><br>\r\n");
+          out.write("            </div>\r\n");
+          out.write("          </div>\r\n");
+          out.write("          <hr>\r\n");
+          out.write("          ");
+          int evalDoAfterBody = _jspx_th_c_005fforEach_005f0.doAfterBody();
+          if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+            break;
+        } while (true);
+      }
+      if (_jspx_th_c_005fforEach_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_c_005fforEach_005f0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_c_005fforEach_005f0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_c_005fforEach_005f0.doFinally();
+      _005fjspx_005ftagPool_005fc_005fforEach_0026_005fvar_005fitems.reuse(_jspx_th_c_005fforEach_005f0);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_c_005fout_005f0(javax.servlet.jsp.tagext.JspTag _jspx_th_c_005fforEach_005f0, PageContext _jspx_page_context, int[] _jspx_push_body_count_c_005fforEach_005f0)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:out
+    org.apache.taglibs.standard.tag.rt.core.OutTag _jspx_th_c_005fout_005f0 = (org.apache.taglibs.standard.tag.rt.core.OutTag) _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.get(org.apache.taglibs.standard.tag.rt.core.OutTag.class);
+    _jspx_th_c_005fout_005f0.setPageContext(_jspx_page_context);
+    _jspx_th_c_005fout_005f0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_005fforEach_005f0);
+    // /categories.jsp(102,62) name = value type = null reqTime = true required = true fragment = false deferredValue = false expectedTypeName = null deferredMethod = false methodSignature = null
+    _jspx_th_c_005fout_005f0.setValue((java.lang.Object) org.apache.jasper.runtime.PageContextImpl.proprietaryEvaluate("${category.name}", java.lang.Object.class, (PageContext)_jspx_page_context, null, false));
+    int _jspx_eval_c_005fout_005f0 = _jspx_th_c_005fout_005f0.doStartTag();
+    if (_jspx_th_c_005fout_005f0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f0);
+      return true;
+    }
+    _005fjspx_005ftagPool_005fc_005fout_0026_005fvalue_005fnobody.reuse(_jspx_th_c_005fout_005f0);
+    return false;
+  }
+}
