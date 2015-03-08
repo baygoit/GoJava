98081bbcf1f96d27e17dff76cf61bdaa5e31f29e
beinlove.19@gmail.com <beinlove.19@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-08 21:53:25 +0000

    QuoteStorage з масиву на колекцію
    git-svn-id: http://go-java-1.googlecode.com/svn@1873 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/src/com/kickstarter/model/QuotesStorage.java
+++ trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/src/com/kickstarter/model/QuotesStorage.java
@@ -1,27 +1,29 @@
 package com.kickstarter.model;
+
+import java.util.ArrayList;
 import java.util.Random;
+
 public class QuotesStorage {
-	
-	  private Random random;
-	  
-	  public QuotesStorage(Random random) {
-          this.random = random;
-  }
-		
-	public String getRundomQuote() {
-			String[] strings = new String[] {
-					"Lost time is never found again.\"",
-			    	"The future belongs to those, who believe of their dreams.\"",
-			    	"If you never try you'll never know.\"",
-			    	"The only way to do great work, is to love what you do.\"",
-			    	"Every thing is easy, when you are crazy and nothing is easy when you are lazy.\"",
-			    	"An investment in knowledge always pays the best interest.\"",
-			    	"It does not matter how slowly you go so long as you do not stop.\"",
-			    	"Money spent on the brain, is never spent in vain.\"",
-					
-    			};
-			int index = random.nextInt(strings.length);  
-			return strings[index];
+
+	private Random random;
+
+	public QuotesStorage(Random random) {
+		this.random = random;
 	}
-	
+
+	public String getRundomQuote() {
+		ArrayList<String> quotes = new ArrayList<String>();
+		quotes.add("Lost time is never found again.\"");
+		quotes.add("The future belongs to those, who believe of their dreams.\"");
+		quotes.add("If you never try you'll never know.\"");
+		quotes.add("The only way to do great work, is to love what you do.\"");
+		quotes.add("Every thing is easy, when you are crazy and nothing is easy when you are lazy.\"");
+		quotes.add("An investment in knowledge always pays the best interest.\"");
+		quotes.add("It does not matter how slowly you go so long as you do not stop.\"");
+		quotes.add("Money spent on the brain, is never spent in vain.\"");
+
+		int index = random.nextInt(quotes.size());
+		return quotes.get(index);
+	}
+
 }
--- trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/test/com/kickstarter/model/QuotesStorageTest.java
+++ trunk/GoJava2/natalia.burtnyk/module2/NewKickstarter/test/com/kickstarter/model/QuotesStorageTest.java
@@ -1,4 +1,5 @@
 package com.kickstarter.model;
+
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
@@ -9,37 +10,39 @@ import java.util.Random;
 import org.junit.Test;
 
 public class QuotesStorageTest {
-	
-	private QuotesStorage quoteStorage;
-	
-	        class FakeRandom extends Random {
-	                private List<Integer> numbers;
 
-	                public FakeRandom(Integer... numbers) {
-	                        this.numbers = new LinkedList(Arrays.asList(numbers));  
-	                }
-	                
-	                @Override
-	                public int nextInt(int i) { 
-	                        return numbers.remove(0);
-	                }
-	        }
-	        
-	        @Test
-	        public void shouldGenerateNewQuote() {
-	                // given
-	        		QuotesStorage quotesStorage = new QuotesStorage(new FakeRandom(0, 1)); 
-	                
-	                // when 
-	                String quote = quotesStorage.getRundomQuote();
-	                
-	                // then
-	                assertEquals("Lost time is never found again.\"", quote);
-	                
-	                // when 
-	                String quote2 = quotesStorage.getRundomQuote();
-	                
-	                // then
-	                assertEquals("The future belongs to those, who believe of their dreams.\"", quote2);
-	        } 
+	private QuotesStorage quoteStorage;
+
+	class FakeRandom extends Random {
+		private List<Integer> numbers;
+
+		public FakeRandom(Integer... numbers) {
+			this.numbers = new LinkedList(Arrays.asList(numbers));
+		}
+
+		@Override
+		public int nextInt(int i) {
+			return numbers.remove(0);
+		}
 	}
+
+	@Test
+	public void shouldGenerateNewQuote() {
+		// given
+		QuotesStorage quotesStorage = new QuotesStorage(new FakeRandom(0, 1));
+
+		// when
+		String quote = quotesStorage.getRundomQuote();
+
+		// then
+		assertEquals("Lost time is never found again.\"", quote);
+
+		// when
+		String quote2 = quotesStorage.getRundomQuote();
+
+		// then
+		assertEquals(
+				"The future belongs to those, who believe of their dreams.\"",
+				quote2);
+	}
+}
