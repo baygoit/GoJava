0d043c3b07beb430aed400e2d2e386b50d87b3f1
webnet75@gmail.com <webnet75@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-09 14:11:16 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@1885 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/volodymyr.ialoviy/Spring/KickstarterSpring/src/main/java/ua/com/goit/gojava2/vova/kickstarter/presenter/MainServlet.java
+++ trunk/GoJava2/volodymyr.ialoviy/Spring/KickstarterSpring/src/main/java/ua/com/goit/gojava2/vova/kickstarter/presenter/MainServlet.java
@@ -11,7 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
 import ua.com.goit.gojava2.vova.kickstarter.model.CategoriesDAO;
+import ua.com.goit.gojava2.vova.kickstarter.model.Categories;
 import ua.com.goit.gojava2.vova.kickstarter.model.Category;
 import ua.com.goit.gojava2.vova.kickstarter.model.Project;
 import ua.com.goit.gojava2.vova.kickstarter.model.ProjectsDAO;
@@ -19,54 +23,58 @@ import ua.com.goit.gojava2.vova.kickstarter.model.ProjectsDAO;
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	static{
+	static {
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
 		String action = getAction(req);
 		Connection connection = getConnection(req);
-		
-		if(action.startsWith("/categories")){
-			CategoriesDAO categoriesFromDB = new CategoriesDAO(connection);
-			List<Category> categories = categoriesFromDB.getCategories();
-			
+
+		WebApplicationContext springContext = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		Categories categoriesDAO = (Categories) springContext.getBean("categoriesDAO");
+
+		if (action.startsWith("/categories")) {
+
+			List<Category> categories = categoriesDAO.getCategories();
+
 			req.setAttribute("categories", categories);
 			req.getRequestDispatcher("categories.jsp").forward(req, resp);
-		} else if (action.startsWith("/projects")){
-			int categoryID = Integer.valueOf(req.getParameter("category"));
-			
-			ProjectsDAO projectsFromDB = new ProjectsDAO(connection);
-			List<Project> projects = projectsFromDB.getProgectsForCategory(categoryID);
-			
-			req.setAttribute("projects", projects);
-			req.getRequestDispatcher("projects.jsp").forward(req, resp);
-		} else if (action.startsWith("/project")){
-		int projectID = Integer.valueOf(req.getParameter("project"));
-		
-		ProjectsDAO projectsFromDB = new ProjectsDAO(connection);
-		Project project = projectsFromDB.getProgect(projectID);
-
-		req.setAttribute("project", project);
-		req.getRequestDispatcher("project.jsp").forward(req, resp);
+		} else if (action.startsWith("/projects")) {
+//			int categoryID = Integer.valueOf(req.getParameter("category"));
+//
+//			ProjectsDAO projectsDAO = new ProjectsDAO(connection);
+//			List<Project> projects = projectsDAO.getProgectsForCategory(categoryID);
+//
+//			req.setAttribute("projects", projects);
+//			req.getRequestDispatcher("projects.jsp").forward(req, resp);
+		} else if (action.startsWith("/project")) {
+//			int projectID = Integer.valueOf(req.getParameter("project"));
+//
+//			ProjectsDAO projectsFromDB = new ProjectsDAO(connection);
+//			Project project = projectsFromDB.getProgect(projectID);
+//
+//			req.setAttribute("project", project);
+//			req.getRequestDispatcher("project.jsp").forward(req, resp);
 		}
-		
+
 	}
 
 	private String getAction(HttpServletRequest req) {
 		String requestURI = req.getRequestURI();
-		String action = requestURI.substring(req.getContextPath().length(), requestURI.length());
+		String action = requestURI.substring(req.getContextPath().length(),
+				requestURI.length());
 		return action;
 	}
 
 	private Connection getConnection(HttpServletRequest req) {
-		Connection rezult = (Connection)req.getSession().getAttribute("connection");
-		if (rezult == null){
+		Connection rezult = (Connection) req.getSession().getAttribute("connection");
+		if (rezult == null) {
 			try {
 				rezult = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/kickstarter", "postgres", "7575");
 			} catch (SQLException e) {
@@ -78,9 +86,9 @@ public class MainServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		System.out.println(req.getParameterMap().toString());
 	}
 
 }
-
