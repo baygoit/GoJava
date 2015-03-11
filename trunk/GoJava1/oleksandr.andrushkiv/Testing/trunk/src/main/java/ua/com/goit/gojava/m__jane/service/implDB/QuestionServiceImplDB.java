686597a1543d8c070ab14f1cb23f2d5998975d9a
AndrushkivAN@gmail.com <AndrushkivAN@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-11 14:52:53 +0000

    added logger
    git-svn-id: http://go-java-1.googlecode.com/svn@1914 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.andrushkiv/Testing/trunk/src/main/java/ua/com/goit/gojava/m__jane/service/implDB/QuestionServiceImplDB.java
+++ trunk/GoJava1/oleksandr.andrushkiv/Testing/trunk/src/main/java/ua/com/goit/gojava/m__jane/service/implDB/QuestionServiceImplDB.java
@@ -1,6 +1,11 @@
 package ua.com.goit.gojava.m__jane.service.implDB;
 
+import static ua.com.goit.gojava.m__jane.utils.ClassNameUtil.getCurrentClassName;
+
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ua.com.goit.gojava.m__jane.dao.QuestionDAO;
 import ua.com.goit.gojava.m__jane.dao.impl.QuestionDAOImpl;
@@ -11,12 +16,12 @@ import ua.com.goit.gojava.m__jane.service.QuestionService;
 
 public class QuestionServiceImplDB implements QuestionService {
 
-	
 	private QuestionDAO questionDAO;
-		
-	
+	private static Logger log = LoggerFactory.getLogger(getCurrentClassName());
+
 	public QuestionServiceImplDB() {
 		questionDAO = new QuestionDAOImpl();
+		log.info("create QuestionServiceImplDB!");
 	}
 
 	@Override
@@ -33,35 +38,35 @@ public class QuestionServiceImplDB implements QuestionService {
 
 	@Override
 	public void saveQuestion(Question question) {
-		
-		//TODO divide SimpleQuestion and MultipleQuestion
-		
+
+		// TODO divide SimpleQuestion and MultipleQuestion
+
 		questionDAO.saveSimpleQuestion((SimpleQuestion) question);
-		
+
 	}
-	
+
 	@Override
 	public void deleteQuestion(Integer id) {
-		
-		//TODO divide SimpleQuestion and MultipleQuestion		
+
+		// TODO divide SimpleQuestion and MultipleQuestion
 		questionDAO.deleteQuestion(id);
-		
+
 	}
 
 	@Override
 	public SimpleQuestion getQuestion(Integer id) {
-		//TODO divide SimpleQuestion and MultipleQuestion		
+		// TODO divide SimpleQuestion and MultipleQuestion
 		return questionDAO.getSimpleQuestion(id);
 	}
 
 	@Override
 	public void updateQuestion(SimpleQuestion question) {
-		//TODO divide SimpleQuestion and MultipleQuestion		
-		questionDAO.updateSimpleQuestion(question);		
+		// TODO divide SimpleQuestion and MultipleQuestion
+		questionDAO.updateSimpleQuestion(question);
 	}
-	
+
 	/*getters/setters*/
-	
+
 	public QuestionDAO getQuestionDAO() {
 		return questionDAO;
 	}
