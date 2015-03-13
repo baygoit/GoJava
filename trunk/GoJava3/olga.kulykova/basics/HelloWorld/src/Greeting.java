188e938830204cccf0a2d91e356d92cf06502043
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-13 22:52:08 +0000

    import projects
    git-svn-id: http://go-java-1.googlecode.com/svn@1975 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/olga.kulykova/basics/HelloWorld/src/Greeting.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ольга on 12.03.2015.
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ trunk/GoJava3/olga.kulykova/basics/HelloWorld1/src/HellowWorld1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Ольга on 13.03.2015.
+ */
+public class HellowWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
