53ff6ed6e58792a87599ce60e506f72c27faf7ee
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-14 23:23:59 +0000

    Anagram realise 2
    git-svn-id: http://go-java-1.googlecode.com/svn@2051 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/olga.kulykova/src/Anagram.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Ольга on 15.03.2015.
+ */
+public class Anagram {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text = reader.readLine();
+        StringBuilder word = new StringBuilder();
+        StringBuilder newText = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (' ' != text.charAt(i)) {
+                word.append(text.charAt(i));
+            } else {
+                newText.append(word.reverse());
+                word.delete(0, word.length());
+                newText.append(" ");
+            }
+        }
+        newText.append(word.reverse());
+
+        System.out.println(newText);
+    }
+}
