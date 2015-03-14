0be472f627dff48fa9c46ba5add4b6df5a418fff
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-14 22:38:24 +0000

    one loop
    git-svn-id: http://go-java-1.googlecode.com/svn@2046 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
+++ trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
@@ -1,16 +1,14 @@
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 /**
  * Created by Aleksey Kurkov on 11.03.15.
  */
 
 /*  Test cases
-    Input: 23 45 34 12 45 4 38 56 2 49 100
-
-    Output:
-    min1[8] = 2
-    min2[5] = 4
-    Distance = 3
+    1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1[8] = 2   min2[5] = 4   Distance = 3
+    2) Input: 1 8 8 8 1                          Output: min1[4] = 1   min2[0] = 1   Distance = 4
 */
 
 
@@ -30,28 +28,20 @@ public class DistanceCalculator {
             intArray[i] = Integer.parseInt(stringArray[i]);
         }
 
-        min1 = intArray[0];
-        index1 = 0;
+        min1 = min2 = intArray[0];
+        index1 = index2 = 0;
 
         for (int i = 0; i < intArray.length; i++) {
-            if (intArray[i] < min1) {
+            if (intArray[i] <= min1) {
+                min2 = min1;
                 min1 = intArray[i];
+                index2 = index1;
                 index1 = i;
-            }
-        }
-
-        min2 = intArray[0];
-        index2 = 0;
-
-        for (int i = 0; i < intArray.length; i++) {
-            if ((intArray[i] < min2) && (intArray[i] > min1)) {
-                min2 = intArray[i];
-                index2 = i;
             }
         }
 
         System.out.println("\nmin1[" + index1 + "] = " + min1);
         System.out.println("min2[" + index2 + "] = " + min2);
-        System.out.println("Distance = " + (index1 - index2));
+        System.out.println("Distance = " + abs(index1 - index2));
     }
 }
