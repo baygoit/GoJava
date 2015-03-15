2853fd52dcf915eeecb7d3b5dd30374085056ab9
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 21:21:59 +0000

    There was a problem with my first realization. Unfortunately, now I use more than 1 cycle (
    git-svn-id: http://go-java-1.googlecode.com/svn@2083 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/MinimalDistance/src/MinimalDistance.java
+++ trunk/GoJava3/alex.shramko/MinimalDistance/src/MinimalDistance.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class MinimalDistance {
 
@@ -9,65 +11,57 @@ public class MinimalDistance {
 					"Inrut array should contain at least 2 elements");
 		}
 		int min1 = Integer.parseInt(arrIn[0]);
-		int minPos1 = 0;
 		int min2 = Integer.parseInt(arrIn[1]);
-		int minPos2 = 1;
 		for (int i = 2; i < arrIn.length; i++) {
 			int currentInt = Integer.parseInt(arrIn[i]);
 			if (currentInt < Math.max(min1, min2)) {
-				if (min1 != min2) {
-					if (min1 > min2) {
-						min1 = currentInt;
-						minPos1 = i;
-					} else {
-						min2 = currentInt;
-						minPos2 = i;
-					}
-				} else {
-					if (Math.abs(i - minPos1) < Math.abs(i - minPos2)) {
-						min2 = currentInt;
-						minPos2 = i;
-					} else {
-						min1 = currentInt;
-						minPos1 = i;
-					}
-				}
-			} else if (currentInt == Math.max(min1, min2)) {
-				if ((min1 > min2)
-						&& (Math.abs(i - minPos2) < Math.abs(minPos1 - minPos2))) {
-					minPos1 = i;
-				} else if ((min2 > min1)
-						&& (Math.abs(i - minPos1) < Math.abs(minPos2 - minPos1))) {
-					minPos1 = i;
-				} else if (min2 == min1) {
-					if (Math.abs(i - minPos1) < Math.abs(i - minPos2)) {
-						min2 = currentInt;
-						minPos2 = i;
-					} else {
-						min1 = currentInt;
-						minPos1 = i;
-					}
+				if (min1 > min2)
+					min1 = currentInt;
+				else
+					min2 = currentInt;
+			}
+		}
+
+		Set<Integer> pos1 = new HashSet<Integer>();
+		Set<Integer> pos2 = new HashSet<Integer>();
+
+		for (int i = 0; i < arrIn.length; i++) {
+			int currentInt = Integer.parseInt(arrIn[i]);
+			if (currentInt == min1)
+				pos1.add(i);
+			if (currentInt == min2)
+				pos2.add(i);
+		}
+
+		int minDistance = Integer.MAX_VALUE;
+		for (Integer intPos1 : pos1) {
+			for (Integer intPos2 : pos2) {
+				if (intPos1 != intPos2) {
+					if (minDistance > Math.abs(intPos2 - intPos1))
+						minDistance = Math.abs(intPos2 - intPos1);
 				}
 			}
 		}
-		
+
 		int[] returnValues = new int[3];
-		returnValues[0] = Math.min(min1,min2);
-		returnValues[1] = Math.max(min1,min2);;
-		returnValues[2] = Math.abs(minPos2-minPos1);
+		returnValues[0] = Math.min(min1, min2);
+		returnValues[1] = Math.max(min1, min2);
+		;
+		returnValues[2] = Math.abs(minDistance);
 		return returnValues;
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		System.out.println("Insert your string of numbers separated by space:");
 		Scanner scan = new Scanner(System.in);
 		String input = scan.nextLine();
 		scan.close();
 		int[] values = checkInput(input);
-		
-		System.out.println("Minimal numbers are: "+values[0]+", "+values[1]);
-		
-		System.out.println("Distance between them are: "+values[2]);
+
+		System.out.println("Minimal numbers are: " + values[0] + ", "
+				+ values[1]);
+
+		System.out.println("Distance between them are: " + values[2]);
 
 	}
 
--- trunk/GoJava3/alex.shramko/MinimalDistance/src/Test.java
+++ trunk/GoJava3/alex.shramko/MinimalDistance/src/Test.java
@@ -7,6 +7,10 @@ public class Test {
 		try {
 			assertArrayEquals(new int[] { 1, 2, 1 },
 					MinimalDistance.checkInput("1 2 3"));
+			assertArrayEquals(new int[] { 1, 1, 2 },
+					MinimalDistance.checkInput("1 2 1 2 2 1"));
+			assertArrayEquals(new int[] { 1, 1, 2 },
+					MinimalDistance.checkInput("1 2 2 1 2 1"));
 			assertArrayEquals(new int[] { 2, 4, 3 },
 					MinimalDistance.checkInput("23 45 34 12 45 4 38 56 2 49 100"));
 			assertArrayEquals(new int[] { 4, 4, 1 },
