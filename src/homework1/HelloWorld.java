16f33acf40b9a80d2b2ec215c2e519d2bdf604d5
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 17:53:55 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2073 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ src/homework1/HelloWorld.java
@@ -0,0 +1,11 @@
+package homework1;
+
+/**
+ * Created by Alex on 14.03.2015.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
