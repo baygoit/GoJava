67adfc310c328229d026019936e6905defad0e6e
dmitry.reid <dmitry.reid@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 22:52:14 +0000

    updating structure
    git-svn-id: http://go-java-1.googlecode.com/svn@2087 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/dmytro.reydalov/module2/Interview Simulator/src/ua/com/goit/gojava/solo307/interview/dao/LoggerDAO.java
+++
@@ -1,9 +0,0 @@
-package ua.com.goit.gojava.solo307.interview.dao;
-
-import org.apache.log4j.Logger;
-
-public class LoggerDAO {
-
-	static final Logger daoLogger = Logger.getLogger(LoggerDAO.class);
-
-}
--- trunk/GoJava1/dmytro.reydalov/module2/Interview Simulator/src/ua/com/goit/gojava/solo307/interview/domain/LoggerDomain.java
+++
@@ -1,9 +0,0 @@
-package ua.com.goit.gojava.solo307.interview.domain;
-
-import org.apache.log4j.Logger;
-
-public class LoggerDomain {
-
-	static final Logger domainLogger = Logger.getLogger(LoggerDomain.class);
-	
-}
