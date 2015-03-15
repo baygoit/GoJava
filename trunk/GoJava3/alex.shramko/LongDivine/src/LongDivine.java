805d443af7a02d9aaa7fd50cb61c604254434928
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 18:53:21 +0000

    Solve small problem (2/10 was 02 not 0.2)
    git-svn-id: http://go-java-1.googlecode.com/svn@2078 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/LongDivine/src/LongDivine.java
+++ trunk/GoJava3/alex.shramko/LongDivine/src/LongDivine.java
@@ -2,8 +2,8 @@ public class LongDivine {
 
 	public static String longDivine(int divisible, int divisor) {
 		System.out.println("=================");
-		System.out.println(divisible+" / "+divisor+" = ");
-		System.out.println(divisible+"|"+divisor);
+		System.out.println(divisible + " / " + divisor + " = ");
+		System.out.println(divisible + "|" + divisor);
 		StringBuilder str = new StringBuilder();
 		StringBuilder indent = new StringBuilder();
 		StringBuilder strAfterZero = new StringBuilder();
@@ -12,22 +12,20 @@ public class LongDivine {
 			int modulo = divisible % divisor;
 			int result = divisible / divisor;
 			if (result != 0) {
-				System.out.println(indent.toString()+" "+divisible);
-				System.out.println(indent.toString()+"-"+divisor*result);
-				System.out.println(indent.toString()+"---");
+				System.out.println(indent.toString() + " " + divisible);
+				System.out.println(indent.toString() + "-" + divisor * result);
+				System.out.println(indent.toString() + "---");
 				indent.append(" ");
 			}
 			if (x == 0)
 				str.append(result);
 			if (modulo == 0) {
-				System.out.println(indent.toString()+" 0");
+				System.out.println(indent.toString() + " 0");
 				if (x > 0)
 					strAfterZero.append(result);
 				break;
 			}
 			if (x > 0) {
-				if (x == 1)
-					str.append(".");
 				int index = strAfterZero.indexOf(Integer.toString(result));
 				if (index >= 0) {
 					strAfterZero.insert(index, "(").append(")");
@@ -38,21 +36,22 @@ public class LongDivine {
 			divisible = modulo * 10;
 			x++;
 		}
-		str.append(strAfterZero);
+		str.append(".").append(strAfterZero);
 		System.out.println("result:");
 		System.out.println(str.toString());
 		return str.toString();
 	}
 
 	public static void main(String[] args) {
+		longDivine(97, 18);
+//		longDivine(2, 10);
+//		longDivine(3, 55);
+//		longDivine(1, 3);
+//		longDivine(1, 4);
+//		longDivine(50, 3);
+//		longDivine(1, 6);
+//		longDivine(12, 42);
 
-		 longDivine(6, 3);
-		 longDivine(3, 55);
-		 longDivine(1, 3);
-		 longDivine(1, 4);
-		 longDivine(50, 3);
-		 longDivine(1, 6);
-		 longDivine(12, 42);
 	}
 
 }
