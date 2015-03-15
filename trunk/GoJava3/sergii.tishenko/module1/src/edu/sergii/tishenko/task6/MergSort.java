bdd0f45591305e892cf49c16a123a69fdb95ffa8
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 01:27:31 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2056 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/sergii.tishenko/module1/src/edu/sergii/tishenko/task6/MergSort.java
+++ trunk/GoJava3/sergii.tishenko/module1/src/edu/sergii/tishenko/task6/MergSort.java
@@ -9,10 +9,10 @@ public class MergSort {
 
 	public static void main(String[] args) {
 		
-		final List inParam = new LinkedList<Integer>(Arrays.asList(32,5,7,92,3,5,65,23,123,7,9));
+		final List<Integer> inParam = new LinkedList<Integer>(Arrays.asList(32,-5,0,7,92,3,5,65,23,123,7,9));
 		
-		List res =  SortArray(inParam.subList(0, inParam.size() / 2),
-				inParam.subList(inParam.size() / 2, inParam.size()));
+		List<Integer> res =  SortArray(inParam.subList(0, inParam.size() / 2),
+				              inParam.subList(inParam.size() / 2, inParam.size()));
 		
 		System.out.println("-----------------" );
 		System.out.println("Result:" + res );
@@ -23,32 +23,26 @@ public class MergSort {
 	static List<Integer> SortArray(List<Integer> argList1, List<Integer> argList2) {
 		
 		System.out.println("step in: {" + argList1 + " , " + argList2 + "}" );
-		
-		List<Integer> resList1 = null;
-		List<Integer> resList2 = null;
 		List<Integer> resultList = new LinkedList<Integer>();
 
 		if (argList1.size() > 1) {
-			resList1 = SortArray(argList1.subList(0, argList1.size() / 2),
+			argList1 = SortArray(argList1.subList(0, argList1.size() / 2),
 					             argList1.subList(argList1.size() / 2, argList1.size()));
-		} else resList1 = argList1;
+		} 
 
 		
 		if (argList2.size() > 1) {
-			resList2 = SortArray(argList2.subList(0, argList2.size() / 2),
+			argList2 = SortArray(argList2.subList(0, argList2.size() / 2),
 					argList2.subList(argList2.size() / 2, argList2.size()));
-		} else resList2 = argList2;
+		} 
 
 		
-		
-		Iterator<Integer> iter1 = resList1.iterator();
-		Iterator<Integer> iter2 = resList2.iterator();
-		
-		int val1 =0; 
-		int val2 =0; 
+		Iterator<Integer> iter1 = argList1.iterator();
+		Iterator<Integer> iter2 = argList2.iterator();
 
-		val1 = iter1.next();
-		val2 = iter2.next();
+		int  val1 = iter1.next();
+		int  val2 = iter2.next();
+
 
 		while(true){
 			if (val1 < val2) {
@@ -84,7 +78,6 @@ public class MergSort {
 		} ;
 		
 		System.out.println("step out: " + resultList+ "" );		
-
 		return resultList;
 	}
 
