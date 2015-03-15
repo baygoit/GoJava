e94f16a3de04523ce1333bcf454e82b272c1b3da
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 23:35:38 +0000

    Учитывается дублирование минимумов Выводит расстояние последних двух минимумов
    git-svn-id: http://go-java-1.googlecode.com/svn@2095 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olga.kulykova/DistanceFromTwoLeast/src/Distance.java
+++ trunk/GoJava3/olga.kulykova/DistanceFromTwoLeast/src/Distance.java
@@ -1,17 +1,31 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+
 /**
  * Created by Ольга on 11.03.2015.
  */
+
+
 /*
  * Твоя задача создать консольное приложение, которое принимало бы на вход ряд чисел
  * и выводило расстояние между двумя наименьшими. Например, дано ряд чисел: "23 45 34 12 45 4 38 56 2 49 100".
  * Наименьшие числа в нем 2 и 4. Расстояние между ними - 3.
  */
+
+/*
+Test cases
+Input: 1 2 2 1 2 1
+Output: 2
+
+Input: 1 2 1 2 2 1
+Output: 3
+ */
+
 public class Distance {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -25,13 +39,13 @@ public class Distance {
             list.add(n);
         }
 
-        int min1 = Integer.MAX_VALUE;
-        int min2 = Integer.MAX_VALUE;
+        int min1 = list.get(0);
+        int min2 = list.get(0);
         int indexOfMin1 = 0;
         int indexOfMin2 = 0;
 
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) < min2) {
+            if (list.get(i) <= min1) {
                 min2 = min1;
                 indexOfMin2 = indexOfMin1;
                 min1 = list.get(i);
@@ -39,8 +53,11 @@ public class Distance {
             }
         }
 
-        int distance = Math.max(indexOfMin1, indexOfMin2) - Math.min(indexOfMin1, indexOfMin2);
+        int distance = Math.abs(indexOfMin1 - indexOfMin2);
 
         System.out.println(String.format("Two min numbers are %d and %d, distance between them is %d", min1, min2, distance));
     }
 }
+
+
+
