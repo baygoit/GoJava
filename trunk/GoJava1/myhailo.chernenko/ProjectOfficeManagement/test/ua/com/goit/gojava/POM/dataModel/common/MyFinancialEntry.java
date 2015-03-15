cb51b876bb092ca041c581097e027fadd141f306
mchernenkoua@gmail.com <mchernenkoua@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-15 19:50:48 +0000

    Moving to maven project...
    git-svn-id: http://go-java-1.googlecode.com/svn@2079 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/myhailo.chernenko/ProjectOfficeManagement/test/ua/com/goit/gojava/POM/dataModel/common/MyFinancialEntry.java
+++
@@ -1,9 +0,0 @@
-package ua.com.goit.gojava.POM.dataModel.common;
-
-import ua.com.goit.gojava.POM.dataModel.temporaryUnusedClases.FinancialEntry;
-
-public class MyFinancialEntry extends FinancialEntry {
-
-	private static final long serialVersionUID = -5119463255302552385L;
-	
-}
