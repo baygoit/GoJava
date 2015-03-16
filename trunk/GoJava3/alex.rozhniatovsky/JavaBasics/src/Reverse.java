35db097c0d4d08c93b24faa5059a23460357cf1b
roznalex@gmail.com <roznalex@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 17:07:29 +0000

    Reverse.java has been modified
    git-svn-id: http://go-java-1.googlecode.com/svn@2130 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/Reverse.java
+++ trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/Reverse.java
@@ -8,7 +8,37 @@ public class Reverse {
         System.out.println(result);
     }
 
+    /**
+     * Function for revers the line
+     * "мама мыла раму"-->"амам алым умар"
+     *
+     * @param line Input line of text
+     * @return result Reversed line of text
+     */
     String reverseText(String line) {
+        String temp = "";
+        String result = "";
+
+        for (int i = 0; i < line.length(); i++) {
+            temp += line.charAt(i);
+            if ((line.charAt(i) == ' ') || (i == line.length() - 1)) {
+                if (i == line.length() - 1) {
+                    temp += " ";
+                }
+                for (int j = temp.length() - 2; j >= 0; j--) {
+                    result += temp.charAt(j);
+                    if((j == 0) && (i != line.length() - 1))
+                        result += " ";
+                }
+                temp = "";
+            }
+        }
+
+        return result;
+    }
+   /* Version with additional using od memory
+
+   String reverseText(String line) {
         StringBuilder builder = new StringBuilder(line.length());
         String reversedLine;
         String resultLine = "";
@@ -25,4 +55,6 @@ public class Reverse {
         }
         return resultLine.trim();
     }
+
+    */
 }
