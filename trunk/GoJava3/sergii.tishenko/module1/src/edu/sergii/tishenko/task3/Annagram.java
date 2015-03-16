b7515fb8dd57567d6d15e0031c98009f57280ccc
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 09:28:35 +0000

    added  solution without additional String[].
    git-svn-id: http://go-java-1.googlecode.com/svn@2111 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/sergii.tishenko/module1/src/edu/sergii/tishenko/task3/Annagram.java
+++ trunk/GoJava3/sergii.tishenko/module1/src/edu/sergii/tishenko/task3/Annagram.java
@@ -8,14 +8,35 @@ public class Annagram {
 
 		String strInputLine;
 		Scanner in = new Scanner(System.in);
-
 		System.out.println("Enter string:");
 		strInputLine = in.nextLine();
 		strInputLine = strInputLine.trim();
+		int pos = 0 ,startPos =0;
 		in.close();
 
-		String[] strArr = strInputLine.split(" ");
+		StringBuilder strResult = new StringBuilder();
 		
+		// 1: Solution without additional String[] 
+
+		for (int i = 0; i < strInputLine.length() ; i++) {
+			if(strInputLine.charAt(i) == ' '  || i == strInputLine.length() - 1){
+				
+				if(i == strInputLine.length() - 1) startPos = i;
+				else startPos = i - 1;
+					
+				for(int k =startPos; k >= pos; k--){
+					strResult.append(strInputLine.charAt(k));
+				}
+				strResult.append(' ');
+				pos = i + 1;
+			}
+		}
+		System.out.println("1:" + strResult);
+
+		
+		// 2: Solution with additional String[] 
+		String[] strArr = strInputLine.split(" ");
+		System.out.print("2:");
 		for (int i = 0; i < strArr.length; i++) {
 			System.out.print(new StringBuilder(strArr[i]).reverse().toString() + " ");
 		}
