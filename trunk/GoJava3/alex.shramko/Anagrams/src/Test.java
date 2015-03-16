f40607a89c113ea2fe06e58a35c501ab64c47359
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 13:53:56 +0000

    Changed tab to 4 spaces
    git-svn-id: http://go-java-1.googlecode.com/svn@2120 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/Anagrams/src/Test.java
+++ trunk/GoJava3/alex.shramko/Anagrams/src/Test.java
@@ -2,17 +2,13 @@ import static org.junit.Assert.*;
 
 public class Test {
 
-	@org.junit.Test
-	public void test() {
-			assertEquals("amam alim ymar",
-					Anagrams.changeString("mama mila ramy"));
-			assertEquals("ytyd eerf",
-					Anagrams.changeString("dyty free"));
-			assertEquals("aaa bb ccc",
-					Anagrams.changeString("aaa bb ccc"));	
-			assertEquals("bbaa dcba fed",
-					Anagrams.changeString("aabb abcd def"));	
+    @org.junit.Test
+    public void test() {
+        assertEquals("amam alim ymar", Anagrams.changeString("mama mila ramy"));
+        assertEquals("ytyd eerf", Anagrams.changeString("dyty free"));
+        assertEquals("aaa bb ccc", Anagrams.changeString("aaa bb ccc"));
+        assertEquals("bbaa dcba fed", Anagrams.changeString("aabb abcd def"));
 
-	}
+    }
 
 }
