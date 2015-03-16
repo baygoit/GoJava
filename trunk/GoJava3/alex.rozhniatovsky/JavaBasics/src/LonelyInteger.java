670ff4f0d9cd8d90b2de2d404e627df31aba0e65
roznalex@gmail.com <roznalex@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 20:42:41 +0000

    Errors have been correct
    git-svn-id: http://go-java-1.googlecode.com/svn@2142 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/LonelyInteger.java
+++ trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/LonelyInteger.java
@@ -4,28 +4,32 @@
 public class LonelyInteger {
     public static void main(String[] args) {
         LonelyInteger test = new LonelyInteger();
-        int[] numbers = {2, 3, 2, 4, 3, 7, 2, 3, 4, 4, 7, 7, 13};
+        int[] numbers = {20, 2, 3, 2, 3, 2, 3, 4, 4, 4, 1, 1, 1, 5, 5, 5, 6, 6, 6, 7, 7, 7};
         int singleton = test.searchSingleton(numbers);
         System.out.println(singleton);
     }
 
-
     int searchSingleton(int[] numbers) {
-        StringBuilder builder = new StringBuilder();
-        String temp;
-        int sum = 0;
+        int[] buffer = new int[32];
+        int bufferLength = buffer.length;
+        int singleton = 0;
 
         for (int i = 0; i < numbers.length; i++) {
-            temp = Integer.toBinaryString(numbers[i]);
-            sum += Integer.parseInt(temp);
+            int count = buffer.length - 1;
+            while (numbers[i] > 0) {
+                //use the mask
+                if ((numbers[i] & 1) == 1) {
+                    buffer[count] += 1;
+                }
+                //bit with shift
+                numbers[i] >>=  1;
+                count--;
+            }
         }
-        char[] chars = Integer.toString(sum).toCharArray();
-        for (int i = 0; i < chars.length; i++) {
-            builder.append(Character.getNumericValue(chars[i]) % 3);
+        for (int i = 0; i < buffer.length; i++) {
+            buffer[i] %= 3;
+            singleton += buffer[i] * Math.pow(2, bufferLength - i - 1);
         }
-        temp = builder.toString();
-        sum = Integer.parseInt(temp, 2);
-
-        return sum;
+        return singleton;
     }
 }
