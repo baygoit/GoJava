eae195fa063d8ca1475044577ce1c0d1f03fc9df
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 22:06:44 +0000

    minimal distance (array with repeating items)
    git-svn-id: http://go-java-1.googlecode.com/svn@2145 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
+++ trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
-import static java.lang.Math.abs;
+import static java.lang.Math.*;
 
 /**
  * Created by Aleksey Kurkov on 11.03.15.
  */
 
 /*  Test cases
-    1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1[8] = 2   min2[5] = 4   Distance = 3
-    2) Input: 1 8 8 8 1                          Output: min1[4] = 1   min2[0] = 1   Distance = 4
+    1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1[5] = 4   min2[8] = 2   Distance = 3
+    2) Input: 1 8 8 8 1                          Output: min1[0] = 1   min2[4] = 1   Distance = 4
+    3) Input: 3 1 2 2                            Output: min1[1] = 1   min2[3] = 2   Distance = 1
 */
 
 
 public class DistanceCalculator {
+    private static int distance;
+    private static int min1;
+    private static int min2;
+    private static int index1;
+    private static int index2;
+
     public static void main(String[] args) {
 
-        int min1, min2, index1, index2;
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input string of numbers: ");
 
@@ -28,20 +34,32 @@ public class DistanceCalculator {
             intArray[i] = Integer.parseInt(stringArray[i]);
         }
 
-        min1 = min2 = intArray[0];
-        index1 = index2 = 0;
+        min1 = intArray[0];
+        index1 = 0;
+        min2 = intArray[1];
+        index2 = 1;
 
-        for (int i = 0; i < intArray.length; i++) {
-            if (intArray[i] <= min1) {
-                min2 = min1;
-                min1 = intArray[i];
-                index2 = index1;
-                index1 = i;
+        for (int i = 2; i < intArray.length; i++) {
+            if (intArray[i] < max(min1, min2)) {
+                if (min1 < min2) {
+                    min2 = intArray[i];
+                    index2 = i;
+                } else {
+                    min1 = intArray[i];
+                    index1 = i;
+                }
+                distance = abs(index1 - index2);
+            } else if (intArray[i] == max(min1, min2)) {
+                min1 = min(min(min1, min2), intArray[i]);
+                index1 = min(index1, index2);
+                min2 = intArray[i];
+                index2 = i;
+                distance = min(distance, abs(index1 - index2));
             }
         }
 
         System.out.println("\nmin1[" + index1 + "] = " + min1);
         System.out.println("min2[" + index2 + "] = " + min2);
-        System.out.println("Distance = " + abs(index1 - index2));
+        System.out.println("Distance = " + distance);
     }
 }
