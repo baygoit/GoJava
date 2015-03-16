2f18f95253fc79553f7e81169bcbf69e39de247d
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 13:54:32 +0000

    Changed tab to 4 spaces
    git-svn-id: http://go-java-1.googlecode.com/svn@2125 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/MinimalDistance/src/MinimalDistance.java
+++ trunk/GoJava3/alex.shramko/MinimalDistance/src/MinimalDistance.java
@@ -4,55 +4,56 @@ import java.util.Set;
 
 public class MinimalDistance {
 
-	public static int calculateDistance(String input) throws Exception {
-		String[] arrIn = input.split(" ");
-		if (arrIn.length < 2) {
-			throw new Exception(
-					"Inrut array should contain at least 2 elements");
-		}
-		int min1 = Integer.parseInt(arrIn[0]);
-		int min2 = Integer.parseInt(arrIn[1]);
-		for (int i = 2; i < arrIn.length; i++) {
-			int currentInt = Integer.parseInt(arrIn[i]);
-			if (currentInt < Math.max(min1, min2)) {
-				if (min1 > min2)
-					min1 = currentInt;
-				else
-					min2 = currentInt;
-			}
-		}
+    public static int calculateDistance(String input) throws Exception {
+        String[] arrIn = input.split(" ");
+        if (arrIn.length < 2) {
+            throw new Exception(
+                    "Inrut array should contain at least 2 elements");
+        }
+        int min1 = Integer.parseInt(arrIn[0]);
+        int min2 = Integer.parseInt(arrIn[1]);
+        for (int i = 2; i < arrIn.length; i++) {
+            int currentInt = Integer.parseInt(arrIn[i]);
+            if (currentInt < Math.max(min1, min2)) {
+                if (min1 > min2)
+                    min1 = currentInt;
+                else
+                    min2 = currentInt;
+            }
+        }
 
-		Set<Integer> pos1 = new HashSet<Integer>();
-		Set<Integer> pos2 = new HashSet<Integer>();
+        Set<Integer> pos1 = new HashSet<Integer>();
+        Set<Integer> pos2 = new HashSet<Integer>();
 
-		for (int i = 0; i < arrIn.length; i++) {
-			int currentInt = Integer.parseInt(arrIn[i]);
-			if (currentInt == min1)
-				pos1.add(i);
-			if (currentInt == min2)
-				pos2.add(i);
-		}
+        for (int i = 0; i < arrIn.length; i++) {
+            int currentInt = Integer.parseInt(arrIn[i]);
+            if (currentInt == min1)
+                pos1.add(i);
+            if (currentInt == min2)
+                pos2.add(i);
+        }
 
-		int minDistance = Integer.MAX_VALUE;
-		for (Integer intPos1 : pos1) {
-			for (Integer intPos2 : pos2) {
-				if (intPos1 != intPos2) {
-					minDistance = Math.min(minDistance, Math.abs(intPos1 - intPos2));
-				}
-			}
-		}
-		int returnValue = Math.abs(minDistance);
-		return returnValue;
-	}
+        int minDistance = Integer.MAX_VALUE;
+        for (Integer intPos1 : pos1) {
+            for (Integer intPos2 : pos2) {
+                if (intPos1 != intPos2) {
+                    minDistance = Math.min(minDistance,
+                            Math.abs(intPos1 - intPos2));
+                }
+            }
+        }
+        int returnValue = Math.abs(minDistance);
+        return returnValue;
+    }
 
-	public static void main(String[] args) throws Exception {
-		System.out.println("Insert your string of numbers separated by space:");
-		Scanner scan = new Scanner(System.in);
-		String input = scan.nextLine();
-		scan.close();
-		int distance = calculateDistance(input);
-		System.out.println(distance);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Insert your string of numbers separated by space:");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+        int distance = calculateDistance(input);
+        System.out.println(distance);
 
-	}
+    }
 
 }
--- trunk/GoJava3/alex.shramko/MinimalDistance/src/Test.java
+++ trunk/GoJava3/alex.shramko/MinimalDistance/src/Test.java
@@ -2,24 +2,25 @@ import static org.junit.Assert.*;
 
 public class Test {
 
-	@org.junit.Test
-	public void test() {
-		try {
-			assertEquals(1,
-					MinimalDistance.calculateDistance("1 2 3"));
-			assertEquals(2,
-					MinimalDistance.calculateDistance("1 2 1 2 2 1"));
-			assertEquals(2,
-					MinimalDistance.calculateDistance("1 2 2 1 2 1"));
-			assertEquals(3,
-					MinimalDistance.calculateDistance("23 45 34 12 45 4 38 56 2 49 100"));
-			assertEquals(1,
-					MinimalDistance.calculateDistance("23 45 34 4 45 4 38 56 4 4 100"));	
-			assertEquals(2,
-					MinimalDistance.calculateDistance("23 45 34 2 45 4 38 56 2 4 2"));	
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @org.junit.Test
+    public void test() {
+        try {
+            assertEquals(1, MinimalDistance.calculateDistance("1 2 3"));
+            assertEquals(2, MinimalDistance.calculateDistance("1 2 1 2 2 1"));
+            assertEquals(2, MinimalDistance.calculateDistance("1 2 2 1 2 1"));
+            assertEquals(
+                    3,
+                    MinimalDistance
+                            .calculateDistance("23 45 34 12 45 4 38 56 2 49 100"));
+            assertEquals(1,
+                    MinimalDistance
+                            .calculateDistance("23 45 34 4 45 4 38 56 4 4 100"));
+            assertEquals(2,
+                    MinimalDistance
+                            .calculateDistance("23 45 34 2 45 4 38 56 2 4 2"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
