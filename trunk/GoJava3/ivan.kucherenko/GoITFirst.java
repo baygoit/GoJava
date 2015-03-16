86cb213da4bc597ae0696c093e9c8149f76186f3
ikucherenko128@gmail.com <ikucherenko128@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 09:11:18 +0000

    Min index v3
    git-svn-id: http://go-java-1.googlecode.com/svn@2110 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/ivan.kucherenko/GoITFirst.java
+++ trunk/GoJava3/ivan.kucherenko/GoITFirst.java
@@ -1,36 +1,28 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class GoITFirst {
 
-	public static int one(String[] args){
-		ArrayList<Integer> array = new ArrayList<Integer>();
-		ArrayList<Integer> sorted;
-		for (String i : args)
-			array.add(Integer.parseInt(i));
-			
-		sorted = new ArrayList<Integer>(array);
-		Collections.sort(sorted);
-			int index1 = array.indexOf(sorted.get(0));
-			int index2 = 0;
-			
-			for (int i = 0; i < array.size(); i++){
-				if (array.get(i) == sorted.get(1) && i != index1){
-					index2 = i;
-					break;
-				}
-			}
-		//Возвращаем разницу
-		return Math.abs(index1 - index2);
-	}
-	
 	public static void main(String[] args) {
-		/*Протестировано со следующими вводными данными
-		 * 7 8 0 78 4 5 3 6 5 2
-		 * 8 23  90 8 20 0 3 4 5 2
-		 * 8 23 1 90 20 67 0 3 4 5 2
-		 * */
-		System.out.println(one(args));
-	}
+	    int [] test = {8,5,6,7,1,6,7,4,5,3};
+	    int b = test[0];
+	    int a = test[0];
+	    int first = 0;
+	    int second = 0;
+	    
+	    for (int i = 1; i < test.length; i++)
+	    {
+	       if (test[i] < a) 
+	       	{
+	                a = test[i];
+	                second = first;
+	                first = i;
+	        }else if (test[i] < b)
+	        {
+	                second = i;
+	                b = test[i];
+	        }
+
+	     }
+	   System.out.println(Math.abs(first - second));
+
+}
 }
