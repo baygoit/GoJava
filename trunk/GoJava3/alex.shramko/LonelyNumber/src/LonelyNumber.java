93c47178efc4fa26b7a0ad8dc5fb2659f3516db4
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-16 13:54:13 +0000

    Changed tab to 4 spaces
    git-svn-id: http://go-java-1.googlecode.com/svn@2122 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/LonelyNumber/src/LonelyNumber.java
+++ trunk/GoJava3/alex.shramko/LonelyNumber/src/LonelyNumber.java
@@ -2,51 +2,50 @@ import java.util.Scanner;
 
 public class LonelyNumber {
 
-	public static int lonelyNumber(int[] array) {
-		int[][] binaryArray = new int[array.length][32];
-		for (int i = 0; i < array.length; i++) {
-			String binaryValue = Integer.toBinaryString(array[i]);
-			for (int j = 0; j < binaryArray[i].length; j++) {
-				int shift = binaryArray[i].length - binaryValue.length();
-				if (j > shift)
-					binaryArray[i][j] = Integer.parseInt(
-							binaryValue.substring(j - shift, j - shift + 1));
-			}
-		}
-		int[] binaryResult = new int[32];
-		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < binaryArray[i].length; j++) {
-				binaryResult[j] += binaryArray[i][j];
-			}
-		}
-		for (int i = 0; i < binaryResult.length; i++) {
-			binaryResult[i] = binaryResult[i]%3;
-		}
-		StringBuilder str = new StringBuilder();
-		for (int i = 0; i < binaryResult.length; i++) {
-			str.append(binaryResult[i]);
-		}
-		int result = Integer.parseInt(str.toString(), 2);
-		/*int result = 0;
-		for (int i = 0; i < binaryResult.length; i++) {
-		   result = result | 1<< i;
-		}*/
-		return result;
-	}
+    public static int lonelyNumber(int[] array) {
+        int[][] binaryArray = new int[array.length][32];
+        for (int i = 0; i < array.length; i++) {
+            String binaryValue = Integer.toBinaryString(array[i]);
+            for (int j = 0; j < binaryArray[i].length; j++) {
+                int shift = binaryArray[i].length - binaryValue.length();
+                if (j > shift)
+                    binaryArray[i][j] = Integer.parseInt(binaryValue.substring(
+                            j - shift, j - shift + 1));
+            }
+        }
+        int[] binaryResult = new int[32];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < binaryArray[i].length; j++) {
+                binaryResult[j] += binaryArray[i][j];
+            }
+        }
+        for (int i = 0; i < binaryResult.length; i++) {
+            binaryResult[i] = binaryResult[i] % 3;
+        }
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < binaryResult.length; i++) {
+            str.append(binaryResult[i]);
+        }
+        int result = Integer.parseInt(str.toString(), 2);
+        /*
+         * int result = 0; for (int i = 0; i < binaryResult.length; i++) {
+         * result = result | 1<< i; }
+         */
+        return result;
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Insert your string of numbers separated by space:");
-		Scanner scan = new Scanner(System.in);
-		String input = scan.nextLine();
-		scan.close();
-		String[] arrIn = input.split(" ");
-		int[] values = new int[arrIn.length];
-		for (int i = 0; i < values.length; i++) {
-			values[i] = Integer.parseInt(arrIn[i]);
-		}
-		System.out.println(lonelyNumber(values));
-		
-		
-	}
+    public static void main(String[] args) {
+        System.out.println("Insert your string of numbers separated by space:");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+        String[] arrIn = input.split(" ");
+        int[] values = new int[arrIn.length];
+        for (int i = 0; i < values.length; i++) {
+            values[i] = Integer.parseInt(arrIn[i]);
+        }
+        System.out.println(lonelyNumber(values));
+
+    }
 
 }
