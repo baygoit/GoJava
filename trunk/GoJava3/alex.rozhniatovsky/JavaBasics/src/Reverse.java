b963b99b763efb0e1e326091373595a67692f55d
roznalex@gmail.com <roznalex@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-17 16:42:46 +0000

    Reverse has been comleted
    git-svn-id: http://go-java-1.googlecode.com/svn@2188 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/Reverse.java
+++ trunk/GoJava3/alex.rozhniatovsky/JavaBasics/src/Reverse.java
@@ -4,57 +4,24 @@
 public class Reverse {
     public static void main(String[] args) {
         Reverse test = new Reverse();
-        String result = test.reverseText("мама мыла раму");
-        System.out.println(result);
+        test.reverseText("мама мыла раму");
     }
 
-    /**
-     * Function for revers the line
-     * "мама мыла раму"-->"амам алым умар"
-     *
-     * @param line Input line of text
-     * @return result Reversed line of text
-     */
-    String reverseText(String line) {
-        String temp = "";
-        String result = "";
-
+    void reverseText(String line) {
+        int temp = 0;
         for (int i = 0; i < line.length(); i++) {
-            temp += line.charAt(i);
             if ((line.charAt(i) == ' ') || (i == line.length() - 1)) {
-                if (i == line.length() - 1) {
-                    temp += " ";
+                for (int j = i; j > temp; j--) {
+                    if (j == line.length() - 1) {
+                        System.out.print(line.charAt(j));
+                    }
+                    System.out.print(line.charAt(j - 1));
                 }
-                for (int j = temp.length() - 2; j >= 0; j--) {
-                    result += temp.charAt(j);
-                    if((j == 0) && (i != line.length() - 1))
-                        result += " ";
+                temp = i + 1;
+                if (i != line.length() - 1) {
+                    System.out.print(" ");
                 }
-                temp = "";
             }
         }
-
-        return result;
     }
-   /* Version with additional using od memory
-
-   String reverseText(String line) {
-        StringBuilder builder = new StringBuilder(line.length());
-        String reversedLine;
-        String resultLine = "";
-        char[] chars = line.toCharArray();
-
-        for (int i = chars.length - 1; i >= 0; i--) {
-            builder.append(chars[i]);
-        }
-        reversedLine = builder.toString();
-
-        String[] result = reversedLine.split(" ");
-        for (int  i = result.length - 1; i >= 0; i--) {
-            resultLine += result[i] + " ";
-        }
-        return resultLine.trim();
-    }
-
-    */
 }
