ec8baa6489494e979784a047de2cfb8bbcb3e37c
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-17 08:32:53 +0000

    Updated version which works with repeated min value and repeated next value after min. Additionally  code formatted according Java Code Convention.
    git-svn-id: http://go-java-1.googlecode.com/svn@2166 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/anton.yarosh/HomeTask/src/MinNumber.java
+++ trunk/GoJava3/anton.yarosh/HomeTask/src/MinNumber.java
@@ -1,42 +1,60 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
-
 public class MinNumber {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String input=sc.nextLine();
-		String []m=input.split(" ");
-		int []n=new int[m.length];
-		for(int i=0; i<m.length; i++)
-		{
-			n[i]=Integer.parseInt(m[i]);
+		Scanner sc = new Scanner(System.in);
+		//String input = sc.nextLine();
+		//String input ="0 2 5 1 8 4 0 7 2 6 4 2 1 8 6 0 0 1";
+		//String input ="1 5 5 5 1 0 9 9 0";
+		String input="6 5 3 1 6 2 1 5 8 3 1 1";
+		String[]m = input.split(" ");
+		int []rez = new int[m.length];
+		for (int i = 0; i < m.length; i++) {
+			rez[i] = Integer.parseInt(m[i]);
 		}
-		int minElement=findMin(n)[0];
-		int firstIndex=findMin(n)[1];
-	
-		for(int i=0; i<n.length;i++)
-		{
-			n[i]=n[i]-minElement;
-		}
-
-		int secondIndex=findMin(n)[1];
-
-		System.out.println(Math.abs(firstIndex-secondIndex));
-	}
-
-	static int[] findMin(int []n){
-		int minElement=1;
-		int index=0;
-		for(int i=0; i<n.length;i++)
-		{
-			if(n[i]!=0)
-				if(minElement>=n[i])
-				{
-					minElement=n[i];
-					index=i;
+		int minElement = rez[0];
+		int minIndex = 0;
+		int minGapIndex = 1;
+		int minGap = Math.abs(rez[minIndex] - rez[minGapIndex]);
+		int equalGap = Integer.MAX_VALUE;
+		int finalGap = 0;
+		for (int i = minIndex + 1; i < rez.length; i++) {
+			if (minElement > rez[i]) {
+				minGap = minElement - rez[i];
+				minElement = rez[i];
+				minGapIndex = minIndex;
+				minIndex = i;
+				equalGap = Integer.MAX_VALUE;
+			}
+			else if (minElement == rez[i]) {
+				if (Math.abs(minIndex - i) < equalGap){
+					equalGap = Math.abs(minIndex - i);
+					minIndex = i;
 				}
+				else {
+					minIndex = i;						
+				}
+			}
+			else {
+				if (Math.abs(minElement - rez[i]) < minGap) {
+					minGap = Math.abs(minElement - rez[i]);
+					minGapIndex = i;
+				}
+				else if(Math.abs(minElement - rez[i]) == minGap) {
+					if (Math.abs(minIndex - minGapIndex) > Math.abs(minIndex - i)) {
+						minGapIndex = i;
+					}
+				}
+			}
 		}
-		return new int[]{minElement,index};
+		if (equalGap == Integer.MAX_VALUE) {
+			finalGap = Math.abs(minIndex - minGapIndex); 
+			System.out.println("Min element: " + rez[minIndex] + ", next closets value: " + rez[minGapIndex]);
+		}
+		else {
+			finalGap = equalGap;
+			System.out.println("Minimal repeated value: " + rez[minIndex]);			
+		}
+		System.out.println("Minimal distance: " + finalGap);
 	}
 }
