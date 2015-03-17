a7be6800c3696dbe02eea7f2969bd5fb0b1a6190
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-17 08:26:05 +0000

    Second version without creating additional object
    git-svn-id: http://go-java-1.googlecode.com/svn@2162 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/anton.yarosh/HomeTask/src/Anagram.java
+++ trunk/GoJava3/anton.yarosh/HomeTask/src/Anagram.java
@@ -1,42 +1,28 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
-
-
 public class Anagram {
-
-
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String input=sc.nextLine();
-		String toPrint="";
-		String []m=input.split(" ");
-		String []result=new String[m.length];
-		for(int i=0; i<m.length;i++)
-		{
-			result[i]=convert(m[i]);
-		}
-
-		for(int i=0; i<result.length-1; i++)
-		{
-			toPrint+=result[i]+" ";
-		}
-		toPrint+=result[result.length-1];
-		System.out.println(toPrint);
-
-	}
-
-	static String convert(String m){
-		String toReturn="";
-		Stack conv=new Stack();
-		for(int i=0; i<m.length();i++)
-		{
-			conv.push(m.charAt(i));
-		}
-
-		while(conv.size()!=0)
-			toReturn+=conv.pop();
-
-		return toReturn;
+		String toPrint = "";
+		Scanner sc = new Scanner(System.in);
+		StringBuffer input = new StringBuffer(sc.nextLine());
+		int end = 0;
+		int i = 0;
+		int j = 0;
+		do {
+			end = input.indexOf(" ", end + 1);
+			if(end == -1) {
+				end = input.length();
+			}
+			for (int k = i; k <= (i + end - 1) / 2; k++) {
+				char tmp = input.charAt(k);
+				input.setCharAt(k, input.charAt(end - 1 - j));
+				input.setCharAt(end - 1 - j, tmp);
+				j++;
+			}
+			i = end + 1;
+			j = 0;
+		}while(end != input.length());
+		System.out.println(input.toString());
 	}
 }
