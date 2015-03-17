caf34857d49df11fef2e638050ad545844be38b9
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-17 01:03:33 +0000

    Учитывает дублирование минимумов Прошла все test cases
    git-svn-id: http://go-java-1.googlecode.com/svn@2154 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olga.kulykova/DistanceFromTwoLeast/src/Distance.java
+++ trunk/GoJava3/olga.kulykova/DistanceFromTwoLeast/src/Distance.java
@@ -5,11 +5,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
  * Created by Ольга on 11.03.2015.
  */
-
 
 /*
  * Твоя задача создать консольное приложение, которое принимало бы на вход ряд чисел
@@ -23,7 +21,13 @@ Input: 1 2 2 1 2 1
 Output: 2
 
 Input: 1 2 1 2 2 1
-Output: 3
+Output: 2
+
+Input: 2 9 3 3 3 11 15
+Output: 2
+
+Input: 2 5 8 7 2 2 3 1
+Output: 2
  */
 
 public class Distance {
@@ -40,20 +44,34 @@ public class Distance {
         }
 
         int min1 = list.get(0);
-        int min2 = list.get(0);
+        int min2 = list.get(1);
         int indexOfMin1 = 0;
-        int indexOfMin2 = 0;
+        int indexOfMin2 = 1;
+        int distance = 1;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) <= min1) {
-                min2 = min1;
-                indexOfMin2 = indexOfMin1;
-                min1 = list.get(i);
-                indexOfMin1 = i;
+        for (int i = 2; i < list.size(); i++) {
+            if (list.get(i) < Math.max(min1, min2)) {
+                if (min2 > min1) {
+                    min2 = list.get(i);
+                    indexOfMin2 = i;
+                } else {
+                    min1 = list.get(i);
+                    indexOfMin1 = i;
+                }
+                distance = Math.abs(indexOfMin1 - indexOfMin2);
+
+            } else if (list.get(i) == Math.max(min1, min2)) {
+                if (min1 > min2) {
+                    indexOfMin1 = i;
+                } else if (min2 > min1){
+                    indexOfMin2 = i;
+                } else {
+                    indexOfMin1 = Math.max(indexOfMin1, indexOfMin2);
+                    indexOfMin2 = i;
+                }
+                distance = Math.min(distance, Math.abs(indexOfMin1 - indexOfMin2));
             }
         }
-
-        int distance = Math.abs(indexOfMin1 - indexOfMin2);
 
         System.out.println(String.format("Two min numbers are %d and %d, distance between them is %d", min1, min2, distance));
     }
