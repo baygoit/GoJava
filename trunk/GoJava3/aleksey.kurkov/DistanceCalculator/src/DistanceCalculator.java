a884ab5348f4bd9ca58ee81615ed31231c474277
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-17 13:10:52 +0000

    fixed bugs, deleted indexes
    git-svn-id: http://go-java-1.googlecode.com/svn@2172 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
+++ trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
@@ -7,11 +7,14 @@ import static java.lang.Math.*;
  */
 
 /*  Test cases
-    1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1[5] = 4   min2[8] = 2   Distance = 3
-    2) Input: 1 8 8 8 1                          Output: min1[0] = 1   min2[4] = 1   Distance = 4
-    3) Input: 3 1 2 2                            Output: min1[1] = 1   min2[3] = 2   Distance = 1
+    1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1 = 4   min2 = 2   Distance = 3
+    2) Input: 1 8 8 8 1                          Output: min1 = 1   min2 = 1   Distance = 4
+    3) Input: 3 1 2 2                            Output: min1 = 2   min2 = 1   Distance = 1
+    4) Input: 1 3 3 1 3 1                        Output: min1 = 1   min2 = 1   Distance = 2
+    5) Input: 1 4 1 4 4 1                        Output: min1 = 1   min2 = 1   Distance = 2
+    6) Input: 1 8 5 5 5 10 11                    Output: min1 = 1   min2 = 5   Distance = 2
+    7) Input: 2 5 7 6 2 2 5 1                    Output: min1 = 1   min2 = 2   Distance = 2
 */
-
 
 public class DistanceCalculator {
     private static int distance;
@@ -19,6 +22,7 @@ public class DistanceCalculator {
     private static int min2;
     private static int index1;
     private static int index2;
+    private static int[] intArray;
 
     public static void main(String[] args) {
 
@@ -28,7 +32,7 @@ public class DistanceCalculator {
         String string = scanner.nextLine();
         String[] stringArray = string.split(" ");
 
-        int[] intArray = new int[stringArray.length];
+        intArray = new int[stringArray.length];
 
         for (int i = 0; i < stringArray.length; i++) {
             intArray[i] = Integer.parseInt(stringArray[i]);
@@ -50,16 +54,17 @@ public class DistanceCalculator {
                 }
                 distance = abs(index1 - index2);
             } else if (intArray[i] == max(min1, min2)) {
-                min1 = min(min(min1, min2), intArray[i]);
-                index1 = min(index1, index2);
-                min2 = intArray[i];
-                index2 = i;
+                if (min1 > min2) {
+                    index1 = i;
+                } else if (min2 > min1) {
+                    index2 = i;
+                } else {
+                    index1 = max(index1, index2);
+                    index2 = i;
+                }
                 distance = min(distance, abs(index1 - index2));
             }
         }
-
-        System.out.println("\nmin1[" + index1 + "] = " + min1);
-        System.out.println("min2[" + index2 + "] = " + min2);
-        System.out.println("Distance = " + distance);
+        System.out.println("\nmin1 = " + min1 + "\nmin2 = " + min2 + "\nDistance = " + distance);
     }
 }
