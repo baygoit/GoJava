179c437c0ee3237880f8b4277f2bdc09d5d9deda
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-18 13:52:19 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2203 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/avelitchenko/GoJava3avelitHW1/src/ua/goit/alg/TestRotatedArrays.java
@@ -0,0 +1,25 @@
+package ua.goit.alg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestRotatedArrays {
+
+    @Test
+    public void testPresented() {
+        int[] array = {4, 5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int result = RotatedArrays.binarySearch(array,5);
+        int expectedresult = 1;
+        assertEquals(result, expectedresult);
+    }
+
+    @Test
+    public void testNoPresented() {
+        int[] array = {4, 5, 6, 7, 8, 9, 10, 1, 2, 3};
+        int result = RotatedArrays.binarySearch(array,11);
+        int expectedresult = -1;
+        assertEquals(result, expectedresult);
+    }
+}
