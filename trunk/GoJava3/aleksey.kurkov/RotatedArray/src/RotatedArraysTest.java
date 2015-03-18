bdd10c5ea1b8e4d679bc3944db7cebf06fcf7ef0
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-18 22:52:49 +0000

    more tests added ----other tests----
    git-svn-id: http://go-java-1.googlecode.com/svn@2223 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/RotatedArray/src/RotatedArraysTest.java
+++ trunk/GoJava3/aleksey.kurkov/RotatedArray/src/RotatedArraysTest.java
@@ -26,4 +26,58 @@ public class RotatedArraysTest extends TestCase {
         int actualResult = RotatedArrays.binaryModifiedSearch(array, 3);
         assertEquals(expectedResult, actualResult);
     }
+    //-------------------- Other tests -------------------
+    public void test() {
+        int expected;
+        int actual;
+        expected = 1;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 5);
+        assertEquals(expected, actual);
+        expected = 4;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 8);
+        assertEquals(expected, actual);
+        expected = 0;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 4);
+        assertEquals(expected, actual);
+        expected = 5;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 1);
+        assertEquals(expected, actual);
+        expected = 6;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 1, 2, 3}, 2);
+        assertEquals(expected, actual);
+        expected = -1;
+        actual = RotatedArrays.binaryModifiedSearch(new int[]{4, 5, 6, 7, 1, 2, 3},
+                8);
+        assertEquals(expected, actual);
+        expected = 4;
+        actual = RotatedArrays.binaryModifiedSearch(new int[]{4, 5, 7, 8, 2, 3}, 2);
+        assertEquals(expected, actual);
+        expected = 2;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{6, 7, 8, 1, 2, 3, 4, 5}, 8);
+        assertEquals(expected, actual);
+        expected = 1;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 9, 10, 1, 2, 3}, 5);
+        assertEquals(expected, actual);
+        expected = 5;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 9, 10, 1, 2, 3}, 9);
+        assertEquals(expected, actual);
+        expected = 3;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 9, 10, 1, 2, 3}, 7);
+        assertEquals(expected, actual);
+        expected = 7;
+        actual = RotatedArrays.binaryModifiedSearch(
+                new int[]{4, 5, 6, 7, 8, 9, 10, 1, 2, 3}, 1);
+        assertEquals(expected, actual);
+
+    }
+
 }
