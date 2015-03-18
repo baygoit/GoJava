e80d7194aefca3c4489d4eaf20554e97b3079329
dmitry.reid <dmitry.reid@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-18 14:01:27 +0000

    - Injected dataSource and 1 bean By spring framework(xml configuration). - Replaced dirty scriptlets to nice JSTL tags. - Fixed all problems with folder structure and dependencies after moving on Maven.
    git-svn-id: http://go-java-1.googlecode.com/svn@2204 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava1/dmytro.reydalov/module2/InterSim/src/main/java/ua/com/goit/gojava/solo307/intersim/domain/LoggerDomain.java
@@ -0,0 +1,9 @@
+package ua.com.goit.gojava.solo307.intersim.domain;
+
+import org.apache.log4j.Logger;
+
+public class LoggerDomain {
+
+	static final Logger domainLogger = Logger.getLogger(LoggerDomain.class);
+
+}
