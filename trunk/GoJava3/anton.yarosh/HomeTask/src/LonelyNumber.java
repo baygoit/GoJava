2cca75e5ec8b153adbff97b2e3a6e800cc67dfe9
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-19 13:03:52 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2240 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/anton.yarosh/HomeTask/src/LonelyNumber.java
+++ trunk/GoJava3/anton.yarosh/HomeTask/src/LonelyNumber.java
@@ -8,42 +8,42 @@
 import java.util.Arrays;
 
 public class LonelyNumber {
-	public static void main(String[] args) {
-		int lonelyNumber = 0;
-		int maxIndex = 0;
-		int m[] = {2, 3, 2, 4, 3, 7, 2, 3, 4, 4, 5, 5, 5, 7, 7, 50, 100, 100, 100};
+    public static void main(String[] args) {
+	int lonelyNumber = 0;
+	int maxIndex = 0;
+	int m[] = {2, 3, 2, 4, 3, 7, 2, 3, 4, 4, 5, 5, 5, 7, 7, 50, 100, 100, 100};
 
-		/* In massive rez bitwise sum will be kept*/
-		int rez[] = new int[20];
+	/* In massive rez bitwise sum will be kept*/
+	int rez[] = new int[20];
 
-		for (int i = 0; i < m.length; i++) {
-			int j = 0;
-			int k = m[i];
-			while (k != 0) {
-				if (j >= rez.length - 1) {
+	for (int i = 0; i < m.length; i++) {
+	    int j = 0;
+	    int k = m[i];
+	    while (k != 0) {
+		if (j >= rez.length - 1) {
 
-					/*if max number from initial massive in binary format 
-					 * has more digits than rez length, length of rez will be doubled
-					 */
-					rez = Arrays.copyOf(rez, rez.length * 2);
-				}
-
-				/*convert numbers to binary format*/
-				rez[j] = rez[j] + k % 2;
-				k = k / 2;
-				j++;
-			}
-			if (j > maxIndex) {
-
-				/*keep max digit index*/
-				maxIndex = j;
-			}
+		    /*if max number from initial massive in binary format 
+		     * has more digits than rez length, length of rez will be doubled
+		     */
+		    rez = Arrays.copyOf(rez, rez.length * 2);
 		}
 
-		/*lonely number's back conversion to decimal format */
-		for (int i = 0; i <= maxIndex - 1; i++) {
-			lonelyNumber = (int) (lonelyNumber + (rez[i] % 3 * Math.pow(2, i)));
-		}
-		System.out.println(lonelyNumber);
+		/*convert numbers to binary format*/
+		rez[j] = rez[j] + k % 2;
+		k = k / 2;
+		j++;
+	    }
+	    if (j > maxIndex) {
+
+		/*keep max digit index*/
+		maxIndex = j;
+	    }
 	}
+
+	/*lonely number's back conversion to decimal format */
+	for (int i = 0; i <= maxIndex - 1; i++) {
+	    lonelyNumber = (int) (lonelyNumber + (rez[i] % 3 * Math.pow(2, i)));
+	}
+	System.out.println(lonelyNumber);
+    }
 }
