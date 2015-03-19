23f89c72e151c3ced3b1313e4477d8f7bde87606
ikucherenko128@gmail.com <ikucherenko128@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-19 13:19:43 +0000

    Rotated arrays v2
    git-svn-id: http://go-java-1.googlecode.com/svn@2241 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/ivan.kucherenko/RotatedArrays.java
+++ trunk/GoJava3/ivan.kucherenko/RotatedArrays.java
@@ -1,44 +1,45 @@
+package ua.goit.alg;
+
+/*You have a sorted array of N length which was rotated, e.g. [4, 5, 6, 7, 8, 9, 10, 1, 2, 3].
+*Rotated means that we move first M numbers to the end.
+*You need to find index of target number using binary search.
+*In case there is no such number in array, return -1;
+*All numbers in array are unique.
+*Use TDD approach.
+*
+*0<=N
+*0<=M<=N
+*/
+
 public class RotatedArrays {
-
-	final static int FIRST_INDEX = 0;
-
     public static int binarySearch(int[] array, int target) {
-    	if (array.length == 0){
-    		return -1;
-    	}
-    	int lastIndex = array.length -1;
-    	int middle = FIRST_INDEX + (lastIndex-FIRST_INDEX)/2;
-    	if (array[middle] == target){
-    		return middle;
-    	}
-    	int leftSearchRes = search(array, target, FIRST_INDEX, middle -1);
-    	int rightSearchRes = search(array, target, middle+1, lastIndex); 
-    	
-    	if (leftSearchRes != -1 && target == array[leftSearchRes]){
-    		return leftSearchRes;
-    	} else if (rightSearchRes != -1 && target == array[rightSearchRes]) {
-    		return rightSearchRes;
-    	}
-    	return -1;
-  }
-    public static int search(int[]array, int target, int firstIndex, int lastIndex){
-    	
-    	if (firstIndex > lastIndex){
-    		return -1;
-    	}
-    	int middle = (firstIndex+lastIndex)/2;
-    	if (target == array[middle]) {
-    		return middle;
-    	} else if (target == array[firstIndex]){
-    		return firstIndex;
-    	} else if (middle != 0 && target == array[middle-1]) {
-    		return middle-1;
-    	}else if (target < array[middle]){
-    		return search(array, target, firstIndex, middle-1);
-    	} else if (target > array[middle]){
-    		return search(array, target, middle+1, lastIndex);
-    	} else {
-    		return -1; 
-    	}
+        return splitAndSearch(target, array, 0, array.length-1);
     }
-   }
+  
+    private static int splitAndSearch(int target, int[]array, int firstIndex, int lastIndex) {
+      if (firstIndex > lastIndex){
+        	return -1;
+      }
+        int middle = firstIndex + (lastIndex - firstIndex) / 2;
+         
+     if (firstIndex == lastIndex && firstIndex < target){
+        	return -1;
+        } else if (target == array[firstIndex]){
+        	return firstIndex;
+        } else if (target == array[lastIndex]){
+        	return lastIndex;
+        } else if (target == array[middle]){
+        	return middle;
+        } else if (target <= array[middle -1] && target >= array[firstIndex]) {
+            return splitAndSearch(target, array, firstIndex, middle - 1);
+        } else if (target >= array[middle+1] && target <= array[lastIndex]) {
+            return splitAndSearch(target, array, middle + 1, lastIndex);
+        } else if (target > array[firstIndex] && target <= array[middle-1]){
+            return splitAndSearch(target, array, firstIndex, middle - 1);
+        } else if (target >= array[middle +1] && target >= array[firstIndex]){
+        	return splitAndSearch(target, array, middle + 1, lastIndex);
+        } else {
+        	return splitAndSearch(target, array, middle + 1, lastIndex);
+      }
+     }
+    }
--- trunk/GoJava3/ivan.kucherenko/RotatedArraysTest.java
+++ trunk/GoJava3/ivan.kucherenko/RotatedArraysTest.java
@@ -1,78 +1,127 @@
+package ua.goit.alg;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
 /*You have a sorted array of N length which was rotated, e.g. [4, 5, 6, 7, 8, 9, 10, 1, 2, 3].
-Rotated means that we move first M numbers to the end.
-You need to find index of target number using binary search.
-In case there is no such number in array, return -1;
-All numbers in array are unique.
-Use TDD approach.
+*Rotated means that we move first M numbers to the end.
+*You need to find index of target number using binary search.
+*In case there is no such number in array, return -1;
+*All numbers in array are unique.
+*Use TDD approach.
+*
+*0<=N
+*0<=M<=N
 */
 
 public class RotatedArraysTest {
-	
-	@Test
-	public void testFirst() {
-		int[] testArray = {4,5,6,7,8,9,10,1,2,3,4};
-		int actualResult = RotatedArrays.binarySearch(testArray,5);
-		int expectedresult = 1;
-		assertEquals(actualResult, expectedresult);	;
-	}
-	
-	@Test
-	public void testLeftSide(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3,4};
-		int actualResult = RotatedArrays.binarySearch(testArray, 9);
-		int expectedresult = 5;
-		assertEquals(actualResult,expectedresult);
-	}
-	
-	@Test
-	public void testRightSide(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3};
-		int actualResult = RotatedArrays.binarySearch(testArray, 11);
-		int expectedresult = 7;
-		assertEquals(actualResult,expectedresult);
-	}
-	@Test
-	public void testIsEmpty(){
-		int[] testArray = {};
-		int actualResult = RotatedArrays.binarySearch(testArray, 11);
-		int expectedresult = -1;
-		assertEquals(actualResult,expectedresult);
-	}
-	
-	@Test
-	public void testNoNumber(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3};
-		int actualResult = RotatedArrays.binarySearch(testArray, 21);
-		int expectedresult = -1;
-		assertEquals(actualResult,expectedresult);
-	}
-	
-	@Test
-	public void testIfMiddle(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3};
-		int actualResult = RotatedArrays.binarySearch(testArray, 1);
-		int expectedresult = 8;
-		assertEquals(actualResult,expectedresult);
-	}
-	
-	@Test
-	public void testIfMidle2(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3};
-		int actualResult = RotatedArrays.binarySearch(testArray, 6);
-		int expectedresult = 2;
-		assertEquals(actualResult,expectedresult);
-	}
-	
-	@Test
-	public void testIfLast(){
-		int[] testArray = {4,5,6,7,8,9,10,11,1,2,3};
-		int actualResult = RotatedArrays.binarySearch(testArray, 3);
-		int expectedresult = 10;
-		assertEquals(actualResult,expectedresult);
-	}
-	
+
+    @Test
+    public void test1() {
+        int expected;
+        int actual;
+        expected = 1;
+        actual = RotatedArrays.binarySearch(
+                new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 5);
+        assertEquals(expected, actual);
+        }
+    @Test
+    public void test2() {
+    	int expected = 4;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] {4, 5, 6, 7, 8, 1, 2, 3 }, 8);
+    	assertEquals(expected, actual);
+    	}
+    @Test
+    public void test3() {
+    	int expected = 0;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 4);
+    	assertEquals(expected, actual);
+    	}
+    @Test
+    public void test4() {
+    	int expected = 5;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 1);
+    	assertEquals(expected, actual);
+    	}
+    @Test
+    public void test5() {
+    	int expected = 6;
+        int actual = RotatedArrays.binarySearch(
+                new int[] { 4, 5, 6, 7, 8, 1, 2, 3 }, 2);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test6() {
+    	 int expected = -1;
+         int actual = RotatedArrays.binarySearch(new int[] { 4, 5, 6, 7, 1, 2, 3 },
+                 8);
+         assertEquals(expected, actual);
+    }
+    @Test
+    public void test7() {
+    	int expected = 4;
+    	int actual = RotatedArrays.binarySearch(new int[] { 4, 5, 7, 8, 2, 3 }, 2);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test8() {
+    	int expected = 2;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 6, 7, 8, 1, 2, 3, 4, 5 }, 8);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test9() {
+    	int expected = 1;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 4, 5, 6, 7, 8, 9, 10, 1, 2, 3 }, 5);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test10() {
+    	int expected = 5;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 4, 5, 6, 7, 8, 9, 10, 1, 2, 3 }, 9);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test11() { 
+    	int expected = 3;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] { 4, 5, 6, 7, 8, 9, 10, 1, 2, 3 }, 7);
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void test12() { 
+    	int expected = 1;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] {2,1}, 1);
+    	assertEquals(expected, actual);
+    }
+    
+    @Test
+    public void test13() { 
+    	int expected = 2;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] {3, 1, 2}, 2);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test14() { 
+    	int expected = 2;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] {2, 3, 1}, 1);
+    	assertEquals(expected, actual);
+    }
+    @Test
+    public void test15() { 
+    	int expected = 0;
+    	int actual = RotatedArrays.binarySearch(
+            new int[] {1, 2, 3}, 1);
+    	assertEquals(expected, actual);
+    }
+
 }
