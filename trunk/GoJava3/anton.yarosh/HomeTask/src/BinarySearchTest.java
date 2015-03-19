492ac5b81ae9aba35b2bad001baf4ad7ab98f8ba
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-19 13:02:45 +0000

    Updated code format
    git-svn-id: http://go-java-1.googlecode.com/svn@2239 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/anton.yarosh/HomeTask/src/BinarySearchTest.java
+++ trunk/GoJava3/anton.yarosh/HomeTask/src/BinarySearchTest.java
@@ -5,77 +5,77 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-	@Test
-	public void Element0Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 15);
-		int expectedResult = 0;
-		assertEquals(expectedResult, actualResult);
+    @Test
+    public void element0Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 15);
+	int expectedResult = 0;
+	assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void element1Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 16);
+	int expectedResult = 1;
+	assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void element2Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 2);
+	int expectedResult = 2;
+	assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void element3Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 5);
+	int expectedResult = 3;
+	assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void element4Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 7);
+	int expectedResult = 4;
+	assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void element5Test() {
+	int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 9);
+	int expectedResult = 5;
+	assertEquals(expectedResult, actualResult);
+    }
+
+
+    @Test
+    public void allElementTest() {								 // min element < middle element
+	int[] testMass = {26, 27, 29, 8, 10, 15, 16, 17, 25}; 
+	for (int i = 0; i < testMass.length; i++) {
+	    int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
+	    int expectedResult = i;
+	    assertEquals(expectedResult, actualResult);
+	}
+    }
+
+    @Test
+    public void allElementTest1() {                              // min element > middle element
+	int[] testMass = {5, 7, 8, 9, 11, 12, 15, 1, 2}; 
+	for (int i = 0; i < testMass.length; i++) {
+	    int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
+	    int expectedResult = i;
+	    assertEquals(expectedResult, actualResult);
+	}
+    }
+
+    public void allElementTest2() {                              //middle shift                      
+	int[] testMass = {6, 7, 8, 9, 1, 2, 3, 4, 5}; 
+	for (int i = 0; i < testMass.length; i++) {
+	    int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
+	    int expectedResult = i;
+	    assertEquals(expectedResult, actualResult);
 	}
 
-	@Test
-	public void Element1Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 16);
-		int expectedResult = 1;
-		assertEquals(expectedResult, actualResult);
-	}
-	
-	@Test
-	public void Element2Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 2);
-		int expectedResult = 2;
-		assertEquals(expectedResult, actualResult);
-	}
-	
-	@Test
-	public void Element3Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 5);
-		int expectedResult = 3;
-		assertEquals(expectedResult, actualResult);
-	}
-	
-	@Test
-	public void Element4Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 7);
-		int expectedResult = 4;
-		assertEquals(expectedResult, actualResult);
-	}
-	
-	@Test
-	public void Element5Test() {
-		int actualResult = BinarySearch.returnIndex(new int[]{15, 16, 2, 5, 7, 9}, 9);
-		int expectedResult = 5;
-		assertEquals(expectedResult, actualResult);
-	}
-	
-
-	@Test
-	public void allElementTest() {								 // min element < middle element
-		int[] testMass = {26, 27, 29, 8, 10, 15, 16, 17, 25}; 
-		for (int i = 0; i < testMass.length; i++) {
-			int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
-			int expectedResult = i;
-			assertEquals(expectedResult, actualResult);
-		}
-	}
-
-	@Test
-	public void allElementTest1() {                              // min element > middle element
-		int[] testMass = {5, 7, 8, 9, 11, 12, 15, 1, 2}; 
-		for (int i = 0; i < testMass.length; i++) {
-			int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
-			int expectedResult = i;
-			assertEquals(expectedResult, actualResult);
-		}
-	}
-
-	public void allElementTest2() {                              //middle shift                      
-		int[] testMass = {6, 7, 8, 9, 1, 2, 3, 4, 5}; 
-		for (int i = 0; i < testMass.length; i++) {
-			int actualResult = BinarySearch.returnIndex(testMass, testMass[i]);
-			int expectedResult = i;
-			assertEquals(expectedResult, actualResult);
-		}
-
-	}
+    }
 
 }
