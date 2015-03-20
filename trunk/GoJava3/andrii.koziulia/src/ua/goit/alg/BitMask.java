2f8964b3fbee0dd496eca32058318a4a1b47ef2d
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-20 23:25:44 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2291 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/andrii.koziulia/src/ua/goit/alg/BitMask.java
@@ -0,0 +1,25 @@
+package ua.goit.alg;
+
+/**
+ * Created by Alex on 20.03.2015.
+ */
+public class BitMask {
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2, 5));
+    }
+
+    public static long myPow(long root, long degree) {
+        long mask = 1;
+        long result = 1;
+        long localPow = root;
+        for (int i=0;i<32;i++) {
+            if ((degree & mask) == mask) {
+                result *= localPow;
+            }
+            localPow *= localPow;
+            mask <<= 1;
+        }
+        return result;
+    }
+}
