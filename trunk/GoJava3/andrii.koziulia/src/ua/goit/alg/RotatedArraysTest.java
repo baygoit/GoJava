1e57ce2b34b24d12c36476377d2369cf55bd06e3
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-20 23:25:24 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2290 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/andrii.koziulia/src/ua/goit/alg/RotatedArraysTest.java
+++ trunk/GoJava3/andrii.koziulia/src/ua/goit/alg/RotatedArraysTest.java
@@ -1,72 +1,73 @@
 package ua.goit.alg;
 
 import junit.framework.TestCase;
+
 import java.util.Arrays;
 
 public class RotatedArraysTest extends TestCase {
 
-    public void testAllPossible10Arrays() {
-        int[] array = new int[10];
-        for (int j = 0; j < 10; j++) {
-            for (int i = 0; i < 10; i++) {
-                array[i] = (i + j) % 10;
-            }
-            for (int i = 0; i < 10; i++) {
-                int actual = RotatedArrays.binarySearch(array, i);
-                int expected = (i + j * 9) % 10;
-                if (actual != expected) {
-                    System.out.println(Arrays.toString(array));
-                    System.out.println("Value searched: " + i + ". Expected value: " + expected + ", search provided the following result: " + actual);
-                }
-                assertEquals(expected, actual);
-            }
+  public void testAllPossible10Arrays() {
+    int[] array = new int[10];
+    for (int j = 0; j < 10; j++) {
+      for (int i = 0; i < 10; i++) {
+        array[i] = (i + j) % 10;
+      }
+      for (int i = 0; i < 10; i++) {
+        int actual = RotatedArrays.binarySearch(array, i);
+        int expected = (i + j * 9) % 10;
+        if (actual != expected) {
+          System.out.println(Arrays.toString(array));
+          System.out.println("Value searched: " + i + ". Expected value: " + expected + ", search provided the following result: " + actual);
         }
+        assertEquals(expected, actual);
+      }
     }
+  }
 
-    public void testEmptyArray() {
-        int[] array = new int[0];
-        int actual = RotatedArrays.binarySearch(array, 1);
-        int expected = -1;
-        assertEquals(expected, actual);
-    }
+  public void testEmptyArray() {
+    int[] array = new int[0];
+    int actual = RotatedArrays.binarySearch(array, 1);
+    int expected = -1;
+    assertEquals(expected, actual);
+  }
 
-    public void testSingleIntegerArray() {
-        int[] array = {5};
-        int actual = RotatedArrays.binarySearch(array, 5);
-        int expected = 0;
-        assertEquals(expected, actual);
-        actual = RotatedArrays.binarySearch(array, 3);
-        expected = -1;
-        assertEquals(expected, actual);
-    }
+  public void testSingleIntegerArray() {
+    int[] array = {5};
+    int actual = RotatedArrays.binarySearch(array, 5);
+    int expected = 0;
+    assertEquals(expected, actual);
+    actual = RotatedArrays.binarySearch(array, 3);
+    expected = -1;
+    assertEquals(expected, actual);
+  }
 
-    public void testTwoIntegerArray() {
-        int[] array = {1, 2};
-        int actual = RotatedArrays.binarySearch(array, 1);
-        int expected = 0;
-        assertEquals(expected, actual);
-        actual = RotatedArrays.binarySearch(array, 2);
-        expected = 1;
-        assertEquals(expected, actual);
-    }
+  public void testTwoIntegerArray() {
+    int[] array = {1, 2};
+    int actual = RotatedArrays.binarySearch(array, 1);
+    int expected = 0;
+    assertEquals(expected, actual);
+    actual = RotatedArrays.binarySearch(array, 2);
+    expected = 1;
+    assertEquals(expected, actual);
+  }
 
-    public void testReverseTwoIntegerArray() {
-        int[] array = {2, 1};
-        int actual = RotatedArrays.binarySearch(array, 1);
-        int expected = 1;
-        assertEquals(expected, actual);
-        actual = RotatedArrays.binarySearch(array, 2);
-        expected = 0;
-        assertEquals(expected, actual);
-    }
+  public void testReverseTwoIntegerArray() {
+    int[] array = {2, 1};
+    int actual = RotatedArrays.binarySearch(array, 1);
+    int expected = 1;
+    assertEquals(expected, actual);
+    actual = RotatedArrays.binarySearch(array, 2);
+    expected = 0;
+    assertEquals(expected, actual);
+  }
 
-    public void testNoSuchElement() {
-        int[] array = {7, 8, 9, 1, 2, 3, 4, 5, 6};
-        int actual = RotatedArrays.binarySearch(array, 10);
-        int expected = -1;
-        assertEquals(expected, actual);
-        actual = RotatedArrays.binarySearch(array, -1);
-        expected = -1;
-        assertEquals(expected, actual);
-    }
+  public void testNoSuchElement() {
+    int[] array = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+    int actual = RotatedArrays.binarySearch(array, 10);
+    int expected = -1;
+    assertEquals(expected, actual);
+    actual = RotatedArrays.binarySearch(array, -1);
+    expected = -1;
+    assertEquals(expected, actual);
+  }
 }
