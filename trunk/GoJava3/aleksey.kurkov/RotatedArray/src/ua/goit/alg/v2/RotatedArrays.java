83bea9407a7d287ca71b1d5edefeebff5cddaa54
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-20 20:06:49 +0000

    re-implemented RotatedArrays version 2 (without searching pivot)
    git-svn-id: http://go-java-1.googlecode.com/svn@2287 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/aleksey.kurkov/RotatedArray/src/ua/goit/alg/v2/RotatedArrays.java
@@ -0,0 +1,87 @@
+/**
+ * Created by Aleksey Kurkov on 20.03.15.
+ */
+
+package ua.goit.alg.v2;
+
+public class RotatedArrays {
+  public static int binarySearch(int[] array, int target, int start, int end) {
+    int middle;
+    // array contains 0 items
+    if (array.length == 0) {
+      return -1;
+    }
+    // continue searching while [start,end] is not empty
+    while (end >= start) {
+      // calculate the midpoint for roughly equal partition
+      if (end == start) {
+        middle = start;
+      } else {
+        middle = midpoint(end, start);
+      }
+      if (array[middle] == target) {
+        // target found at index middle
+        return middle;
+      } else if ((middle - 1 > 0) && (array[middle - 1] == target)) {
+        // target found at index middle - 1
+        return middle - 1;
+      } else if ((middle + 1 < array.length - 1) && (array[middle + 1] == target)) {
+        // target found at index middle + 1
+        return middle + 1;
+      } else if (array[start] == target) {
+        // target found at index start
+        return start;
+      } else if (array[start + 1] == target) {
+        // target found at index start + 1
+        return start + 1;
+      } else if (array[end] == target) {
+        // target found at index end
+        return end;
+      } else if (array[end - 1] == target) {
+        // target found at index end - 1
+        return end - 1;
+      }
+      // determine which subarray to search
+      else if (target > array[middle]) {
+        if (target > array[end - 1]) {
+          if (start != middle) {
+            // look at the left part
+            start = middle;
+          } else {
+            // target was not found
+            return -1;
+          }
+        } else {
+          // change start to search upper subarray
+          start++;
+        }
+      } else if (target < array[middle]) {
+        if (target < array[start + 1]) {
+          if (start != middle) {
+            // look at the right part
+            start = middle;
+          } else {
+            // target was not found
+            return -1;
+          }
+        } else {
+          // change end to search lower subarray
+          end--;
+        }
+      } else {
+        // target was not found
+        return -1;
+      }
+    }
+    // target was not found
+    return -1;
+  }
+
+  private static int midpoint(int start, int end) {
+    return (start / 2 + end / 2);
+  }
+
+  public static int binaryModifiedSearch(int[] array, int target) {
+    return binarySearch(array, target, 0, array.length - 1);
+  }
+}
