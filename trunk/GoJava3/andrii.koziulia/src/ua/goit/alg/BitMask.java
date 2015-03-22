e03017add2e74f4c6816a9ea2095e508afdce6de
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 16:50:41 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2353 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/andrii.koziulia/src/ua/goit/alg/BitMask.java
+++ trunk/GoJava3/andrii.koziulia/src/ua/goit/alg/BitMask.java
@@ -6,7 +6,7 @@ package ua.goit.alg;
 public class BitMask {
 
     public static void main(String[] args) {
-        System.out.println(myPow(2, 5));
+        System.out.println(myPow(2, 8));
     }
 
     public static long myPow(long root, long degree) {
