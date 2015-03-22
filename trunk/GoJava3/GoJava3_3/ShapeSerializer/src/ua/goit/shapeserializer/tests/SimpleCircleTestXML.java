410ebf60625405078375940fd0e6894db569a823
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 18:41:36 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2360 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleCircleTestXML.java
@@ -0,0 +1,26 @@
+package ua.goit.shapeserializer.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ua.goit.shapeserializer.basicobjects.Point;
+import ua.goit.shapeserializer.basicobjects.Shape;
+import ua.goit.shapeserializer.main.Factory;
+import ua.goit.shapeserializer.serializers.SerializeFor;
+
+
+public class SimpleCircleTestXML {
+
+  @Test
+  public void test() {
+    Shape circle = Factory.createObject("Circle", null, 5, new Point(50,
+        50));
+    String expected = "<Circle><radius>5</radius><center><x>50</x><y>50</y></center></Circle>";
+    SerializeFor xml = Factory.getSerializerFor("XML");
+    String actual = xml.serialize(circle);
+    assertEquals(expected, actual);
+    
+  }
+
+}
