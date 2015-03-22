3607e71d902bd63449e5fef7e1e52fc42ad4decb
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 22:50:48 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2381 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleSquareTestXML.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleSquareTestXML.java
@@ -16,7 +16,7 @@ public class SimpleSquareTestXML {
   public void test() {
     Shape square = Factory.createObject("Square", null, 0, new Point(30,
         30), new Point(40, 40));
-    String expected = "<Square><a><x>30</x><y>30</y></a><b><x>40</x><y>40</y></b></Square>";
+    String expected = "<square><point><x>30</x><y>30</y></point><point><x>40</x><y>40</y></point></square>";
     SerializerFor xml = Factory.getSerializerFor("XML");
     String actual = xml.serialize(square);
     assertEquals(expected, actual);
--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleTriangleTestXML.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleTriangleTestXML.java
@@ -16,7 +16,7 @@ public class SimpleTriangleTestXML {
   public void test() {
     Shape triangle1 = Factory.createObject("Triangle", null, 0, new Point(30,
         30), new Point(30, 40), new Point(40, 30));
-    String expected = "<Triangle><a><x>30</x><y>30</y></a><b><x>30</x><y>40</y></b><c><x>40</x><y>30</y></c></Triangle>";
+    String expected = "<triangle><point><x>30</x><y>30</y></point><point><x>30</x><y>40</y></point><point><x>40</x><y>30</y></point></triangle>";
     SerializerFor xml = Factory.getSerializerFor("XML");
     String actual = xml.serialize(triangle1);
     assertEquals(expected, actual);
