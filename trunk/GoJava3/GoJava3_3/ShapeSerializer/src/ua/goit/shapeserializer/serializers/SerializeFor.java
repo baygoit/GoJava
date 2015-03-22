d222f9e9068b3839c8802ab91c4c90ff21934695
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 14:06:22 +0000

    serializers initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@2333 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/SerializeFor.java
@@ -0,0 +1,8 @@
+package ua.goit.shapeserializer.serializers;
+
+/**
+ * Created by Aleksey Kurkov on 22.03.15.
+ */
+public interface SerializeFor {
+  String serialize(Shape shape);
+}
---
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/XMLSerializer.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Aleksey Kurkov on 22.03.15.
+ */
+
+package ua.goit.shapeserializer.serializers;
+
+public class XMLSerializer implements SerializeFor {
+  @Override
+  public String serialize(Shape shape) {
+    return null;
+  }
+}
