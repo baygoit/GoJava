351d2fa5047578b5acc52555b3f03aa35f66c024
ZaRkonUA@gmail.com <ZaRkonUA@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 17:34:17 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2354 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/oleksandr.furman/module2/Kickstarter/src/main/java/ua/com/goit/gojava/alexfurman/kickstarter/repository/QuoteRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.com.goit.gojava.alexfurman.kickstarter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ua.com.goit.gojava.alexfurman.kickstarter.entity.Quote;
-
-public interface QuoteRepository extends JpaRepository<Quote, Integer>{
-
-}
