cbe2d79e1b3031d87bd5c253f8d3647885bd555b
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 20:41:39 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2374 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/XMLSquareSerializer.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/XMLSquareSerializer.java
@@ -5,10 +5,18 @@
 package ua.goit.shapeserializer.serializers;
 
 import ua.goit.shapeserializer.basicobjects.Shape;
+import ua.goit.shapeserializer.basicobjects.Square;
 
 public class XMLSquareSerializer extends XMLShapeSerializer {
   @Override
-  public String serialize(Shape circle) {
-    return null;
+  public String serialize(Shape shape) {
+    StringBuilder squareXML = new StringBuilder();
+    Square square = (Square) shape;
+    squareXML.append("<square><point><x>");
+    squareXML.append(square.getA().getX() + "</x><y>");
+    squareXML.append(square.getA().getY() + "</y></point><point><x>");
+    squareXML.append(square.getB().getX() + "</x><y>");
+    squareXML.append(square.getB().getY() + "</y></point><square>");
+    return squareXML.toString();
   }
 }
