7df4368c736d3545f722f231d40cdfa942b1eafa
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 20:03:02 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2371 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/XMLCircleSerializer.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/serializers/XMLCircleSerializer.java
@@ -4,11 +4,18 @@
 
 package ua.goit.shapeserializer.serializers;
 
+import ua.goit.shapeserializer.basicobjects.Circle;
 import ua.goit.shapeserializer.basicobjects.Shape;
 
 public class XMLCircleSerializer extends XMLShapeSerializer {
   @Override
-  public String serialize(Shape circle) {
-    return null;
+  public String serialize(Shape shape) {
+    StringBuilder circleXML = new StringBuilder();
+    Circle circle = (Circle) shape;
+    circleXML.append("<circle><center><point><x>");
+    circleXML.append(circle.getCenter().getX() + "</x><y>");
+    circleXML.append(circle.getCenter().getY());
+    circleXML.append("</y></point></center><radius>");
+    return circleXML.toString();
   }
 }
