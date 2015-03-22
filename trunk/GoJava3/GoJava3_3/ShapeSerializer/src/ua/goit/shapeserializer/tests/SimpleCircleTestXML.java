0da95170637f7c7cb145a4b702054b6408aecee1
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 22:17:57 +0000

    test2
    git-svn-id: http://go-java-1.googlecode.com/svn@2377 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleCircleTestXML.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleCircleTestXML.java
@@ -1,13 +1,15 @@
 package ua.goit.shapeserializer.tests;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
+import ua.goit.shapeserializer.basicobjects.Circle;
 import ua.goit.shapeserializer.basicobjects.Point;
 import ua.goit.shapeserializer.basicobjects.Shape;
 import ua.goit.shapeserializer.main.Factory;
 import ua.goit.shapeserializer.serializers.SerializerFor;
+import ua.goit.shapeserializer.serializers.XMLCircleSerializer;
+import ua.goit.shapeserializer.serializers.XMLShapeSerializer;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class SimpleCircleTestXML {
@@ -21,6 +23,15 @@ public class SimpleCircleTestXML {
     String actual = xml.serialize(circle);
     assertEquals(expected, actual);
     
+  }
+
+  @Test
+  public void test2() {
+    String expected = "<Circle><radius>5</radius><center><x>50</x><y>50</y></center></Circle>";
+    XMLShapeSerializer circle = new XMLCircleSerializer();
+    String actual = circle.serialize(new Circle(5, new Point(50,50)));
+    assertEquals(expected, actual);
+
   }
 
 }
