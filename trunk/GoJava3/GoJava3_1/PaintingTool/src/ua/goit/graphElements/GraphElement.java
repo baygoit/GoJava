5a9c7b930a6158998607e7acae33edd71b651274
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 15:04:57 +0000

    Added abstract method ArrayList <Point> getPoints();
    git-svn-id: http://go-java-1.googlecode.com/svn@2341 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/GraphElement.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/GraphElement.java
@@ -1,5 +1,8 @@
 package ua.goit.graphElements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public abstract class GraphElement implements Iterable <GraphElement> {
     protected String name;
     public GraphElement(String name) {
@@ -9,5 +12,6 @@ public abstract class GraphElement implements Iterable <GraphElement> {
     public abstract void add(GraphElement element);
     public abstract String getName();
     public abstract boolean isElement();
-    public abstract String getType();    
+    public abstract String getType();
+    public abstract ArrayList <Point> getPoints();
 }
