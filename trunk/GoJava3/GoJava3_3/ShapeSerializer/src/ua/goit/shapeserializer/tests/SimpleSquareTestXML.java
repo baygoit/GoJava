120bbe6b2a3550f4090cc87062ff24c15ec051f0
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-22 18:46:31 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2362 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleSquareTestXML.java
+++ trunk/GoJava3/GoJava3_3/ShapeSerializer/src/ua/goit/shapeserializer/tests/SimpleSquareTestXML.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 import ua.goit.shapeserializer.basicobjects.Point;
 import ua.goit.shapeserializer.basicobjects.Shape;
 import ua.goit.shapeserializer.main.Factory;
-import ua.goit.shapeserializer.serializers.SerializeFor;
+import ua.goit.shapeserializer.serializers.SerializerFor;
 
 
 public class SimpleSquareTestXML {
@@ -17,7 +17,7 @@ public class SimpleSquareTestXML {
     Shape square = Factory.createObject("Square", null, 0, new Point(30,
         30), new Point(40, 40));
     String expected = "<Square><a><x>30</x><y>30</y></a><b><x>40</x><y>40</y></b></Square>";
-    SerializeFor xml = Factory.getSerializerFor("XML");
+    SerializerFor xml = Factory.getSerializerFor("XML");
     String actual = xml.serialize(square);
     assertEquals(expected, actual);
     
