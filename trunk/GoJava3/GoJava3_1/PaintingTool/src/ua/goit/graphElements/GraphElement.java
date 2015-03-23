d59d05aaa976ae8090735495d0f35e6c13ec7242
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 22:17:53 +0000

    without Composite pattern
    git-svn-id: http://go-java-1.googlecode.com/svn@2465 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/GraphElement.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/GraphElement.java
@@ -9,10 +9,8 @@ public abstract class GraphElement implements Iterable <GraphElement> {
 	this.name = name;
     }
 
-    public abstract void add(GraphElement element);
     public abstract String getName();
     public abstract boolean isElement();
     public abstract String getType();
-    public abstract ArrayList <Point> getPoints();
-    public abstract void setPoints(ArrayList <Point> pointsList);
+   
 }
