a6ab819a50d29bef5325d3d7556c212f1d47d8a6
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 10:19:15 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2399 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/managers/SerializerFactory.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/managers/SerializerFactory.java
@@ -3,7 +3,7 @@ package ua.goit.managers;
 public class SerializerFactory {
 
   public static Serializer getSerializer(SerializerType type) {
-    if (type==SerializerType.SML){
+    if (type==SerializerType.XML){
       return new XMLSerializer();
     } else if (type==SerializerType.JSON){
       return new JSONSerializer();
