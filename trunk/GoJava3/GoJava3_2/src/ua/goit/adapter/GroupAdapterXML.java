f2298d5a5c463bad694b2ce6afbb0aa39046f551
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 08:08:28 +0000

    format
    git-svn-id: http://go-java-1.googlecode.com/svn@2385 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/GroupAdapterXML.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/GroupAdapterXML.java
@@ -4,23 +4,24 @@ import ua.goit.managers.*;
 import ua.goit.model.*;
 
 public class GroupAdapterXML {
-	Serializer serializer;
-	StringBuilder result = new StringBuilder("");
-	public String serialize(ContainerShapes container){
-		GroupShapes group = (GroupShapes) container;
-		result.append("<group>");
-		for (int i = 0; i < group.size(); i++){
-			
-			if (group.get(i) == null){
-				result.append("<null></null>"); 
-			} else if (group.get(i).getType() == Types.GROUP){
-				result.append(serialize(group.get(i)));
-			} else {
-				serializer = SerializerFactory.getSerializer("xml");
-				result.append(serializer.serialize(container));
-			}
-		}
-		result.append("</group>");
-		return result.toString();
-	}
+  Serializer serializer;
+  StringBuilder result = new StringBuilder("");
+
+  public String serialize(ContainerShapes container) {
+    GroupShapes group = (GroupShapes) container;
+    result.append("<group>");
+    for (int i = 0; i < group.size(); i++) {
+
+      if (group.get(i) == null) {
+        result.append("<null></null>");
+      } else if (group.get(i).getType() == Types.GROUP) {
+        result.append(serialize(group.get(i)));
+      } else {
+        serializer = SerializerFactory.getSerializer("xml");
+        result.append(serializer.serialize(container));
+      }
+    }
+    result.append("</group>");
+    return result.toString();
+  }
 }
