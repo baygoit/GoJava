34fb919be0491ac04a3ffaea16266c65ec3c6ddc
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 08:49:19 +0000

    serializers
    git-svn-id: http://go-java-1.googlecode.com/svn@2386 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/managers/JSONSerializer.java
@@ -0,0 +1,13 @@
+package ua.goit.managers;
+
+import ua.goit.model.ContainerShapes;
+
+public class JSONSerializer implements Serializer {
+
+  @Override
+  public String serialize(ContainerShapes object) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
--- trunk/GoJava3/GoJava3_2/src/ua/goit/managers/SerializerFactory.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/managers/SerializerFactory.java
@@ -2,9 +2,14 @@ package ua.goit.managers;
 
 public class SerializerFactory {
 
-    public static Serializer getSerializer(String string) {
-        // TODO Auto-generated method stub
-        return null;
+  public static Serializer getSerializer(String serializer) {
+    if (serializer.toLowerCase().equals("xml")){
+      return new XMLSerializer();
+    } else if (serializer.toLowerCase().equals("json")){
+      return new JSONSerializer();
+    } else {
+      return new XMLSerializer(); 
     }
 
+  }
 }
