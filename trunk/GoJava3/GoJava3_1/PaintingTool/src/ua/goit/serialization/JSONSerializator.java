72d519fb04eb88ed73ffa21d6d7e955230bca51f
kossovec@gmail.com <kossovec@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 15:43:15 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2451 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/JSONSerializator.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/JSONSerializator.java
@@ -1,19 +1,79 @@
 package ua.goit.serialization;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import ua.goit.graphElements.GraphElement;
+import ua.goit.graphElements.Point;
 
 public class JSONSerializator extends Serializator {
 
-    @Override
-    public StringBuffer serialize(GraphElement element) {
-	
-	return null;
+  private StringBuffer serializeString = new StringBuffer();
+  private StringBuffer tab = new StringBuffer();
+  private String oneTab = "    ";
+  private String enter = "\n";
+  private char dQ = '"';
+  private char openBracket = '{';
+  private char closeBracket = '}';
+  private char openBracketS = '[';
+  private char closeBracketS = ']';
+
+
+  @Override
+  public StringBuffer serialize(GraphElement element) {
+    if (element.isElement()) {
+      tab.append(oneTab);
+      bufAppend(tab.toString() + openBracket + enter);
+      bufAppend(tab.toString() + dQ + "Name" + dQ + " : "
+              + element.getName() + enter);
+      bufAppend(tab.toString() + dQ + "Type" + dQ + " : "
+              + element.getType() + enter);
+      bufAppend(tab.toString() + dQ + "Points" + dQ + " : ");
+      if (element.getPoints() != null) {
+        bufAppend(openBracketS + enter);
+        tab.append(oneTab);
+        for (Point point : element.getPoints()) {
+          bufAppend(tab.toString() + dQ + "Point" + dQ + " : "
+                  + point.getCoordinates() + enter);
+        }
+
+        tab.delete(0, 4);
+        bufAppend(tab.toString() + closeBracketS + enter);
+      }
+
+      bufAppend(tab.toString() + closeBracket + enter);
+      tab.delete(0, 4);
+    } else {
+      bufAppend(tab.toString() + element.getName() + " "
+              + openBracket + enter);
+      tab.append(oneTab);
+      for (GraphElement groupElement : element) {
+        serialize(groupElement);
+      }
+
+      tab.delete(0, 4);
+      bufAppend(tab.toString() + closeBracket + enter);
+
     }
 
-    @Override
-    public void saveToFile(StringBuffer source, File file) {
-	// TODO Auto-generated method stub
-	
+    return serializeString;
+  }
+
+  @Override
+  public void saveToFile(StringBuffer source, File file) {
+    try {
+      FileWriter fileForWrite = new FileWriter(file.getAbsoluteFile());
+      fileForWrite.write(source.toString());
+      fileForWrite.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+  }
+
+  private void bufAppend(String text) {
+    serializeString.append(text);
+  }
 
 }
