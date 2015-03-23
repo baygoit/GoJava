56438a3d3ca68dfedff2a02c199aec64dd277f6f
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 12:58:37 +0000

    add JSON adapter
    git-svn-id: http://go-java-1.googlecode.com/svn@2432 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/CircleAdapterJSON.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/CircleAdapterJSON.java
@@ -1,9 +1,14 @@
 package ua.goit.adapter;
 
 import ua.goit.managers.Serializer;
+import ua.goit.model.Circle;
 
 public class CircleAdapterJSON implements Serializer {
+  private Circle circle;
 
+  CircleAdapterJSON(Circle circle) {
+    this.circle = circle;
+  }
 
   @Override
   public String serialize() {
--- trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/SquareAdapterJSON.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/SquareAdapterJSON.java
@@ -1,9 +1,14 @@
 package ua.goit.adapter;
 
 import ua.goit.managers.Serializer;
+import ua.goit.model.Square;
 
 public class SquareAdapterJSON implements Serializer {
+  private Square square;
 
+  SquareAdapterJSON(Square square) {
+    this.square = square;
+  }
 
   @Override
   public String serialize() {
