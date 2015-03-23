aec862ae5f8162c2893f61e96e04cad57d8d38b1
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 10:37:18 +0000

    square
    git-svn-id: http://go-java-1.googlecode.com/svn@2404 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/model/Square.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/model/Square.java
@@ -3,56 +3,36 @@ package ua.goit.model;
 import java.awt.*;
 
 public class Square implements Shape {
-    private Point point1;
-    private Point point2;
-    private Point point3;
-    private Point point4;
-    private Types type = Types.SQUARE;
+  private Types type = Types.SQUARE;
+  private Point point1;
+  private int length;
 
-    Square() {
+  Square() {
 
-    }
+  }
 
-    Square(Point point1, Point point2, Point point3, Point point4) {
-        this.point1 = point1;
-        this.point2 = point2;
-        this.point3 = point3;
-        this.point4 = point4;
-    }
+  Square(Point point1, int length) {
+    this.point1 = point1;
+    this.length = length;
+  }
 
-    public Types getType() {
-        return type;
-    }
+  public Types getType() {
+    return type;
+  }
 
-    public Point getPoint1() {
-        return point1;
-    }
+  public Point getPoint1() {
+    return point1;
+  }
 
-    public void setPoint1(Point point1) {
-        this.point1 = point1;
-    }
+  public void setPoint1(Point point1) {
+    this.point1 = point1;
+  }
 
-    public Point getPoint2() {
-        return point2;
-    }
+  public int getLength() {
+    return length;
+  }
 
-    public void setPoint2(Point point2) {
-        this.point2 = point2;
-    }
-
-    public Point getPoint3() {
-        return point3;
-    }
-
-    public void setPoint3(Point point3) {
-        this.point3 = point3;
-    }
-
-    public Point getPoint4() {
-        return point4;
-    }
-
-    public void setPoint4(Point point4) {
-        this.point4 = point4;
-    }
+  public void setlength(int length) {
+    this.length = length;
+  }
 }
