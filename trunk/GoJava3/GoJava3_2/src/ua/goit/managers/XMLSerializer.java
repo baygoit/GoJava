9e82b4a57d624f188b7a2ab12d719387f6d3220d
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 10:21:57 +0000

    XMLSerializer
    git-svn-id: http://go-java-1.googlecode.com/svn@2400 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/managers/XMLSerializer.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/managers/XMLSerializer.java
@@ -1,13 +1,42 @@
 package ua.goit.managers;
 
-import ua.goit.model.ContainerShapes;
+import ua.goit.adapter.*;
+import ua.goit.model.*;
 
 public class XMLSerializer implements Serializer {
 
   @Override
   public String serialize(ContainerShapes object) {
+    if (object instanceof GroupShapes) {
+      return serialize((GroupShapes)object);
+    } else if (object instanceof Circle) {
+      return serialize((Circle)object);
+    } else if (object instanceof Square) {
+      return serialize((Square) object);
+    } else if (object instanceof Triangle) {
+      return serialize((Triangle) object);
+    }
+    return null;
+   }
+ 
+  public String serialize(GroupShapes object) {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  public String serialize(Triangle object) {
+    TriangleAdapterXML adapter = new TriangleAdapterXML(object);
+    return adapter.serialize();
+  }
+
+  public String serialize(Circle object) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  public String serialize(Square object) {
+    SquareAdapterXML adapter = new SquareAdapterXML(object);
+    return adapter.serialize();
   }
 
 }
