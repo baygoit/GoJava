a50204106451c16d933c4f4e4d7c8060d791a7b1
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 22:17:22 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2464 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/ElementBehavior.java
@@ -0,0 +1,9 @@
+package ua.goit.graphElements;
+
+import java.util.ArrayList;
+
+public interface ElementBehavior {
+
+    public ArrayList <Point> getPoints();
+    public void setPoints(ArrayList <Point> pointsList);
+}
