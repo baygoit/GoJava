9cdd19bb32d87eee746ecd9f0ba3db22764be4a4
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-23 09:39:25 +0000

    test merge
    git-svn-id: http://go-java-1.googlecode.com/svn@2394 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/CircleAdapterXML.java
+++ trunk/GoJava3/GoJava3_2/src/ua/goit/adapter/CircleAdapterXML.java
@@ -1,6 +1,12 @@
 package ua.goit.adapter;
 
 public class CircleAdapterXML {
-public int f = 2;
+    public int f = 2;
+
+    public String serialize() {
+        StringBuilder xml = new StringBuilder();
+
+        return xml.toString();
+    }
 
 }
