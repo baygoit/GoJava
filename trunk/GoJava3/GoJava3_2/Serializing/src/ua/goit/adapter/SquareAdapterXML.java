01a0d3f140098af666b55696f8b23362028b7f8e
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-24 14:58:18 +0000

    edit SquareAdapterXML
    git-svn-id: http://go-java-1.googlecode.com/svn@2523 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/SquareAdapterXML.java
+++ trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/SquareAdapterXML.java
@@ -5,21 +5,21 @@ import ua.goit.model.Square;
 import ua.goit.serializers.Serializer;
 
 public class SquareAdapterXML implements Serializer {
-  StringBuilder result = new StringBuilder();
 
-  public String serialize(ContainerShapes container) {
-    Square square = (Square) container;
-    result.append("<square>");
+    public String serialize(ContainerShapes container) {
+        StringBuilder result = new StringBuilder();
+        Square square = (Square) container;
+        result.append("<square>");
 
-    result.append("<point1>");
-    result.append("<x>" + square.getPoint1().x + "</x>");
-    result.append("<y>" + square.getPoint1().y + "</y>");
-    result.append("</point1>");
+        result.append("<point1>");
+        result.append("<x>" + square.getPoint1().x + "</x>");
+        result.append("<y>" + square.getPoint1().y + "</y>");
+        result.append("</point1>");
 
-    result.append("<length>" + square.getLength() + "</length>");
+        result.append("<length>" + square.getLength() + "</length>");
 
-    result.append("</square>");
-    return result.toString();
-  }
+        result.append("</square>");
+        return result.toString();
+    }
 
 }
