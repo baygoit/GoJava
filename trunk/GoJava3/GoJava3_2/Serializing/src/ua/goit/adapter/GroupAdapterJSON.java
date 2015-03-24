d240f52e99483c80c0a378959ab57adcfbd80870
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-24 13:02:44 +0000

    fix bug with group serialization
    git-svn-id: http://go-java-1.googlecode.com/svn@2515 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/GroupAdapterJSON.java
+++ trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/GroupAdapterJSON.java
@@ -5,18 +5,19 @@ import ua.goit.serializers.*;
 
 
 public class GroupAdapterJSON implements Serializer {
- StringBuilder result = new StringBuilder("");
- Serializer serializer;
 
-  public String serialize(ContainerShapes container){
-	  result.append("{\"group\":[");
-    GroupShapes group = (GroupShapes) container;
-    for (int i = 0; i < group.size(); i++){
-      ContainerShapes localContainer = group.get(i);
-      serializer = SerializerFactory.getSerializer(SerializerType.JSON);
-      result.append(serializer.serialize(localContainer));
+    public String serialize(ContainerShapes container) {
+        StringBuilder result = new StringBuilder("");
+        Serializer serializer;
+
+        result.append("{\"group\":[");
+        GroupShapes group = (GroupShapes) container;
+        for (int i = 0; i < group.size(); i++) {
+            ContainerShapes localContainer = group.get(i);
+            serializer = SerializerFactory.getSerializer(SerializerType.JSON);
+            result.append(serializer.serialize(localContainer));
+        }
+        result.append("]}");
+        return result.toString();
     }
-    result.append("]}");
-    return result.toString();
-  }
 }
