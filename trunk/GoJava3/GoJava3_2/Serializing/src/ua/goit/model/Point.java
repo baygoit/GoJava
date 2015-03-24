e115356f4f2273573c4dbb1e3e543979101234f5
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-24 15:55:05 +0000

    edit Point
    git-svn-id: http://go-java-1.googlecode.com/svn@2535 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/model/Point.java
+++ trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/model/Point.java
@@ -4,10 +4,6 @@ public class Point {
   private int x;
   private int y;
 
-  Point(){
-
-  }
-
   public Point(int x, int y){
     this.x = x;
     this.y = y;
@@ -17,16 +13,8 @@ public class Point {
     return x;
   }
 
-  public void setX(int x) {
-    this.x = x;
-  }
-
   public int getY() {
     return y;
-  }
-
-  public void setY(int y) {
-    this.y = y;
   }
 
 }
