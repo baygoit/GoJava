60c712cbe21c665f4db10be55457b2dae3733a2f
ikucherenko128@gmail.com <ikucherenko128@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-24 12:30:21 +0000

    GroupAdapterJSON format fix
    git-svn-id: http://go-java-1.googlecode.com/svn@2505 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/GroupAdapterJSON.java
+++ trunk/GoJava3/GoJava3_2/Serializing/src/ua/goit/adapter/GroupAdapterJSON.java
@@ -9,14 +9,14 @@ public class GroupAdapterJSON implements Serializer {
  Serializer serializer;
 
   public String serialize(ContainerShapes container){
-	  result.append("{\"group\":");
+	  result.append("{\"group\":[");
     GroupShapes group = (GroupShapes) container;
     for (int i = 0; i < group.size(); i++){
       ContainerShapes localContainer = group.get(i);
       serializer = SerializerFactory.getSerializer(SerializerType.JSON);
       result.append(serializer.serialize(localContainer));
     }
-    result.append("}");
+    result.append("]}");
     return result.toString();
   }
 }
