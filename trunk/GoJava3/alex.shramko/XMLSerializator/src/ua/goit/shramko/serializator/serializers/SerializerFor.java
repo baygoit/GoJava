ca9bd905eed83a2a8c8c120e09100cb8ca8db04e
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-24 16:16:05 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2541 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/alex.shramko/XMLSerializator/src/ua/goit/shramko/serializator/serializers/SerializerFor.java
+++
@@ -1,9 +0,0 @@
-package ua.goit.shramko.serializator.serializers;
-
-import ua.goit.shramko.serializator.basicobjects.Shape;
-
-public interface SerializerFor {
-
-  public String serialize(Shape shape);
-  
-}
