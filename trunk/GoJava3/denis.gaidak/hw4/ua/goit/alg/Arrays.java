e800295e3afb7dc36f9e83b7066a962292af0069
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-26 16:58:20 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2602 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/denis.gaidak/hw4/ua/goit/alg/Arrays.java
+++ trunk/GoJava3/denis.gaidak/hw4/ua/goit/alg/Arrays.java
@@ -1,18 +1,63 @@
 package ua.goit.alg;
 
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Arrays {
 
   public static void mergeSort(File file) {
 
-    System.getProperties().list(new PrintStream(System.out));
+    String dir = file.getParent();
+    String[] tempFiles = createTempFile(dir, 2);
+
+    try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = bufferedReader.readLine()) != null) {
+
+      }
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    deleteTempFile(tempFiles);
 
   }
 
-  public void createTempFile() {
+  public static String[] createTempFile(String dir, int amount) {
+    String tempName = "temp";
+    String[] tempFiles = new String[amount];
+    for (int i = 0; i < amount; i++) {
+      try {
+        String newTempFileName = dir + "\\" + tempName + i;
+        File testResource = new File(newTempFileName);
+        testResource.createNewFile();
+        tempFiles[i] = newTempFileName;
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 
+    return tempFiles;
   }
 
+  public static void deleteTempFile(String[] tempFiles) {
+
+
+    for (String file : tempFiles) {
+      try {
+        Files.delete(FileSystems.getDefault().getPath(file));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
+    }
+
+  }
 
 }
