c15e105d611f9fd538ad1acf3e1bfb978987a9fd
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-27 19:46:22 +0000

    Reformat code changed indents, deleted "Created by" header.
    git-svn-id: http://go-java-1.googlecode.com/svn@2617 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/MergeSort/src/MergeSort.java
+++ trunk/GoJava3/aleksey.kurkov/MergeSort/src/MergeSort.java
@@ -1,9 +1,5 @@
 import java.util.Scanner;
 
-/**
- * Created by Aleksey Kurkov on 15.03.15.
- */
-
 /*  array[0]...array[array.length-1]
 *   tempArray[start]...tempArray[middle] & tempArray[middlePlusOne]...tempArray[end]
 *   */
@@ -14,84 +10,84 @@ import java.util.Scanner;
 3) Input: 10 8 38 1 99  Output: 1 8 10 38 99 */
 
 public class MergeSort {
+  
+  public static int[] array;
 
-    public static int[] array;
-
-    public static void getArray() {
-        System.out.println("Input array: ");
-        Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        scanner.close();
-        String[] stringArray = string.split(" ");
-        array = new int[stringArray.length];
-        for (int i = 0; i < stringArray.length; i++) {
-            array[i] = Integer.parseInt(stringArray[i]);
-        }
+  public static void getArray() {
+    System.out.println("Input array: ");
+    Scanner scanner = new Scanner(System.in);
+    String string = scanner.nextLine();
+    scanner.close();
+    String[] stringArray = string.split(" ");
+    array = new int[stringArray.length];
+    for (int i = 0; i < stringArray.length; i++) {
+      array[i] = Integer.parseInt(stringArray[i]);
     }
+  }
 
-    public static void sortArray() {
-        int[] tempArray = new int[array.length];
-        sort(tempArray, 0, array.length - 1);
-    }
+  public static void sortArray() {
+    int[] tempArray = new int[array.length];
+    sort(tempArray, 0, array.length - 1);
+  }
 
-    public static void sort(int[] tempArray, int start, int end) {
-        if (end == start) {
-            return;
-        } else {
-            int middle = safeFindMiddle(start, end);
-            int middlePlusOne = middle + 1;
-            sort(tempArray, start, middle);
-            sort(tempArray, middlePlusOne, end);
-            merge(tempArray, start, middlePlusOne, end);
-        }
+  public static void sort(int[] tempArray, int start, int end) {
+    if (end == start) {
+      return;
+    } else {
+      int middle = safeFindMiddle(start, end);
+      int middlePlusOne = middle + 1;
+      sort(tempArray, start, middle);
+      sort(tempArray, middlePlusOne, end);
+      merge(tempArray, start, middlePlusOne, end);
     }
+  }
 
-    public static final int safeFindMiddle(int a, int b) {
-        if (b > 0 ? a > Integer.MAX_VALUE - b
-                : a < Integer.MIN_VALUE - b) {
-            return a + b >>> 1;
-        }
-        return (a + b) / 2;
+  public static final int safeFindMiddle(int a, int b) {
+    if (b > 0 ? a > Integer.MAX_VALUE - b
+            : a < Integer.MIN_VALUE - b) {
+      return a + b >>> 1;
     }
+    return (a + b) / 2;
+  }
 
-    public static void merge(int[] tempArray, int cursor, int middlePlusOne, int end) {
-        int index = 0;
-        int start = cursor;
-        int middle = middlePlusOne - 1;
-        int countArrayElements = end - start + 1;
-        while (start <= middle && middlePlusOne <= end) {
-            if (array[start] < array[middlePlusOne]) {
-                tempArray[index++] = array[start++];
-            } else {
-                tempArray[index++] = array[middlePlusOne++];
-            }
-        }
-        while (start <= middle) {
-            tempArray[index++] = array[start++];
-        }
-        while (middlePlusOne <= end) {
-            tempArray[index++] = array[middlePlusOne++];
-        }
-        for (int i = 0; i < countArrayElements; i++) {
-            array[cursor + i] = tempArray[i];
-        }
+  public static void merge(int[] tempArray, int cursor, int middlePlusOne, int end) {
+    int index = 0;
+    int start = cursor;
+    int middle = middlePlusOne - 1;
+    int countArrayElements = end - start + 1;
+    while (start <= middle && middlePlusOne <= end) {
+      if (array[start] < array[middlePlusOne]) {
+        tempArray[index++] = array[start++];
+      } else {
+        tempArray[index++] = array[middlePlusOne++];
+      }
     }
+    while (start <= middle) {
+      tempArray[index++] = array[start++];
+    }
+    while (middlePlusOne <= end) {
+      tempArray[index++] = array[middlePlusOne++];
+    }
+    for (int i = 0; i < countArrayElements; i++) {
+      array[cursor + i] = tempArray[i];
+    }
+  }
 
-    public static void printSortedArray() {
-        System.out.println("Sorted array: ");
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-            if (i != array.length - 1) {
-                System.out.print(" ");
-            } else {
-                System.out.println();
-            }
-        }
+  public static void printSortedArray() {
+    System.out.println("Sorted array: ");
+    for (int i = 0; i < array.length; i++) {
+      System.out.print(array[i]);
+      if (i != array.length - 1) {
+        System.out.print(" ");
+      } else {
+        System.out.println();
+      }
     }
+  }
 
-    public static void main(String[] args) {
-        getArray();
-        sortArray();
-        printSortedArray();
-    }
+  public static void main(String[] args) {
+    getArray();
+    sortArray();
+    printSortedArray();
+  }
 }
