763cdbaab6e555f47a322484a6f025f6759a9256
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-27 19:32:07 +0000

    Reformat code changed indents, deleted "Created by" header.
    git-svn-id: http://go-java-1.googlecode.com/svn@2614 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/Anagrams/src/Anagrams1.java
+++ trunk/GoJava3/aleksey.kurkov/Anagrams/src/Anagrams1.java
@@ -1,43 +1,39 @@
 import java.util.Scanner;
 
-/**
- * Created by Aleksey Kurkov on 11.03.15.
- */
-
 /* Test cases
 *  Input: Мама мыла раму
 *  Output: амаМ алым умар
 *  */
 
 public class Anagrams1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input your phrase: ");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Input your phrase: ");
 
-        StringBuilder phrase = new StringBuilder();
-        phrase.append(scanner.nextLine());
-        scanner.close();
+    StringBuilder phrase = new StringBuilder();
+    phrase.append(scanner.nextLine());
+    scanner.close();
 
-        System.out.print("Anagram: ");
+    System.out.print("Anagram: ");
 
-        for (int i = 0, start = 0; i < phrase.length(); i++) {
-            // looking for a space char
-            if (phrase.charAt(i) == ' ') {
-                System.out.print(reverse(phrase.substring(start, i)) + " ");
-                start = i + 1;
-            }
-            // looking for the end of line
-            if (i == phrase.length() - 1) {
-                System.out.print(reverse(phrase.substring(start, i + 1)));
-            }
-        }
+    for (int i = 0, start = 0; i < phrase.length(); i++) {
+      // looking for a space char
+      if (phrase.charAt(i) == ' ') {
+        System.out.print(reverse(phrase.substring(start, i)) + " ");
+        start = i + 1;
+      }
+      // looking for the end of line
+      if (i == phrase.length() - 1) {
+        System.out.print(reverse(phrase.substring(start, i + 1)));
+      }
     }
+  }
 
-    public static String reverse(String s) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int j = s.length() - 1; j >= 0; j--) {
-            stringBuilder.append(s.charAt(j));
-        }
-        return stringBuilder.toString();
+  public static String reverse(String s) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int j = s.length() - 1; j >= 0; j--) {
+      stringBuilder.append(s.charAt(j));
     }
+    return stringBuilder.toString();
+  }
 }
--- trunk/GoJava3/aleksey.kurkov/Anagrams/src/Anagrams2.java
+++ trunk/GoJava3/aleksey.kurkov/Anagrams/src/Anagrams2.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
-/**
- * Created by Aleksey Kurkov on 11.03.15.
- */
-
 public class Anagrams2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input your phrase: ");
-        String phrase = scanner.nextLine();
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Input your phrase: ");
+    String phrase = scanner.nextLine();
 
-        String[] phraseArray;
-        phraseArray = phrase.split(" ");
+    String[] phraseArray;
+    phraseArray = phrase.split(" ");
 
-        System.out.print("Anagram: ");
+    System.out.print("Anagram: ");
 
-        for (int i = 0; i < phraseArray.length; i++) {
-            System.out.print(reverse(phraseArray[i]));
+    for (int i = 0; i < phraseArray.length; i++) {
+      System.out.print(reverse(phraseArray[i]));
 
-            if (i != phraseArray.length - 1) {
-                System.out.print(" ");
-            }
-        }
+      if (i != phraseArray.length - 1) {
+        System.out.print(" ");
+      }
     }
+  }
 
-    public static String reverse(String s) {
-        StringBuffer stringBuffer = new StringBuffer(s.subSequence(0, s.length()));
-        return stringBuffer.reverse().toString();
-    }
+  public static String reverse(String s) {
+    StringBuffer stringBuffer = new StringBuffer(s.subSequence(0, s.length()));
+    return stringBuffer.reverse().toString();
+  }
 }
