c6bc59f7b27e98a13cbaf962ea3d73cb65e14e56
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-27 19:36:36 +0000

    Reformat code changed indents, deleted "Created by" header.
    git-svn-id: http://go-java-1.googlecode.com/svn@2615 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
+++ trunk/GoJava3/aleksey.kurkov/DistanceCalculator/src/DistanceCalculator.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 import static java.lang.Math.*;
 
-/**
- * Created by Aleksey Kurkov on 11.03.15.
- */
-
 /*  Test cases
     1) Input: 23 45 34 12 45 4 38 56 2 49 100    Output: min1 = 4   min2 = 2   Distance = 3
     2) Input: 1 8 8 8 1                          Output: min1 = 1   min2 = 1   Distance = 4
@@ -17,54 +13,54 @@ import static java.lang.Math.*;
 */
 
 public class DistanceCalculator {
-    private static int distance;
-    private static int min1;
-    private static int min2;
-    private static int index1;
-    private static int index2;
-    private static int[] intArray;
+  private static int distance;
+  private static int min1;
+  private static int min2;
+  private static int index1;
+  private static int index2;
+  private static int[] intArray;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input string of numbers: ");
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Input string of numbers: ");
 
-        String string = scanner.nextLine();
-        String[] stringArray = string.split(" ");
+    String string = scanner.nextLine();
+    String[] stringArray = string.split(" ");
 
-        intArray = new int[stringArray.length];
+    intArray = new int[stringArray.length];
 
-        for (int i = 0; i < stringArray.length; i++) {
-            intArray[i] = Integer.parseInt(stringArray[i]);
-        }
-
-        min1 = intArray[0];
-        index1 = 0;
-        min2 = intArray[1];
-        index2 = 1;
-
-        for (int i = 2; i < intArray.length; i++) {
-            if (intArray[i] < max(min1, min2)) {
-                if (min1 < min2) {
-                    min2 = intArray[i];
-                    index2 = i;
-                } else {
-                    min1 = intArray[i];
-                    index1 = i;
-                }
-                distance = abs(index1 - index2);
-            } else if (intArray[i] == max(min1, min2)) {
-                if (min1 > min2) {
-                    index1 = i;
-                } else if (min2 > min1) {
-                    index2 = i;
-                } else {
-                    index1 = max(index1, index2);
-                    index2 = i;
-                }
-                distance = min(distance, abs(index1 - index2));
-            }
-        }
-        System.out.println("\nmin1 = " + min1 + "\nmin2 = " + min2 + "\nDistance = " + distance);
+    for (int i = 0; i < stringArray.length; i++) {
+      intArray[i] = Integer.parseInt(stringArray[i]);
     }
+
+    min1 = intArray[0];
+    index1 = 0;
+    min2 = intArray[1];
+    index2 = 1;
+
+    for (int i = 2; i < intArray.length; i++) {
+      if (intArray[i] < max(min1, min2)) {
+        if (min1 < min2) {
+          min2 = intArray[i];
+          index2 = i;
+        } else {
+          min1 = intArray[i];
+          index1 = i;
+        }
+        distance = abs(index1 - index2);
+      } else if (intArray[i] == max(min1, min2)) {
+        if (min1 > min2) {
+          index1 = i;
+        } else if (min2 > min1) {
+          index2 = i;
+        } else {
+          index1 = max(index1, index2);
+          index2 = i;
+        }
+        distance = min(distance, abs(index1 - index2));
+      }
+    }
+    System.out.println("\nmin1 = " + min1 + "\nmin2 = " + min2 + "\nDistance = " + distance);
+  }
 }
