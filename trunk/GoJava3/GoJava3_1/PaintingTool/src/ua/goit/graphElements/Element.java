9df3b27871060ceda66add4fb4249837c33f00e0
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-29 17:55:13 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2686 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/Element.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/Element.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 /**
  * Interface for single element
- * @author Anton Yarosh
- *
+ * 
  */
 public interface Element {
 
     public void addPoint(Point point);
     public List<Point> getPoints();
     public String getName();
-    public String getType();
 }
