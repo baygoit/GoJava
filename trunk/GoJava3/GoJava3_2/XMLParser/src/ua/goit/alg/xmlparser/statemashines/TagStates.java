2edbdcdc0cc3cbd111e15227a8b97f75cc9a9b20
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 10:24:29 +0000

    main automat
    git-svn-id: http://go-java-1.googlecode.com/svn@2723 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/statemashines/TagStates.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/statemashines/TagStates.java
@@ -23,8 +23,21 @@ public enum TagStates {
       if (c == '?') {
         result = START;
       }
-      if (c == '<') {
+      if (c == '/') {
+        result = CLOSETAG;
+      }
+      return result;
+    }
+  },
+  CLOSETAG{
+    @Override
+    public TagStates next(char c, ParserData parserData) {
+      TagStates result = INVALID;
+      if (c == '?') {
         result = START;
+      }
+      if (c == '>') {
+        result = CLOSETAG;
       }
       return result;
     }
@@ -38,6 +51,31 @@ public enum TagStates {
       }
       if (c == '<') {
         result = START;
+      }
+      return result;
+    }
+  },
+  ELEMENT {
+    @Override
+    public TagStates next(char c, ParserData parserData) {
+      TagStates result = INVALID;
+      if (c == '?') {
+        result = INIT;
+      }
+      if (c == '<') {
+        result = START;
+      }
+      return result;
+    }
+  },
+  NODE {
+    @Override
+    public TagStates next(char c, ParserData parserData) {
+      TagStates result = INVALID;
+      if (c == '<') {
+        result = OPENTAG;
+      } else {
+        parserData.setText(parserData.getText() + c);
       }
       return result;
     }
