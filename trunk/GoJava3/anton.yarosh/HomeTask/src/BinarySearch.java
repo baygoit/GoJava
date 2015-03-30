d8c09db940bd1ef79aa767a2d7714030dac53f08
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 10:25:21 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2724 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/anton.yarosh/HomeTask/src/BinarySearch.java
+++ trunk/GoJava3/anton.yarosh/HomeTask/src/BinarySearch.java
@@ -5,11 +5,11 @@ public class BinarySearch {
 	System.out.println(returnIndex(initialMass, 26));
     }
 
-    public static int returnIndex(int []m, int elementToSearch) {
+    public static int returnIndex(int[] m, int elementToSearch) {
 	int startIndex = 0;
-	int endIndex = m.length-1;
+	int endIndex = m.length - 1;
 	int middleIndex = (endIndex - startIndex) / 2;
-	while (elementToSearch != m[middleIndex]&&startIndex<endIndex) {
+	while (elementToSearch != m[middleIndex] && startIndex < endIndex) {
 	    if (middleIndex == startIndex || middleIndex == endIndex) {
 		return -1;
 	    }
@@ -17,39 +17,35 @@ public class BinarySearch {
 		if (elementToSearch >= m[startIndex]) {
 		    endIndex = middleIndex;
 		    middleIndex = backward(startIndex, endIndex);
-		}
-		else if (m[startIndex] > m[middleIndex]) {
+		} else if (m[startIndex] > m[middleIndex]) {
 		    endIndex = middleIndex;
 		    middleIndex = backward(startIndex, endIndex);
-		}
-		else {
+		} else {
 		    startIndex = middleIndex;
 		    middleIndex = forward(startIndex, endIndex);
 		}
-	    }
-	    else {
+	    } else {
 		if (elementToSearch <= m[endIndex]) {
 		    startIndex = middleIndex;
 		    middleIndex = forward(startIndex, endIndex);
-		}
-		else if (m[endIndex] < m[middleIndex]){
+		} else if (m[endIndex] < m[middleIndex]){
 		    startIndex = middleIndex;
 		    middleIndex = forward(startIndex, endIndex);
-		}
-		else {
+		} else {
 		    endIndex = middleIndex;
 		    middleIndex = backward(startIndex, endIndex);
 		}
 	    }
 	}
+	
 	return middleIndex; 
     }
 
-    private static int forward (int startIndex, int endIndex){
+    private static int forward (int startIndex, int endIndex) {
 	return startIndex + Math.abs(endIndex - startIndex + 1) / 2;
     }
 
-    private static int backward (int startIndex, int endIndex){
+    private static int backward (int startIndex, int endIndex) {
 	return endIndex - Math.abs(endIndex - startIndex + 1) / 2;
     }
 }
