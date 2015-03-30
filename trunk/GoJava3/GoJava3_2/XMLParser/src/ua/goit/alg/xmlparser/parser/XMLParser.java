a5e8e58a88eafa30008ac86c849a3020a2a9297f
ikucherenko128@gmail.com <ikucherenko128@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 15:01:17 +0000

    Parser
    git-svn-id: http://go-java-1.googlecode.com/svn@2749 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/XMLParser.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/XMLParser.java
@@ -3,23 +3,27 @@ package ua.goit.alg.xmlparser.parser;
 import java.io.File;
 
 import ua.goit.alg.xmlparser.input.InputStream;
+import ua.goit.alg.xmlparser.statemashines.StateMashineTag;
 public class XMLParser {
   
-  private StringBuilder result = new StringBuilder("");
   private InputStream stream = new InputStream();
   private ParserData parserData = null;
-  
+  private StateMashineTag tag = new StateMashineTag(this);
   public void update(ParserData parserData){
     this.parserData = parserData;
   }
   
   public String parse(String string) {
-    stream.parse(string);
+    for(char c: stream.parse(string)){
+      tag.next(c);
+    }
     return "tag";
   }
   
   public String parse(File file) {
-    stream.parse(file);
+    for(char c: stream.parse(file)){
+      tag.next(c);
+    }
     return "tag";
   }
 
