a3f5cada9eae494abe5dd3fcbd6f566b83dd907b
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 21:19:12 +0000

    refactor
    git-svn-id: http://go-java-1.googlecode.com/svn@2777 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olga.kulykova/RotatedArray/src/ua/goit/alg/RotatedArray.java
+++ trunk/GoJava3/olga.kulykova/RotatedArray/src/ua/goit/alg/RotatedArray.java
@@ -1,28 +1,17 @@
 package ua.goit.alg;
 
-/**
- * Created by Ольга on 18.03.2015.
- */
 public class RotatedArray {
-    //Iterative way of Binary Search
     public static int binarySearch(int[] array, int target) {
         int min = 0;
         int max = array.length - 1;
 
-        //If the array is empty
-        if (max < min) {
-            return -1;
-        }
-        //If the array has one element
-        if (array.length == 1) {
-            return array[0];
-        }
-
-        while (max >= min) {
-            //Find the middle point
+        while (min < max) {
             int middle = min + ((max - min) / 2);
+            boolean moreMin = target > array[min];
+            boolean lessMiddle = target < array[middle];
+            boolean moreMiddle = target > array[middle];
+            boolean lessMax = target < array[max];
 
-            //Comparisons
             if (array[middle] == target) {
                 return middle;
             } else if (array[min] == target) {
@@ -30,13 +19,13 @@ public class RotatedArray {
             } else if (array[max] == target) {
                 return max;
             } else if (array[middle] > array[min]) {
-                if (target > array[min] && target < array[middle]) {
-                    max = middle - 1;
+                if (moreMin && lessMiddle) {
+                    max = middle;
                 } else {
                     min = middle + 1;
                 }
             } else {
-                if (target > array[middle] && target < array[max]) {
+                if (moreMiddle && lessMax) {
                     min = middle + 1;
                 } else {
                     max = middle - 1;
