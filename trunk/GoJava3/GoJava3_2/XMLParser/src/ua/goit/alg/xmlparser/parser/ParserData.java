b3b14e8f437e034ab58581e0e5a65a4007f66342
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 10:22:42 +0000

    getters & setters
    git-svn-id: http://go-java-1.googlecode.com/svn@2722 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/ParserData.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/ParserData.java
@@ -3,6 +3,24 @@ package ua.goit.alg.xmlparser.parser;
 import java.util.HashMap;
 
 public class ParserData {
+  public String getTag() {
+    return tag;
+  }
+  public void setTag(String tag) {
+    this.tag = tag;
+  }
+  public HashMap<String, String> getAttributes() {
+    return attributes;
+  }
+  public void setAttributes(HashMap<String, String> attributes) {
+    this.attributes = attributes;
+  }
+  public String getText() {
+    return text;
+  }
+  public void setText(String text) {
+    this.text = text;
+  }
   String tag;
   HashMap<String, String> attributes;
   String text;
