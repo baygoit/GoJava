69586432b83fa403badad0c24632dfb49943c2e7
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 12:48:02 +0000

    in class Tag delete set(), add(), remove()
    git-svn-id: http://go-java-1.googlecode.com/svn@2741 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/Tag.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/Tag.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tag {
-  private String tag;
   private TagType type;
   private String name;
   private Map<String, String> params = new HashMap<String, String>();
@@ -23,36 +22,16 @@ public class Tag {
     this.params = params;
   }
 
-  public String add(String param, String value) {
-    return params.put(param, value);
-  }
-
-  public String remove(String param) {
-    return params.remove(param);
-  }
-
   public TagType getType() {
     return type;
-  }
-
-  public void setType(TagType type) {
-    this.type = type;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public Map<String, String> getParams() {
     return params;
-  }
-
-  public void setParams(Map<String, String> params) {
-    this.params = params;
   }
 
   @Override
@@ -60,20 +39,18 @@ public class Tag {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
 
-    Tag tag1 = (Tag) o;
+    Tag tag = (Tag) o;
 
-    if (tag != null ? !tag.equals(tag1.tag) : tag1.tag != null) return false;
-    if (getType() != tag1.getType()) return false;
-    if (getName() != null ? !getName().equals(tag1.getName()) : tag1.getName() != null)
+    if (getType() != tag.getType()) return false;
+    if (getName() != null ? !getName().equals(tag.getName()) : tag.getName() != null)
       return false;
-    return !(getParams() != null ? !getParams().equals(tag1.getParams()) : tag1.getParams() != null);
+    return !(getParams() != null ? !getParams().equals(tag.getParams()) : tag.getParams() != null);
 
   }
 
   @Override
   public int hashCode() {
-    int result = tag != null ? tag.hashCode() : 0;
-    result = 31 * result + (getType() != null ? getType().hashCode() : 0);
+    int result = getType() != null ? getType().hashCode() : 0;
     result = 31 * result + (getName() != null ? getName().hashCode() : 0);
     result = 31 * result + (getParams() != null ? getParams().hashCode() : 0);
     return result;
