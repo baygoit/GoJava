fee254aeef4bc6f2d12c17bd2c064612d2d2408e
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 06:31:59 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2699 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/State.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/State.java
@@ -2,29 +2,42 @@ package ua.goit.xmlparser;
 
 public enum State {
   START {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   HEADER_TAG {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   OPEN_TAG {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   SINGLE_TAG {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   TEXT_VALUE {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   CLOSE_TAG {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   VALID_END {
-    
+    public State next(String str) {
+      return INVALID_END;
+    }
   }, 
   INVALID_END {
-    
-    public State next(char c, ParserData data) {
+    public State next(String str) {
       return INVALID_END;
     }
   }
