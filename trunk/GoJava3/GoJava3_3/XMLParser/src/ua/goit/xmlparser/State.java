e3d9735e23568bc20361368e61a2d81d1847f126
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-30 06:36:37 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2700 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/State.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/ua/goit/xmlparser/State.java
@@ -3,39 +3,57 @@ package ua.goit.xmlparser;
 public enum State {
   START {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+      TagParser tagParser = new TagParser();
+      Tag tag = tagParser.parse(str);
+      if (tag.getType() == TagType.OPEN) {
+        result = HEADER_TAG;
+      }
+      return result;
     }
-  }, 
+  },
   HEADER_TAG {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   OPEN_TAG {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   SINGLE_TAG {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   TEXT_VALUE {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   CLOSE_TAG {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   VALID_END {
     public State next(String str) {
-      return INVALID_END;
+      State result = INVALID_END;
+
+      return result;
     }
-  }, 
+  },
   INVALID_END {
     public State next(String str) {
       return INVALID_END;
