9b6672fbe7717aa899f9f429caa502e6e6f0a0b7
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-03-31 09:34:49 +0000

    +4
    git-svn-id: http://go-java-1.googlecode.com/svn@2787 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/XMLParser.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/parser/XMLParser.java
@@ -39,34 +39,34 @@ public class XMLParser {
     return result.toString();
 }
   
-  public void onOpenTag(Handler handler){
-    handler.onOpenTag(parserData);
+  public void onOpenTag(ParserData parserData2){
+    //parserData2.onOpenTag(parserData);
     result.append(parserData.getText());
     result.append("<").append(parserData.getTag()).append(">");
   }
-  public void onCloseTag(Handler handler){
-    handler.onCloseTag(parserData);
+  public void onCloseTag(ParserData parserData2){
+    //handler.onCloseTag(parserData);
     result.append(parserData.getText());
     result.append("<").append(parserData.getTag()).append("/>");
   }
-  public void onTextValue(Handler handler){
-    handler.onTextValue(parserData);
+  public void onTextValue(ParserData parserData2){
+    //handler.onTextValue(parserData);
     result.append(parserData.getText());
     result.append("<").append(parserData.getTag()).append(">");
   }
   
-  public void onStart(Handler handler){
-    handler.onStart(parserData);
+  public void onStart(ParserData parserData2){
+    //handler.onStart(parserData);
     result.append(parserData.getText());
     result.append("<").append(parserData.getTag()).append(">");
   }
-  public void onEnd(Handler handler){
-    handler.onEnd(parserData);
+  public void onEnd(ParserData parserData2){
+    //handler.onEnd(parserData);
     result.append(parserData.getText());
     result.append("<").append(parserData.getTag()).append(">");
   }
-  public void onError(Handler handler){
-    handler.onError(parserData);
+  public void onError(ParserData parserData2){
+    //handler.onError(parserData);
     result.append("<").append(parserData.getTag()).append(">");
   }
 }
