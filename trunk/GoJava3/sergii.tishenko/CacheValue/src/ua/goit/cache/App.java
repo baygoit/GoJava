7908554a0ff5d65841a5151fd12f43f3e3eee4bc
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-01 12:22:20 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2852 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/sergii.tishenko/CacheValue/src/ua/goit/cache/App.java
+++ trunk/GoJava3/sergii.tishenko/CacheValue/src/ua/goit/cache/App.java
@@ -6,31 +6,6 @@ public class App {
 
   public static void main(String[] args) {
 
-    NodeTable nodeTable = new NodeTable(3);
-    nodeTable.put(1, 10);
-    nodeTable.put(2, 20);
-    nodeTable.put(1, 30);
-    nodeTable.put(2, 40);
-    nodeTable.put(5, 50);
-    nodeTable.put(1, 30);
-
-    System.out.println(nodeTable.getCapacity());
-    
-    
-    int actual = nodeTable.get(1);
-    int expected = 30;
-    
-    System.out.println("actual - expected" + actual + "- " + expected);
-
-    nodeTable.put(2, 10);
-    nodeTable.put(5, 10);
-    nodeTable.put(6, 10);
-
-    expected = -1;
-    actual = nodeTable.get(1);
-    
-    System.out.println("actual - expected" + actual + "- " + expected);
-
   }
 
 }
