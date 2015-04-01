397e758cd88c12cd127c03be9983dfa89f06eec7
Alexander.Velitchenko@gmail.com <Alexander.Velitchenko@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-01 09:25:37 +0000

    Initial import.
    git-svn-id: http://go-java-1.googlecode.com/svn@2842 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/avelitchenko/Cache/src/ua/goit/alg/LRUCache.java
@@ -0,0 +1,60 @@
+package ua.goit.alg;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class LRUCache {
+
+  private int sizeLimit;
+  private Map<Integer,Integer> cacheMap = new HashMap<>();
+  private List<Integer> cacheList = new LinkedList<>();
+  
+  public LRUCache(int sizeLimit) {
+    this.sizeLimit = sizeLimit;
+  }
+
+  public void put(int key, int value) {
+    int index = getIndex(key);
+    if (getSize() == sizeLimit || index != -1){
+      if (index == -1){
+        int keyToRemove = cacheList.get(0);
+        cacheMap.remove(keyToRemove);
+        cacheList.remove(0);
+      } else {
+        cacheMap.remove(key);
+        cacheList.remove(index);
+      }
+    } 
+    cacheMap.put(key, value);
+    cacheList.add(key);
+  }
+
+  public Integer get(int key) {
+    Integer result = null;
+    int index = getIndex(key);
+    if (index != -1){
+      cacheList.remove(index);
+      cacheList.add(key);
+      result = cacheMap.get(key);
+    }
+    return result;
+  }
+
+  private int getIndex(int key) {
+    return cacheList.indexOf(key);
+  }
+
+  public int getSize() {
+    return cacheMap.size();
+  }
+
+  public String getList() {
+    StringBuilder builder = new StringBuilder();
+    for (Integer element : cacheList) {
+      builder.append(element).append(" ");
+    }
+    return builder.toString();
+  }
+}
---
+++ trunk/GoJava3/avelitchenko/Cache/src/ua/goit/alg/TestCache.java
@@ -0,0 +1,40 @@
+package ua.goit.alg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCache {
+
+  @Test
+  public void testCache() {
+    LRUCache cache = new LRUCache(5);
+    cache.put(1,10);
+    cache.put(2,20);
+    assertEquals(2, cache.getSize());
+  }
+
+  @Test
+  public void testCacheReplace() {
+    LRUCache cache = new LRUCache(5);
+    cache.put(1,10);
+    cache.put(2,20);
+    cache.put(3,30);
+    cache.put(2,200);
+    assertEquals(3, cache.getSize());
+  }
+
+  @Test
+  public void testCacheLong() {
+    LRUCache cache = new LRUCache(5);
+    cache.put(1,10);
+    cache.put(2,20);
+    cache.put(3,30);
+    cache.put(2,200);
+    cache.get(2);
+    cache.put(4,40);
+    cache.put(5,50);
+    cache.put(2,20);
+    assertEquals("1 3 4 5 2 ", cache.getList());
+  }
+}
