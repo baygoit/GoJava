c11d583c8b54ad7ab0f5f5ba1a8da7d5b1ee46d0
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-02 08:18:56 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2877 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/XMLSerializator.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/XMLSerializator.java
@@ -13,83 +13,82 @@ public class XMLSerializator extends Serializator {
     private StringBuffer buffer = new StringBuffer();
 
     public  void openGroupTagWithAttr(Group group) {
-        buffer.append("<");
-        buffer.append(group.getType());
-        buffer.append(" name");
-        buffer.append("=\"");
-        buffer.append(group.getName());
-        buffer.append("\">");
+	buffer.append("<");
+	buffer.append(group.getType());
+	buffer.append(" name");
+	buffer.append("=\"");
+	buffer.append(group.getName());
+	buffer.append("\">");
     }
 
     public void openElementTagWithAttr(Element element) {
-        buffer.append("<");
-        buffer.append(element.getClass().getSimpleName());
-        buffer.append(" name");
-        buffer.append("=\"");
-        buffer.append(element.getName());
-        buffer.append("\">");
+	buffer.append("<");
+	buffer.append(element.getClass().getSimpleName());
+	buffer.append(" name");
+	buffer.append("=\"");
+	buffer.append(element.getName());
+	buffer.append("\">");
     }
 
     @Override
     public StringBuffer serialize(Element element) {
-            openElementTagWithAttr(element);
-            if (element.getPoints() == null) {
-                buffer.append("<Points> </Points>");
-            }
-            else {
-                buffer.append("<Points>");
-                for (Point point : element.getPoints()) {
-                    buffer.append("<Point>");
-                    buffer.append("<x>");
-                    buffer.append(point.getX());
-                    buffer.append("</x>");
-                    buffer.append("<y>");
-                    buffer.append(point.getY());
-                    buffer.append("</y>");
-                    buffer.append("</Point>");
-                }
-                buffer.append("</Points>");
-            }
-            buffer.append("</");
-            buffer.append(element.getClass().getSimpleName());
-            buffer.append(">");
+	openElementTagWithAttr(element);
+	if (element.getPoints() == null) {
+	    buffer.append("<Points> </Points>");
+	}
+	else {
+	    buffer.append("<Points>");
+	    for (Point point : element.getPoints()) {
+		buffer.append("<Point>");
+		buffer.append("<x>");
+		buffer.append(point.getX());
+		buffer.append("</x>");
+		buffer.append("<y>");
+		buffer.append(point.getY());
+		buffer.append("</y>");
+		buffer.append("</Point>");
+	    }
+	    buffer.append("</Points>");
+	}
 
-        return buffer;
+	buffer.append("</");
+	buffer.append(element.getClass().getSimpleName());
+	buffer.append(">");
+	return buffer;
     }
 
     @Override
     public StringBuffer serialize(Group group) {
-        if (group.getGroups() != null || group.getGroups().size() <= 0 ) {
-            openGroupTagWithAttr(group);
-            for (Group inGroup : group.getGroups()) {
-                serialize(inGroup);
-            }
-        }
-        if (group.getElements() != null || group.getElements().size() <= 0) {
-            for (Element element : group.getElements()) {
-                serialize(element);
-            }
-        }
+	if (group.getGroups() != null || group.getGroups().size() <= 0 ) {
+	    openGroupTagWithAttr(group);
+	    for (Group inGroup : group.getGroups()) {
+		serialize(inGroup);
+	    }
+	}
+	if (group.getElements() != null || group.getElements().size() <= 0) {
+	    for (Element element : group.getElements()) {
+		serialize(element);
+	    }
+	}
 
-        buffer.append("</");
-        buffer.append(group.getType());
-        buffer.append(">");
-
-        return buffer;
+	buffer.append("</");
+	buffer.append(group.getType());
+	buffer.append(">");
+	return buffer;
     }
 
     @Override
-    public void saveToFile(String source, File file) {
-        try {
-            FileWriter fw = new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
-            bw.write("<elements>");
-            bw.write(source);
-            bw.write("</elements>");
-            bw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void saveToFile(StringBuffer source, File file) {
+	try {
+	    FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	    BufferedWriter bw = new BufferedWriter(fw);
+	    bw.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+	    bw.write("<elements>");
+	    bw.write(source.toString());
+	    bw.write("</elements>");
+	    bw.close();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 }
