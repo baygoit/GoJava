9382edd516122a0a03c2b67cbb3df8c590cf82bc
a.khambir@gmail.com <a.khambir@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-03 10:49:29 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2906 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/akhambir/basics/RotatedArrays.java
@@ -0,0 +1,49 @@
+package ua.goit.alg;
+
+public class RotatedArrays {
+
+    public static int binarySearch(int[] array, int target) {
+
+        int middlePoint = array.length / 2;
+        int leftPoint = 0;
+        int rightPoint = array.length - 1;
+
+        while (target != array[middlePoint]) {
+            if (target > array[middlePoint]) {
+                if (array[middlePoint] > array[rightPoint] && target >= array[rightPoint]) {
+                    leftPoint = middlePoint;
+                    middlePoint = goRight(leftPoint, rightPoint, middlePoint);
+                } else if (array[middlePoint] < array[rightPoint] && target <= array[rightPoint]) {
+                    leftPoint = middlePoint;
+                    middlePoint = goRight(leftPoint, rightPoint, middlePoint);
+                } else {
+                    rightPoint = middlePoint;
+                    middlePoint = goLeft(leftPoint, rightPoint);
+                }
+            } else {
+                if (target >= array[leftPoint]) {
+                    rightPoint = middlePoint;
+                    middlePoint = goLeft(leftPoint, rightPoint);
+                } else if (array[leftPoint] > array[middlePoint]) {
+                    rightPoint = middlePoint;
+                    middlePoint = goLeft(leftPoint, rightPoint);
+                } else {
+                    leftPoint = middlePoint;
+                    middlePoint = goRight(leftPoint, rightPoint, middlePoint);
+                }
+            }
+        }
+
+
+        return middlePoint;
+    }
+
+    public static int goRight(int leftPoint, int rightPoint, int middlePoint) {
+        return (rightPoint - leftPoint + 1) / 2 + middlePoint;
+    }
+
+    public static int goLeft(int leftPoint, int rightPoint) {
+        return (leftPoint + rightPoint) / 2;
+    }
+
+}
