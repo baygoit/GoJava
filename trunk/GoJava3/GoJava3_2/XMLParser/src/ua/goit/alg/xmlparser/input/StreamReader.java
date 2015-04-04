4c7bf046ab39f2e3e0afa6c555c7a770faf05798
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-04 16:39:23 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2970 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/input/StreamReader.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/ua/goit/alg/xmlparser/input/StreamReader.java
@@ -1,51 +1,31 @@
 package ua.goit.alg.xmlparser.input;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class StreamReader {
   private BufferedReader inputStreamFile = null;
-  private String inputStreamString = null;
-  private boolean isString = true;
-  private int index = 0;
 
-  public StreamReader(String inputData) throws FileNotFoundException{
-    inputStreamString = inputData;
-    isString = true;
+  public StreamReader(String inputData) throws FileNotFoundException {
+    inputStreamFile = new BufferedReader(new StringReader(inputData));
   }
 
-  public StreamReader(File inputData) throws FileNotFoundException{
+  public StreamReader(File inputData) throws FileNotFoundException {
     inputStreamFile = new BufferedReader(new FileReader(inputData));
-    isString = false;
   }
 
-  public int read() throws IOException{
-    if (isString){
-    return readString();
-    } else {
-      int i = inputStreamFile.read();
-      if (i==-1) {
-        close();
-      }
-      return i;
-    }
+  public StreamReader(InputStream inputStream) {
+    inputStreamFile = new BufferedReader(new InputStreamReader(inputStream));
   }
-  
-  public void close() throws IOException{
+
+  public int read() throws IOException {
+    int i = inputStreamFile.read();
+    if (i == -1) {
+      close();
+    }
+    return i;
+  }
+
+  public void close() throws IOException {
     inputStreamFile.close();
-  }
-  
-  private int readString(){
-    if (index < inputStreamString.length()){
-      int result = inputStreamString.charAt(index); //toCharArray()[index];
-      index++;
-      return result;
-    } else {
-      index = 0;
-      return -1;
-    }
   }
 }
