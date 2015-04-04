65625a903168bde5aff2d7db34257bef58dba629
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-04 22:26:19 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3001 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/main/ua/goit/xmlparsertdd/ParserBuilder.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/main/ua/goit/xmlparsertdd/ParserBuilder.java
@@ -3,4 +3,11 @@ package ua.goit.xmlparsertdd;
 public class ParserBuilder {
   private Handler onOpenHandler;
 
+  public void onOpen(Handler handler) {
+    onOpenHandler = handler;
+  }
+
+  public Parser build() {
+    return new XMLParser(onOpenHandler);
+  }
 }
