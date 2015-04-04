005ae2c0cd8687f627eb421f0d6fbb35908febeb
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-04 20:07:29 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@2990 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/ConcreteFactory.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/ConcreteFactory.java
@@ -2,23 +2,18 @@ package ua.goit.serialization;
 
 /**
  * Class of concrete serialization factory 
- * @author Anton Yarosh
- *
  */
 public class ConcreteFactory extends SerializationFactory{
-    Serializator serializator;
 
-    /*Return type of serializator*/
+    /** Return type of serializator*/
     @Override
     public Serializator getSerializationFor(SerializationType type) {
 	switch (type) {
 	case  XML: 
-	    serializator = new XMLSerializator();
-	    break;
+	    return new XMLSerializator();
 	case JSON:
-	    serializator = new JSONSerializator();
-	    break;
+	    return new JSONSerializator();	  
 	}
-	return serializator;
+	return null;
     }   
 }
