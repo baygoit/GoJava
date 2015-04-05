f3bc18201d2fd3dc04fd74668d10a4d2b1b8d809
masyakot@gmail.com <masyakot@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-05 23:09:45 +0000

    add tests
    git-svn-id: http://go-java-1.googlecode.com/svn@3055 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olga.kulykova/LRUCache/src/ua/goit/lrucache/LRUCache.java
+++ trunk/GoJava3/olga.kulykova/LRUCache/src/ua/goit/lrucache/LRUCache.java
@@ -1,6 +1,7 @@
 package ua.goit.lrucache;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class LRUCache {
@@ -16,21 +17,32 @@ public class LRUCache {
 
   public void set(int key, int value) {
     Node node = new Node(value);
-    if (map.size() < capacity) {
+    if (map.size() < capacity || map.containsKey(key)) {
+      if (map.containsKey(key)) {
+        Node oldNode = map.get(key);
+        cutNode(oldNode);
+      }
       map.put(key, node);
       insertLastNode(node);
     } else {
-      removeFirstNode();
+      if (capacity == 0) {
+        return;
+      }
+      Node firstNode = head;
+      cutNode(firstNode);
+      removeFirstNode(firstNode);
       map.put(key, node);
       insertLastNode(node);
     }
   }
 
-  private void removeFirstNode() {
-    for (Map.Entry<Integer, Node> entry : map.entrySet()) {
+  private void removeFirstNode(Node node) {
+    Iterator<Map.Entry<Integer, Node>> iterator = map.entrySet().iterator();
+    while (iterator.hasNext()){
+      Map.Entry<Integer, Node> entry = iterator.next();
       Node value = entry.getValue();
-      if (value.equals(head)) {
-        map.remove(entry.getKey());
+      if (value.equals(node)) {
+        iterator.remove();
       }
     }
   }
@@ -49,7 +61,10 @@ public class LRUCache {
   private void cutNode(Node currentNode) {
     Node prev = currentNode.prev;
     Node next = currentNode.next;
-    if (prev == null) {
+    if (prev == null && next == null) {
+      head = null;
+      tail = null;
+    }else if (prev == null) {
       next.prev = null;
       head = next;
     } else if (next == null) {
@@ -62,16 +77,29 @@ public class LRUCache {
 
   private void insertLastNode(Node currentNode) {
     currentNode.prev = tail;
+    if (currentNode.prev != null) {
+      Node prev = currentNode.prev;
+      prev.next = currentNode;
+    }
+    if (map.size() == 1) {
+      head = currentNode;
+    }
     tail = currentNode;
     currentNode.next = null;
   }
 
-  public Node getTail() {
-    return tail;
-  }
-
-  public Node getHead() {
-    return head;
+  @Override
+  public String toString() {
+    StringBuilder result = new StringBuilder("");
+    if (head != null) {
+      Node node = head;
+      result.append(node.value).append(" ");
+      while (node.next != null) {
+        node = node.next;
+        result.append(node.value).append(" ");
+      }
+    }
+    return result.toString();
   }
 
   class Node {
@@ -81,10 +109,6 @@ public class LRUCache {
 
     Node(int value) {
       this.value = value;
-    }
-
-    public int getValue() {
-      return value;
     }
   }
 }
--- trunk/GoJava3/olga.kulykova/LRUCache/src/ua/goit/lrucache/LRUCacheTest.java
+++ trunk/GoJava3/olga.kulykova/LRUCache/src/ua/goit/lrucache/LRUCacheTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class LRUCacheTest {
 
   @Test
-  public void givenCacheCapacityFive_WhenGetThirdElement_ThenItShouldReplaceToLastElement() {
+  public void replaceMiddleElementToTail() {
     LRUCache cache = new LRUCache(5);
     cache.set(1, 1);
     cache.set(2, 2);
@@ -15,8 +15,70 @@ public class LRUCacheTest {
     cache.set(4, 4);
     cache.set(5, 5);
     cache.get(3);
-    LRUCache.Node node = cache.getTail();
-    assertEquals(3, node.getValue());
+    String expected = "1 2 4 5 3 ";
+    String actual = cache.toString();
+    assertEquals(expected, actual);
   }
 
+  @Test
+  public void getAbsentElement() {
+    LRUCache cache = new LRUCache(5);
+    cache.set(1, 1);
+    cache.set(2, 2);
+    cache.set(3, 3);
+    cache.set(4, 4);
+    cache.set(5, 5);
+    int result = cache.get(6);
+    assertEquals(-1, result);
+  }
+
+  @Test
+  public void putExtraElement() {
+    LRUCache cache = new LRUCache(5);
+    cache.set(1, 1);
+    cache.set(2, 2);
+    cache.set(3, 3);
+    cache.set(4, 4);
+    cache.set(5, 5);
+    cache.set(6, 6);
+    String expected = "2 3 4 5 6 ";
+    String actual = cache.toString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void putNewValueWithOldKey() {
+    LRUCache cache = new LRUCache(5);
+    cache.set(1, 1);
+    cache.set(2, 2);
+    cache.set(3, 3);
+    cache.set(4, 4);
+    cache.set(5, 5);
+    cache.set(2, 6);
+    String expected = "1 3 4 5 6 ";
+    String actual = cache.toString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void putIntoOneCapacityCache() {
+    LRUCache cache = new LRUCache(1);
+    cache.set(1, 1);
+    cache.set(2, 2);
+    cache.set(3, 3);
+    String expected = "3 ";
+    String actual = cache.toString();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void putIntoZeroCapacityCache() {
+    LRUCache cache = new LRUCache(0);
+    cache.set(1, 1);
+    cache.set(2, 2);
+    cache.set(3, 3);
+    String expected = "";
+    String actual = cache.toString();
+    assertEquals(expected, actual);
+  }
 }
