fb29868ba8b7bc66ac21bf0d359674c303a3ab53
Aleksey.Kurkov@gmail.com <Aleksey.Kurkov@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-05 17:42:48 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3033 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/aleksey.kurkov/LRUCache/src/ua/goit/alg/LRUCacheTest.java
@@ -0,0 +1,89 @@
+package ua.goit.alg;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LRUCacheTest {
+
+  @Test
+  public void checkCacheCapacity() {
+    LRUCache cache = new LRUCache(3);
+    int expectedValue = 3;
+    int actualValue = cache.getCapacity();
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void checkCacheSize_AfterTwoOperations() {
+    LRUCache cache = new LRUCache(5);
+    cache.put(0, 0);
+    cache.put(1, 10);
+    int expectedValue = 2;
+    int actualValue = cache.getSize();
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void checkCacheSize_AfterFifteenOperations() {
+    LRUCache cache = new LRUCache(5);
+    for (int i = 0; i < 16; i++) {
+      cache.put(i, i * 10);
+    }
+    int expectedValue = 5;
+    int actualValue = cache.getSize();
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void checkIfKeyNotExists() {
+    LRUCache cache = new LRUCache(5);
+    int expectedValue = -1;
+    int actualValue = cache.get(3);
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void
+  setCacheValues_countLessCacheSize_getThirdValue_checkIfItEqualsToSetValue() {
+    LRUCache cache = new LRUCache(10);
+    cache.put(0, 0);
+    cache.put(1, 10);
+    cache.put(2, 20);
+    cache.put(3, 30);
+    int expectedValue = 30;
+    int actualValue = cache.get(3);
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void
+  setCacheValues_countMoreCacheSize_getThirdValue_checkIfItEqualsToSetValue() {
+    LRUCache cache = new LRUCache(5);
+    cache.put(0, 0);
+    cache.put(1, 10);
+    cache.put(2, 20);
+    cache.put(3, 30);
+    cache.put(4, 40);
+    cache.put(5, 50);
+    cache.put(6, 60);
+    int expectedValue = 30;
+    int actualValue = cache.get(3);
+    assertEquals(expectedValue, actualValue);
+  }
+
+  @Test
+  public void
+  checkIfTheOldestKey_IsDeleted_AfterGetAndPutOperations() {
+    LRUCache cache = new LRUCache(4);
+    cache.put(1, 10);
+    cache.put(2, 20);
+    cache.put(3, 30);
+    cache.put(4, 40);
+    cache.get(1);
+    cache.put(5, 50);
+    int expectedValue = -1;
+    int actualValue = cache.get(3);
+    assertEquals(expectedValue, actualValue);
+  }
+}
