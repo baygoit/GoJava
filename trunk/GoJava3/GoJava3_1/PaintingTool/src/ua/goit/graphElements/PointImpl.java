f956fc66b12abcd0609c5adc093ad7b132355ead
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-05 13:12:13 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3012 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/PointImpl.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/graphElements/PointImpl.java
@@ -4,18 +4,28 @@ public class PointImpl implements Point {
     private int x;
     private int y;
 
-    /** Point constructor*/
+    /** 
+     * Point constructor
+     * @param x point x coordinate
+     * @param y point y coordinate
+     */
     public PointImpl(int x, int y) {
 	this.x = x;
 	this.y = y;
     }
 
-    /** Return x coordinate*/
+    /** 
+     * Return x coordinate
+     * @return x coordinate
+     */
     public int getX() {
 	return x;
     }
-    
-    /** Return y coordinate*/
+
+    /**
+     * Return y coordinate
+     * @return y coordinate
+     */
     public int getY() {
 	return y;
     }
