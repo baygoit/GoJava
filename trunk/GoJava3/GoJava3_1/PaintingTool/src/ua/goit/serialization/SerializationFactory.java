0ef3483f70a3f60a49f361e625650d04439a1fc1
antossio1@gmail.com <antossio1@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-05 13:14:44 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3014 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/SerializationFactory.java
+++ trunk/GoJava3/GoJava3_1/PaintingTool/src/ua/goit/serialization/SerializationFactory.java
@@ -1,7 +1,7 @@
 package ua.goit.serialization;
 
 /**
- * Abstract class for serializator creating factory
+ * Abstract class for serializer creating factory
  */
 public abstract class SerializationFactory {
     abstract public Serializer getSerializationFor(SerializationType type);
