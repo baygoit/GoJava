ef51e69d4f4bc2a02fc45cfb85cea7686d59ff7c
zozich2010 <zozich2010@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-06 12:20:00 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3076 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/XMLParserTest.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/XMLParserTest.java
@@ -12,7 +12,7 @@ public class XMLParserTest {
   public void doubleStartTag() throws IOException {
       Parser parser = new MockXMLParser();
       String result = parser.parse("<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
-      String expectedResult = "<?xml><start><tag>text</tag><tag2></tag2></start>";
+      String expectedResult = "<?xml?><start>";
       System.out.println(result);
       assertEquals(expectedResult, result);
   }
