d49bb090f9a036fa649dc96fb2689c927b48f78f
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-06 10:18:33 +0000

    add TestStreamReader
    git-svn-id: http://go-java-1.googlecode.com/svn@3063 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/TestStreamReader.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/TestStreamReader.java
@@ -1,17 +1,44 @@
 package test.ua.goit.alg.xmlparser;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ua.goit.alg.xmlparser.input.StreamReader;
+import java.io.IOException;
+import static org.junit.Assert.assertEquals;
 
 public class TestStreamReader {
 
   @Test
-  public void testRead_String() {
+  public void testStreamReader_read_String() throws IOException {
 
-    String xmlString = "";
-    StreamReader streamReader = new StreamReader(xmlString);
+    final String xmlString = "<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>";
+    StreamReader stream = new StreamReader(xmlString);
 
+    String actual = "";
+    int symbol;
+    do {
+      symbol = stream.read();
+      if (symbol == -1) break;
+      actual += (char) symbol;
+    } while (true);
 
+    assertEquals(xmlString, actual);
+  }
+
+  @Test
+  public void testStreamReader_read_emptyString() throws IOException {
+    final String xmlString = "";
+    StreamReader stream = new StreamReader(xmlString);
+
+    String actual = "";
+    int symbol;
+    do {
+      symbol = stream.read();
+      if (symbol == -1) break;
+      actual += (char) symbol;
+    } while (true);
+
+    assertEquals(xmlString, actual);
   }
 
 }
