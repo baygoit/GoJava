e85dfb0cc2cfadfb0d43fb9ef32054382e79a442
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-06 12:16:03 +0000

    edit  TestStreamReader
    git-svn-id: http://go-java-1.googlecode.com/svn@3075 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/TestStreamReader.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/TestStreamReader.java
@@ -1,24 +1,51 @@
 package test.ua.goit.alg.xmlparser;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ua.goit.alg.xmlparser.input.StreamReader;
-import java.io.IOException;
+
+import java.io.*;
+import java.nio.Buffer;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestStreamReader {
+  private File testXMLFile;
+
+  @Before
+  public void createTempResource() throws IOException {
+
+    String path = System.getProperty("user.dir") + "\\resource\\textXMLFile.xml";
+    testXMLFile = new File(path);
+    testXMLFile.createNewFile();
+    String xmlString = "<?xml version=\"1.0\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>";
+
+    PrintWriter out = null;
+    try {
+      out = new PrintWriter(testXMLFile);
+      out.print(xmlString);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } finally {
+      if(out != null) {
+        out.close();
+      }
+    }
+
+  }
 
   @Test
   public void testStreamReader_read_String() throws IOException {
-
-    final String xmlString = "<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>";
+    String xmlString = "<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>";
     StreamReader stream = new StreamReader(xmlString);
 
     String actual = "";
     int symbol;
     do {
       symbol = stream.read();
-      if (symbol == -1) break;
+      if (symbol == -1) {
+        break;
+      }
       actual += (char) symbol;
     } while (true);
 
@@ -27,17 +54,38 @@ public class TestStreamReader {
 
   @Test
   public void testStreamReader_read_emptyString() throws IOException {
-    final String xmlString = "";
+    String xmlString = "";
     StreamReader stream = new StreamReader(xmlString);
 
     String actual = "";
     int symbol;
     do {
       symbol = stream.read();
-      if (symbol == -1) break;
+      if (symbol == -1) {
+        break;
+      }
       actual += (char) symbol;
     } while (true);
 
     assertEquals(xmlString, actual);
   }
+
+  @Test
+  public void testStreamReader_read_File() throws IOException {
+    String xmlString = "<?xml version=\"1.0\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>";
+    StreamReader stream = new StreamReader(testXMLFile);
+
+    String actual = "";
+    int symbol;
+    do {
+      symbol = stream.read();
+      if (symbol == -1) {
+        break;
+      }
+      actual += (char) symbol;
+    } while (true);
+
+    assertEquals(xmlString, actual);
+  }
+
 }
