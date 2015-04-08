82c9832fc4bb99f6d29cdd85d7e9ded8b0e49ca2
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-08 16:12:20 +0000

    Initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@3163 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/test/ua/goit/xmlparsertdd/AlexSimpleTest.java
@@ -0,0 +1,19 @@
+package ua.goit.xmlparsertdd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AlexSimpleTest {
+  
+  
+  @Before
+  
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
