199e17409484bcf2612f50452ab241639fb425cc
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-08 16:44:13 +0000

    Simple test added
    git-svn-id: http://go-java-1.googlecode.com/svn@3164 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/test/ua/goit/xmlparsertdd/AlexSimpleTest.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/test/ua/goit/xmlparsertdd/AlexSimpleTest.java
@@ -6,14 +6,37 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class AlexSimpleTest {
-  
-  
+  final Tag myTag = new Tag();
+  Handler handler;
+  XMLParser.Builder builder;
+  Parser parser;
+
   @Before
-  
+  public void crateVariables() {
+    builder = XMLParser.Builder.newParserBuilder();
+    parser = builder.build();
+  }
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void givenOpenTag_WhenHandleSetsNameOfTheTag_ThenNameOfTheTagEqualsNameOfTheInputTag() {
+
+    // given
+    String inputString = "<name>";
+
+    // when
+    handler = new Handler() {
+      @Override
+      public void handle(Tag tag) {
+        myTag.setName(tag.getName());
+      }
+    };
+    builder.onOpenTag(handler);
+    parser.parse(inputString);
+    String actual = myTag.getName();
+    String expected = "name";
+
+    // then
+    assertEquals(expected, actual);
   }
 
 }
