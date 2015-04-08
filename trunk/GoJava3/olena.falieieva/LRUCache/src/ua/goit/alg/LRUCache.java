1618dc390b4566dae5389bb23c6bc45a68c97ab9
elena.faleeva@gmail.com <elena.faleeva@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-08 15:35:36 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3160 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/LRUCache.java
+++ trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/LRUCache.java
@@ -1,71 +1,77 @@
 package ua.goit.alg;
-import java.util.HashMap;
 
+import java.util.HashMap;
 
 public class LRUCache {
 
 	private Node head;
 	private Node tail;
-	public int capacity;
+	private final int capacity;
+	private final HashMap<Integer, Node> map;
 
-	HashMap<Integer, Node> map = new HashMap<Integer, Node>();
 
 	public LRUCache(int capacity) { 
+		map = new HashMap<Integer, Node>();
 		this.capacity = capacity;
 	}
 
-	public void put(int key, int value){
-
+	public void put(int key, int value) {
 		if (map.size() == capacity) {
 			map.remove(head.key);
 			removeHead();
 		}
 
-		Node newNode = new Node(key,value);
-		if(!map.containsKey(key)){
-			if (tail!=null){
-				appendToTail(newNode);		
-			} else {
-				head = newNode;
-				tail = head;
-			};
-
-		} else {
+		if (map.containsKey(key)) {
 			Node oldNode = map.get(key);
 			oldNode.value = value;
-			if (oldNode != tail){
+			if (oldNode != tail) {
 				addFromMiddleToTail(oldNode);
 			}
 			if (oldNode == head) {
 				removeHead();
 				appendToTail(oldNode);
 			} 
-
-			oldNode.value = value;
+		} else {
+			Node newNode = new Node(key,value);
+			if (tail != null) {
+				appendToTail(newNode);		
+			} else {
+				head = newNode;
+				tail = head;
+			}
+			map.put(key, newNode);
 		}
-		map.put(key, newNode);
 	}
 
 	public int get(int key) {
-		if(map.containsKey(key)) {
-			Node getted = map.get(key);
-			if (getted == head){
+		if (map.containsKey(key)) {
+			Node gettedNode = map.get(key);
+			if (gettedNode == head) {
 				removeHead();
-				appendToTail(getted);
+				appendToTail(gettedNode);
 			}
-			if (getted !=tail) {
-				addFromMiddleToTail(getted);
+			if (gettedNode != tail) {
+				addFromMiddleToTail(gettedNode);
 			}
-			return tail.value;
+			return gettedNode.value;
 		} else {
 			return -1;
 		}
 	}
-	
+
+	public void cleanCache() {
+		map.clear();
+	}
+
+	public int getCacheSize() {
+		return map.size();
+	}
+
 	private void removeHead() {
 		head = head.next;
 		head.prev = null; 
 	}
+
 	private void addFromMiddleToTail(Node node) {
 		Node prev = node.prev;
 		Node next = node.next;
@@ -85,16 +91,14 @@ public class LRUCache {
 
 	private static class Node {
 		int value;
-		int key;
+		final int key;
 		Node next;
 		Node prev;
-		public Node(int key,int value){
+		public Node(int key,int value) {
 			this.value = value;
 			this.key = key;
 		}
 	}
-
-	
 }
 
 
--- trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/TestCache.java
+++ trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/TestCache.java
@@ -2,38 +2,72 @@ package ua.goit.alg;
 
 import org.junit.*;
 
-public class TestCache{
+public class TestCache {
 
 	LRUCache cache  = new LRUCache(5);
 
 	@Before
-	public void putIntoCache(){
-		cache.put(0,0);
+	public void putIntoCache() {
+		cache.put(0, 0);
+		cache.put(1, 1);
+		cache.put(2, 2);
+		cache.put(3, 3);
+		cache.put(4, 4);
+	}
+
+	@Test	
+	public void getExistValue() {
+		Assert.assertEquals(cache.get(2), 2);
+	}
+
+	@Test	
+	public void getNotExistValue() {
+		Assert.assertEquals(cache.get(6), -1);
+	}
+
+	@Test
+	public void putValue_returnValue() {
+		cache.put(5, 5);
+		Assert.assertEquals(cache.get(5), 5);
+	}
+	@Test
+	public void putLatestValue_returnNotExistLatestValue_deletedFromHead() {
+		cache.put(5, 5);
+		Assert.assertEquals(cache.get(0), -1);
+	}
+
+	@Test
+	public void putValueWithExistedKey_getValueWithExistedKey() {
+		cache.put(3,100500);
+		Assert.assertEquals(cache.get(3), 100500);
+	}
+	
+	@Test
+	public void putValueWithNotExistedKey_getValueWithNotExistedKey() {
+		cache.put(100500,3);
+		Assert.assertEquals(cache.get(100500), 3);
+	}
+	
+	@Test
+	public void cleanCache_returnCacheSize() {
+		cache.cleanCache();
+		Assert.assertEquals(cache.getCacheSize(), 0);
+		
+	}
+	@Test
+	public void cleanCache_returnNotExistedVal() {
+		cache.cleanCache();
+		Assert.assertEquals(cache.get(3), -1);
+		
+	}
+	
+	@Test
+	public void putValueToEmtyCache_chekSize() {
+		cache.cleanCache();
 		cache.put(1,1);
 		cache.put(2,2);
-		cache.put(3,3);
-		cache.put(4,4);
+		Assert.assertEquals(cache.getCacheSize(), 2);
 	}
-
-	@Test	
-	public void getExistValue () {
-			Assert.assertEquals(cache.get(2), 2);
-	}
-
-	@Test	
-	public void getNotExistValue () {
-			Assert.assertEquals(cache.get(6), -1);
-	}
-
-	@Test
-	public void putValue_returnExistLatestValue(){
-			cache.put(5,5);
-			Assert.assertEquals(cache.get(5), 5);
-	}
-	@Test
-	public void putLatestValue_returnNotExistLatestValue_deletedFromHead(){
-			cache.put(5,5);
-			Assert.assertEquals(cache.get(0), -1);
-	}
-
 }
+
+
