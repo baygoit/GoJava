f7eddd3582acc5762a1e2f6fa69999aed5df56a2
shramko82@gmail.com <shramko82@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-08 16:56:50 +0000

    simple test added
    git-svn-id: http://go-java-1.googlecode.com/svn@3166 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/test/ua/goit/xmlparsertdd/AlexSimpleTest.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/test/ua/goit/xmlparsertdd/AlexSimpleTest.java
@@ -18,7 +18,7 @@ public class AlexSimpleTest {
   }
 
   @Test
-  public void givenOpenTag_WhenHandleSetsNameOfTheTag_ThenNameOfTheTagEqualsNameOfTheInputTag() {
+  public void givenOpenTag_WhenHandleSetsNameOfTag_ThenNameOfTheTagEqualsNameOfTheInputTag() {
 
     // given
     String inputString = "<name>";
@@ -38,5 +38,51 @@ public class AlexSimpleTest {
     // then
     assertEquals(expected, actual);
   }
+  
+  @Test
+  public void givenOpenTag_WhenHandleSetsTypeOfTag_ThenTypeOfTheTagEqualsOPEN() {
+
+    // given
+    String inputString = "<name>";
+
+    // when
+    handler = new Handler() {
+      @Override
+      public void handle(Tag tag) {
+        myTag.setType(tag.getType());
+      }
+    };
+    builder.onOpenTag(handler);
+    parser.parse(inputString);
+    TagType actual = myTag.getType();
+    TagType expected = TagType.OPEN;
+
+    // then
+    assertEquals(expected, actual);
+  }
+  
+  @Test
+  public void givenCloseTag_WhenHandleSetsTypeOfTag_ThenTypeOfTheTagEqualsCLOSE() {
+
+    // given
+    String inputString = "</name>";
+
+    // when
+    handler = new Handler() {
+      @Override
+      public void handle(Tag tag) {
+        myTag.setType(tag.getType());
+      }
+    };
+    builder.onEnd(handler);
+    parser.parse(inputString);
+    TagType actual = myTag.getType();
+    TagType expected = TagType.CLOSE;
+
+    // then
+    assertEquals(expected, actual);
+  }
+  
+  
 
 }
