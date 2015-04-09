8a333264ccef09cc60b334719cfc14ef226b7ee5
cockroa4es@gmail.com <cockroa4es@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-09 20:14:45 +0000

    edit ParserTest
    git-svn-id: http://go-java-1.googlecode.com/svn@3194 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/ParserTest.java
+++ trunk/GoJava3/GoJava3_2/XMLParser/src/test/ua/goit/alg/xmlparser/ParserTest.java
@@ -1,8 +1,6 @@
 package test.ua.goit.alg.xmlparser;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -11,137 +9,116 @@ import ua.goit.alg.xmlparser.parser.Parser;
 
 public class ParserTest {
 
-  @Test
-  public void whenCompareInside() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<s><t t=\"<1\"></t></s>");
-    String expectedResult = "<s><t></t></s>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void when2Open2Close() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<s><t></t></s>");
-    String expectedResult = "<s><t></t></s>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenAttr() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<start atr1=\"3\"><tag></tag><tag2></tag2></start>");
-    String expectedResult = "<start><tag></tag><tag2></tag2></start>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenClosableTag() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<start atr1=\"3\"><tag></tag><tag2/></start>");
-    String expectedResult = "<start><tag></tag><tag2></tag2></start>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenText() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<start atr1=\"3\"><tag>text</tag><tag2/></start>");
-    String expectedResult = "<start><tag>text</tag><tag2></tag2></start>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenFull() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
-    String expectedResult = "<?xml?><start><tag>text</tag><tag2></tag2></start>";
-    Assert.assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenDoubleStartTag() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String expectedResult = "<?xml?><start>";
-    String result = parser.parse("<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><?xml doctype=\"1\"><tag>text</tag><tag2/></start>");
-    assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void whenCloseTag_notEquals_OpenTag() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String expectedResult = "<s><t></o>";
-    String result = parser.parse("<s><t></o></r>");
-    assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void attributeWithoutValue() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String expectedResult = "<?xml?><start>";
-    String result = parser.parse("<start atr1 atr2=\"4\"></start>");
-    assertEquals(expectedResult, result);
-  }
-
-  @Test
-  public void parsingTwoStringsWithOneParser() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    String result1 = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
-    String expectedResult = "<?xml?><start><tag>text</tag><tag2></tag2></start>";
-    Assert.assertEquals(expectedResult, result1);
-
-    String result2 = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
-    Assert.assertEquals(expectedResult, result2);
-  }
-
-  @Test
-  public void invalidCloseTag() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    try {
-      String result = parser.parse("<s/s>");
-      fail();
-    } catch (RuntimeException e) {
-      String errorMessage = "Invalid format error";
-      assertEquals(e.getMessage(), errorMessage);
-    } catch (Exception e) {
-      fail();
+    @Test
+    public void whenCompareInside() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<s><t t=\"<1\"></t></s>");
+        String expectedResult = "<s><t></t></s>";
+        Assert.assertEquals(expectedResult, result);
     }
-  }
 
-  @Test
-  public void invalidTagName() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    try {
-      String result = parser.parse("<C++></C++>");
-      fail();
-    } catch (RuntimeException e) {
-      String errorMessage = "Invalid format error";
-      assertEquals(e.getMessage(), errorMessage);
-    } catch (Exception e) {
-      fail();
+    @Test
+    public void when2Open2Close() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<s><t></t></s>");
+        String expectedResult = "<s><t></t></s>";
+        Assert.assertEquals(expectedResult, result);
     }
-  }
 
-  @Test
-  public void tagNameWithInvalidSpace() throws IOException {
-    Parser parser = new ParserBuilderForFullTesting();
-    try {
-      String result = parser.parse("<star t></start>");
-      fail();
-    } catch (RuntimeException e) {
-      String errorMessage = "Invalid format error";
-      assertEquals(e.getMessage(), errorMessage);
-    } catch (Exception e) {
-      fail();
+    @Test
+    public void whenAttr() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<start atr1=\"3\"><tag></tag><tag2></tag2></start>");
+        String expectedResult = "<start><tag></tag><tag2></tag2></start>";
+        Assert.assertEquals(expectedResult, result);
     }
-  }
 
-  @Test
-  public void atributesAndValues() throws IOException {
-    Parser parser = new ParserBuilderForAtributeTesting();
-    String result = parser.parse("<start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
-    String expectedResult = "<start atr1=\"3\" atr2=\"4\"><tag><tag2>";
-    assertEquals(expectedResult, result);
-  }
+    @Test
+    public void whenClosableTag() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<start atr1=\"3\"><tag></tag><tag2/></start>");
+        String expectedResult = "<start><tag></tag><tag2></tag2></start>";
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void whenText() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<start atr1=\"3\"><tag>text</tag><tag2/></start>");
+        String expectedResult = "<start><tag>text</tag><tag2></tag2></start>";
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void whenFull() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
+        String expectedResult = "<?xml?><start><tag>text</tag><tag2></tag2></start>";
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void whenDoubleStartTag() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String expectedResult = "<?xml?><start>";
+        String result = parser.parse("<?xml doctype=\"1\"><start atr1=\"3\" atr2 = \"4\"><?xml doctype=\"1\"><tag>text</tag><tag2/></start>");
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void whenCloseTag_notEquals_OpenTag() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String expectedResult = "<s><t></o>";
+        String result = parser.parse("<s><t></o></r>");
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void attributeWithoutValue() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String expectedResult = "<?xml?><start>";
+        String result = parser.parse("<start atr1 atr2=\"4\"></start>");
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void parsingTwoStringsWithOneParser() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result1 = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
+        String expectedResult = "<?xml?><start><tag>text</tag><tag2></tag2></start>";
+        Assert.assertEquals(expectedResult, result1);
+
+        String result2 = parser.parse("<?xml doctype=\"1\"?><start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
+        Assert.assertEquals(expectedResult, result2);
+    }
+
+    @Test
+    public void invalidCloseTag() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<s/s>");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void invalidTagName() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<C++></C++>");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void tagNameWithInvalidSpace() throws IOException {
+        Parser parser = new ParserBuilderForFullTesting();
+        String result = parser.parse("<star t></start>");
+        assertEquals("", result);
+    }
+
+    @Test
+    public void atributesAndValues() throws IOException {
+        Parser parser = new ParserBuilderForAtributeTesting();
+        String result = parser.parse("<start atr1=\"3\" atr2 = \"4\"><tag>text</tag><tag2/></start>");
+        String expectedResult = "<start atr1=\"3\" atr2=\"4\"><tag><tag2>";
+        assertEquals(expectedResult, result);
+    }
 }
   
