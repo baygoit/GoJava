30e9ccd1f5ac1402a5069d68adb4cae692987008
elena.faleeva@gmail.com <elena.faleeva@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-10 10:31:23 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3198 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/TestCache.java
+++ trunk/GoJava3/olena.falieieva/LRUCache/src/ua/goit/alg/TestCache.java
@@ -1,52 +1,54 @@
 package ua.goit.alg;
 
-import org.junit.*;
-    
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestCache {
 
-	LRUCache cache  = new LRUCache(5);
+    LRUCache cache  = new LRUCache(5);
 
-	@Before
-	public void putIntoCache() {
-		cache.set(0, 0);
-		cache.set(1, 1);
-		cache.set(2, 2);
-		cache.set(3, 3);
-		cache.set(4, 4);
-	}
-	@Test	
-	public void getExistValue() {
-		Assert.assertEquals(cache.get(2), 2);
-	}
-	@Test	
-	public void getNotExistValue() {
-		Assert.assertEquals(cache.get(6), -1);
-	}
-	@Test
-	public void setValue_returnValue() {
-		cache.set(5, 5);
-		Assert.assertEquals(cache.get(5), 5);
-	}
-	@Test
-	public void setLatestUsedValue_returnNotExistLatestValue_deletedFromHead() {
-		cache.set(5, 5);
-		Assert.assertEquals(cache.get(0), -1);
-	}
-	@Test
-	public void setValueWithExistedKey_getValueWithExistedKey() {
-		cache.set(3, 100500);
-		Assert.assertEquals(cache.get(3), 100500);
-	}	
+    @Before
+    public void putIntoCache() {
+	cache.set(0, 0);
+	cache.set(1, 1);
+	cache.set(2, 2);
+	cache.set(3, 3);
+	cache.set(4, 4);
+    }
+    @Test	
+    public void getExistValue() {
+	Assert.assertEquals(cache.get(2), 2);
+    }
+    @Test	
+    public void getNotExistValue() {
+	Assert.assertEquals(cache.get(6), -1);
+    }
+    @Test
+    public void setValue_returnValue() {
+	cache.set(5, 5);
+	Assert.assertEquals(cache.get(5), 5);
+    }
+    @Test
+    public void setLatestUsedValue_returnNotExistLatestValue_deletedFromHead() {
+	cache.set(5, 5);
+	Assert.assertEquals(cache.get(0), -1);
+    }
+    @Test
+    public void setValueWithExistedKey_getValueWithExistedKey() {
+	cache.set(3, 100500);
+	Assert.assertEquals(cache.get(3), 100500);
+    }	
 
-	@Test
-	public void getFourValues_setOneValueWithNotExistedKey_getLatestUsedValue() {
-		cache.get(1);
-		cache.get(2);
-		cache.get(3);
-		cache.get(0);
-		cache.set(7, 7);
-		Assert.assertEquals(cache.get(4), -1);
-	}
+    @Test
+    public void getFourValues_setOneValueWithNotExistedKey_getLatestUsedValue() {
+	cache.get(1);
+	cache.get(2);
+	cache.get(3);
+	cache.get(0);
+	cache.set(7, 7);
+	Assert.assertEquals(cache.get(4), -1);
+    }
 }
 
 
