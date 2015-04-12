50e8c7e1531e50f082eee66172420ac6601bfc03
webnet75@gmail.com <webnet75@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-12 16:11:44 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3252 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/volodymyr.ialoviy/SpringHibernateMaven/src/main/java/ua/com/goit/gojava2/vova/kickstarter/service/DonationService.java
@@ -0,0 +1,9 @@
+package ua.com.goit.gojava2.vova.kickstarter.service;
+
+import ua.com.goit.gojava2.vova.kickstarter.model.Donation;
+
+public interface  DonationService {
+	
+	void saveDonation(Donation donation);
+	
+}
