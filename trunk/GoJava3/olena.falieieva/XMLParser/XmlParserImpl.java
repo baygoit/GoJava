f9c8da9a61fb9b4cf1faaed48f1846bf88ee6a1b
elena.faleeva@gmail.com <elena.faleeva@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-12 23:49:47 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3264 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/olena.falieieva/XMLParser/XmlParserImpl.java
@@ -0,0 +1,67 @@
+package ua.goit.alg.parser;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import ua.goit.alg.parser.StateMachine.State;
+
+public class XmlParserImpl implements XmlParser {
+    private Map<State, Handler> handlers = new HashMap<State, Handler>();
+    StateMachine stateMachine = new StateMachine();
+
+    public void parse(String xml) throws IOException {
+	parser(new ByteArrayInputStream(xml.getBytes()));
+    }
+
+    public void parse(File filePath) throws IOException {
+	if (filePath.isFile() && filePath.exists()) {
+	    FileInputStream readFromFile = new FileInputStream(filePath);
+	    parser(readFromFile);
+	}
+    }
+
+    private void parser(InputStream readFromStream) throws IOException {
+	char c = 0;
+	while ((c = (char) readFromStream.read()) != -1) {
+	    stateMachine.next(c);
+	}
+	Tag emptyTag = new Tag();
+	handle(State.END, emptyTag);
+    }
+
+    public void onOpenTag(Handler handler) {
+	handlers.put(State.OPEN_TAG, handler);
+    }
+
+    public void onCloseTag(Handler handler) {
+	handlers.put(State.CLOSE_TAG, handler);
+    }
+
+    public void onTextValue(Handler handler) {
+	handlers.put(State.ON_TEXT, handler);
+    }
+
+    public void onStart(Handler handler) {
+	handlers.put(State.START, handler);
+    }
+
+    public void onEnd(Handler handler) {
+	handlers.put(State.END, handler);
+    }
+
+    public void onError(Handler handler) {
+	handlers.put(State.INVALID_END, handler);
+    }
+
+    public void handle(State state, Tag tag) {
+	Handler handel = handlers.get(state);
+	if (handel != null) {
+	    handel.handle(tag);
+	}
+    }
+}
