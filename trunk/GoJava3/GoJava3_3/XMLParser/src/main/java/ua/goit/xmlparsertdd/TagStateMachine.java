d18e5586a08f1aa6307b8fca60077eeb4e9ad043
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-15 13:32:39 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3321 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/main/java/ua/goit/xmlparsertdd/TagStateMachine.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/main/java/ua/goit/xmlparsertdd/TagStateMachine.java
@@ -1,14 +1,33 @@
 package ua.goit.xmlparsertdd;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class TagStateMachine {
+  
+  final static Set<TagState> commentStates= new HashSet<>();
+  static{
+    commentStates.add(TagState.COMMENT);
+    commentStates.add(TagState.OPEN);
+    commentStates.add(TagState.EXCLAM_MARK);
+    commentStates.add(TagState.HYPHEN_BEFORE);
+    commentStates.add(TagState.FIRST_HYPHEN_AFTER);
+    commentStates.add(TagState.SECOND_HYPHEN_AFTER);
+    commentStates.add(TagState.CLOSE);
+    commentStates.add(TagState.VALID_COMMENT_END);
+  }
+  
   TagState currentState = TagState.INIT;
   private TagElement.Builder tagBuilder = TagElement.Builder.newBuilder();
   private TextElement.Builder textBuilder = TextElement.Builder.newBuilder();
   private ErrorElement.Builder errorBuilder = ErrorElement.Builder.newBuilder();
-
+  private boolean hasNotSentTextValue = false;
+  
   public TagState next(char c, XMLParser parser) {
     TagState previousState = currentState;
     currentState = currentState.next(c, tagBuilder);
+
+    
     if (currentState == TagState.VALID_TAG_END) {
       createTagElement(parser);
     } else if (currentState == TagState.TEXT_VALUE) {
@@ -17,13 +36,36 @@ public class TagStateMachine {
       String textError = "XMLSyntaxException caught";
       errorBuilder.setErrorMessage(textError);
       parser.sendEventToHandler(Event.INVALID_END, errorBuilder.build());
-    } else if (previousState != currentState) {
+    } else if (currentState == TagState.INVALID_TAG_END) {
+      String textError = "XMLSyntaxException caught";
+      errorBuilder.setErrorMessage(textError);
+      parser.sendEventToHandler(Event.INVALID_END, errorBuilder.build());
+    } else if (currentState == TagState.INVALID_TAG_END) {
+      String textError = "XMLSyntaxException caught";
+      errorBuilder.setErrorMessage(textError);
+      parser.sendEventToHandler(Event.INVALID_END, errorBuilder.build());
+    } 
+    
+    
+    if (previousState != currentState) {
       if (previousState == TagState.TEXT_VALUE) {
-        parser.sendEventToHandler(Event.TEXT_VALUE, textBuilder.build());
-        textBuilder.resetTextValue();
+        hasNotSentTextValue = true;
+        //parser.sendEventToHandler(Event.TEXT_VALUE, textBuilder.build());
+        //textBuilder.resetTextValue();
       }
     }
+    
+    if(hasNotSentTextValue && !commentStates.contains(currentState) && currentState != TagState.TEXT_VALUE){
+      parser.sendEventToHandler(Event.TEXT_VALUE, textBuilder.build());
+      textBuilder.resetTextValue();
+      hasNotSentTextValue = false;      
+    } else if (currentState == TagState.INVALID_TAG_END) {
+      String textError = "XMLSyntaxException caught";
+      errorBuilder.setErrorMessage(textError);
+      parser.sendEventToHandler(Event.INVALID_END, errorBuilder.build());
+    }
     return currentState;
+ 
   }
 
   private void createTagElement(XMLParser parser) {
