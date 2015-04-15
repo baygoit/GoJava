bdb5bbe87b63a3b7048e9b03f7786355672d4c7f
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-15 18:10:04 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3332 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/main/java/ua/goit/xmlparsertdd/TagStack.java
@@ -0,0 +1,35 @@
+package ua.goit.xmlparsertdd;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class TagStack {
+  Deque<TagElement> tagStack = new LinkedList<TagElement>();
+  boolean hadRootTag = false;
+
+  void push(TagElement tag) throws XMLNestingException {
+
+    if (tag.getType() == TagElementType.HEADER && hadRootTag) {
+      throw new XMLNestingException("Error nesting header tag - " + tag);
+    }
+
+    if (tag.getType() == TagElementType.OPEN || tag.getType() == TagElementType.SINGLE) {
+      if (tagStack.size() == 0) {
+        if (hadRootTag) {
+          throw new XMLNestingException("More than one root - " + tag);
+        } else {
+          hadRootTag = true;
+        }
+      }
+      tagStack.addLast(tag);
+    }
+  }
+
+  TagElement get() {
+    return tagStack.getLast();
+  }
+
+  TagElement pop() {
+    return tagStack.pollLast();
+  }
+}
