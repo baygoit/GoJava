571e0f3d43b9f6565aad163386b55c306777d42b
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-16 11:35:51 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3346 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/main/java/ua/goit/xmlparsertdd/TagStack.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/main/java/ua/goit/xmlparsertdd/TagStack.java
@@ -6,14 +6,22 @@ import java.util.LinkedList;
 public class TagStack {
   Deque<TagElement> tagStack = new LinkedList<>();
   boolean hadRootTag = false;
+  boolean hadHeaderTag = false;
 
   void push(TagElement tag) throws XMLNestingException {
 
-    if (tag.getType() == TagElementType.HEADER && hadRootTag) {
-      throw new XMLNestingException("Error nesting header tag - " + tag);
+    boolean headerAllowed = !hadRootTag && !hadHeaderTag;
+
+    if (tag.getType() == TagElementType.HEADER) {
+      if (headerAllowed) {
+        hadHeaderTag = true;
+      } else {
+        throw new XMLNestingException("Error nesting header tag - " + tag);
+      }
     }
 
-    if (tag.getType() == TagElementType.OPEN || tag.getType() == TagElementType.SINGLE) {
+    if (tag.getType() == TagElementType.OPEN
+        || tag.getType() == TagElementType.SINGLE) {
       if (tagStack.size() == 0) {
         if (hadRootTag) {
           throw new XMLNestingException("More than one root - " + tag);
