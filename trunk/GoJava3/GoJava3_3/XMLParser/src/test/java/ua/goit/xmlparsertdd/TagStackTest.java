401d1cec77b5a85990baf04f9a8b4fd25e1a7a28
tishenko.sergey@gmail.com <tishenko.sergey@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-16 11:36:02 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3347 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava3/GoJava3_3/XMLParser/src/test/java/ua/goit/xmlparsertdd/TagStackTest.java
+++ trunk/GoJava3/GoJava3_3/XMLParser/src/test/java/ua/goit/xmlparsertdd/TagStackTest.java
@@ -1,33 +1,51 @@
 package ua.goit.xmlparsertdd;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import org.junit.Test;
+
 
 public class TagStackTest {
   
-  void runMachine(String testString) throws XMLNestingException{
+  TagState runMachine(String testString) throws XMLNestingException{
     XMLParser.Builder builder = XMLParser.Builder.newParserBuilder();
     XMLParser parser = builder.build();
     TagStateMachine machine = new TagStateMachine();
+    TagState currentState = TagState.INIT;
     char c;
     try (InputStreamReader inputStreamReader = new InputStreamReader(new ByteArrayInputStream(testString.getBytes()))) {
         while (inputStreamReader.ready()) {
         c = (char) inputStreamReader.read();
-        TagState currentState = machine.next(c, parser);
+        currentState = machine.next(c, parser);
         }
       } catch (IOException e) {
         e.printStackTrace();
       }
+    return currentState;
   }
   
   @Test (expected = XMLNestingException.class)
-  public void Error_on_DoubleHeader() throws XMLNestingException, IOException {
+  public void Error_on_Header_Tag_Header() throws XMLNestingException, IOException {
     String  testString = " < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?> <aaa par1=\"par_value1\" par2=\'par_value2\'> < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?><bbbb par3='val3'/>bla1<!-- comment -->bla2</aaa>";
     runMachine(testString);
+  }
+  
+  @Test (expected = XMLNestingException.class)
+  public void Error_on_DoubleHeader_Tags() throws XMLNestingException, IOException {
+    String  testString = " < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?> < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?><bbbb par3='val3'/>bla1<!-- comment -->bla2</aaa>";
+    runMachine(testString);
+  }
+  @Test   (expected = XMLNestingException.class)
+  public void Error_on_DoubleHeader() throws XMLNestingException, IOException {
+    String  testString = " < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?> < ?xml version=\"1.0\" \n encoding=\"UTF-8\"?>";
+    
+    TagState state = runMachine(testString);
+    
+    assertEquals(state,TagState.INVALID_TAG_END);
   }
   
   @Test (expected = XMLNestingException.class)
