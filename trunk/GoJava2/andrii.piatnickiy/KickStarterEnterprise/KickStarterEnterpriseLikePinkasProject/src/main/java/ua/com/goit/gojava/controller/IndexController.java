6ac5c8d9448f05e97ce1686d13268325e7fb4a3b
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-17 10:24:17 +0000

    Simple index.html with spring mvc
    git-svn-id: http://go-java-1.googlecode.com/svn@3357 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava2/andrii.piatnickiy/KickStarterEnterprise/KickStarterEnterpriseLikePinkasProject/src/main/java/ua/com/goit/gojava/controller/IndexController.java
@@ -0,0 +1,12 @@
+package ua.com.goit.gojava.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	@RequestMapping("/index")
+	public String index(){
+		return "/WEB-INF/jsp/index.jsp";
+	}
+}
