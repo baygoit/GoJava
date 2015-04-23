4493dac6f0238f7173b4a245764d22027581ba27
pyatnitskiy.a@gmail.com <pyatnitskiy.a@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-23 15:48:48 +0000

    add Project entity. add relationships. install jpa editor.
    git-svn-id: http://go-java-1.googlecode.com/svn@3385 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava2/andrii.piatnickiy/KickStarterEnterprise/KickStarterEnterpriseLikePinkasProject/src/main/java/ua/com/goit/gojava/entity/Category.java
+++ trunk/GoJava2/andrii.piatnickiy/KickStarterEnterprise/KickStarterEnterpriseLikePinkasProject/src/main/java/ua/com/goit/gojava/entity/Category.java
@@ -1,9 +1,12 @@
 package ua.com.goit.gojava.entity;
 
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Category {
@@ -11,5 +14,34 @@ public class Category {
 	@Id
 	@GeneratedValue
 	private Integer id;
+	
+	private String name;
+	
+	@OneToMany(mappedBy="category")
+	private List<Project> projects;
+	
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(List<Project> projects) {
+		this.projects = projects;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
 }
