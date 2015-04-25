a79655bf3ec8aa1552c14da9829918b37385ced4
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-25 11:59:34 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3410 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/max.gladovskyy/JavaBasics/TwoMinDistance/src/TwoMinDistanceClass.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+
+public class TwoMinDistanceClass {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> list = new ArrayList<Integer>();
+		int aval,apos,bval,bpos;
+		String temp;
+		
+		System.out.println("Enter integers pressing Enter after each. When finish press Enter after blank field");
+		
+		while(true) {
+			temp = reader.readLine();
+			if (temp.equals("")) break;
+			list.add(Integer.parseInt(temp));
+		}
+		
+		aval=list.get(0);
+		bval=list.get(1);
+		apos=0;
+		bpos=1;
+		
+		for (int i=1; i<list.size(); i++) {
+			if (list.get(i)<aval) {
+				bval = aval;
+				bpos = apos;
+				aval = list.get(i);
+				apos = i;
+				continue;
+			}
+			
+			if (list.get(i)>=aval && list.get(i)<bval) {
+				bval = list.get(i);
+				bpos = i;
+			}
+		}
+		
+		System.out.println("Distance is: "+Math.abs(apos-bpos));
+
+	}
+
+}
