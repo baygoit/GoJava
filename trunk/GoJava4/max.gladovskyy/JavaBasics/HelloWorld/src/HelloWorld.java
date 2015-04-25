a3c95dadc5784cffe14ac53a7b4b4bae95f82a71
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-25 11:04:56 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3408 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/max.gladovskyy/JavaBasics/HelloWorld/src/HelloWorld.java
+++ trunk/GoJava4/max.gladovskyy/JavaBasics/HelloWorld/src/HelloWorld.java
@@ -1,10 +1,6 @@
-/*   
-   This demonstrates a variable.  
  
-   Call this file Example2.java.  
-*/   
 class HelloWorld {   
 	public static void main(String args[]) {
-			 System.out.println("Hello world");
+		System.out.println("Hello world");
 	}
 }
