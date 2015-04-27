e9878a310bee05122f3ddcf4bec014d80826ec04
nissserge@gmail.com <nissserge@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-27 09:47:32 +0000

    Anagrams maker class
    git-svn-id: http://go-java-1.googlecode.com/svn@3450 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/serge.savin/Anagramms/com/sergiisavin/AnagramsMaker.java
@@ -0,0 +1,63 @@
+/*
+ * Service class. Makes anagram
+ * spaces between words are reduced to one space
+ * spaces at the beginning and the end of the line are trimmed
+ */
+
+package com.sergiisavin;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class AnagramsMaker {
+
+	public static String makeAnagram(String inputString){
+		String anagram = null;
+		
+		String[] words = inputString.split(" ");
+		
+		reverseWords(words);
+
+		anagram = constructAnagram(words);
+		
+		return anagram;
+	}
+
+	private static String constructAnagram(String[] words) {
+		String anagram = null;
+		
+		//in our anagram will be one space between words
+		StringBuilder sb = new StringBuilder();
+		for(String s : words){
+			sb.append(s);
+			sb.append(" ");
+		}
+		//there will be no spaces at the beginning and the end
+		anagram = sb.toString().trim();
+		return anagram;
+	}
+
+	//reverse word as char array 
+	private static void reverseWords(String[] words) {
+		
+		for(int i = 0; i < words.length; i++){
+			
+			words[i] = words[i].trim();
+			char[] wordInChars = words[i].toCharArray();
+			
+			int lastIndex = wordInChars.length - 1;
+			for(int j = 0; j < (wordInChars.length)/2; j++){
+				
+				char temp = wordInChars[j];
+				wordInChars[j] = wordInChars[lastIndex - j];
+				wordInChars[lastIndex - j] = temp;
+				
+			}
+			
+			words[i] = new String(wordInChars);
+			
+		}
+		
+	}
+}
