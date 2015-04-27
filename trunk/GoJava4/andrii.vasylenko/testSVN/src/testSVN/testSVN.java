bbeccf0c8fec7fac173a522b1a2f3f6127d24c04
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-27 20:27:32 +0000

    Test SVN && eclipse: second commit;)
    git-svn-id: http://go-java-1.googlecode.com/svn@3468 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/andrii.vasylenko/testSVN/src/testSVN/testSVN.java
+++ trunk/GoJava4/andrii.vasylenko/testSVN/src/testSVN/testSVN.java
@@ -5,6 +5,7 @@ public class testSVN {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello SVN!!");
+		System.out.println("Hello, Hello!!");
 	}
 
 }
