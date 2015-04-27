aeab16aa0da28bbed7b4f38314bed4872511413f
nissserge@gmail.com <nissserge@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-27 09:46:34 +0000

    User Interface class
    git-svn-id: http://go-java-1.googlecode.com/svn@3449 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/serge.savin/Anagramms/com/UserInterface.java
@@ -0,0 +1,47 @@
+/*
+ * User Interface. Prompts input data 
+ * Provides a way to finish the program
+ * displays the result
+ * 
+ * 
+ */
+
+package com.sergiisavin;
+
+import java.util.Scanner;
+
+public class UserInterface {
+
+	public static void main(String[] args){
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String inputData = null;
+		
+		do{
+			
+			System.out.print("Do you want to continue? (y/n): ");
+			
+			inputData = scanner.nextLine();
+			
+			inputData = inputData.trim();
+			inputData = inputData.toLowerCase();
+			
+			switch(inputData){
+			
+			case "n":
+				System.out.println("Goodby!");
+				System.exit(0);
+				break;
+			case "y":
+				System.out.print("Enter some text: ");
+				inputData = scanner.nextLine();
+				inputData = inputData.trim();
+				System.out.println("The anagram: " + AnagramsMaker.makeAnagram(inputData));
+				break;
+			}
+							
+			
+		}while(true);
+	}
+}
