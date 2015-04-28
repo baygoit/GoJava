0f5a2262ebaa7dd1bb8fbffdc7227fb012ee6b50
Pozharskyi@gmail.com <Pozharskyi@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-28 10:01:36 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3511 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/ivan.pozharskyi/Anagrama/src/Anagrama.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Anagrama {
+
+	public static void main(String[] args) {
+
+		System.out.println("Enter words:");
+		String enteredData = readFromConsole();
+
+		String[] result = makeAnagrama(enteredData.split(" "));
+		printResult(result);
+	}
+
+	private static String readFromConsole() {
+
+		Scanner scanner;
+		scanner = new Scanner(System.in);
+		String enteredData = scanner.nextLine();
+		scanner.close();
+
+		return enteredData;
+	}
+
+	private static void printResult(String[] result) {
+		
+		System.out.println("Result:");
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + " ");
+		}
+	}
+
+	private static String[] makeAnagrama(String[] enteredData) {
+		String[] result = enteredData;
+		for (int i = 0; i < enteredData.length; i++) {
+
+			char[] words = enteredData[i].toCharArray();
+			char[] reverseWord = reversingWord(words);
+			result[i] = String.valueOf(reverseWord);
+
+		}
+		return result;
+
+	}
+
+	private static char[] reversingWord(char[] words) {
+
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < words.length; i++) {
+			stack.push(words[i]);
+		}
+
+		char[] reverseWord = words;
+		for (int i = 0; i < words.length; i++) {
+			reverseWord[i] = (char) stack.pop();
+
+		}
+
+		return reverseWord;
+	}
+}
