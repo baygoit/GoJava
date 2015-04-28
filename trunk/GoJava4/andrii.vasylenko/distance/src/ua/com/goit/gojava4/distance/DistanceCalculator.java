a19b99428309f725f9d2a5d2ada17dad2654e29d
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-28 19:33:55 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3576 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/andrii.vasylenko/distance/src/ua/com/goit/gojava4/distance/DistanceCalculator.java
+++ trunk/GoJava4/andrii.vasylenko/distance/src/ua/com/goit/gojava4/distance/DistanceCalculator.java
@@ -6,6 +6,10 @@ import java.io.InputStreamReader;
 
 public class DistanceCalculator {
 	public static final String SEPARATOR = " ";
+	public static final int MINIMUM_ARRAY_SIZE = 2;
+	public static final int FIRST_INIT_INDEX = 0;
+	public static final int SECOND_INIT_INDEX = 0;
+
 	private String lineOfNumbers;
 
 	public DistanceCalculator(String lineOfNumbers) {
@@ -28,41 +32,68 @@ public class DistanceCalculator {
 	}
 
 	public int getDistance() throws NumberFormatException {
-		return getDistance(lineOfNumbers.split(" "));
-	}
-
-	public int getDistance(String[] numbers) throws NumberFormatException {
-		int[] ints = new int[numbers.length];
-		for (int i = 0; i < ints.length; i++) {
-			ints[i] = Integer.parseInt(numbers[i]);
-		}
-		return getDistance(ints);
-	}
-
-	public int getDistance(int[] numbers) throws NumberFormatException {
-		if (numbers.length < 2)
-			throw new NumberFormatException();
+		int[] numbers = getNumbers(lineOfNumbers);
+		check(numbers);
 		return calculateDistance(numbers);
 	}
 
+	private void check(int[] numbers) throws NumberFormatException {
+		if (numbers.length < MINIMUM_ARRAY_SIZE)
+			throw new NumberFormatException();
+	}
+
+	private int[] getNumbers(String numbers) throws NumberFormatException {
+		return convert(lineOfNumbers.split(SEPARATOR));
+	}
+
+	private int[] convert(String[] numbers) throws NumberFormatException {
+		int[] result = new int[numbers.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = Integer.parseInt(numbers[i]);
+		}
+		return result;
+	}
+
 	private int calculateDistance(int[] numbers) {
-		int indexOfMin1 = 0;
-		int indexOfMin2 = 1;
-		for (int i = 2; i < numbers.length; i++) {
-			if (numbers[indexOfMin1] > numbers[i]) {
-				if (numbers[indexOfMin2] > numbers[indexOfMin1]) {
-					indexOfMin2 = indexOfMin1;
-				}
-				indexOfMin1 = i;
-			} else if (numbers[indexOfMin2] > numbers[i]) {
-				if (numbers[indexOfMin1] > numbers[indexOfMin2]) {
-					indexOfMin1 = indexOfMin2;
-				}
-				indexOfMin2 = i;
+		int indexOfFirstMinimum = FIRST_INIT_INDEX;
+		int indexOfSecondMinimum = SECOND_INIT_INDEX;
+		calculateIndexsOfTwoMinimums(indexOfFirstMinimum, indexOfSecondMinimum, numbers);
+		
+		int result = indexOfSecondMinimum - indexOfFirstMinimum;
+		if (indexOfSecondMinimum < indexOfFirstMinimum) {
+			result *= -1;
+		}
+		return result;
+	}
+
+	private void calculateIndexsOfTwoMinimums(int indexOfFirstMinimum,
+			int indexOfSecondMinimum, int[] numbers) {
+		for (int index = 2; index < numbers.length; index++) {
+			if (checkIndexOfMinimum(indexOfFirstMinimum, index, numbers)) {
+				setIndexOfMinimum(indexOfFirstMinimum, indexOfSecondMinimum,
+						index, numbers);
+			} else if (checkIndexOfMinimum(indexOfSecondMinimum, index, numbers)) {
+				setIndexOfMinimum(indexOfSecondMinimum, indexOfFirstMinimum,
+						index, numbers);
 			}
 		}
+	}
 
-		return indexOfMin2 > indexOfMin1 ? indexOfMin2 - indexOfMin1
-				: indexOfMin1 - indexOfMin2;
+	private void setIndexOfMinimum(int indexOfMinimum,
+			int indexOfAdditionalMinimum, int currentIndex, int[] numbers) {
+
+		if (numbers[indexOfAdditionalMinimum] > numbers[indexOfMinimum]) {
+			indexOfAdditionalMinimum = indexOfMinimum;
+		}
+		indexOfMinimum = currentIndex;
+	}
+
+	private boolean checkIndexOfMinimum(int indexOfMinimum, int currentIndex,
+			int[] numbers) {
+
+		if (numbers[indexOfMinimum] > numbers[currentIndex]) {
+			return true;
+		}
+		return false;
 	}
 }
