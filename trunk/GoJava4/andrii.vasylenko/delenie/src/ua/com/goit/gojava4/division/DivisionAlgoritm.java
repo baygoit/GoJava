ad62868c1a696c65b4f3fdeb8f77717688a97949
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-28 15:46:22 +0000

    try to refactor smth
    git-svn-id: http://go-java-1.googlecode.com/svn@3558 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/andrii.vasylenko/delenie/src/ua/com/goit/gojava4/division/DivisionAlgoritm.java
+++ trunk/GoJava4/andrii.vasylenko/delenie/src/ua/com/goit/gojava4/division/DivisionAlgoritm.java
@@ -5,27 +5,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class DivisionAlgoritm {
-	
-	private String sNumber1, sNumber2;
-	private int number1, number2;
+
 	private int accuracy = 6;
+	private int dividend, divisor;
+	private String dividendWithAccuracy;
 	
 	public DivisionAlgoritm(String formula) throws NumberFormatException {
-		String[] numbers = formula.split("/"); 
-		if (numbers.length != 2) {
-			throw new NumberFormatException();
-		}
-		this.sNumber1 = numbers[0];
-		for (int i = 0; i < accuracy; i++) {
-			this.sNumber1 += "0";
-		}
-		this.sNumber2 = numbers[1];
-		this.number1 = Integer.parseInt(this.sNumber1);
-		this.number2 = Integer.parseInt(this.sNumber2);
+		String[] numbers = split(formula);
+		dividend = convert(numbers[0]);
+		divisor = convert(numbers[1]);
+		dividendWithAccuracy = addAccuracy(numbers[0]);
 	}
 
 	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
 		try {
 			System.out.println("Input formula:");
 			String formula = reader.readLine();
@@ -39,52 +33,91 @@ public class DivisionAlgoritm {
 	}
 
 	public void showResult() {
-		while (getNext()) {
-			calcResult();
+		StringBuilder result = new StringBuilder();
+		StringBuilder log = new StringBuilder();
+		StringBuilder spaces = new StringBuilder();
+		int currentNumber = 0;
+		
+		for (int index = 0; hasNextDigit(index); index++) {
+			getNextDigit(currentNumber, index, spaces, log);
+			calcResult(result, currentNumber, spaces, log);
 		}
-		outputResult();
+		
+		outputResult(result, log);
 	}
-
-	private StringBuilder sResult = new StringBuilder();
-	private StringBuilder logResult = new StringBuilder();
-	private StringBuilder spaces = new StringBuilder();
-	private int index = 0;
-	private int currentNumber = 0;
-	private void calcResult() {
-		int localResult = currentNumber / number2;
-		sResult.append(localResult);
-		localResult *= number2;
-		logResult.append(spaces.toString()+localResult+"\n");
-		spaces.append(" ");
-		currentNumber -= localResult;
-	}
-
-	private boolean getNext() {
-		if (index >= sNumber1.length()) {
-			logResult.append(spaces.toString()+currentNumber+"\n");
+	
+	
+	private boolean hasNextDigit(int index)
+	{
+		if (index >= dividendWithAccuracy.length()) {
 			return false;
-		}
-		currentNumber *= 10;
-		currentNumber += Integer.parseInt(sNumber1.substring(index,index+1));
-		index++;
-		if (currentNumber != 0 && currentNumber / number2 < 1) {
-			getNext();
-		}
-		if (spaces.length() > 0) {
-			logResult.append(spaces.toString()+currentNumber+"\n");			
 		}
 		return true;
 	}
+
+	private void calcResult(StringBuilder sResult, int currentNumber, StringBuilder spaces, StringBuilder log) {
+		int localResult = currentNumber / divisor;
+		sResult.append(localResult);
+		localResult *= divisor;
+		log.append(spaces.toString() + localResult + "\n");
+		spaces.append(" ");
+		currentNumber -= localResult;
+	}
 	
-	private void outputResult() {
-		int number1 = this.number1;
+	private int getNextDigit(int currentNumber, int index, StringBuilder spaces, StringBuilder log) {
+		if (!hasNextDigit(index)) {
+			log.append(spaces.toString() + currentNumber + "\n");
+			return currentNumber;
+		}
+		currentNumber *= 10;
+		currentNumber += Integer.parseInt(dividendWithAccuracy.substring(index,
+				index + 1));
+		
+		if (currentNumber != 0 && currentNumber / divisor < 1) {
+			getNextDigit(currentNumber, ++index, spaces, log);
+		}
+		if (spaces.length() > 0) {
+			log.append(spaces.toString() + currentNumber + "\n");
+		}
+		return currentNumber;
+	}
+	
+	private void outputResult(StringBuilder sResult, StringBuilder log) {
+		int number1 = this.dividend;
 		double localResult = Integer.parseInt(sResult.toString());
 		for (int i = 0; i < accuracy; i++) {
 			number1 /= 10;
 			localResult /= 10;
 		}
-		System.out.println(number1 + " | "+ number2);
-		System.out.println(spaces.toString() + "  |" + localResult);
-		System.out.println(logResult.toString());
+		System.out.println(number1 + " | " + divisor);
+		//System.out.println(spaces.toString() + "  |" + localResult);
+		System.out.println(log.toString());
 	}
+	private String[] split(String formula) throws NumberFormatException {
+		String[] numbers = formula.split("/");
+		check(numbers);
+		return numbers;
+	}
+
+	private void check(String[] numbers) throws NumberFormatException {
+		if (numbers.length != 2) {
+			throw new NumberFormatException();
+		}
+		// try to:
+		convert(numbers[0]);
+		convert(numbers[1]);
+	}
+
+	private String addAccuracy(String number) {
+		String result = number;
+		for (int i = 0; i < accuracy; i++) {
+			this.dividendWithAccuracy += "0";
+		}
+		return result;
+	}
+
+	private int convert(String number) throws NumberFormatException {
+		return Integer.parseInt(number);
+	}
+
 }
