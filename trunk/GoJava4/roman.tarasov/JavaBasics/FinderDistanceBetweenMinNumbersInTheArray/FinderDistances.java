524d2877671363bb2f11f9aa460cb72323d01810
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-28 22:08:34 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3591 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/JavaBasics/FinderDistanceBetweenMinNumbersInTheArray/FinderDistances.java
+++ trunk/GoJava4/roman.tarasov/JavaBasics/FinderDistanceBetweenMinNumbersInTheArray/FinderDistances.java
@@ -1,15 +1,18 @@
 import java.util.Arrays;
 
 public class FinderDistances {
-	int[] indexesOfSorted;
+	int[] index;
 	int[] markers;
 	int[] sorted;
-	int[] pieceOfWork;
-	int sizeOfPieceOfWork;
-	long minDistance;
-	long maxDistance;
-	int pointerToMin;
-	int pointerToMax;
+	int[] arrayToWork;
+	int sizeOfArrayToWork;
+	int minDistance;
+	int maxDistance;
+	int toFirstResultOfMinDist;
+	int toSecondResultOfMinDist;
+	int toFirstResultOfMaxDist;
+	int toSecondResultOfMaxDist;
+	int countDuplicates;
 
 	public static void main(String[] args) {
 		FinderDistances finder = new FinderDistances();
@@ -17,175 +20,175 @@ public class FinderDistances {
 	}
 
 	public void testFinder(FinderDistances finder) {
-		pieceOfWork = new int[] { 1, 1, 1, 1 };
+		arrayToWork = new int[] { 0, 1, 1, 0, 1, 0 };
 		System.out.println("input                 "
-				+ Arrays.toString(pieceOfWork));
+				+ Arrays.toString(arrayToWork));
 		finder.startWorker();
 		System.out.println("-----------------------------------------");
 
-		pieceOfWork = new int[] { 23, 45, 34, 12, 45, 4, 38, 56, 2, 49, 100 };
+		arrayToWork = new int[] { 23, 45, 34, 12, 45, 4, 38, 56, 2, 49, 100 };
 		System.out.println("input                 "
-				+ Arrays.toString(pieceOfWork));
+				+ Arrays.toString(arrayToWork));
 		finder.startWorker();
 		System.out.println("-----------------------------------------");
 
-		pieceOfWork = new int[] { 1, 1, 2, 0, 1, 1, 4, 1 };
+		arrayToWork = new int[] { 1, 1, 2, 0, 1, 1, 4, 1 };
 		System.out.println("input                 "
-				+ Arrays.toString(pieceOfWork));
+				+ Arrays.toString(arrayToWork));
 		finder.startWorker();
 		System.out.println("-----------------------------------------");
 
-		pieceOfWork = new int[] { 1, 1, 2, 0, 1, 0, 4, 0 };
+		arrayToWork = new int[] { 1, 1, 2, 0, 1, 0, 4, 0 };
 		System.out.println("input                 "
-				+ Arrays.toString(pieceOfWork));
+				+ Arrays.toString(arrayToWork));
 		finder.startWorker();
 		System.out.println("-----------------------------------------");
 	}
 
-	void validateThePieceOfWork() {
+	void validateArrayToWork() {
 
 	}
 
 	void makeSortedAndIndexes() {
 		final int MARKED = 1;
-		int minimalInPieceOfWork = pieceOfWork[0];
+		final int POINTER_TO_FIRST_ELEMENT = 0;
+		int minimalInArrayToWork = arrayToWork[POINTER_TO_FIRST_ELEMENT];
 		int whereMinimal = 0;
 
-		for (int whereDoIndexes = 0; whereDoIndexes < sizeOfPieceOfWork; whereDoIndexes++) {
+		for (int pointer = POINTER_TO_FIRST_ELEMENT; pointer < sizeOfArrayToWork; pointer++) {
 			boolean minFinded = false;
-			for (int whereDoSort = 0; whereDoSort < sizeOfPieceOfWork; whereDoSort++) {
+			for (int whereDoSort = POINTER_TO_FIRST_ELEMENT; whereDoSort < sizeOfArrayToWork; whereDoSort++) {
 				if (markers[whereDoSort] == MARKED) {
 					continue;
 				}
 				if (minFinded) {
-					if (pieceOfWork[whereDoSort] < minimalInPieceOfWork) {
-						minimalInPieceOfWork = pieceOfWork[whereDoSort];
+					if (arrayToWork[whereDoSort] < minimalInArrayToWork) {
+						minimalInArrayToWork = arrayToWork[whereDoSort];
 						whereMinimal = whereDoSort;
 						continue;
 					}
 					continue;
 				}
-				minimalInPieceOfWork = pieceOfWork[whereDoSort];
+				minimalInArrayToWork = arrayToWork[whereDoSort];
 				minFinded = true;
 				whereMinimal = whereDoSort;
 			}
-			indexesOfSorted[whereDoIndexes] = whereMinimal;
-			sorted[whereDoIndexes] = minimalInPieceOfWork;
+			index[pointer] = whereMinimal;
+			sorted[pointer] = minimalInArrayToWork;
 			markers[whereMinimal] = MARKED;
 		}
 	}
 
-	int countDuplicatesOfFirstElementInSorted() {
-		int firstElement = sorted[0];
+	int countDuplicatesOfFirstSortedElement() {
+		final int POINTER_TO_FIRST_ELEMENT = 0;
+		final int POINTER_TO_SECOND_ELEMENT = 1;
+		int firstSortedElement = sorted[POINTER_TO_FIRST_ELEMENT];
 		int counter;
-		for (counter = 1; counter < sizeOfPieceOfWork; counter++) {
-			if (sorted[counter] != firstElement) {
+		for (counter = POINTER_TO_SECOND_ELEMENT; counter < sizeOfArrayToWork; counter++) {
+			if (sorted[counter] != firstSortedElement) {
 				break;
 			}
 		}
 		return counter;
 	}
 
-	void searchMinMaxDistanceFirstAndLotDuplicates() {
-		int firstElement = indexesOfSorted[0];
-		int secondElement = sorted[1];
-		minDistance = Math.abs(indexesOfSorted[0] - indexesOfSorted[1]);
+	void doForSecondDuplicatedElements() {
+		final int POINTER_TO_FIRST_ELEMENT = 0;
+		final int POINTER_TO_SECOND_ELEMENT = 1;
+
+		int firstIndexedElement = index[POINTER_TO_FIRST_ELEMENT];
+		int secondSortedElement = sorted[POINTER_TO_SECOND_ELEMENT];
+
+		minDistance = Math.abs(index[POINTER_TO_FIRST_ELEMENT]
+				- index[POINTER_TO_SECOND_ELEMENT]);
 		maxDistance = minDistance;
-		pointerToMin = 1;
-		pointerToMax = 1;
+		int toFirstResult = POINTER_TO_SECOND_ELEMENT;
+		int toSecondResult = POINTER_TO_SECOND_ELEMENT;
 
-		for (int pointerToElem = 1; pointerToElem < sizeOfPieceOfWork; pointerToElem++) {
+		for (int currentPointerToElem = POINTER_TO_SECOND_ELEMENT; currentPointerToElem < sizeOfArrayToWork; currentPointerToElem++) {
 
-			if (sorted[pointerToElem] != secondElement) {
+			if (sorted[currentPointerToElem] != secondSortedElement) {
+
 				break;
 			}
-			long searchedDistance = Math.abs(indexesOfSorted[pointerToElem]
-					- firstElement);
 
-			if (searchedDistance < minDistance) {
-				minDistance = searchedDistance;
-				pointerToMin = pointerToElem;
+			int obtainedDistance = Math.abs(index[currentPointerToElem]
+					- firstIndexedElement);
+
+			if (obtainedDistance < minDistance) {
+				minDistance = obtainedDistance;
+				toFirstResult = currentPointerToElem;
 			}
-			if (searchedDistance > maxDistance) {
-				maxDistance = searchedDistance;
-				pointerToMax = pointerToElem;
+			if (obtainedDistance > maxDistance) {
+				maxDistance = obtainedDistance;
+				toSecondResult = currentPointerToElem;
 			}
 		}
+		toFirstResultOfMinDist = firstIndexedElement;
+		toSecondResultOfMinDist = index[toFirstResult];
+		toFirstResultOfMaxDist = firstIndexedElement;
+		toSecondResultOfMaxDist = index[toSecondResult];
 	}
 
 	void showResult() {
 		System.out.println("min distance " + minDistance + " between "
-				+ pieceOfWork[indexesOfSorted[0]] + "[" + indexesOfSorted[0]
-				+ "]" + " and " + pieceOfWork[indexesOfSorted[pointerToMin]]
-				+ "[" + indexesOfSorted[pointerToMin] + "]");
+				+ arrayToWork[toFirstResultOfMinDist] + "["
+				+ toFirstResultOfMinDist + "]" + " and "
+				+ arrayToWork[toSecondResultOfMinDist] + "["
+				+ toSecondResultOfMinDist + "]");
+
 		System.out.println("max distance " + maxDistance + " between "
-				+ pieceOfWork[indexesOfSorted[0]] + "[" + indexesOfSorted[0]
-				+ "]" + " and " + pieceOfWork[indexesOfSorted[pointerToMax]]
-				+ "[" + indexesOfSorted[pointerToMax] + "]");
+				+ arrayToWork[toFirstResultOfMaxDist] + "["
+				+ toFirstResultOfMaxDist + "]" + " and "
+				+ arrayToWork[toSecondResultOfMaxDist] + "["
+				+ toSecondResultOfMaxDist + "]");
 	}
 
 	void startWorker() {
-		sizeOfPieceOfWork = pieceOfWork.length;
-		indexesOfSorted = new int[sizeOfPieceOfWork];
-		markers = new int[sizeOfPieceOfWork];
-		sorted = new int[sizeOfPieceOfWork];
 
-		validateThePieceOfWork();
+		sizeOfArrayToWork = arrayToWork.length;
+		index = new int[sizeOfArrayToWork];
+		markers = new int[sizeOfArrayToWork];
+		sorted = new int[sizeOfArrayToWork];
+
+		validateArrayToWork();
 		makeSortedAndIndexes();
 
 		System.out.println("sorted array          " + Arrays.toString(sorted));
-		System.out.println("index of sorted array "
-				+ Arrays.toString(indexesOfSorted));
+		System.out.println("index of sorted array " + Arrays.toString(index));
 
-		int countDuplicates = countDuplicatesOfFirstElementInSorted();
-		int currentIndex = countDuplicates;
+		int countDuplicates = countDuplicatesOfFirstSortedElement();
 
-		if (currentIndex == 1) {
-			searchMinMaxDistanceFirstAndLotDuplicates();
+		if (countDuplicates == 1) {
+			doForSecondDuplicatedElements();
 			showResult();
 			return;
 		}
+		doForFirstDuplicatedSortedElements();
+		showResult();
+	}
 
-		int minDistance = indexesOfSorted[1] - indexesOfSorted[0];
+	void doForFirstDuplicatedSortedElements() {
+		final int POINTER_TO_FIRST_ELEMENT = 0;
+		final int POINTER_TO_SECOND_ELEMENT = 1;
+		minDistance = index[POINTER_TO_SECOND_ELEMENT]
+				- index[POINTER_TO_FIRST_ELEMENT];
+		int obtainedDistance = minDistance;
+		int currentPointerToSecondResult = POINTER_TO_SECOND_ELEMENT;
+		for (int currentPointerToElem = POINTER_TO_SECOND_ELEMENT; currentPointerToElem < countDuplicates; currentPointerToElem++) {
 
-		int currentMinDist = minDistance;
+			obtainedDistance = index[currentPointerToElem]
+					- index[currentPointerToElem - 1];
 
-		int minIndex = 1;
-		for (int index = 1; index < currentIndex; index++) {
-
-			currentMinDist = indexesOfSorted[index]
-					- indexesOfSorted[index - 1];
-
-			if (currentMinDist < minDistance) {
-				minDistance = currentMinDist;
-				minIndex = index;
+			if (obtainedDistance < minDistance) {
+				minDistance = obtainedDistance;
+				currentPointerToSecondResult = currentPointerToElem;
 			}
 		}
-
-		int maxDistance = indexesOfSorted[currentIndex - 1]
-				- indexesOfSorted[0];
-		showResult();
-		/*
-		System.out.println("min distance " + minDistance + " between "
-				+ pieceOfWork[indexesOfSorted[minIndex - 1]] + "["
-				+ indexesOfSorted[minIndex - 1] + "]" + " and "
-				+ pieceOfWork[indexesOfSorted[minIndex]] + "["
-				+ indexesOfSorted[minIndex] + "]");
-
-		System.out.println("max distance " + maxDistance + " between "
-				+ pieceOfWork[indexesOfSorted[0]] + "[" + indexesOfSorted[0]
-				+ "]" + " and "
-				+ pieceOfWork[indexesOfSorted[currentIndex - 1]] + "["
-				+ indexesOfSorted[currentIndex - 1] + "]");
-		*/
+		toFirstResultOfMinDist = index[currentPointerToSecondResult - 1];
+		toSecondResultOfMinDist = index[currentPointerToSecondResult];
+		maxDistance = index[countDuplicates - 1] - index[0];
+		toFirstResultOfMaxDist = index[0];
+		toSecondResultOfMaxDist = index[countDuplicates - 1];
 	}
 }
-// currentIndex--;
-/*
- * System.out.println("min distance " + minDistance + " between " +
- * pieceOfWork[indexesOfSorted[0]] + "[" + indexesOfSorted[0] + "]" + " and " +
- * pieceOfWork[indexesOfSorted[indMin]] + "[" + indexesOfSorted[indMin] + "]");
- * System.out.println("max distance " + maxDistance + " between " +
- * pieceOfWork[indexesOfSorted[0]] + "[" + indexesOfSorted[0] + "]" + " and " +
- * pieceOfWork[indexesOfSorted[indMax]] + "[" + indexesOfSorted[indMax] + "]");
- */
