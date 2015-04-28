93ef9469ff0b4b84e830018fb738d31aef06cc8d
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-28 19:14:41 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3569 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
+++ trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
@@ -1,28 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 
 public class Anagrams {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-		
-		String userString = consoleReader.readLine();
-		String[] userStringSplited = userString.split(" ");
-		
-		for (String word: userStringSplited) {
-			System.out.print(reverse(word)+" ");
-		}
-		
+	public static void main(String[] args) {
+		String userString = readLine();
+		System.out.println(reversString(userString));
 	}
 
-	private static String reverse(String word) {
+	private static String reversString(String userString) {
+		String[] userStringSplited = userString.split(" ");
+		String userStringReversed = new String();
+		
+		for (String word: userStringSplited) {
+			userStringReversed = userStringReversed + reverseWord(word) + " ";
+		}
+		return userStringReversed;
+	}
+	
+	private static String readLine() {
+		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			return consoleReader.readLine();
+		} catch (IOException e) {
+			System.out.println("You entered incorrect data (words separated with space is needed)");
+		}
+		return null;
+	}
+
+	private static String reverseWord(String word) {
 		char[] wordChars = word.toCharArray();
 		char[] result = new char[wordChars.length];
 		
 		for (int i=0; i<wordChars.length; i++) {
-			result[wordChars.length-1-i] = wordChars[i];
+			
+			if (Character.isLetter(wordChars[i])) {
+				result[wordChars.length-1-i] = wordChars[i];
+			} else {
+				System.err.println(wordChars[i] + "is not a letter symbol in your string. "
+						+ "Please enter string with only letters and spaces");
+				System.exit(0);
+			}
+			
 		}
 		return new String(result);
 	}
