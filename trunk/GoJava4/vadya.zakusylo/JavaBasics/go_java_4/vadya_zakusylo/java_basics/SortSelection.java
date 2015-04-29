f8ee8bdd66ce6b40056beb23d3906a5c2d7d6c62
vadya.chmil@gmail.com <vadya.chmil@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 13:50:15 +0000

    Refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@3630 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/vadya.zakusylo/JavaBasics/go_java_4/vadya_zakusylo/java_basics/SortSelection.java
+++ trunk/GoJava4/vadya.zakusylo/JavaBasics/go_java_4/vadya_zakusylo/java_basics/SortSelection.java
@@ -1,4 +1,4 @@
-package swap;
+package go_java_4.vadya_zakusylo.java_basics;
 
 import java.util.Arrays;
 
@@ -8,18 +8,18 @@ public class SortSelection {
 		int[] a = { 5, 3, 5, 6, 8, 9, 10, 2, 1, 6, 3 };
 
 		for (int index = 0; index < a.length - 1; index++) {
-			for (int scan = index + 1; scan < a.length; scan++) {
-				if (a[index] > a[scan]) {
-					swap(a, index, scan);
+			for (int workedIndex = index + 1; workedIndex < a.length; workedIndex++) {
+				if (a[index] > a[workedIndex]) {
+					swap(a, index, workedIndex);
 				}
 			}
 		}
 		System.out.println(Arrays.toString(a));
 	}
 
-	private static void swap(int[] a, int index, int scan) {
-		int temp = a[index];
-		a[index] = a[scan];
-		a[scan] = temp;
+	private static void swap(int[] a, int index, int workedIndex) {
+		int temporaryIndex = a[index];
+		a[index] = a[workedIndex];
+		a[workedIndex] = temporaryIndex;
 	}
 }
