17ab444c191c6b9d73194914da7a77b20e1d17f0
dens.gorban@gmail.com <dens.gorban@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 15:27:13 +0000

    + fix code style
    git-svn-id: http://go-java-1.googlecode.com/svn@3646 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/dens.gorban/Anagrams/src/Anagrams.java
+++ trunk/GoJava4/dens.gorban/Anagrams/src/Anagrams.java
@@ -1,35 +1,35 @@
 import java.io.PrintStream;
 import java.util.Scanner;
 
-
 public class Anagrams {
-	
+
 	private static final String EXIT = "-1";
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		PrintStream out = System.out;
 		Scanner in = new Scanner(System.in);
-		
-		out.println("для завершеня программы введите '" + EXIT + "'");
-		
-		while(true){
-			out.print("Введите фразу:");
-			String phrase = in.nextLine();
-			
-			if (phrase.equalsIgnoreCase(EXIT))
+
+		out.println("To exit type '" + EXIT + "'");
+
+		while (true) {
+			out.print("Please type your phrase:");
+			String inputPhrase = in.nextLine();
+
+			if (inputPhrase.equalsIgnoreCase(EXIT))
 				System.exit(0);
-			
-			String[] tokens = phrase.split(" ");
-			StringBuilder sb = new StringBuilder();
-			for(String s : tokens){
-				char[] chars = s.toCharArray();
-				for(int i = chars.length-1;  i>=0; i--){
-					sb.append(chars[i]);
+
+			String[] inputPhraseTokens = inputPhrase.split(" ");
+			StringBuilder outputPhrase = new StringBuilder();
+			for (String token : inputPhraseTokens) {
+				char[] chars = token.toCharArray();
+				int tokenLength = chars.length;
+				for (int i = 1; i <= tokenLength; i++) {
+					outputPhrase.append(chars[tokenLength - i]);
 				}
-				sb.append(" ");
-			}		
-			out.println(sb.toString());
+				outputPhrase.append(" ");
+			}
+			out.println(outputPhrase.toString());
 		}
 	}
 
