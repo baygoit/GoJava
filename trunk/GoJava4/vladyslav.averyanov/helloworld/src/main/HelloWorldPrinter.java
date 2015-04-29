cf8dad2a760ebdc8d8e8ee8f0728f6b21e4e8278
avevladyslav <avevladyslav@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 12:56:43 +0000

    renamed class
    git-svn-id: http://go-java-1.googlecode.com/svn@3620 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/vladyslav.averyanov/helloworld/src/main/HelloWorldPrinter.java
+++ trunk/GoJava4/vladyslav.averyanov/helloworld/src/main/HelloWorldPrinter.java
@@ -1,9 +1,8 @@
 package main;
 
-public class Hello {
+public class HelloWorldPrinter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 	}
 
