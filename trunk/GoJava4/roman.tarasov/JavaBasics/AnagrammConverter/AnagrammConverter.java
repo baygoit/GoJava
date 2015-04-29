f66b338d7b8887c1128b59f77d1d91a3f573a75b
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 11:14:03 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3611 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/roman.tarasov/JavaBasics/AnagrammConverter/AnagrammConverter.java
@@ -0,0 +1,95 @@
+public class AnagrammConverter {
+	int pointToResult = 0;
+	char[] result;
+	char[] buffer;
+	int lenParsed;
+	int bufferPoint;
+
+	public static void main(String[] args) {
+		String inputString = " мама мыла раму ";
+
+		AnagrammConverter converter = new AnagrammConverter();
+		System.out.println(converter.doAnagramm(inputString));
+
+	}
+
+	String doAnagramm(String input) {
+		String output = "";
+		final int NULL_POINTER = 0;
+		final char spaceSymbol = ' ';
+		char[] parsedInput = input.toCharArray();
+		lenParsed = parsedInput.length;
+
+		result = new char[lenParsed];
+		buffer = new char[lenParsed];
+
+		boolean doWordFlag = false;
+
+		pointToResult = 0;
+		bufferPoint = NULL_POINTER;
+		System.out.println(new String(parsedInput));
+		for (int pointParsed = NULL_POINTER; pointParsed < lenParsed; pointParsed++) {
+
+			char extracted = parsedInput[pointParsed];
+
+			if (extracted != spaceSymbol) {
+
+				if (!doWordFlag) {
+					doWordFlag = true;
+					bufferPoint = NULL_POINTER;
+				}
+				buffer[bufferPoint] = extracted;
+				bufferPoint++;
+				continue;
+			}
+
+			if (doWordFlag) {
+				doWordFlag = false;
+				reverseWord();
+				addBuffToResult();
+				bufferPoint = NULL_POINTER;
+
+			}
+			addCharToResult(' ');
+
+		}
+
+		output = new String(result);
+		return output;
+	}
+
+	void addBuffToResult() {
+		int pointer;
+
+		for (pointer = 0; pointer < bufferPoint; pointer++) {
+			result[pointer + pointToResult] = buffer[pointer];
+		}
+		pointToResult += pointer;
+	}
+
+	void addCharToResult(char charToAdd) {
+		result[pointToResult] = charToAdd;
+		pointToResult++;
+	}
+
+	void reverseWord() {
+		char[] reversedBuffer = new char[lenParsed];
+		for (int index = 0; index < bufferPoint; index++) {
+			reversedBuffer[bufferPoint - 1 - index] = buffer[index];
+		}
+		buffer=reversedBuffer;
+
+	}
+
+	void printFromChars(String inputString) {
+		char[] chars = inputString.toCharArray();
+		String fromChars = new String(chars);
+		char charr = chars[0];
+		String fromChar = new String(new char[] { charr });
+
+		for (int pointer : chars) {
+			System.out.println(fromChars + fromChar);
+		}
+
+	}
+}
