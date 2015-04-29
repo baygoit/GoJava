fb45c4df604b9a9b5758c0122742a6e69a878642
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 11:23:09 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3614 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/JavaBasics/AnagrammConverter/AnagrammConverter.java
+++ trunk/GoJava4/roman.tarasov/JavaBasics/AnagrammConverter/AnagrammConverter.java
@@ -4,36 +4,30 @@ public class AnagrammConverter {
 	char[] buffer;
 	int lenParsed;
 	int bufferPoint;
-
+	final int NULL_POINTER = 0;
+	
 	public static void main(String[] args) {
-		String inputString = " мама мыла раму ";
-
+		String inputString = " for test  ";
+		System.out.println(inputString);
 		AnagrammConverter converter = new AnagrammConverter();
 		System.out.println(converter.doAnagramm(inputString));
-
 	}
-
 	String doAnagramm(String input) {
 		String output = "";
-		final int NULL_POINTER = 0;
+	
 		final char spaceSymbol = ' ';
 		char[] parsedInput = input.toCharArray();
 		lenParsed = parsedInput.length;
-
 		result = new char[lenParsed];
 		buffer = new char[lenParsed];
-
 		boolean doWordFlag = false;
 
-		pointToResult = 0;
+		pointToResult = NULL_POINTER;
 		bufferPoint = NULL_POINTER;
-		System.out.println(new String(parsedInput));
+
 		for (int pointParsed = NULL_POINTER; pointParsed < lenParsed; pointParsed++) {
-
 			char extracted = parsedInput[pointParsed];
-
 			if (extracted != spaceSymbol) {
-
 				if (!doWordFlag) {
 					doWordFlag = true;
 					bufferPoint = NULL_POINTER;
@@ -42,42 +36,34 @@ public class AnagrammConverter {
 				bufferPoint++;
 				continue;
 			}
-
 			if (doWordFlag) {
 				doWordFlag = false;
 				reverseWord();
 				addBuffToResult();
 				bufferPoint = NULL_POINTER;
-
 			}
 			addCharToResult(' ');
-
 		}
-
 		output = new String(result);
 		return output;
 	}
-
 	void addBuffToResult() {
 		int pointer;
 
-		for (pointer = 0; pointer < bufferPoint; pointer++) {
+		for (pointer = NULL_POINTER; pointer < bufferPoint; pointer++) {
 			result[pointer + pointToResult] = buffer[pointer];
 		}
 		pointToResult += pointer;
 	}
-
 	void addCharToResult(char charToAdd) {
 		result[pointToResult] = charToAdd;
 		pointToResult++;
 	}
-
 	void reverseWord() {
 		char[] reversedBuffer = new char[lenParsed];
-		for (int index = 0; index < bufferPoint; index++) {
-			reversedBuffer[bufferPoint - 1 - index] = buffer[index];
+		for (int pointer = NULL_POINTER; pointer < bufferPoint; pointer++) {
+			reversedBuffer[bufferPoint - 1 - pointer] = buffer[pointer];
 		}
-		buffer = reversedBuffer;
-
+		buffer=reversedBuffer;
 	}
 }
