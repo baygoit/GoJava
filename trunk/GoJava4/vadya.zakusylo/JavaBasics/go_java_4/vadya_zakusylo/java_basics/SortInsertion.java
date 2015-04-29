e767fb24c53447d0afd142e53c39bd6e882da38d
vadya.chmil@gmail.com <vadya.chmil@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-04-29 13:47:59 +0000

    Refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@3629 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/vadya.zakusylo/JavaBasics/go_java_4/vadya_zakusylo/java_basics/SortInsertion.java
+++ trunk/GoJava4/vadya.zakusylo/JavaBasics/go_java_4/vadya_zakusylo/java_basics/SortInsertion.java
@@ -1,4 +1,4 @@
-package swap;
+package go_java_4.vadya_zakusylo.java_basics;
 
 import java.util.Arrays;
 
@@ -6,13 +6,13 @@ public class SortInsertion {
 
 	public static void main(String[] args) {
 		int[] a = { 5, 3, 5, 6, 8, 9, 10, 2, 1, 6, 3 };
-		for (int i = 1; i < a.length; i++) {
-			int currElem = a[i];
-			int prevKey = i - 1;
-			while (prevKey >= 0 && a[prevKey] > currElem) {
-				a[prevKey + 1] = a[prevKey];
-				a[prevKey] = currElem;
-				prevKey--;
+		for (int index = 1; index < a.length; index++) {
+			int workedIndex = a[index];
+			int prevIndex = index - 1;
+			while (prevIndex >= 0 && a[prevIndex] > workedIndex) {
+				a[prevIndex + 1] = a[prevIndex];
+				a[prevIndex] = workedIndex;
+				prevIndex--;
 			}
 		}
 		System.out.println(Arrays.toString(a));
