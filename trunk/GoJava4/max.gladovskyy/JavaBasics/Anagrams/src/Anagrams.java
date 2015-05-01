b2a6e468e8e7144da9154a88288b2cbcdcc45d29
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-01 21:11:18 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3680 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
+++ trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
@@ -1,61 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 
 public class Anagrams {
 
 	public static void main(String[] args) {
-		String userString = readLine();
-		System.out.println(reversString(userString));
+		System.out.println("Enter words divided by spaces and press enter to make anagrams");
+		Scanner reader = new Scanner(System.in);
+		String userString = reader.nextLine();
+		System.out.println(makeAnagrams(userString));
 	}
 
-	private static String reversString(String userString) {
+	private static String makeAnagrams(String userString) {
 		String[] userStringSplited = userString.split(" ");
-		String result = new String();
+		StringBuilder result = new StringBuilder();
+		Pattern nonAlphabetPattern = Pattern.compile("[^a-zA-Z]");
 		
 		for (String word: userStringSplited) {
-			result = result + reverseWord(word) + " ";
+			if(nonAlphabetPattern.matcher(word).find()) {
+				System.err.println("Entered words contains not alphabetic characters.");
+				System.exit(0);
+			}
+			result.append(new StringBuilder(word).reverse().toString() + " ");
 		}
-		return result;
-	}
-	
-	private static String readLine() {
-		BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String line = consoleReader.readLine();
-			checkIfEmpty(line);
-			return line;
-		} catch (IOException e) {
-			System.err.println("You entered incorrect data (words separated with space is needed)");
-		}
-		return null;
-	}
-
-	private static void checkIfEmpty(String line) {
-		if (line.equals("")) {
-			System.err.println("Cant revert empty string");	
-			System.exit(0);
-		}
-	}
-
-	private static String reverseWord(String word) {
-		char[] wordLetters = word.toCharArray();
-		char[] result = new char[wordLetters.length];
-		
-		for (int i=0; i<wordLetters.length; i++) {
-			result[wordLetters.length-1-i] = filterNotLetters(wordLetters[i]);
-		}
-		return new String(result);
-	}
-	
-	private static Character filterNotLetters (Character letter) {
-		if (!Character.isLetter(letter)) {
-			System.err.println(letter + " is not a letter symbol in your string. "
-					+ "Please enter string with only letters and spaces");
-			System.exit(0);
-		}
-		return letter;
+		return result.toString();
 	}
 
 }
