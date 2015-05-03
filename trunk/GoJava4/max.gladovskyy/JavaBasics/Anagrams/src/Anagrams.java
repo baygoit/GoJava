69d5c595828873395d53e3f2fc0d4c79a0cbcf14
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-03 19:05:13 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3695 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
+++ trunk/GoJava4/max.gladovskyy/JavaBasics/Anagrams/src/Anagrams.java
@@ -6,21 +6,26 @@ public class Anagrams {
 
 	public static void main(String[] args) {
 		System.out.println("Enter words divided by spaces and press enter to make anagrams");
-		System.out.println(makeAnagrams(new Scanner(System.in).nextLine()));
+		System.out.println(makeAnagrams());
 	}
 
-	private static String makeAnagrams(String userString) {
+	private static String makeAnagrams() {
+		String userString = new Scanner(System.in).nextLine();
 		StringBuilder result = new StringBuilder();
-		Pattern nonAlphabet = Pattern.compile("[^a-zA-Z]");
 		
 		for (String word: userString.split(" ")) {
-			if(nonAlphabet.matcher(word).find()) {
-				System.err.println("Entered words contains not alphabetic characters.");
-				System.exit(0);
-			}
-			result.append(new StringBuilder(word).reverse().toString() + " ");
+			processOneWord(result, word);
 		}
 		return result.toString();
+	}
+
+	private static void processOneWord(StringBuilder result, String word) {
+		Pattern nonAlphabet = Pattern.compile("[^a-zA-Z]");
+		if(nonAlphabet.matcher(word).find()) {
+			System.err.println("Entered words contains not alphabetic characters.");
+			System.exit(0);
+		}
+		result.append(new StringBuilder(word).reverse().toString() + " ");
 	}
 
 }
