81b62ed61ca644889871315e2b3039f7d7294d9e
alexmirn2@gmail.com <alexmirn2@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-05 18:17:34 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3730 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/alex.mirnyi/Kickstarter/Quotes.java
@@ -0,0 +1,9 @@
+package Kickstarter;
+
+public class Quotes {
+	final static String[] quotes = {
+		"Impossible is nothing",
+		"Just do it",
+		"To be or not to be"
+		};
+}
---
+++ trunk/GoJava4/alex.mirnyi/Kickstarter/QuotesChoose.java
@@ -0,0 +1,15 @@
+package Kickstarter;
+
+import java.util.Random;
+import java.util.logging.Logger;
+
+public class QuotesChoose {
+	private static String[] quotes = Quotes.quotes;
+	private static int index = new Random().nextInt(quotes.length);
+	private static String random = (quotes[index]);
+	
+	public static String getRandom() {
+		return random;
+	}
+
+}
