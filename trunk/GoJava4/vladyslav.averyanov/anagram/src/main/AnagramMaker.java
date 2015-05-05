bc8105a7b2a64290fe479c292ba18b289b3c5c08
avevladyslav <avevladyslav@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-05 09:08:57 +0000

    initial AnagramMaker
    git-svn-id: http://go-java-1.googlecode.com/svn@3716 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/vladyslav.averyanov/anagram/src/main/AnagramMaker.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.io.*;
+
+public class AnagramMaker {
+	
+	public static final String STANDART_SEPARATOR = "[,;:.!?\\s]+";
+	public static final String STANDART_USER_INPUT = "мама мыла раму";
+
+	public static void main(String[] args) throws IOException {
+		System.out.print("Enter words to inverse them: ");
+		//String userInput = readUserInput();
+		String userInput = STANDART_USER_INPUT;
+		System.out.println(userInput);
+		System.out.println(doAnagram(userInput, STANDART_SEPARATOR));
+	}
+	
+	//TODO make exception handling 
+	public static String readUserInput() throws IOException {
+		InputStreamReader myInputStreamReader = new InputStreamReader(System.in);
+		BufferedReader myBufferedReader = new BufferedReader(myInputStreamReader);
+		String result = myBufferedReader.readLine();
+		return result;
+	}
+	
+	public static String doAnagram(String userInput, String separator) {
+		String [] separateWords = makeSeparateWords(userInput, separator);
+		String [] inversedWords = inverseEachWord(separateWords);
+		String result = makeOneString(inversedWords);
+		return result;
+	}
+	
+	public static String[] makeSeparateWords(String inputString, String separator) {
+		String [] result = inputString.trim().split(separator); //.split("[,;:.!?\\s]+");
+		return result;
+	}
+	
+	public static String[] inverseEachWord(String[] words) {
+		String [] result = new String [words.length];
+		int wordIndex = 0;
+		for (String word : words){
+			result[wordIndex] = inverseWord(word);
+			wordIndex++;
+		}
+		return result;
+	}
+	
+	public static String inverseWord(String word) {
+		StringBuffer result = new StringBuffer();
+		for (int index = word.length()-1; index>=0; index--){
+			result.append(word.charAt(index));
+		}
+		return result.toString();
+	}
+	
+	public static String makeOneString(String[] words) {
+		StringBuffer result = new StringBuffer();
+		for (String word : words){
+			result.append(word).append(" ");
+		}
+
+		return result.toString();
+	}
+}
