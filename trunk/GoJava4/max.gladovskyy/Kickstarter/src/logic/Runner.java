1b0ec643c653180cd4f4d816ac79598868bfd527
glmax132@gmail.com <glmax132@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-07 20:22:29 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3760 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/max.gladovskyy/Kickstarter/src/logic/Runner.java
+++ trunk/GoJava4/max.gladovskyy/Kickstarter/src/logic/Runner.java
@@ -1,17 +1,38 @@
 package logic;
 
+import java.util.List;
+
+import pages.*;
 import UserInterface.Input;
 import UserInterface.Output;
 import datasource.DataSource;
+import entities.*;
 
 public class Runner {
-
+	private Output output;
+	private Input input;
+	private DataSource dataSource;
+	
 	public Runner(Output output, Input input, DataSource dataSource) {
-		
+		this.output = output;
+		this.input = input;
+		this.dataSource = dataSource;
 	}
 
 	public void run() {
-		
+		int[] menuCurrentPosition = new int[2];
+		while (true) {
+			if (menuCurrentPosition[0] == 0 && menuCurrentPosition[1] == 0) {
+			Quote quote = new Quote();
+			List<String> categorys = dataSource.getCategorysNames();
+			Page mainPage = new MainPage(quote, categorys);
+			output.print(mainPage);
+			} else if (menuCurrentPosition[1] == 0) {
+				String categoryName = dataSource.get
+			} else {
+				
+			}
+		}
 		
 	}
 
