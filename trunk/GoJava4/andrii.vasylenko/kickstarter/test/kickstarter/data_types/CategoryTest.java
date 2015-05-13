8401a97ee00f91619d4e97d282592ffe08fe779a
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-13 06:45:16 +0000

    add test for class Category
    git-svn-id: http://go-java-1.googlecode.com/svn@3827 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter/test/kickstarter/data_types/CategoryTest.java
@@ -0,0 +1,26 @@
+package kickstarter.data_types;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CategoryTest {
+	
+	@Test
+	public void shouldNextId_whenCreateNew() {
+		Category firstCategory = new Category("Name");
+		int firstId = firstCategory.getId();
+		
+		for (int i = ++firstId; i < 10 + firstId; i++, firstId++) {
+			Category currentCategory = new Category("Name"+i);
+			assertEquals(i, currentCategory.getId());
+		}
+	}
+
+	@Test
+	public void shouldStringName_whenStringName() {
+		Category category = new Category("Name");
+		assertEquals("Name", category.getName());
+	}
+
+}
