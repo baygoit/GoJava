2ad947167774d6c22e5c12e6805b6bce408c32a3
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-14 13:36:04 +0000

    add ConsolePrinter Test
    git-svn-id: http://go-java-1.googlecode.com/svn@3855 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter/test/kickstarter/interfaces/printers/ConsolePrinterTest.java
@@ -0,0 +1,29 @@
+package kickstarter.interfaces.printers;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+public class ConsolePrinterTest {
+
+	@Test
+	public void shouldOutputStringMessage_whenShowMessage() {
+		PrintStream defaultStream = System.out;
+		
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		PrintStream myStream = new PrintStream(outputStream);;
+		
+		System.setOut(myStream);
+		
+		Printer printer = new ConsolePrinter();
+		printer.showMessage("TEST MESSAGE");
+		
+		System.setOut(defaultStream);
+		
+		assertEquals("TEST MESSAGE\r\n", outputStream.toString());
+	}
+
+}
