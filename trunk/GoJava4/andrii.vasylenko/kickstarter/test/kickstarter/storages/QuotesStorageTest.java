77bc5d82cf5b7086dea4b29bcbd067c242def08a
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-14 12:54:05 +0000

    add QuotesStorage Test
    git-svn-id: http://go-java-1.googlecode.com/svn@3852 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter/test/kickstarter/storages/QuotesStorageTest.java
@@ -0,0 +1,49 @@
+package kickstarter.storages;
+
+import static org.junit.Assert.*;
+import kickstarter.engine.Quote;
+
+import org.junit.Test;
+
+public class QuotesStorageTest {
+
+	@Test
+	public void shouldZeroSize_whenClearStorage() {
+		QuotesStorage storage = new QuotesStorage();
+		assertEquals(0, storage.size());
+		assertTrue(storage.empty());
+	}
+	
+	@Test
+	public void shouldCorrectValue_whenAddNewElemetnts() {
+		Quote quote1 = new Quote("Quote1");
+		Quote quote2 = new Quote("Quote2");
+		Quote quote3 = new Quote("Quote3");
+		
+		QuotesStorage storage = new QuotesStorage();
+		storage.add(quote1);
+		assertEquals(1, storage.size());
+		storage.add(quote2);
+		assertEquals(2, storage.size());
+		storage.add(quote3);
+		assertEquals(3, storage.size());
+		assertFalse(storage.empty());
+		
+		assertEquals(quote1, storage.get(0));
+		assertEquals(quote2, storage.get(1));
+		assertEquals(quote3, storage.get(2));
+		
+		assertEquals(quote1, storage.getById(quote1.getId()));
+		assertEquals(quote2, storage.getById(quote2.getId()));
+		assertEquals(quote3, storage.getById(quote3.getId()));
+		
+		assertTrue(storage.getRandom().getId() > 0);
+	}	
+
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void shouldException_whenGetRandomFromEmptyStorage() {
+		QuotesStorage storage = new QuotesStorage();
+		storage.getRandom();
+	}
+	
+}
