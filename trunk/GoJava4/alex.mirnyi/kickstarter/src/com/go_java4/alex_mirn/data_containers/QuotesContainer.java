e20fb7e5ded7ff453e27019a51e07dd0f463c359
alexmirn2@gmail.com <alexmirn2@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-15 14:30:01 +0000

    divided kickstarter logic on different classes
    git-svn-id: http://go-java-1.googlecode.com/svn@3907 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/alex.mirnyi/kickstarter/src/com/go_java4/alex_mirn/data_containers/QuotesContainer.java
@@ -0,0 +1,44 @@
+package com.go_java4.alex_mirn.data_containers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.go_java4.alex_mirn.data.Quote;
+
+public class QuotesContainer {
+	private List<Quote> quotes;
+
+	public QuotesContainer() {
+		quotes = new ArrayList<Quote>();
+	}
+
+	public void add(Quote quote) {
+		quotes.add(quote);
+	}
+
+	public Quote get(int index) {
+		return quotes.get(index);
+	}
+
+	public int size() {
+		return quotes.size();
+	}
+
+	public Quote getRandom() throws IndexOutOfBoundsException {
+		if (empty()) {
+			throw new IndexOutOfBoundsException();
+		}
+		int randomIndex = getRandomIndex();
+		return quotes.get(randomIndex);
+	}
+
+	private int getRandomIndex() {
+		return new Random().nextInt(size());
+	}
+
+	public boolean empty() {
+		return quotes.isEmpty();
+	}
+
+}
