f46d0dc3caed8715adbe33377ddec54f20cc6c6b
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-15 06:29:45 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3865 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/kickstarter/test/kickstarter/Test/ExitUI.java
+++ trunk/GoJava4/roman.tarasov/kickstarter/test/kickstarter/Test/ExitUI.java
@@ -2,21 +2,15 @@ package kickstarter.Test;
 
 import kickstarter.ui.UserInterface;
 
-
-
-
 public class ExitUI implements  UserInterface {
 
 	@Override
 	public String inputString() {
-
 		return "e";
 	}
 
 	@Override
 	public void display(String stringToDisplay) {
 		System.out.println(stringToDisplay);
-
 	}
-
 }
