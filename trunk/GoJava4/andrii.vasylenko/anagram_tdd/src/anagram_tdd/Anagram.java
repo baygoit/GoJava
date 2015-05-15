de63b836a27ccc33bf8c6fbae201de0d7d8d5b98
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-15 14:24:57 +0000

    refactoring
    git-svn-id: http://go-java-1.googlecode.com/svn@3904 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/andrii.vasylenko/anagram_tdd/src/anagram_tdd/Anagram.java
+++ trunk/GoJava4/andrii.vasylenko/anagram_tdd/src/anagram_tdd/Anagram.java
@@ -26,7 +26,7 @@ public class Anagram {
 		if (reverser == null) {
 			return false;
 		}
-		if (!line.isEmpty() && !line.matches("([A-Za-z0-9 ]+)")) {
+		if (!line.matches("([A-Za-z0-9 ]*)")) {
 			return false;
 		}
 
