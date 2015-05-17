765393179c336cd054a3bed7ad07da97e26e2a70
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-17 15:12:06 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3928 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/kickstarter/src/kickstarter/Kickstarter.java
+++ trunk/GoJava4/roman.tarasov/kickstarter/src/kickstarter/Kickstarter.java
@@ -1,37 +1,44 @@
 package kickstarter;
 
-import kickstarter.entities.Category;
-import kickstarter.entities.Comments;
-import kickstarter.entities.Project;
-import kickstarter.entities.Quote;
+
+import kickstarter.entities.QuestionsAndAnswers;
+
 import kickstarter.mvc.Controller;
 import kickstarter.mvc.Model;
+import kickstarter.mvc.ModelPage;
 import kickstarter.mvc.View;
 import kickstarter.pages.CategoriesPage;
 import kickstarter.pages.DetailedProject;
 import kickstarter.pages.Page;
 import kickstarter.pages.ProjectsPage;
+import kickstarter.pages.TheEndPage;
 import kickstarter.pages.WrongChoicePage;
+import kickstarter.repository.CategoriesRepository;
 import kickstarter.repository.EntityStorage;
+import kickstarter.repository.ProjectRepository;
+import kickstarter.repository.QuotesRepository;
 import kickstarter.repository.Storage;
 import kickstarter.ui.ConsoleUI;
 import kickstarter.ui.UserInterface;
 
 public class Kickstarter {
-	Storage<Category> categories;
-	Storage<Project> projects;
-	Storage<Comments> allComments;
-	Storage<Quote> quotes;
+
+
+	Storage<QuestionsAndAnswers> allComments;
+
+	QuotesRepository quotes;
+	CategoriesRepository categories;
+	ProjectRepository projects;
 	Model model;
 	View view;
 	public Controller controller;
 	UserInterface ui;
 
 	public Kickstarter() {
-		categories = new EntityStorage<Category>();
-		projects = new EntityStorage<Project>();
-		allComments = new EntityStorage<Comments>();
-		quotes = new EntityStorage<Quote>();
+		quotes = new QuotesRepository();
+		projects = new ProjectRepository();
+		allComments = new EntityStorage<QuestionsAndAnswers>();
+		categories = new CategoriesRepository();
 		ui = new ConsoleUI();
 		model = new Model();
 		view = new View(model, ui);
@@ -49,7 +56,8 @@ public class Kickstarter {
 	}
 
 	private void run() {
-		String command;
+		String command = null;
+
 		while (true) {
 			controller.printView();
 			command = ui.inputString();
@@ -57,79 +65,35 @@ public class Kickstarter {
 		}
 	}
 
-	public void testLoadQuote(String stringToQuote) {
-		Quote quote = new Quote();
-		quote.setQuote(stringToQuote);
-		quotes.add(quote);
-	}
-
-	public void testLoadComments() {
-		Category category = new Category("test comments");
-		Project project = new Project("test comments", category);
-		Comments comments = new Comments(project);
-		for (int number = 0; number < 30; number++) {
-			comments.addComment(1, "comment");
-		}
-	}
 
 	public void load() {
 
-		Quote quote = new Quote();
-		quote.setQuote("Explore projects, everywhere");
-		quotes.add(quote);
-
-		quote = new Quote();
-		quote.setQuote("'To be is to do'—Socrates. 'To do is to be'—Jean-Paul Sartre. 'Do be do be do'—Frank Sinatra");
-		quotes.add(quote);
-
-		Category category;
-
 		Page page = new CategoriesPage(categories, ui, quotes);
 		page.pageId = 0;
-		controller.addPage(page);
+		ModelPage modelPage = new ModelPage(0, 1, 3, 4);
+		modelPage.setCommentOfoptions("Select category by ID ; e - End");
+		controller.addPage(page, modelPage);
 		controller.setPage(0);
 
-		Project project;
-		category = new Category("Social");
-		category.ID = 4;
-		categories.add(category);
-
-		project = new Project("Paint the fence of the school", category);
-		project.description = "raising money for paint";
-		project.ID = 8;
-		projects.add(project);
-		Comments comments = new Comments(project);
-		comments.addComment(1, "What color will paint?");
-		comments.addComment(2, "Paint is Green");
-		allComments.add(comments);
-
-		category = new Category("Technology");
-		category.ID = 5;
-		categories.add(category);
-
-		project = new Project("Create electrobike", category);
-		project.description = "high efficiency";
-		project.shortDescription = "short description";
-		project.history = "history of bike creation";
-		project.linkToVideo = "www.link.to.demo.video";
-		project.pledged = 25;
-		project.goal = 2000;
-		project.ID = 23;
-		projects.add(project);
-		comments = new Comments(project);
-		comments.addComment(3, "how much weight the bike?");
-		comments.addComment(2, "The weight of bike is 15 kilo");
-		allComments.add(comments);
-
-		project = new Project("Create quadrocopter", category);
-		project.ID = 4;
-		projects.add(project);
-
 		page = new ProjectsPage(projects, ui);
-		controller.addPage(page);
+		modelPage = new ModelPage(0, 2, 3, 4);
+		modelPage
+				.setCommentOfoptions("Select project by ID ; c - to Category Page");
+		controller.addPage(page, modelPage);
+
 		page = new DetailedProject(ui, allComments, projects);
-		controller.addPage(page);
-		page= new WrongChoicePage(ui);
-		controller.addPage(page);
+		modelPage = new ModelPage(1, 2, 3, 4);
+		modelPage.setCommentOfoptions("c - to Category Page");
+		controller.addPage(page, modelPage);
+
+		page = new WrongChoicePage(ui);
+		modelPage = new ModelPage(1, 2, 3, 4);
+		modelPage.setCommentOfoptions("p - to previous Page");
+		controller.addPage(page, modelPage);
+
+		page = new TheEndPage(ui);
+		modelPage = new ModelPage(1, 2, 3, 4);
+
+		controller.addPage(page, modelPage);
 	}
 }
