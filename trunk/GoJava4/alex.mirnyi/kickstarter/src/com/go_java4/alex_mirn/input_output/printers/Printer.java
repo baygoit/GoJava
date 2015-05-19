969ef7f7532cb7348db0a26ddf11d9d239274efa
alexmirn2@gmail.com <alexmirn2@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-19 16:41:37 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@3977 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/alex.mirnyi/kickstarter/src/com/go_java4/alex_mirn/input_output/printers/Printer.java
+++
@@ -1,9 +0,0 @@
-package com.go_java4.alex_mirn.input_output.printers;
-
-public interface Printer {
-
-	public void print(String string);
-
-	public void println(String string);
-
-}
--- trunk/GoJava4/alex.mirnyi/kickstarter/src/com/go_java4/alex_mirn/input_output/readers/Reader.java
+++
@@ -1,9 +0,0 @@
-package com.go_java4.alex_mirn.input_output.readers;
-
-import java.io.IOException;
-
-public interface Reader {
-
-	public String readline() throws IOException;
-
-}
