de1a01c55bebe3d998455242a7eff6effa079920
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-21 13:52:49 +0000

    add UserStory1 Test
    git-svn-id: http://go-java-1.googlecode.com/svn@4014 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter/test/kickstarter/UserStory1Test.java
@@ -0,0 +1,78 @@
+package kickstarter;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import kickstarter.interfaces.printers.Printer;
+import kickstarter.interfaces.readers.Reader;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+public class UserStory1Test {
+
+	private ArgumentCaptor<String> messages;
+
+	@Before
+	public void initKickstarter() {
+		Printer printer = mock(Printer.class);
+
+		Reader reader = mock(Reader.class);
+
+		programmingReaderOnlyForRunAndExit(reader);
+
+		runKickstarter(printer, reader);
+
+		messages = getDisplayedMessages(printer);
+	}
+
+	@Test
+	public void shouldDisplaySomeQuote_whenRunKickstarter() {
+
+		int indexOfFirstMessage = 0;
+		String result = messages.getAllValues().get(indexOfFirstMessage);
+
+		assertTrue(result.contains("Quote:"));
+		assertTrue(result.length() > "Quote:".length());
+
+	}
+
+	@Test
+	public void shouldDisplayGoodLuck_whenExitFromKickstarter() {
+
+		int indexOfLastMessage = messages.getAllValues().size() - 1;
+		String result = messages.getAllValues().get(indexOfLastMessage);
+
+		assertEquals("Good Luck!", result);
+
+	}
+
+	private void runKickstarter(Printer printer, Reader reader) {
+		Kickstarter kickstarter = new Kickstarter(initModel(), new View(printer, reader));
+		kickstarter.run();
+	}
+
+	private ArgumentCaptor<String> getDisplayedMessages(Printer printer) {
+		ArgumentCaptor<String> result = ArgumentCaptor.forClass(String.class);
+
+		verify(printer, atLeastOnce()).showMessage(result.capture());
+
+		return result;
+	}
+
+	private Model initModel() {
+		Runner runner = new Runner();
+		return runner.initModel();
+	}
+
+	private void programmingReaderOnlyForRunAndExit(Reader reader) {
+		try {
+			when(reader.getLine()).thenReturn("0");
+		} catch (IOException e) {
+			fail("IOException from getLine method");
+		}
+	}
+}
