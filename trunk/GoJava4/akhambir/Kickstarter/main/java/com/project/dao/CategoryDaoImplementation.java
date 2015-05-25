988592d775b18d2d91d813c8254a9bce32b51b21
a.khambir@gmail.com <a.khambir@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-25 16:53:06 +0000

    newKickstarter
    git-svn-id: http://go-java-1.googlecode.com/svn@4073 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/akhambir/Kickstarter/main/java/com/project/dao/CategoryDaoImplementation.java
@@ -0,0 +1,12 @@
+package com.project.dao;
+
+import com.project.model.Category;
+
+import java.util.List;
+
+public class CategoryDaoImplementation implements CategoryDao {
+  @Override
+  public List<Category> findAll() {
+    return null;
+  }
+}
