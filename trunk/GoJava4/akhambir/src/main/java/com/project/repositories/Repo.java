1ea1ddd358296378f3440c271bcdc63b624b402c
a.khambir@gmail.com <a.khambir@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-05-25 17:00:34 +0000

    initial commit for newKickstarter
    git-svn-id: http://go-java-1.googlecode.com/svn@4074 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/akhambir/src/main/java/com/project/repositories/Repo.java
@@ -0,0 +1,7 @@
+package com.project.repositories;
+
+import java.util.List;
+
+public interface Repo {
+  List findAll();
+}
---
+++ trunk/GoJava4/akhambir/src/main/java/com/project/service/CategoryService.java
@@ -0,0 +1,9 @@
+package com.project.service;
+
+import com.project.model.Category;
+
+import java.util.List;
+
+public interface CategoryService {
+  List<Category> findAll();
+}
