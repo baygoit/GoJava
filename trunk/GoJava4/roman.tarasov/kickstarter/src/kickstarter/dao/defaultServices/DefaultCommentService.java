854b941aa75d87b0affb7f863bdbf9443898907d
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-05 14:30:54 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4193 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/kickstarter/src/kickstarter/dao/defaultServices/DefaultCommentService.java
+++ trunk/GoJava4/roman.tarasov/kickstarter/src/kickstarter/dao/defaultServices/DefaultCommentService.java
@@ -23,12 +23,14 @@ public class DefaultCommentService implements iCommentService {
 		ProjectComment comment = new ProjectComment();
 		comment.setComment("What color will paint?");
 		comment.setUserID(3);
+		comment.setCommentID(10);
 		comment.setProjectID(projectID);
 		commentsInProject.add(comment);
 
 		comment = new ProjectComment();
 		comment.setComment("Paint is Green");
 		comment.setUserID(2);
+		comment.setCommentID(11);
 		comment.setProjectID(projectID);
 		commentsInProject.add(comment);
 		allComments.put(projectID, commentsInProject);
@@ -38,12 +40,14 @@ public class DefaultCommentService implements iCommentService {
 		comment = new ProjectComment();
 		comment.setComment("how much weight the bike?");
 		comment.setUserID(4);
+		comment.setCommentID(8);
 		comment.setProjectID(projectID);
 		commentsInProject.add(comment);
 
 		comment = new ProjectComment();
 		comment.setComment("The weight of bike is 15 kilo");
 		comment.setUserID(5);
+		comment.setCommentID(20);
 		comment.setProjectID(projectID);
 		commentsInProject.add(comment);
 		allComments.put(projectID, commentsInProject);
@@ -53,11 +57,13 @@ public class DefaultCommentService implements iCommentService {
 		comment = new ProjectComment();
 		comment.setComment("One request: make sure your documentation and tutorials are crystal clear and checked by a native English speaker. (At one point they weren't) That's half of the product :)");
 		comment.setUserID(8);
+		comment.setCommentID(30);
 		comment.setProjectID(projectID);
 		commentsInProject.add(comment);
 
 		comment = new ProjectComment();
 		comment.setUserID(9);
+		comment.setCommentID(32);
 		comment.setProjectID(projectID);
 		comment.setComment("Will your company be considering a camera module, fingerprint scanner or a capacitive lcd/led display with fingerprint scanner ability?");
 		commentsInProject.add(comment);
@@ -84,13 +90,17 @@ public class DefaultCommentService implements iCommentService {
 		return allComments.get(projectID);
 	}
 
-
 	@Override
 	public void deleteComment(int projectID, int commentID)
 			throws ServiceException {
 		List<ProjectComment> listFromAllComments = getCommentsByProjectID(projectID);
 		if (listFromAllComments != null) {
-			listFromAllComments.remove(commentID);
+			for (int index = 0; index < listFromAllComments.size(); index++) {
+				if (listFromAllComments.get(index).getCommentID() == commentID) {
+					listFromAllComments.remove(index);
+				}
+			}
+
 			return;
 		}
 		throw new ServiceException("error delete comment ");
@@ -102,7 +112,6 @@ public class DefaultCommentService implements iCommentService {
 	}
 
 	@Override
-	public void createComments(iDAO sourceDAO)
-			throws SQLException {
+	public void createComments(iDAO sourceDAO) throws SQLException {
 	}
 }
