61c545b2bdf1ad6f4373bfc2246b308c0de5113d
alexmirn2@gmail.com <alexmirn2@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-12 07:15:26 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4243 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/alex.mirnyi/kickstarter2/src/com/go_java4/alex_mirn/dao/Dao.java
+++
@@ -1,8 +0,0 @@
-package com.go_java4.alex_mirn.dao;
-
-import java.sql.SQLException;
-
-import com.go_java4.alex_mirn.data.Project;
-
-public interface Dao extends QuotesDao, CategoriesDao, ProjectsDao {
-}
