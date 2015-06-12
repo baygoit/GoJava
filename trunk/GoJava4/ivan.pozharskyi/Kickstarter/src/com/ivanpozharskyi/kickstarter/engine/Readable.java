78d113037cb6c7290d2eb87ac9fb94917d953548
Pozharskyi@gmail.com <Pozharskyi@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-12 10:52:18 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4257 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/ivan.pozharskyi/Kickstarter/src/com/ivanpozharskyi/kickstarter/engine/Readable.java
+++
@@ -1,6 +0,0 @@
-package com.ivanpozharskyi.kickstarter.engine;
-
-public interface Readable {
-	int read();
-
-}
--- trunk/GoJava4/ivan.pozharskyi/Kickstarter/src/com/ivanpozharskyi/kickstarter/userinterface/Menu.java
+++
@@ -1,8 +0,0 @@
-package com.ivanpozharskyi.kickstarter.userinterface;
-
-public interface Menu {
-	void show();
-
-	void setChoise(Object choice);
-
-}
