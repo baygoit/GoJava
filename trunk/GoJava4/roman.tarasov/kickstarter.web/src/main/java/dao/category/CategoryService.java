c1a8aec423ef19d03cb84c5bb804b1d8653111c1
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-16 18:12:06 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4279 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/kickstarter.web/src/main/java/dao/category/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package dao.category;
-
-import java.util.List;
-import database.KickstarterException;
-
-public interface CategoryService {
-
-	List<Category> getAll() throws KickstarterException;
-}
--- trunk/GoJava4/roman.tarasov/kickstarter.web/src/main/java/dao/quote/QuoteService.java
+++
@@ -1,9 +0,0 @@
-package dao.quote;
-
-import database.KickstarterException;
-
-public interface QuoteService {
-
-	Quote getRandomQuote() throws KickstarterException;
-
-}
