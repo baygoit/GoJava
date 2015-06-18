4607e045d87a2186301e291f8e69e81f9442ca0b
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-18 07:53:21 +0000

    Initial import.
    git-svn-id: http://go-java-1.googlecode.com/svn@4295 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter_mvn/src/main/java/kickstarter/Kickstarter.java
@@ -0,0 +1,21 @@
+package kickstarter;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/")
+public class Kickstarter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher view = request.getRequestDispatcher("/test.jsp");
+		view.forward(request, response);
+	}
+
+}
---
+++ trunk/GoJava4/andrii.vasylenko/kickstarter_mvn/src/main/webapp/test.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h1>kickstarter_mvn</h1>
+<h2>Hello World!</h2>
+</body>
+</html>
