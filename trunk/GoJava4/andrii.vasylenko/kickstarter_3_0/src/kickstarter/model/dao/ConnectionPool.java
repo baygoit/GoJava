383043b0c12090a96da17d30f63d0776e1ea7368
AVP256COM@gmail.com <AVP256COM@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-18 14:58:40 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4311 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/andrii.vasylenko/kickstarter_3_0/src/kickstarter/model/dao/ConnectionPool.java
+++
@@ -1,8 +0,0 @@
-package kickstarter.model.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionPool {
-	public Connection getConnection() throws SQLException;
-}
--- trunk/GoJava4/andrii.vasylenko/kickstarter_3_0/src/kickstarter/model/dao/QuestionsDAO.java
+++
@@ -1,9 +0,0 @@
-package kickstarter.model.dao;
-
-import java.sql.SQLException;
-
-public interface QuestionsDAO {
-	void addQuestion(int projectId, String question) throws SQLException;
-
-	void createTableQuestions() throws SQLException;
-}
