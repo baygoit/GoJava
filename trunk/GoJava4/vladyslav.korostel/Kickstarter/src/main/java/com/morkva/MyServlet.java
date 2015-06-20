bea282e826286aa05b3df3300d22bb87a2bb119e
korostel.v.s@gmail.com <korostel.v.s@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-20 21:59:27 +0000

    Create hello wordl jsp file.
    git-svn-id: http://go-java-1.googlecode.com/svn@4336 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/vladyslav.korostel/Kickstarter/src/main/java/com/morkva/MyServlet.java
+++ trunk/GoJava4/vladyslav.korostel/Kickstarter/src/main/java/com/morkva/MyServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * Created by koros on 19.06.2015.
@@ -15,9 +14,6 @@ import java.io.PrintWriter;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-
-        out.println("Hello World");
+        req.getRequestDispatcher("index.jsp").forward(req, resp);
     }
 }
---
+++ trunk/GoJava4/vladyslav.korostel/Kickstarter/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: koros
+  Date: 20.06.2015
+  Time: 18:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>TITLE</title>
+</head>
+<body>
+  <h1>Hello JSP</h1>
+</body>
+</html>
