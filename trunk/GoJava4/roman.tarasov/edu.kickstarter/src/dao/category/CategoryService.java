97e74fd6dfa643ba4a70252a25821451ab528811
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-21 19:23:16 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4338 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/edu.kickstarter/src/dao/category/CategoryService.java
+++
@@ -1,9 +0,0 @@
-package dao.category;
-
-import java.util.List;
-import dao.pool.KickstarterException;
-
-public interface CategoryService {
-
-	List<Category> getAll() throws KickstarterException;
-}
--- trunk/GoJava4/roman.tarasov/edu.kickstarter/src/view/eViews.java
+++
@@ -1,9 +0,0 @@
-package view;
-
-public enum eViews {
-CATEGORIES_V,
-PROJECTS_V,
-DETAILED_PROJECT_V,
-LOGIN_V,
-ERROR_V
-}
