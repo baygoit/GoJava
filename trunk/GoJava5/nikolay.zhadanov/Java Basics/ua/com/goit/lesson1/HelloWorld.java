116a5174d9257a576f89790c67c3ce36a1bc9640
dogsgod@gmail.com <dogsgod@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-23 21:07:01 +0000

    Nic Zhadanov 24/06/2015 Lesson 1 - Task 1
    git-svn-id: http://go-java-1.googlecode.com/svn@4355 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.zhadanov/Java Basics/ua/com/goit/lesson1/HelloWorld.java
@@ -0,0 +1,19 @@
+package ua.com.goit.lesson1;
+
+/*
+ * Lesson 1 - Task 1
+ * Если ты раньше не программировал на java - то первая задача, естественно, 
+ * сделать вывод сообщения "Hello world" в консоли. 
+ * Сделай это, чтобы убедиться, что Java и Eclipse настроены правильно. 
+ */
+
+public class HelloWorld {
+	
+	public static final String HELLO_WORLD = "Hello world!";
+	
+	public static void main(String[] args)
+	{
+		System.out.print(HELLO_WORLD);
+	}
+
+}
