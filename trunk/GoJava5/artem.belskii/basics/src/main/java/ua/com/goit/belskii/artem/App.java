8b1e62520926e56b7255a20aa6cf6e65d28c98cc
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-24 09:16:03 +0000

    initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@4365 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/artem.belskii/basics/src/main/java/ua/com/goit/belskii/artem/App.java
@@ -0,0 +1,15 @@
+package ua.com.goit.belskii.artem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	HelloWorld hello = new HelloWorld();
+    	hello.setName("World");
+        System.out.println( hello.sayHello() );
+    }
+}
