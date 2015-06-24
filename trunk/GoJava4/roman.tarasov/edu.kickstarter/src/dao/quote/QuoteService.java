9434e0e9bff61dad825062b4aa51f1b5fb9519cd
Roman.Tarasov.mail@gmail.com <Roman.Tarasov.mail@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-24 22:28:37 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4381 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava4/roman.tarasov/edu.kickstarter/src/dao/quote/QuoteService.java
+++
@@ -1,9 +0,0 @@
-package dao.quote;
-
-import dao.pool.KickstarterException;
-
-public interface QuoteService {
-
-	Quote getRandomQuote() throws KickstarterException;
-
-}
--- trunk/GoJava4/roman.tarasov/edu.kickstarter/src/model/eModels.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public enum eModels {
-MAIN_M,
-PROJECTS_M,
-PROJECT_M,
-LOGIN_M,
-ERROR_M
-}
