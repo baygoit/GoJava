205d430647448eff10f438c186448b7e213d3a85
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-24 11:14:03 +0000

    Initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@4373 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/artem.belskii/basics/Distance/src/main/java/ua/com/goit/belskii/artem/App.java
@@ -0,0 +1,13 @@
+package ua.com.goit.belskii.artem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ trunk/GoJava5/artem.belskii/basics/Distance/src/test/java/ua/com/goit/belskii/artem/AppTest.java
@@ -0,0 +1,69 @@
+package ua.com.goit.belskii.artem;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AppTest{
+	Distance list;
+	@Before
+	public void setUp(){
+		list = new Distance();
+	}
+@Test
+public void testMin(){
+	int [] elements={5,7,2,16,13,4};
+	assertEquals(2, list.findMin( elements ));
+}
+
+@Test
+public void testMin1(){
+	int [] elements={1,2,3,4,5,6,7,8,9};
+	assertEquals(1, list.findMin( elements ));
+}
+
+@Test
+public void testMax(){
+	int [] elements={5,7,2,16,13,4};
+	assertEquals(16, list.findMax( elements ));
+}
+
+@Test
+public void testMax1(){
+	int [] elements={9,8,7,6,5,4,3,2,1};
+	assertEquals(9, list.findMax( elements ));
+}
+
+@Test
+public void testPosition(){
+	int [] elements={5,7,2,16,13,4};
+	assertEquals(0, list.findPosition(elements, 5));
+}
+
+@Test
+public void testPosition2(){
+	int [] elements={5,7,2,16,13,4};
+	assertEquals(3, list.findPosition(elements, 16));
+}
+
+@Test
+public void testPosition3(){
+	int [] elements={5,7,2,16,13,16};
+	assertEquals(3, list.findPosition(elements, 16));
+}
+
+@Test
+public void testDistance(){
+	int [] elements={5,7,2,1,13,16};
+	assertEquals(2, list.findDistance(elements));
+}
+
+@Test
+public void testDistance1(){
+	int [] elements={1,2,3,4,5,6,7,8,9};
+	assertEquals(8, list.findDistance(elements));
+}
+
+
+}
