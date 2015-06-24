576303aef953e95e040aa1e6c79a3c0a8047541c
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-24 09:29:31 +0000

    Initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@4371 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/artem.belskii/basics/Anagramma/src/test/java/com/ua/goit/belskii/artem/AppTest.java
@@ -0,0 +1,22 @@
+package com.ua.goit.belskii.artem;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest{
+	private Convert anagramma ;
+	
+	@Before 
+	public void setUp(){
+		anagramma = new Convert();
+	}
+	
+	@Test
+	public void test(){
+		assertEquals("амам алым умар ",anagramma.convert("мама мыла раму"));
+	    
+	}
+	
+}
