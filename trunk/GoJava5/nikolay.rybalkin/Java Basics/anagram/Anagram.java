64279bc33a37ef7cd3ac9bd22ce8a820c9938e4f
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-25 20:00:32 +0000

    add ln
    git-svn-id: http://go-java-1.googlecode.com/svn@4398 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Java Basics/anagram/Anagram.java
+++ trunk/GoJava5/nikolay.rybalkin/Java Basics/anagram/Anagram.java
@@ -6,7 +6,7 @@ public class Anagram {
 	
 	 public static void main(String[] args) {
 
-		 System.out.print("Enter the console words separated by a space, then press enter: ");
+		 System.out.println("Enter the console words separated by a space, then press enter: ");
 		 Scanner scanConsole = new Scanner(System.in);
          String line = scanConsole.nextLine();        
          String[] array = line.split(" ");   
