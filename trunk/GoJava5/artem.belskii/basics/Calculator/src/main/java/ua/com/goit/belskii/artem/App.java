5d90052a9f9845e8f5a560078d28810001214102
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-26 12:59:34 +0000

    Initial commit
    git-svn-id: http://go-java-1.googlecode.com/svn@4407 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/artem.belskii/basics/Calculator/src/main/java/ua/com/goit/belskii/artem/App.java
@@ -0,0 +1,19 @@
+package ua.com.goit.belskii.artem;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+    	Input consoleIn = new Input();
+    	consoleIn.setString();
+    	consoleIn.getInput();
+    	Division division = new Division(consoleIn.getInput());
+    	division.calculate();
+    	
+    		
+    	}
+
+    	
+}
---
+++ trunk/GoJava5/artem.belskii/basics/Calculator/src/main/java/ua/com/goit/belskii/artem/Division.java
@@ -0,0 +1,76 @@
+package ua.com.goit.belskii.artem;
+
+import java.util.ArrayList;
+
+public class Division {
+	String divident, divisor;
+	int divisorInt, dividentPartEndPos;
+	private int remainder;
+	private int dividentPart;
+	ArrayList divisionProcess = new ArrayList();
+	ArrayList divisionResult = new ArrayList();
+	int dividentPartStartPos=0;
+	int factorialCount=0;
+	int result=Integer.MAX_VALUE;
+
+	Division(String [] expression){
+		divident=expression[0];
+		divisor=expression[1];
+		divisorInt=Integer.parseInt(divisor);	
+		dividentPartEndPos=divisor.length();
+	}
+
+	public void getPartDivident(){
+		    	
+		if (dividentPartStartPos == 0){
+			dividentPart=Integer.parseInt(divident.substring(dividentPartStartPos,dividentPartEndPos));
+		}
+		if (dividentPart < divisorInt && dividentPartStartPos == 0 && dividentPartStartPos<divisor.length()) {
+    		dividentPartEndPos++;
+    		dividentPart=Integer.parseInt(divident.substring(dividentPartStartPos,dividentPartEndPos));
+    	} else if(dividentPartStartPos > 0 && dividentPartStartPos<divident.length()){
+    		dividentPart=Integer.parseInt(Integer.toString(remainder)+divident.substring(dividentPartStartPos,dividentPartEndPos));
+    	} else if(dividentPartStartPos>divisor.length()){
+			if(factorialCount==0){
+				divisionResult.add(".");
+				dividentPart=remainder*10;
+				factorialCount++;
+			}
+			if(factorialCount > 0 && factorialCount<=120){
+				dividentPart=remainder*10;
+				factorialCount++;
+			}
+    	}
+    	dividentPartStartPos=dividentPartEndPos;
+    	dividentPartEndPos++;
+	}
+	
+	public void partDivision(){
+		remainder=dividentPart%divisorInt;
+		result=dividentPart/divisorInt;
+		divisionResult.add(result);
+		divisionProcess.add("   "+dividentPart);
+		divisionProcess.add("-");
+		divisionProcess.add("   "+result*divisorInt);
+		divisionProcess.add("______________________");
+	}
+	
+	public void calculate(){
+		while (result>0){
+    	this.getPartDivident();
+    	this.partDivision();
+		}
+		this.getResult();
+	} 
+	
+	public String getResult(){
+		StringBuffer result = new StringBuffer();
+		for (int i=0; i<divisionProcess.size();i++){
+			System.out.println(divisionProcess.get(i));			
+		}
+		for (int i=0; i<divisionResult.size();i++){
+			System.out.print(divisionResult.get(i));			
+		}
+		return result.toString();
+	}
+}
