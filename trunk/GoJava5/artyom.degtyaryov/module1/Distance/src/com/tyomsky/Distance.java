90728808c857854b7d49bf9e8985c25773843d40
tyomsky <tyomsky@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-26 13:15:41 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4409 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artyom.degtyaryov/module1/Distance/src/com/tyomsky/Distance.java
+++ trunk/GoJava5/artyom.degtyaryov/module1/Distance/src/com/tyomsky/Distance.java
@@ -1,53 +1,81 @@
 package com.tyomsky;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Distance {
-    public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); //todo: вынести в глобальную переменную
-        int[] inputArray;
-        int distanceBetweenMins;
+	public static final String delimiter = " +";
+	public static BufferedReader consoleReader;
 
-        System.out.println("Enter values:");
-        inputArray = getArrayOfIntegersFromConsole(reader);
-        if (inputArray == null) {return;}
+	public static void main(String[] args) throws IOException {
+		consoleReader = new BufferedReader(new InputStreamReader(System.in));
+		int[] inputArray;
+		int distanceBetweenMins;
+		boolean isFinished = false;
+		while (!isFinished) {
+			System.out.println("Enter values:");
+			inputArray = getArrayOfIntegersFromConsole(consoleReader);
+			if (inputArray != null) {
+				distanceBetweenMins = getDistanceBetweenMins(inputArray);
+				if (distanceBetweenMins == 0) {
+					System.out.println("Enter more then one number.");
+				} else {
+					System.out.println("Distance between mins: " + distanceBetweenMins);
+				}
+			}
+			System.out.println("Try again? (y/n)");
+			if (!consoleReader.readLine().equals("y")) {
+				isFinished = true;
+			}
+		}
+	}
 
-        distanceBetweenMins = getDistanceBetweenMins(inputArray);
-        System.out.println("Distance between mins: "+distanceBetweenMins);
-    }
+	public static int getDistanceBetweenMins(int[] inputArray) {
+		int result = 0;
+		if (inputArray != null && inputArray.length > 1) {
+			int firstMinValueIndex = 0;
+			int secondMinValueIndex = 1;
+			for (int i = 0; i < inputArray.length; i++) {
+				if (inputArray[firstMinValueIndex] > inputArray[i] && i != secondMinValueIndex) {
+					if (inputArray[secondMinValueIndex] > inputArray[firstMinValueIndex]) {
+						secondMinValueIndex = firstMinValueIndex;
+					}
+					firstMinValueIndex = i;
+				} else if (inputArray[secondMinValueIndex] > inputArray[i] && i != firstMinValueIndex) {
+					secondMinValueIndex = i;
+				}
+			}
+			result = Math.abs(firstMinValueIndex - secondMinValueIndex);
+		}
+		return result;
+	}
 
-    private static int getDistanceBetweenMins(int[] inputArray) {
-        if(inputArray != null && inputArray.length > 1){
-            int firstMinValueIndex = 0;
-            int secondMinValueIndex = 0;
-            for (int i = 0; i < inputArray.length; i++) {
-                if (inputArray[firstMinValueIndex] <= inputArray[i]) {firstMinValueIndex = i;}
-            }
-            for (int i = 0; i < inputArray.length; i++) {
-                if (i == firstMinValueIndex) {continue;}
-                if (inputArray[secondMinValueIndex] <= inputArray[i]) {secondMinValueIndex = i;}
-            }
-            return Math.abs(firstMinValueIndex - secondMinValueIndex);
-        }
-        return 0;
-    }
-
-    public static int[] getArrayOfIntegersFromConsole(BufferedReader reader){
-        String[] inputStringArray;
-        int[] result;
-        try {
-            inputStringArray = reader.readLine().split(" ");
-            result = new int[inputStringArray.length];
-            for (int i = 0; i < inputStringArray.length; i++){
-                result[i] = Integer.parseInt(inputStringArray[i]);
-            }
-        }catch (Exception e){ /// думаю тут  не важно что ловить
-            System.out.println("Invalid input");
-            return null;
-        }
-        return result;
-    }
+	public static int[] getArrayOfIntegersFromConsole(BufferedReader reader) {
+		String[] inputStringArray;
+		int[] result = null;
+		try {
+			inputStringArray = reader.readLine().split(delimiter);
+			if (inputStringArray.length == 1 && inputStringArray[0].equals("")) {
+				System.out.println("I cant read your mind. Enter smtn!");
+				return null;
+			}
+			result = new int[inputStringArray.length];
+			for (int i = 0; i < inputStringArray.length; i++) {
+				result[i] = Integer.parseInt(inputStringArray[i]);
+			}
+		} catch (NumberFormatException e) {
+			System.out.println("I work only with integers ):");
+			result = null;
+		} catch (IOException e) {
+			System.out.println("Something wrong with IO! ):");
+			result = null;
+		} catch (Exception e) {
+			System.out.println("Unexpected exception!");
+			System.out.println(e.getStackTrace());
+			result = null;
+		}
+		return result;
+	}
 
 }
