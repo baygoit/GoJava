3ccedc1dea2e3c3ab22aa331157505aff11cc594
dogsgod@gmail.com <dogsgod@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-29 11:16:17 +0000

    fix braces
    git-svn-id: http://go-java-1.googlecode.com/svn@4435 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.zhadanov/Basics/goit/nz/lesson1/HelloWorld.java
+++ trunk/GoJava5/nikolay.zhadanov/Basics/goit/nz/lesson1/HelloWorld.java
@@ -8,12 +8,8 @@ package goit.nz.lesson1;
  */
 
 public class HelloWorld {
-	
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		final String HELLO_WORLD = "Hello world!";
-		
 		System.out.print(HELLO_WORLD);
 	}
-
 }
