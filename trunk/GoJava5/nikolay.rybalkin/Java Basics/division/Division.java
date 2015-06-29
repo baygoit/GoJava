4a7160f2af49250cbeb8b66b8f0a512b71ad677f
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-29 13:03:35 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4438 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Java Basics/division/Division.java
@@ -0,0 +1,73 @@
+package division;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Division {
+
+    static int division;
+    static int module;
+    static int count = 0;
+    static StringBuilder buf = new StringBuilder();
+    static StringBuilder stolbikString = new StringBuilder();
+    static ArrayList<Integer> stolbikArray = new ArrayList<Integer>();
+    static boolean hasPeriod = false;
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter two numbers are stripped of their \"/\" : ");
+        Scanner scanConsole = new Scanner(System.in);
+        String numbers = scanConsole.nextLine();
+        String[] tokens = numbers.split("/");
+        int num1 = Integer.parseInt(tokens[0]);
+        int num2 = Integer.parseInt(tokens[1]);
+
+        devision(num1, num2);
+    }
+
+    public static void devision(int a, int b) {
+        String ab = String.valueOf(a) + "|" + String.valueOf(b);
+        while (true) {
+            division = a / b;
+            module = a % b;
+            if (division == 0) {
+                if (buf.length() == 0) {
+                    buf.append("0.(");
+                }
+                if (buf.toString().contains(".") == false) {
+                    buf.append(".(");
+                }
+                a *= 10;
+            } else {
+                count++;
+                buf.append(division);
+                stolbikString.append(String.format("%" + count + "s", " ") + "-" + (division * b) + "\n");
+                stolbikString.append(String.format("%" + count + "s", " ") + "------" + "\n");
+                stolbikString.append(String.format("%" + count + "s", " ") + "  " + module * 10 + "\n");
+                a = module;
+
+                stolbikArray.add(module);
+                for (int i = 1; i < stolbikArray.size(); i++) {
+                    if (stolbikArray.size() > 1) {
+                        if (stolbikArray.get(0) == stolbikArray.get(i)) {
+                            hasPeriod = true;
+                        }
+                    }
+                }
+            }
+
+            if (module == 0 || count == 100 || hasPeriod) {
+                break;
+            }
+        }
+
+        if (!hasPeriod) {
+            buf.deleteCharAt(buf.indexOf("("));
+        } else {
+            buf.append(")");
+        }
+
+        System.out.println(String.format("%" + 3 + "s", " ") + ab);
+        System.out.println(String.format("%" + 5 + "s", "") + "|" + buf);
+        System.out.println(stolbikString);
+    }
+}
