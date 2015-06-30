eb817738ac9c15024d3aad304f6eeab593a9f871
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-30 16:22:06 +0000

    fix code style
    git-svn-id: http://go-java-1.googlecode.com/svn@4444 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/basics/HelloWorld/src/main/java/ua/com/goit/belskii/artem/App.java
+++ trunk/GoJava5/artem.belskii/basics/HelloWorld/src/main/java/ua/com/goit/belskii/artem/App.java
@@ -4,12 +4,10 @@ package ua.com.goit.belskii.artem;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	HelloWorld hello = new HelloWorld();
-    	hello.setName("World");
-        System.out.println( hello.sayHello() );
-    }
+public class App {
+	public static void main(String[] args) {
+		HelloWorld hello = new HelloWorld();
+		hello.setName("World");
+		System.out.println(hello.sayHello());
+	}
 }
--- trunk/GoJava5/artem.belskii/basics/HelloWorld/src/test/java/ua/com/goit/belskii/artem/HelloWorldTest.java
+++ trunk/GoJava5/artem.belskii/basics/HelloWorld/src/test/java/ua/com/goit/belskii/artem/HelloWorldTest.java
@@ -7,20 +7,22 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HelloWorldTest {
- private HelloWorld hello;
-    @Before
-    public void setUp() {
-        hello = new HelloWorld();
-        hello.setName("World");
-    }
-    @Test
-    public void testName() {
-    	assertEquals("World", hello.getName());
-    }
-    
-    @Test
-    public void testMessage() {
-    	assertEquals("Hello, World!", hello.sayHello());
-    }
+	private HelloWorld hello;
+
+	@Before
+	public void setUp() {
+		hello = new HelloWorld();
+		hello.setName("World");
+	}
+
+	@Test
+	public void testName() {
+		assertEquals("World", hello.getName());
+	}
+
+	@Test
+	public void testMessage() {
+		assertEquals("Hello, World!", hello.sayHello());
+	}
 
 }
