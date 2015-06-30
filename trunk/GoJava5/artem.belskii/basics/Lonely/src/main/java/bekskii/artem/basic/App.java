f7c56f86d586da3bb0961b0cd935c71fb665d6f2
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-06-30 16:37:11 +0000

    fix code style
    git-svn-id: http://go-java-1.googlecode.com/svn@4446 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/basics/Lonely/src/main/java/bekskii/artem/basic/App.java
+++ trunk/GoJava5/artem.belskii/basics/Lonely/src/main/java/bekskii/artem/basic/App.java
@@ -4,13 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class App {
-  public static void main(String[] args) {
-	  Input consoleIn = new Input();
-	  Lonely lonely = new Lonely();
-	  consoleIn.setString();
-	  System.out.println("lonely nymber:    " +lonely.findLonely(consoleIn.getInput()));
-	  
-
-
-  }
+	public static void main(String[] args) {
+		Input consoleIn = new Input();
+		Lonely lonely = new Lonely();
+		consoleIn.setString();
+		System.out.println("lonely nymber:    "
+				+ lonely.findLonely(consoleIn.getInput()));
+	}
 }
--- trunk/GoJava5/artem.belskii/basics/Lonely/src/main/java/bekskii/artem/basic/Lonely.java
+++ trunk/GoJava5/artem.belskii/basics/Lonely/src/main/java/bekskii/artem/basic/Lonely.java
@@ -4,17 +4,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Lonely {
-	public Set<Integer> findLonely(int[] data){
-	    Set<Integer> uniques = new HashSet<Integer>();
-	    Set<Integer> dups = new HashSet<Integer>();
-	    //Integer [] data = new Integer []{2, 3, 2, 4, 3, 7, 2, 3, 4, 4};
-	    for (Integer a : data){
-	      if (!uniques.add(a)){
-	        dups.add(a);
-	      }
-	    }
-	    uniques.removeAll(dups);
-	    return uniques;
+	
+	public Set<Integer> findLonely(int[] data) {
+		Set<Integer> uniques = new HashSet<Integer>();
+		Set<Integer> dups = new HashSet<Integer>();
+		for (Integer a : data) {
+			if (!uniques.add(a)) {
+				dups.add(a);
+			}
+		}
+		uniques.removeAll(dups);
+		return uniques;
 	}
 
 }
