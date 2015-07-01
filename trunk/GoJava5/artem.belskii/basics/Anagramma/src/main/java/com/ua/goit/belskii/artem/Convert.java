faf231b75f520a139beb10fa57a3e7dd6fb0ea7b
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-01 14:57:18 +0000

    Use StringBuilder for answer variable.
    git-svn-id: http://go-java-1.googlecode.com/svn@4478 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/basics/Anagramma/src/main/java/com/ua/goit/belskii/artem/Convert.java
+++ trunk/GoJava5/artem.belskii/basics/Anagramma/src/main/java/com/ua/goit/belskii/artem/Convert.java
@@ -3,17 +3,17 @@ package com.ua.goit.belskii.artem;
 public class Convert {
 
 	public String convert(String originalText) {
-		String answer = "";
+		StringBuilder answer = new StringBuilder();
 		String[] separateWords = originalText.split(" ");
 		for (int i = 0; i < separateWords.length; i++) {
 			for (int i1 = separateWords[i].length(); i1 > 0; i1--) {
-				answer = answer + separateWords[i].substring(i1 - 1, i1);
+				answer.append(separateWords[i].substring(i1 - 1, i1));
 			}
 			if (i<=separateWords.length-2){
-				answer = answer + " ";
+				answer.append(" ");
 			}
 		}
-		return answer;
+		return answer.toString();
 	}
 
 }
