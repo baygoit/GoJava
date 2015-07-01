620c2e2196138ddd3281d3bbe1bf47ce96bb5ce3
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-01 13:32:13 +0000

    remove unused call
    git-svn-id: http://go-java-1.googlecode.com/svn@4462 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/OOP/Calc/src/main/java/belskii/artem/calc/Calc.java
+++ trunk/GoJava5/artem.belskii/OOP/Calc/src/main/java/belskii/artem/calc/Calc.java
@@ -5,7 +5,6 @@ public class Calc {
 	public int calculate(String[] expression) {
 		int answer = Integer.MIN_VALUE;
 		Parser parser = new Parser();
-		parser.validate(expression);
 		if (parser.validate(expression)) { // change to try catch block
 			int operand1 = Integer.parseInt(expression[0]);
 			int operand2 = Integer.parseInt(expression[2]);
