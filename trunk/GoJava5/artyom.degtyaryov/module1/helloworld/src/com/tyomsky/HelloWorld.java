a7df71a79cf2f234f855cc9204c1167a6cc8d7d5
tyomsky <tyomsky@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-01 10:04:00 +0000

    refactor after code review
    git-svn-id: http://go-java-1.googlecode.com/svn@4449 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artyom.degtyaryov/module1/helloworld/src/com/tyomsky/HelloWorld.java
+++ trunk/GoJava5/artyom.degtyaryov/module1/helloworld/src/com/tyomsky/HelloWorld.java
@@ -1,10 +1,10 @@
 package com.tyomsky;
 
 public class HelloWorld {
-	public static final String HELLO_STRING = "Hello, World!";
+	public static final String MESSAGE = "Hello, World!";
 	public static void main(String[] args){
 	
-		System.out.println(HELLO_STRING);
+		System.out.println(MESSAGE);
 	
 	}
 	
