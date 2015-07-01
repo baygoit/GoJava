d794cc92be7e45d616f77614a004c65739af72c7
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-01 14:59:01 +0000

    use DELIMETER as argument for split
    git-svn-id: http://go-java-1.googlecode.com/svn@4479 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/basics/Anagramma/src/main/java/com/ua/goit/belskii/artem/Convert.java
+++ trunk/GoJava5/artem.belskii/basics/Anagramma/src/main/java/com/ua/goit/belskii/artem/Convert.java
@@ -4,7 +4,8 @@ public class Convert {
 
 	public String convert(String originalText) {
 		StringBuilder answer = new StringBuilder();
-		String[] separateWords = originalText.split(" ");
+		String DELIMETER=" ";
+		String[] separateWords = originalText.split(DELIMETER);
 		for (int i = 0; i < separateWords.length; i++) {
 			for (int i1 = separateWords[i].length(); i1 > 0; i1--) {
 				answer.append(separateWords[i].substring(i1 - 1, i1));
