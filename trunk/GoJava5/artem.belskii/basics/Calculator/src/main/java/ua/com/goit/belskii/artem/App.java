7f928fec54cae87901f11e8712a2c9e7ab2c88ff
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-02 08:42:49 +0000

    use while loops for input
    git-svn-id: http://go-java-1.googlecode.com/svn@4482 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/basics/Calculator/src/main/java/ua/com/goit/belskii/artem/App.java
+++ trunk/GoJava5/artem.belskii/basics/Calculator/src/main/java/ua/com/goit/belskii/artem/App.java
@@ -1,13 +1,22 @@
 package ua.com.goit.belskii.artem;
 
+import java.util.Scanner;
+
 public class App {
 	public static void main(String[] args) {
-
-		Input consoleIn = new Input();
-		consoleIn.setString();
-		consoleIn.getInput();
-		Division division = new Division(consoleIn.getInput());
-		division.calculate();
+		Scanner input = new Scanner(System.in);
+		boolean repeat = true;
+		while (repeat) {
+			Input consoleIn = new Input();
+			consoleIn.setString();
+			consoleIn.getInput();
+			Division division = new Division(consoleIn.getInput());
+			division.calculate();
+			System.out.println("Try another? Press enter and put new value! If you want close app, pleas put \"q\", and press enter");
+			if (input.nextLine().equals("q")){
+				repeat=false;
+			}
+		}
 	}
 
 }
