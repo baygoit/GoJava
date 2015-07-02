fdae8aa4a9297a4b277ef3da56ad97258a035652
tyomsky@gmail.com <tyomsky@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-02 07:48:33 +0000

    fixed code structure
    git-svn-id: http://go-java-1.googlecode.com/svn@4481 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artyom.degtyaryov/module1/Calculator/src/com/tyomsky/Calculator.java
+++ trunk/GoJava5/artyom.degtyaryov/module1/Calculator/src/com/tyomsky/Calculator.java
@@ -2,25 +2,23 @@ package com.tyomsky;
 
 import com.tyomsky.ui.Console;
 
-/**
- * Created by tyoms_000 on 30.06.2015.
- */
 public class Calculator {
+
     public void calculate(){
         Console console = new Console();
         Parser parser = new Parser();
         while (true){
             console.write("Enter expression or q for exit");
             String input = console.read();
-            if (input.equals("q")){break;}
-
-            if(parser.verify(input) == true){
+            if (input.equals("q")){
+                break;
+            }
+            if(parser.verify(input)){
               Expression expression = parser.parseExpression(input);
               console.write(String.valueOf(expression.getValue()));
             }else{
                 console.write("Your expression does not satisfies pattern [x + y =]");
             }
-
         }
     }
 }
