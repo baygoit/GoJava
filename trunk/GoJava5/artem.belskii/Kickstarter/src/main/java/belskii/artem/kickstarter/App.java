ffe4abd2c0f5e503f333109a2ae18e5b520ebef4
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-06 09:57:23 +0000

    remove unused import
    git-svn-id: http://go-java-1.googlecode.com/svn@4527 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/App.java
+++ trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/App.java
@@ -2,15 +2,12 @@ package belskii.artem.kickstarter;
 
 import java.util.Random;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Motivation motivation = initializeMotivationFromDatabase();
-        Random randomID;
-        System.out.println(motivation.getMotivation());
-        
-		Category categoryModel = retriveCategoryFromDatabase ();
+public class App {
+	public static void main(String[] args) {
+		Motivation motivation = initializeMotivationFromDatabase();
+		System.out.println(motivation.getMotivation());
+
+		Category categoryModel = retriveCategoryFromDatabase();
 		CategoryView categoryView = new CategoryView();
 		CategoryController categoryController = new CategoryController(categoryModel, categoryView);
 		categoryController.addCategory("Music");
@@ -18,14 +15,13 @@ public class App
 
 		Projects projectsModel = retriveProjectFromDatabase();
 		ProjectsView projectsView = new ProjectsView();
-		ProjectsController projectsController = new ProjectsController(projectsModel, projectsView);
+		ProjectsController projectsController = new ProjectsController(
+				projectsModel, projectsView);
 		projectsController.printProjectsList();
-		
-		
-    }
 
-	
-    private static Motivation initializeMotivationFromDatabase(){
+	}
+
+	private static Motivation initializeMotivationFromDatabase() {
 		Motivation motivation = new Motivation();
 		motivation.addMotivation("Рекорды существуют для того, чтобы их бить.");
 		motivation.addMotivation("Знаний не достаточно, ты должен применять их. Желания не достаточно, ты должен делать.");
@@ -33,9 +29,10 @@ public class App
 		motivation.addMotivation("Ночь не может длиться вечно.");
 		motivation.addMotivation("Пессимист видит трудность в любой возможности; оптимист – видит возможность в любой трудности.");
 		return motivation;
-    	
-    }
-    private static Category retriveCategoryFromDatabase() {
+
+	}
+
+	private static Category retriveCategoryFromDatabase() {
 		Category category = new Category();
 		category.addCategory("Techno");
 		category.addCategory("Art");
@@ -43,14 +40,13 @@ public class App
 		category.addCategory("Some category");
 		return category;
 	}
-    
-    private static Projects retriveProjectFromDatabase(){
+
+	private static Projects retriveProjectFromDatabase() {
 		Projects projects = new Projects();
 		projects.addProject("Мой самый лучшей проект", "Выберете нас, и мы сделаем все отлично!");
 		projects.addProject("Мой ееще более лучшей проект", "Лучше выберене нас!");
-    	return projects;
-    	
-    }
-	
-	
+		return projects;
+
+	}
+
 }
--- trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/Motivation.java
+++ trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/Motivation.java
@@ -1,8 +1,6 @@
 package belskii.artem.kickstarter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 public class Motivation {
