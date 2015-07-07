048257c4c53dbc636fb3b8bafa3c6e47003c53d7
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-07 19:09:15 +0000

    change code
    git-svn-id: http://go-java-1.googlecode.com/svn@4560 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Java Basics/anagram/Anagram.java
+++ trunk/GoJava5/nikolay.rybalkin/Java Basics/anagram/Anagram.java
@@ -7,7 +7,6 @@ public class Anagram {
 
     private static String SPACE = " ";
     private static Scanner scanConsole;
-    private static String ewer = "_";
 
     public static void main(String[] args) {
 
@@ -19,7 +18,7 @@ public class Anagram {
         for(int i = 0; i < array.length; i++) {
             System.out.print(new StringBuilder(array[i]).reverse().toString());
             if (i < array.length - 1){
-                System.out.print(ewer);
+                System.out.print(SPACE);
             }
         }
     }
