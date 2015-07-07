64ed3bfb1be41868994e8c03b194cba844f4a2be
h.vitya56@gmail.com <h.vitya56@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-07 15:07:28 +0000

    Added basic structure.
    git-svn-id: http://go-java-1.googlecode.com/svn@4555 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/victor.homyak/ConsoleKickstarter/src/kickStarter/Router.java
@@ -0,0 +1,25 @@
+package kickStarter;
+
+public class Router {
+
+	HierarchyLevels layer;
+
+	public Router(HierarchyLevels layer) {
+		this.layer = layer;
+	}
+
+	public void PickCase() {
+		switch (layer) {
+		case CATEGORY:
+
+		case LISTOFCATEGORIES:
+
+		case PROJECT:
+
+		case START:
+
+		}
+
+	}
+
+}
