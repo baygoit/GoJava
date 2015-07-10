a7626fad1319c444849fe5485f51d4398bffdbe9
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-10 23:55:27 +0000

    refactorinag  вынеы main в новый класс running
    git-svn-id: http://go-java-1.googlecode.com/svn@4591 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/Runner.java
@@ -0,0 +1,31 @@
+
+public class Runner {
+
+    public static void main(String[] args){
+
+        Category category1 = new Category("Game");
+        Category category2 = new Category("Design");
+        Category category3 = new Category("Technology");
+
+        Categories categories = new Categories();
+        categories.add(category1);
+        categories.add(category2);
+        categories.add(category3);
+
+        Project project1 = new Project("Game \"Popcorn\"", 10000, 15, "Interesting game");
+        Project project2 = new Project("Design \"New Design\"", 10000, 15, "New innovation design");
+        Project project3 = new Project("Technology \"Wi-Fi\"", 10000, 15, "New technology");
+
+        project1.setCategory(category1);
+        project2.setCategory(category2);
+        project3.setCategory(category3);
+
+        Projects projects = new Projects();
+        projects.add(project1);
+        projects.add(project2);
+        projects.add(project3);
+
+        Main app = new Main(categories, projects);
+        app.run();
+    }
+}
