175eeeee640394e6a1009cfc2cd89eb0dc8ab7d0
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-10 23:24:45 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4583 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/Category.java
@@ -0,0 +1,13 @@
+
+public class Category {
+
+	private String name;
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
