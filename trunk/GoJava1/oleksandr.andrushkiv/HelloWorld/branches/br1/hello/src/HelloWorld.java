73f47742f055a1daaad86aa1214f9b3c613b160d
AndrushkivAN@gmail.com <AndrushkivAN@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-12 18:50:29 +0000

    trifecta HW
    git-svn-id: http://go-java-1.googlecode.com/svn@4644 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.andrushkiv/HelloWorld/branches/br1/hello/src/HelloWorld.java
+++ trunk/GoJava1/oleksandr.andrushkiv/HelloWorld/branches/br1/hello/src/HelloWorld.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Hello World!");
+		}
+
 	}
 
 }
