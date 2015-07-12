4268eaa3a856494eef9745af30c9f86275c4037e
AndrushkivAN@gmail.com <AndrushkivAN@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-12 20:48:25 +0000

    br1
    git-svn-id: http://go-java-1.googlecode.com/svn@4664 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava1/oleksandr.andrushkiv/hw_branches/br2/src/HelloWorld.java
+++ trunk/GoJava1/oleksandr.andrushkiv/hw_branches/br2/src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World??? of course!!");
+		System.out.println("trunk!");
+		System.out.println("trunk or branch");
 	}
 
 }
