c4040e84ad9ba0f2bfe6d2dcb2b07cdfc5db8a46
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-12 23:11:03 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4672 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/OOP/src/calculator/ScanConsole.java
+++ trunk/GoJava5/nikolay.rybalkin/OOP/src/calculator/ScanConsole.java
@@ -12,7 +12,7 @@ public class ScanConsole {
             if(scanner.hasNextInt()){
                 number = scanner.nextLine();
             }
-        } catch (Exception e) {
+        } catch (java.util.InputMismatchException e) {
             System.out.println("You entered is not a number!");
         }
         return number;
