348667984ceed30aedc010b55dd0ebf0c97a6fe4
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-12 09:17:00 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4604 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/division/Division.java
+++ trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/division/Division.java
@@ -1,32 +1,28 @@
 package division;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Division {
 
-    static int division;
-    static int module;
-    static int count = 0;
-    static StringBuilder buf = new StringBuilder();
-    static StringBuilder stolbikString = new StringBuilder();
-    static ArrayList<Integer> stolbikArray = new ArrayList<Integer>();
-    static boolean hasPeriod = false;
-    private static Scanner scanConsole;
-
     public static void main(String[] args) {
 
-        System.out.println("Enter two numbers are stripped of their \"/\" : ");
-        scanConsole = new Scanner(System.in);
-        String numbers = scanConsole.nextLine();
-        String[] tokens = numbers.split("/");
-        int num1 = Integer.parseInt(tokens[0]);
-        int num2 = Integer.parseInt(tokens[1]);
-
-        devision(num1, num2);
+        System.out.println("Enter two numbers (for example -  4/2) : ");
+        String numbers = "";
+        String[] tokens =  consoleScan(numbers).split("/");
+        divisions(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
     }
 
-    public static void devision(int a, int b) {
+    public static void divisions(int a, int b) {
+
+        int division;
+        int module;
+        int count = 0;
+        StringBuilder buf = new StringBuilder();
+        ArrayList<Integer> columnArray = new ArrayList<Integer>();
+        boolean hasPeriod = false;
+
         String ab = String.valueOf(a) + "|" + String.valueOf(b);
         while (true) {
             division = a / b;
@@ -35,22 +31,22 @@ public class Division {
                 if (buf.length() == 0) {
                     buf.append("0.(");
                 }
-                if (buf.toString().contains(".") == false) {
+                if (!buf.toString().contains(".")) {
                     buf.append(".(");
                 }
                 a *= 10;
             } else {
                 count++;
                 buf.append(division);
-                stolbikString.append(String.format("%" + count + "s", " ") + "-" + (division * b) + "\n");
-                stolbikString.append(String.format("%" + count + "s", " ") + "------" + "\n");
-                stolbikString.append(String.format("%" + count + "s", " ") + "  " + module * 10 + "\n");
+                buf.append(String.format("%" + count + "s", " ") + "-" + (division * b) + "\n");
+                buf.append(String.format("%" + count + "s", " ") + "------" + "\n");
+                buf.append(String.format("%" + count + "s", " ") + "  " + module * 10 + "\n");
                 a = module;
 
-                stolbikArray.add(module);
-                for (int i = 1; i < stolbikArray.size(); i++) {
-                    if (stolbikArray.size() > 1) {
-                        if (stolbikArray.get(0) == stolbikArray.get(i)) {
+                columnArray.add(module);
+                for (int i = 1; i < columnArray.size(); i++) {
+                    if (columnArray.size() > 1) {
+                        if (columnArray.get(0).equals(columnArray.get(i))) {
                             hasPeriod = true;
                         }
                     }
@@ -70,6 +66,20 @@ public class Division {
 
         System.out.println(String.format("%" + 3 + "s", " ") + ab);
         System.out.println(String.format("%" + 5 + "s", "") + "|" + buf);
-        System.out.println(stolbikString);
+        System.out.println(buf);
     }
+
+    public static String consoleScan(String number){
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            scanner.hasNextInt();
+            number = scanner.nextLine();
+        } catch (Exception e) {
+            System.out.println("You entered is not a number!");
+        }
+        return number;
+    }
+
 }
