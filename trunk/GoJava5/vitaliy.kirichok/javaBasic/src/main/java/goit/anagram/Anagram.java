d5b9d3f26240ff4fb7ac8e8675c3b12a12508e42
vitaliy.kyrychok@gmail.com <vitaliy.kyrychok@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-13 12:19:12 +0000

    Рефакторинг, т.к. TDD не взлетает при такой реализации
    git-svn-id: http://go-java-1.googlecode.com/svn@4678 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/vitaliy.kirichok/javaBasic/src/main/java/goit/anagram/Anagram.java
+++ trunk/GoJava5/vitaliy.kirichok/javaBasic/src/main/java/goit/anagram/Anagram.java
@@ -1,26 +1,14 @@
 package goit.anagram;
 
-import goit.common.Console;
-
 public class Anagram {
 
     private static final String DELIMITER = " ";
-    private static final String INVITE_MESSAGE = "Enter statement:";
 
-    public static void main(String[] args) {
-        Console console = new Console();
-        do {
-            String input = console.read(INVITE_MESSAGE);
-            boolean firstIteration = false;
-            for (String part : input.split(DELIMITER)) {
-                if (firstIteration) {
-                    console.write(DELIMITER);
-                }
-                console.write(new StringBuilder(part).reverse().toString());
-                firstIteration = true;
-            }
-            console.write(System.lineSeparator());
-        } while (console.hasNext());
-        console.close();
+    public String getAnargam(String inputString) {
+        String result = "";
+        for (String part : inputString.split(DELIMITER)) {
+            result = result.concat(new StringBuilder(part).reverse().toString()).concat(DELIMITER);
+        }
+        return result.trim();
     }
 }
