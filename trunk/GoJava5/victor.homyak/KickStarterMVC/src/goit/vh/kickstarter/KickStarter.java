065de246d93f83e60e021035308dbeaf4f7928fa
h.vitya56@gmail.com <h.vitya56@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-14 22:08:49 +0000

    commit
    git-svn-id: http://go-java-1.googlecode.com/svn@4697 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/victor.homyak/KickStarterMVC/src/goit/vh/kickstarter/KickStarter.java
+++ trunk/GoJava5/victor.homyak/KickStarterMVC/src/goit/vh/kickstarter/KickStarter.java
@@ -1,8 +1,11 @@
 package goit.vh.kickstarter;
 
 import goit.vh.kickstarter.model.Category;
+import goit.vh.kickstarter.mvc.controller.CategoryController;
 import goit.vh.kickstarter.mvc.controller.MainPageController;
+import goit.vh.kickstarter.mvc.model.CategoryModel;
 import goit.vh.kickstarter.mvc.model.MainPageModel;
+import goit.vh.kickstarter.mvc.view.CategoryView;
 import goit.vh.kickstarter.mvc.view.MainPageView;
 
 /**
@@ -14,8 +17,10 @@ import goit.vh.kickstarter.mvc.view.MainPageView;
  */
 public class KickStarter {
 
-    private MainPageController controller;
-
+    private MainPageController mainPageController;
+    private CategoryController categoryController;
+    private MainPageView mainPageView;
+    private LocationManager locationManager;
     public static void main(String[] args) {
         KickStarter kickStarter = new KickStarter();
         kickStarter.init();
@@ -23,17 +28,24 @@ public class KickStarter {
     }
 
     private void init() {
+
         DataRegistry dataRegistry = new DataRegistry();
         dataRegistry.registerCategories(new Category[]{new Category("Sport"), new Category("Game"), new Category("Video")});
 
-        MainPageModel model = new MainPageModel();
-        model.setDataRegistry(dataRegistry);
+        MainPageModel mainPageModel = new MainPageModel();
+        mainPageModel.setDataRegistry(dataRegistry);
+        CategoryModel categoryModel = new CategoryModel(dataRegistry);
+         mainPageView = new MainPageView(new Output());
 
-        controller = new MainPageController(new MainPageView(new Output()), model);
+        mainPageController = new MainPageController(mainPageView, mainPageModel);
+        categoryController = new CategoryController(new CategoryView(new Output()),categoryModel);
+        locationManager = new LocationManager(mainPageController,categoryController);
+        mainPageController.setLocationManager(locationManager);
+        categoryController.setLocationManager(locationManager);
     }
 
     private void start() {
-        controller.onApplicationStart();
+        locationManager.onApplicationStart();
     }
 
 
