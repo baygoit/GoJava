cbcb13b5e15eecf7197161da0ae60dacfc52eceb
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-16 22:16:09 +0000

    Тест для проектов
    git-svn-id: http://go-java-1.googlecode.com/svn@4735 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Kickstarter/test/goit5/nikfisher/kickstarter/model/ProjectsTest.java
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/test/goit5/nikfisher/kickstarter/model/ProjectsTest.java
@@ -1,21 +1,94 @@
 package test.goit5.nikfisher.kickstarter.model;
 
+import goit5.nikfisher.kickstarter.model.Categories;
+import goit5.nikfisher.kickstarter.model.Category;
+import goit5.nikfisher.kickstarter.model.Project;
+import goit5.nikfisher.kickstarter.model.Projects;
 import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public class ProjectsTest {
 
-//    @Test
-//    public void testAdd() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void testGetProgects() throws Exception {
-//
-//    }
-//
-//    @Test
-//    public void testGet() throws Exception {
-//
-//    }
+    @Test
+    public void  shouldProjectsWenNoProjectsWithNoSameCategory() throws Exception {
+        //given
+        Projects projects = new Projects();
+        Category category1 = new Category("Game");
+        Category category2 = new Category("Design");
+
+        Project project1 = new Project("name1 \"Popcorn\"", 10000, 10, "Interesting game");
+        Project project2 = new Project("name2 \"Popcorn\"", 10000, 10, "Interesting game");
+
+        project1.setCategory(category1);
+        project2.setCategory(category1);
+
+        projects.add(project1);
+        projects.add(project2);
+
+        //when
+        Project[] found = projects.getProgects(category2);
+
+        //then
+        assertEquals(0, found.length);
+    }
+
+    @Test
+    public void  shouldProjectsWenNoProjectsWithSameCategory() throws Exception {
+        //given
+        Projects projects = new Projects();
+        Category category1 = new Category("Game");
+        Category category2 = new Category("Design");
+
+        Project project1 = new Project("name1 \"Popcorn\"", 10000, 10, "Interesting game");
+        Project project2 = new Project("name2 \"Popcorn\"", 10000, 10, "Interesting game");
+        Project project3 = new Project("name2 \"Popcorn\"", 10000, 10, "Interesting game");
+
+        project1.setCategory(category1);
+        project2.setCategory(category2);
+        project3.setCategory(category2);
+
+        projects.add(project1);
+        projects.add(project2);
+        projects.add(project3);
+
+        //when
+        Project[] found = projects.getProgects(category2);
+
+        //then
+        assertEquals(2, found.length);
+        assertSame(project2, found[0]);
+        assertSame(project3, found[1]);
+    }
+
+    @Test
+    public void  shouldProjectsWenNoProjects() throws Exception {
+        //given
+        Projects projects = new Projects();
+
+        //when
+        Project[] found = projects.getProgects(new Category("Game"));
+
+        //then
+        assertEquals(0, found.length);
+    }
+
+    @Test
+    public void shouldProjectsIndex() throws Exception {
+        //given
+        Projects projects = new Projects();
+
+        //when
+        Project project1 = new Project("name1 \"Popcorn\"", 10000, 10, "Interesting game");
+        projects.add(project1);
+        Project project2 = new Project("name2 \"Popcorn\"", 10000, 10, "Interesting game");
+        projects.add(project2);
+
+        //then
+        assertEquals(project1, projects.get(0));
+        assertEquals(project2, projects.get(1));
+    }
 }
