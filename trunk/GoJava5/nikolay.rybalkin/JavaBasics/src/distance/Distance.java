52a7b5c56882482a0ac8fce8b65f974e10c41bad
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-16 17:50:58 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4719 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/distance/Distance.java
+++ trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/distance/Distance.java
@@ -4,28 +4,25 @@ import java.util.Scanner;
 
 public class Distance {
 
-    public static void main(String[] args) {
+    private static int  distance;
+    private static int max1 = Integer.MAX_VALUE;
+    private static int max2 = Integer.MAX_VALUE;
+    private static int firstMinIndex = -1;
+    private static int secondMinIndex = -1;
 
-        int  distance;
-        int max1 = Integer.MAX_VALUE;
-        int max2 = Integer.MAX_VALUE;
-        int firstMinIndex = -1;
-        int secondMinIndex = -1;
+    public static void main(String[] args) {
 
         System.out.println("Enter the console 10 number: ");
         Scanner scanConsole = new Scanner(System.in);
         int array[] = new int[10];
 
-        int count = 0;
-        while (count < 10) {
-            if (scanConsole.hasNextInt()) {
-                array[count++] = scanConsole.nextInt(); //При каждой итерации ложишь новое значение в новую ячейку
-            } else {
-                String input = scanConsole.next();  // А почему не nextLine() ?
-                System.out.println("Your input: \"" + input + "\" is not a number");
-            }
-        }
+        parser(scanConsole, array);
+        calculation_distance(array);
 
+        System.out.println("Distance = " + distance);
+    }
+
+    private static void calculation_distance(int[] array) {
         for (int i = 0; i < array.length; i++) {
             if (array[i] < max1) {
                 max2 = max1;
@@ -38,7 +35,17 @@ public class Distance {
             }
         }
         distance = Math.abs(firstMinIndex - secondMinIndex);
-        System.out.println("Distance = " + distance);
+    }
 
+    private static void parser(Scanner scanConsole, int[] array) {
+        int count = 0;
+        while (count < 10) {
+            if (scanConsole.hasNextInt()) {
+                array[count++] = scanConsole.nextInt(); //При каждой итерации ложишь новое значение в новую ячейку
+            } else {
+                String input = scanConsole.next();  // А почему не nextLine() ?
+                System.out.println("Your input: \"" + input + "\" is not a number");
+            }
+        }
     }
 }
