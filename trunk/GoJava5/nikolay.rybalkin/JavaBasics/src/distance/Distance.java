4a4091487aebcec63a1545fea31be8227392c775
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-16 17:43:02 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4718 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/distance/Distance.java
+++ trunk/GoJava5/nikolay.rybalkin/JavaBasics/src/distance/Distance.java
@@ -6,7 +6,6 @@ public class Distance {
 
     public static void main(String[] args) {
 
-        Scanner scanConsole;
         int  distance;
         int max1 = Integer.MAX_VALUE;
         int max2 = Integer.MAX_VALUE;
@@ -14,31 +13,32 @@ public class Distance {
         int secondMinIndex = -1;
 
         System.out.println("Enter the console 10 number: ");
-        scanConsole = new Scanner(System.in);
+        Scanner scanConsole = new Scanner(System.in);
         int array[] = new int[10];
 
-        try {
-            scanConsole.hasNextInt();
-            for (int i = 0; i < array.length; i++){
-                array[i] = scanConsole.nextInt();
+        int count = 0;
+        while (count < 10) {
+            if (scanConsole.hasNextInt()) {
+                array[count++] = scanConsole.nextInt(); //При каждой итерации ложишь новое значение в новую ячейку
+            } else {
+                String input = scanConsole.next();  // А почему не nextLine() ?
+                System.out.println("Your input: \"" + input + "\" is not a number");
             }
-
-            for (int i = 0; i < array.length; i++) {
-                if (array[i] < max1) {
-                    max2 = max1;
-                    secondMinIndex = firstMinIndex;
-                    max1 = array[i];
-                    firstMinIndex = i;
-                } else if (array[i] < max2) {
-                    max2 = array[i];
-                    secondMinIndex = i;
-                }
-            }
-            distance = Math.abs(firstMinIndex - secondMinIndex);
-            System.out.println("Distance = " + distance);
-
-        } catch (Exception e) {
-            System.out.println("You entered is not a number!");
         }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < max1) {
+                max2 = max1;
+                secondMinIndex = firstMinIndex;
+                max1 = array[i];
+                firstMinIndex = i;
+            } else if (array[i] < max2) {
+                max2 = array[i];
+                secondMinIndex = i;
+            }
+        }
+        distance = Math.abs(firstMinIndex - secondMinIndex);
+        System.out.println("Distance = " + distance);
+
     }
 }
