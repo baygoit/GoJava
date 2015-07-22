4a929f02797124c3dfe14908c22917d13b26555f
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-22 09:32:51 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4821 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/goit5/nikfisher/kickstarter/SetElementsInProject.java
@@ -0,0 +1,43 @@
+package goit5.nikfisher.kickstarter;
+
+
+import goit5.nikfisher.kickstarter.model.Project;
+
+public class SetElementsInProject {
+
+    static void SetAnsverInProject(Project project1, Project project2, Project project3, Project project4, Project project5, Project project6) {
+        project1.setAnsver("Still no one answered");
+        project2.setAnsver("Still no one answered");
+        project3.setAnsver("Still no one answered");
+        project4.setAnsver("Still no one answered");
+        project5.setAnsver("Still no one answered");
+        project6.setAnsver("Still no one answered");
+    }
+
+    static void SetQuestionsInProject(Project project1, Project project2, Project project3, Project project4, Project project5, Project project6) {
+        project1.setQuestion("No one has asked the question.");
+        project2.setQuestion("No one has asked the question.");
+        project3.setQuestion("No one has asked the question.");
+        project4.setQuestion("No one has asked the question.");
+        project5.setQuestion("No one has asked the question.");
+        project6.setQuestion("No one has asked the question.");
+    }
+
+    static void SetVideoInProject(Project project1, Project project2, Project project3, Project project4, Project project5, Project project6) {
+        project1.setVideo("https://www.youtube.com");
+        project2.setVideo("https://www.youtube.com");
+        project3.setVideo("https://www.youtube.com");
+        project4.setVideo("https://www.youtube.com");
+        project5.setVideo("https://www.youtube.com");
+        project6.setVideo("https://www.youtube.com");
+    }
+
+    static void setHistoryInProject(Project project1, Project project2, Project project3, Project project4, Project project5, Project project6) {
+        project1.setHistory("History this project");
+        project2.setHistory("History this project");
+        project3.setHistory("History this project");
+        project4.setHistory("History this project");
+        project5.setHistory("History this project");
+        project6.setHistory("History this project");
+    }
+}
