06e6400f2ee9104e10af11b460eb37de8027a287
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-22 09:32:38 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4819 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/goit5/nikfisher/kickstarter/Main.java
@@ -0,0 +1,35 @@
+package goit5.nikfisher.kickstarter;
+
+import goit5.nikfisher.kickstarter.menu.CategoryMenu;
+import goit5.nikfisher.kickstarter.model.*;
+import goit5.nikfisher.kickstarter.streams.InputOutputConsole;
+import goit5.nikfisher.kickstarter.streams.InputOutputConsoleInterface;
+
+public class Main {
+
+	private Categories categories;
+	private Projects projects;
+	private InputOutputConsoleInterface io;
+	private QuoteGenerate generator;
+
+	public Main(Categories categories, Projects projects, InputOutputConsoleInterface io, QuoteGenerate generator) {
+
+		this.categories = categories;
+		this.projects = projects;
+		this.io = io;
+		this.generator = generator;
+	}
+
+	public void run() {
+
+		io.println(generator.quoteGenerate());
+
+		CategoryMenu categoryMenu = new CategoryMenu(new InputOutputConsole(),  projects, categories);
+		categoryMenu.categoryMenu();
+
+		io.println("Sank!");
+	}
+}
+
+
+
