4235d92879a9028c0baef8737ed2cf7fa4d402d8
h.vitya56@gmail.com <h.vitya56@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-22 14:33:30 +0000

    5 use case
    git-svn-id: http://go-java-1.googlecode.com/svn@4831 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/victor.homyak/KickStarterMVC/test/goit/vh/kickstarter/mvc/view/MainPageViewTest.java
+++ trunk/GoJava5/victor.homyak/KickStarterMVC/test/goit/vh/kickstarter/mvc/view/MainPageViewTest.java
@@ -50,37 +50,37 @@ public class MainPageViewTest {
 
         when(input.getInput()).thenReturn(EXPECTED_INPUT);
 
-        MainPageView mainPageView = new MainPageView(output);
-     //   mainPageView.readUserInput();
+//        MainPageView mainPageView = new MainPageView(output);
+//        mainPageView.readUserInput();
 //        Assert.assertEquals(
 //            "User Input should be correct",
 //    EXPECTED_INPUT,
 //            mainPageView.getUserInput());
 }
 
-//    @Test
-//    public void whenRenderMainPageThanPrintHelloMsgAndCategoriesList() throws Exception {
-//        when(model.getHelloMsg()).thenReturn("Test Msg");
-//        CategoryModel[] CategoryModel = {new CategoryModel("CategoryModel 1"), new CategoryModel("CategoryModel 2"), new CategoryModel("CategoryModel 3")};
-//        when(model.getCategories()).thenReturn(CategoryModel);
-//
-//        final List<String> view = new ArrayList<>();
-//
-//        // Here we mock output.println() to store received argument in List of Strings
-//        // so than we can compare it with expected result
-//        doAnswer(new Answer<Object>() {
-//            @Override
-//            public Object answer(InvocationOnMock invocation) throws Throwable {
-//                Object[] arguments = invocation.getArguments();
-//                String str = (String) arguments[0];
-//                view.add(str);
-//                return null;
-//            }}).when(output).println(anyString()); // here we use anyString matcher so this mocked method
-//                                                   // will be call with any string passed to println method
-//        MainPageView mainPageView = new MainPageView(output);
-//
-//        mainPageView.render(model);
-//        String[] expectedResult = {"Test Msg", "1 CategoryModel 1", "2 CategoryModel 2", "3 CategoryModel 3"};
-//        Assert.assertArrayEquals(expectedResult, view.toArray());
-//    }
+    @Test
+    public void whenRenderMainPageThanPrintHelloMsgAndCategoriesList() throws Exception {
+        when(model.getHelloMsg()).thenReturn("Test Msg");
+        CategoryModel[] CategoryModel = {new CategoryModel("CategoryModel 1"), new CategoryModel("CategoryModel 2"), new CategoryModel("CategoryModel 3")};
+        when(model.getCategories()).thenReturn(CategoryModel);
+
+        final List<String> view = new ArrayList<>();
+
+        // Here we mock output.println() to store received argument in List of Strings
+        // so than we can compare it with expected result
+        doAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Object[] arguments = invocation.getArguments();
+                String str = (String) arguments[0];
+                view.add(str);
+                return null;
+            }}).when(output).println(anyString()); // here we use anyString matcher so this mocked method
+                                                   // will be call with any string passed to println method
+        MainPageView mainPageView = new MainPageView(output);
+
+        mainPageView.render(model);
+        String[] expectedResult = {"Test Msg", "1 CategoryModel 1", "2 CategoryModel 2", "3 CategoryModel 3"};
+        Assert.assertArrayEquals(expectedResult, view.toArray());
+    }
 }
