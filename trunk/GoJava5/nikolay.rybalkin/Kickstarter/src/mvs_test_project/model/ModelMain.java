469f43e3165d552a557409c43deab25ccfee2758
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-23 13:28:20 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4840 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/mvs_test_project/model/ModelMain.java
@@ -0,0 +1,16 @@
+package model;
+
+public class ModelMain {
+
+    private String a;
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+
+}
---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/mvs_test_project/view/ViewMain.java
@@ -0,0 +1,9 @@
+package view;
+
+public class ViewMain {
+
+    public static void addText(String a){
+        System.out.println(a);
+    }
+
+}
