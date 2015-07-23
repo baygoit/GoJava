8cd60895f663fef304a12f05f0b3384627d59ba9
rybalkin.nikolay@gmail.com <rybalkin.nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-23 20:19:10 +0000

    Реорганизация проекта
    git-svn-id: http://go-java-1.googlecode.com/svn@4844 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/Main.java
@@ -0,0 +1,38 @@
+package goit5.nikfisher.kickstarter;
+
+import goit5.nikfisher.kickstarter.menu.CategoryMenu;
+import goit5.nikfisher.kickstarter.model.Categories;
+import goit5.nikfisher.kickstarter.model.InMemoryCategories;
+import goit5.nikfisher.kickstarter.streams.InputOutputConsole;
+import goit5.nikfisher.kickstarter.streams.InputOutputConsoleInterface;
+import goit5.nikfisher.kickstarter.model.Projects;
+import goit5.nikfisher.kickstarter.model.QuoteGenerate;
+
+public class Main {
+
+	private Categories categories;
+	private Projects projects;
+	private InputOutputConsoleInterface io;
+	private QuoteGenerate generator;
+
+	public Main(Categories categories, Projects projects, InputOutputConsoleInterface io, QuoteGenerate generator) {
+
+		this.categories = categories;
+		this.projects = projects;
+		this.io = io;
+		this.generator = generator;
+	}
+
+	public void run() {
+
+		io.println(generator.quoteGenerate());
+
+		CategoryMenu categoryMenu = new CategoryMenu(new InputOutputConsole(),  projects, categories);
+		categoryMenu.categoryMenu();
+
+		io.println("Sank!");
+	}
+}
+
+
+
---
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/model/InFileCategories.java
@@ -0,0 +1,5 @@
+package goit5.nikfisher.kickstarter.model;
+
+
+public class InFileCategories {
+}
