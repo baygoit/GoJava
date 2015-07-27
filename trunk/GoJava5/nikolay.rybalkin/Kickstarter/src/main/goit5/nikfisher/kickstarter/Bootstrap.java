d099d37c9564a2416102bcbba3cc1317a7a605c9
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-27 15:01:38 +0000

    Правка тестов
    git-svn-id: http://go-java-1.googlecode.com/svn@4902 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/Bootstrap.java
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/Bootstrap.java
@@ -4,11 +4,15 @@ import goit5.nikfisher.kickstarter.model.*;
 import goit5.nikfisher.kickstarter.streams.ConsoleIO;
 
 import java.util.Random;
+import java.util.logging.Logger;
 
 public class Bootstrap {
+
+	private static final Logger log = Logger.getLogger(String.valueOf(Bootstrap.class));
 
 	public static void main(String[] args){
 		Main app = new Main(new ConsoleIO(), new QuoteGenerate(new Random()));
 		app.run();
+		log.info("This is an informational message!");
 	}
 }
