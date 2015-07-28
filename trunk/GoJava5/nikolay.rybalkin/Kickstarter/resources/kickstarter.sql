e2844db6f9908f7e6d64583602826d1cfe78c9ae
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-28 14:50:31 +0000

    Создал базу
    git-svn-id: http://go-java-1.googlecode.com/svn@4922 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Kickstarter/resources/kickstarter.sql
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/resources/kickstarter.sql
@@ -1,4 +1,5 @@
 BEGIN TRANSACTION;
+
 CREATE TABLE `Projects` (
   `id`          INTEGER NOT NULL UNIQUE,
   `category_id` INTEGER NOT NULL,
@@ -6,12 +7,15 @@ CREATE TABLE `Projects` (
   `description` TEXT,
   PRIMARY KEY (id, category_id)
 );
+
 INSERT INTO `Projects` (id, category_id, name, description) VALUES (1, 1, 'Project1', NULL),
   (2, 1, 'Project2', NULL);
+
 CREATE TABLE `Category` (
   `id`   INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
   `name` TEXT    NOT NULL UNIQUE
 );
+
 INSERT INTO `Category` (id, name) VALUES (1, 'Game'),
   (2, 'Video'),
   (3, 'Music'),
