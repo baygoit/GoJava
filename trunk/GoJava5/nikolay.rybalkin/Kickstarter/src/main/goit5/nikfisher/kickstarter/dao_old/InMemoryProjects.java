0eea12cd3397b5f846ef564e2a4ebc01c1827078
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-29 13:52:50 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4950 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/dao_old/InMemoryProjects.java
+++ trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/main/goit5/nikfisher/kickstarter/dao_old/InMemoryProjects.java
@@ -11,37 +11,37 @@ import java.util.Map;
 
 public class InMemoryProjects implements Projects {
 
-//    private int index = 0;
-//
-//    private Map<Integer, Project> projects = new HashMap<>();
-//
-//    @Override
-//    public void add(Project project) {
-//        projects.put(index++, project);
-//    }
-//
-//    @Override
-//    public List<Project> getProjects(Category category) {
-//
-//        List<Project> result = new ArrayList<>();
-//        int found = 0;
-//
-//        for (int i = 0; i < index; i++) {
-//            Project project = projects.get(i);
-//
-//            if (project.getCategory().equals(category)) {
-//                result.add(found, project);
-//                found++;
-//            }
-//        }
-//
-//        return result;
-//    }
-//
-//    @Override
-//    public Project get(int index) {
-//        return projects.get(index);
-//    }
+    private int index = 0;
+
+    private Map<Integer, Project> projects = new HashMap<>();
+
+    @Override
+    public void add(Project project) {
+        projects.put(index++, project);
+    }
+
+    @Override
+    public List<Project> getProjects(Category category) {
+
+        List<Project> result = new ArrayList<>();
+        int found = 0;
+
+        for (int i = 0; i < index; i++) {
+            Project project = projects.get(i);
+
+            if (project.getCategory().equals(category)) {
+                result.add(found, project);
+                found++;
+            }
+        }
+
+        return result;
+    }
+
+    @Override
+    public Project get(int index) {
+        return projects.get(index);
+    }
 
 
 }
