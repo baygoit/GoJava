01d79a15a693024eec9d831ee66767f4a78fb479
Rybalkin.Nikolay@gmail.com <Rybalkin.Nikolay@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-07-29 13:34:16 +0000

    git-svn-id: http://go-java-1.googlecode.com/svn@4946 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/nikolay.rybalkin/Kickstarter/src/test/goit5/nikfisher/kickstarter/dao/InMemoryCategoriesTest.java
+++
@@ -1,9 +0,0 @@
-package goit5.nikfisher.kickstarter.dao;
-
-public class InMemoryCategoriesTest /*xtends CategoriesTest*/ {
-
-//    @Override
-//    Categories getCategories() {
-//        return new InMemoryCategories();
-//    }
-}
