11309e8d158c1210c362194d963e77cbae5d8f8e
belskiiartem@gmail.com <belskiiartem@gmail.com@371e26f7-1d5d-4d7b-6139-2def7fa80f1e>
2015-08-07 12:52:12 +0000

    simplified main
    git-svn-id: http://go-java-1.googlecode.com/svn@5045 371e26f7-1d5d-4d7b-6139-2def7fa80f1e

--- trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/Kickstarter.java
+++ trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/Kickstarter.java
@@ -3,13 +3,7 @@ package belskii.artem.kickstarter;
 public class Kickstarter {
 	
 	public static void main(String[] args) {
-		run();
-	}
-
-	private static void run() {
 		DispatcherController dispatcher = new DispatcherController();
 		dispatcher.start();
 	}
-	
-
 }
