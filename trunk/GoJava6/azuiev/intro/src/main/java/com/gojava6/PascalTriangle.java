49c936a756df5e1cb53a56c34dea5500671d6140
azuiev <aleksei.zuew@gmail.com>
2015-09-15 23:05:44 +0300

    home work 1.1

--- trunk/GoJava6/azuiev/intro/src/main/java/com/gojava6/PascalTriangle.java
+++ trunk/GoJava6/azuiev/intro/src/main/java/com/gojava6/PascalTriangle.java
@@ -4,14 +4,34 @@ package com.gojava6;
  * Created by sergiigetman on 8/26/15.
  */
 public class PascalTriangle {
+    int level;
 
     public PascalTriangle(int level) {
-
+        this.level = level;
     }
 
     public int[][] calculateTriangle() {
-        //TODO: write you code here
-        return null;
+        if (level < 1) {
+            return null;
+        }
+        int result[][] = new int[level][];
+        for (int i = 0; i <level; i++) {
+            result[i]=new int[i+1];
+            for (int j = 0; j < i+1; j++) {
+                if ((j==0)||(i==j)) {
+                    result[i][j]=1;
+                } else {
+                    result[i][j]=result[i-1][j-1]+result[i-1][j];
+                }
+            }
+        }
+        for (int i = 0; i < level; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j]);
+            }
+            System.out.println();
+        }
+        return result;
     }
 
 }
