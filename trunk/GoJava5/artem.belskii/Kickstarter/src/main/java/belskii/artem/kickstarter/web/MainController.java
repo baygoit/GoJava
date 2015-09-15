57e8c321de20ad335768bcc951f6619a789f6373
Belskii Artem <belskiiartem@gmail.com>
2015-09-15 12:38:07 +0300

    test commit to local repo

---
+++ trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/web/MainController.java
@@ -0,0 +1,16 @@
+package belskii.artem.kickstarter.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+ 
+		String message = "<br><div style='text-align:center;'>"
+				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+		return new ModelAndView("welcome", "message", message);
+	}
+}
