b8ceb6712092aac1cebfb0d2fb0be66889bde76f
Sergey Onishchenko <onishchenkos.0@gmail.com>
2015-09-16 19:18:08 +0300

    test

--- trunk/GoJava6/sonishchenko/intro/src/main/java/com/gojava6/PascalTriangle.java
+++ trunk/GoJava6/sonishchenko/intro/src/main/java/com/gojava6/PascalTriangle.java
@@ -1,26 +1,41 @@
-public class PascalTriangle {
-    public static void main(String[] args) {
-        PascalTriangle pascalTriangle = new PascalTriangle();
+package com.gojava6;
 
-        int triangleHeight = 9;
-        pascalTriangle.createPascalTriangle(triangleHeight);
+
+public class PascalTriangle {
+    private int level;
+
+    public PascalTriangle(int level) {
+        this.level = level;
     }
 
-    void createPascalTriangle(int triangleHeight) {
-        int elementOfTriangle = 1;
+    public int[][] calculateTriangle() {
+        int[][] triangle = new int[level + 1][(level + 1) *2];
+        int leftOne, rightOne;
 
-        if (triangleHeight < 1) {
-            System.out.println("cant build pascal triangle");
-        } else {
-            for (int i = 0; i < triangleHeight; i++) {
-                System.out.print(elementOfTriangle);
-                for (int j = 1; j <= i; j++) {
-                    elementOfTriangle *= (i - j + 1);
-                    elementOfTriangle /= j;
-                    System.out.print(" " + elementOfTriangle);
-                }
-                System.out.println();
+        triangle[0][level - 1] = 1;
+        leftOne = rightOne = level - 1;
+
+        for (int i = 1; i < level; i++) {
+            leftOne--;
+            rightOne++;
+            triangle[i][leftOne] = 1;
+            triangle[i][rightOne] = 1;
+
+            for (int j = 1; j <= i; j++) {
+                int left = triangle[i - 1][leftOne + (2 * j) - 1];
+                int right = triangle[i - 1][leftOne + (2 * j) + 1];
+                triangle[i][leftOne + (2 * j)] = left + right;
             }
-        }
+        }return null;
+    }
+
+
+    public static void main(String[] args) {
+        PascalTriangle pascalTriangle = new PascalTriangle(10);
+        pascalTriangle.calculateTriangle();
+
     }
 }
+
+
+
