30656a18809478824d163b56c36bf8b1763363f0
dtarasenko <dmitriy.tarasenko@live.co.uk>
2015-09-18 02:29:58 +0300

    HomeWork 1

--- trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/IslandProblemResolving.java
+++ trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/IslandProblemResolving.java
@@ -1,4 +1,4 @@
-package com.gojava6;
+package java.com.gojava6;
 
 /**
  * Created by sergiigetman on 8/25/15.
@@ -7,13 +7,41 @@ package com.gojava6;
  */
 public class IslandProblemResolving {
 
-    public IslandProblemResolving(int[][] islandMap) {
-    }
+    private int[][] islandMap;
 
+    public IslandProblemResolving(int[][] islandMap) {
+        this.islandMap = islandMap;
+    }
 
     public int countIslands() {
-        //TODO: write you code here
-        return 0;
-    }
+        int[][] map = this.islandMap;
+        int count = 0;
 
+        for (int i = 0; i < map.length - 1; i++) {
+            for (int k = 0; k < map[i].length - 1; k++) {
+                if (map[i][k] == 0) {
+                }
+                    else {
+                    if (map[i][k] == map[i][k + 1]) {
+                        count++;
+                    }
+                    if (map[i][k] == map[i + 1][k]) {
+                        count++;
+                    }
+                    if (map[i][k] == map[i + 1][k + 1]) {
+                        count++;
+                    }
+                }
+                if ((k == map[i].length - 2) && (map[i][k + 1] == 1) &&
+                        (map[i][k + 1] == map[i + 1][k + 1])) {
+                    count++;
+                }
+                if ((i == map.length - 2) && (map[i + 1][k] == 1) &&
+                        (map[i + 1][k] == map[i + 1][k + 1])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
--- trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/PascalTriangle.java
+++ trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/PascalTriangle.java
@@ -1,17 +1,29 @@
-package com.gojava6;
+package java.com.gojava6;
 
 /**
  * Created by sergiigetman on 8/26/15.
  */
 public class PascalTriangle {
 
-    public PascalTriangle(int level) {
+    private int level;
 
+    public PascalTriangle(int level) {
+        this.level = level;
     }
 
     public int[][] calculateTriangle() {
-        //TODO: write you code here
-        return null;
+        int[][] triangleArray = new int[this.level][];
+        for (int i = 0; i < triangleArray.length; i++) {
+            triangleArray[i] = new int[i + 1];
+        }
+        for (int i = 0; i < triangleArray.length; i++) {
+            for (int k = 0; k < triangleArray[i].length; k++) {
+                triangleArray[i][k] = 1;
+                if ((i > 1) && (k > 0) && (k < triangleArray[i].length - 1)) {
+                    triangleArray[i][k] = triangleArray[i - 1][k - 1] + triangleArray[i - 1][k];
+                }
+            }
+        }
+        return triangleArray;
     }
-
 }
