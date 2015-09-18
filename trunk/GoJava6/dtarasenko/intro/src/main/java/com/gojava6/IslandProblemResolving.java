9c3ad0ec9eee34d7e3e327a80b77c104306319f6
dtarasenko <dmitriy.tarasenko@live.co.uk>
2015-09-18 15:53:19 +0300

    HomeWork 1

--- trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/IslandProblemResolving.java
+++ trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/IslandProblemResolving.java
@@ -17,28 +17,45 @@ public class IslandProblemResolving {
         int[][] map = this.islandMap;
         int count = 0;
 
-        for (int i = 0; i < map.length - 1; i++) {
-            for (int k = 0; k < map[i].length - 1; k++) {
-                if (map[i][k] == 0) {
-                }
-                    else {
-                    if (map[i][k] == map[i][k + 1]) {
-                        count++;
-                    }
-                    if (map[i][k] == map[i + 1][k]) {
-                        count++;
-                    }
-                    if (map[i][k] == map[i + 1][k + 1]) {
+        for (int i = 0; i < islandMap.length; i++) {
+            for (int k = 0; k < islandMap[i].length - 1; k++) {
+                if (k == 0) {
+                    if ((islandMap[i][k] == 1) && (islandMap[i][k + 1] == 1)) {
                         count++;
                     }
                 }
-                if ((k == map[i].length - 2) && (map[i][k + 1] == 1) &&
-                        (map[i][k + 1] == map[i + 1][k + 1])) {
-                    count++;
+                else {
+                    if ((islandMap[i][k] == 1) && (islandMap[i][k + 1] == 1) && (islandMap[i][k - 1] != 1)) {
+                        count++;
+                    }
                 }
-                if ((i == map.length - 2) && (map[i + 1][k] == 1) &&
-                        (map[i + 1][k] == map[i + 1][k + 1])) {
-                    count++;
+            }
+        }
+        for (int k = 0; k < islandMap[0].length; k++) {
+            for (int i = 0; i < islandMap.length - 1; i++) {
+                if (i == 0) {
+                    if ((islandMap[i][k] == 1) && (islandMap[i + 1][k] == 1)) {
+                        count++;
+                    }
+                }
+                else {
+                    if ((islandMap[i][k] == 1) && (islandMap[i + 1][k] == 1) && (islandMap[i - 1][k] != 1)) {
+                        count++;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < islandMap.length - 1; i++) {
+            for (int k = 0; k < islandMap[i].length - 1; k++) {
+                if ((i == 0) || (k == 0)) {
+                    if ((islandMap[i][k] == 1) && (islandMap[i + 1][k + 1] == 1)) {
+                        count++;
+                    }
+                }
+                else {
+                    if ((islandMap[i][k] == 1) && (islandMap[i + 1][k + 1] == 1) && (islandMap[i - 1][k - 1] != 1)) {
+                        count++;
+                    }
                 }
             }
         }
