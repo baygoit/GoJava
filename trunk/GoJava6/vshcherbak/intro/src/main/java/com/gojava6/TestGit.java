1e155cd894bb94be543e98d32b4540a25097102a
Slavik Shcherbak <slva@ukr.net>
2015-09-18 12:44:33 +0300

    test push to github

---
+++ trunk/GoJava6/vshcherbak/intro/src/main/java/com/gojava6/TestGit.java
@@ -0,0 +1,10 @@
+package com.gojava6;
+
+/**
+ * Created by RZA on 18.09.2015.
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Test push to github");
+    }
+}
