f27f977d6053c5360f858f930b15d013a29be8f4
Belskii Artem <belskiiartem@gmail.com>
2015-09-21 18:24:12 +0300

    try catch in DAO

--- trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/dao/project/ProjectDaoImplHiber.java
+++ trunk/GoJava5/artem.belskii/Kickstarter/src/main/java/belskii/artem/kickstarter/dao/project/ProjectDaoImplHiber.java
@@ -12,46 +12,69 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
-
-public class ProjectDaoImplHiber implements  ProjectDao{
+public class ProjectDaoImplHiber implements ProjectDao {
 	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-	
+
 	private Long getNextId() {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		Criteria criteria = session
-			    .createCriteria(Project.class)
-			    .setProjection(Projections.max("projectId"));
-			Long maxId = (Long)criteria.uniqueResult();
+		Session session = null;
+		Transaction transaction = null;
+		Long maxId=null;
+		try{
+		session = sessionFactory.openSession();
+		transaction = session.beginTransaction();
+		transaction.begin();
+		Criteria criteria = session.createCriteria(Project.class).setProjection(Projections.max("projectId"));
+		maxId = (Long) criteria.uniqueResult();
+		session.close();
+		if (maxId == null) {
+			maxId = 0L;
+		}
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
 			session.close();
-			if (maxId==null){
-				maxId=0L;
-			}
-			return maxId+1;
+		}
+		return maxId + 1;
 	}
-	
+
 	@Override
 	public void addProject(Project projectDetails) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		projectDetails.setProjectId(getNextId());
-		session.save(projectDetails);
-		session.getTransaction().commit();
-		session.close();
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			transaction.begin();
+			projectDetails.setProjectId(getNextId());
+			session.save(projectDetails);
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public Map<Long, Project> getProjectList() {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		
-		ArrayList<Project> arr = (ArrayList<Project>) session.createCriteria(Project.class).list();
-		System.out.println(arr.size());
-		HashMap<Long,Project> map=new HashMap<Long,Project>();
-		long iL=0;
-		for (int i=0;arr.size()>i;i++) {
-			map.put(iL,arr.get(i));
-			iL++;
+		Session session = null;
+		Transaction transaction = null;
+		HashMap<Long, Project> map = new HashMap<Long, Project>();
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			transaction.begin();
+			ArrayList<Project> arr = (ArrayList<Project>) session.createCriteria(Project.class).list();
+
+			long iL = 0;
+			for (int i = 0; arr.size() > i; i++) {
+				map.put(iL, arr.get(i));
+				iL++;
+			}
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
 		}
 		return map;
 
@@ -59,44 +82,68 @@ public class ProjectDaoImplHiber implements  ProjectDao{
 
 	@Override
 	public Project getProjectDetails(int id) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		Project project = session.get(Project.class, new Long(id));
+		Session session = null;
+		Transaction transaction = null;
+		Project project=new Project();
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			transaction.begin();
+			project = session.get(Project.class, new Long(id));
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
 		return project;
 	}
 
 	@Override
 	public Map<Long, Project> getProjectFromCategory(int id) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-		
-		ArrayList<Project> arr = (ArrayList<Project>) session.createCriteria(Project.class).add(Restrictions.eq("categoryId", id)).list();
-		System.out.println(arr.size());
-		HashMap<Long,Project> map=new HashMap<Long,Project>();
-		long iL=0;
-		for (int i=0;arr.size()>i;i++) {
-			map.put(iL,arr.get(i));
-			iL++;
+		Session session = null;
+		Transaction transaction = null;
+		HashMap<Long, Project> map = new HashMap<Long, Project>();
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			transaction.begin();
+			ArrayList<Project> arr = (ArrayList<Project>) session.createCriteria(Project.class).add(Restrictions.eq("categoryId", id)).list();
+			long iL = 0;
+			for (int i = 0; arr.size() > i; i++) {
+				map.put(iL, arr.get(i));
+				iL++;
+			}
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
 		}
 		return map;
 
 	}
 
-
 	@Override
 	public void update(Project updatedProject) {
-		Session session = sessionFactory.openSession();
-		Transaction transaction = session.beginTransaction();
-		transaction.begin();
-		session.saveOrUpdate(updatedProject);
-		transaction.commit();
-		session.close();
+		Session session = null;
+		Transaction transaction = null;
+		try {
+			session = sessionFactory.openSession();
+			transaction = session.beginTransaction();
+			transaction.begin();
+			session.saveOrUpdate(updatedProject);
+			transaction.commit();
+		} catch (Exception e) {
+			transaction.rollback();
+		} finally {
+			session.close();
+		}
+
 	}
-	
+
 	@Override
 	public void initDemoDB() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
