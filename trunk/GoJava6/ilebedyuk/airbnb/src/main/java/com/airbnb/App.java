a6f079365955a0cf29f62c6707612c587704297b
ilebedyuk <ilebeduyk@gmail.com>
2015-09-21 10:53:14 +0300

    register+observer

--- trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
@@ -1,10 +1,74 @@
 package com.airbnb;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Игорь on 20.09.2015.
+ * Created by пїЅпїЅпїЅпїЅпїЅ on 20.09.2015.
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+    private List<User> users = new ArrayList<User>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void register(User user) throws Exception {
+        if(App.validateName(user.getName()) == true &&
+                App.validateEmail(user.getEmail()) == true &&
+                App.validateName(user.getSurname()) == true){
+            users.add(user);
+            System.out.println("Hello! " + user.getName() + ", you've been registered successfully!");
+        } else throw new Exception();
+
+        }
+
+    public static boolean validateName(String name) {
+        if (name == null || name.isEmpty() || isAlpha(name) == false){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean validateEmail(String email) {
+        if (isValidEmailAddress(email) == false || email == null || email.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateCity(String city) {
+        if (city == null || city.isEmpty() || isAlpha(city) == false){
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isAlpha(String name) {
+        char[] chars = name.toCharArray();
+
+        for (char c : chars) {
+            if (!Character.isLetter(c)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 }
---
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/Client.java
@@ -0,0 +1,6 @@
+package com.airbnb;
+
+/**
+ * Created by Игорь on 20.09.2015.
+ */
+public class Client extends User {}
