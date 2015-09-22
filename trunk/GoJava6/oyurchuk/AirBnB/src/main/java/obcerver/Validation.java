0a9209a07c073f32d29e9ab703be0b932154f268
Olga Yurchuk <olga-mala@mail.ru>
2015-09-22 23:45:55 +0300

    HomeWork_2

---
+++ trunk/GoJava6/oyurchuk/AirBnB/src/main/java/obcerver/Validation.java
@@ -0,0 +1,50 @@
+package obcerver;
+
+/**
+ * Created by macmini on 20.09.15.
+ */
+
+
+    import java.util.regex.Matcher;
+    import java.util.regex.Pattern;
+
+    public class Validation {
+        private static String nameValidationPatternString =
+                "([A-Z][a-z]{1,39})(-([A-Z][a-z]{1,39}))*";
+        private static String emailValidationPatternString =
+                "([a-z0-9]+([-.]?[a-z0-9])*)@(([a-z](-?[a-z0-9])*){1,40}\\.[a-z]{2,6})";
+        private static String cityValidationPatternString =
+                "([A-Z][a-z]{1,39})(-?([A-Z][a-z]{1,39}))*";
+
+        private Validation() {}
+
+        public static boolean validateName(String name) {
+            return validateString(nameValidationPatternString, name);
+        }
+
+        public static boolean validateSurname(String surname) {
+            return validateString(nameValidationPatternString, surname);
+        }
+
+        public static boolean validateEmail(String email) {
+            return validateString(emailValidationPatternString, email);
+        }
+
+        public static boolean validateCity(String city) {
+            return validateString(cityValidationPatternString, city);
+        }
+
+        private static boolean validateString(String patternString, String value) {
+            if (value == null) return false;
+
+            Pattern pattern = Pattern.compile(patternString);
+            Matcher matcher = pattern.matcher(value);
+
+            return matcher.matches();
+        }
+    }
+
+
+
+
+
