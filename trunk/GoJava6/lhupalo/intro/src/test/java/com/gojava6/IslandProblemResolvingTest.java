36ba703a25bd73be361fbb917668a9e4b5536041
LiubaHupalo <liuba.hupalo@gmail.com>
2015-09-22 23:48:22 +0300

    done islands

--- trunk/GoJava6/lhupalo/intro/src/test/java/com/gojava6/IslandProblemResolvingTest.java
+++ trunk/GoJava6/lhupalo/intro/src/test/java/com/gojava6/IslandProblemResolvingTest.java
@@ -14,17 +14,57 @@ public class IslandProblemResolvingTest {
 
     @Before
     public void setUp() {
-        islandMap = new int[][]{
+        int[][] islandMap;
+        int count ;
+    }
+
+    @Test
+    public void testCountIslands1() {
+        int[][]islandMap = {
+                {1, 1, 0, 0, 1},
+                {1, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1},
+                {1, 0, 1, 1, 1}
+        };
+        count = 4;
+
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
+    }
+
+    @Test
+    public void testCountIslands2() {
+        int[][]islandMap = {
                 {1, 1, 0, 0, 1},
                 {1, 0, 0, 0, 0},
                 {0, 0, 1, 0, 1},
                 {1, 0, 0, 1, 1}
         };
         count = 5;
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
     }
-
     @Test
-    public void testCountIslands() {
+    public void testCountIslands3() {
+        int[][]islandMap = {
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1}
+        };
+        count = 1;
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
+    }
+    @Test
+    public void testCountIslands4() {
+        int[][]islandMap = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+        };
+        count = 0;
         IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
         assertEquals(count, islandProblemResolving.countIslands());
     }
