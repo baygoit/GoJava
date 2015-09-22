dde1595fbf1f17a359a7c2ba61db7bc69571b3d2
anton_nakonechnyi <polipoli>
2015-09-22 23:18:44 +0300

    2nd home work

---
+++ trunk/GoJava6/anakonechnyi/MyAirbnb/src/main/Observer.java
@@ -0,0 +1,9 @@
+package main;
+
+/**
+ * Created by user on 19.09.2015.
+ */
+public interface Observer {
+    public void update(Object obj);
+    public void loyalty(int discountPercent, Subject s);
+}
