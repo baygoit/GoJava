67ead0115cb809ff2d051ef751ff0b163684b634
ilebedyuk <ilebeduyk@gmail.com>
2015-09-22 10:26:04 +0300

    register+observer_v2

--- trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
@@ -20,55 +20,14 @@ public class App {
     }
 
     public void register(User user) throws Exception {
-        if(App.validateName(user.getName()) == true &&
-                App.validateEmail(user.getEmail()) == true &&
-                App.validateName(user.getSurname()) == true){
+        if(user.validateName(user.getName()) == true &&
+                user.validateEmail(user.getEmail()) == true &&
+                user.validateName(user.getSurname()) == true){
             users.add(user);
             System.out.println("Hello! " + user.getName() + ", you've been registered successfully!");
         } else throw new Exception();
 
         }
 
-    public static boolean validateName(String name) {
-        if (name == null || name.isEmpty() || isAlpha(name) == false){
-            return false;
-        }
-        return true;
-    }
 
-    public static boolean validateEmail(String email) {
-        if (isValidEmailAddress(email) == false || email == null || email.isEmpty()){
-            return false;
-        }
-        return true;
-    }
-
-    public boolean validateCity(String city) {
-        if (city == null || city.isEmpty() || isAlpha(city) == false){
-            return false;
-        }
-        return true;
-    }
-
-    public static boolean isAlpha(String name) {
-        char[] chars = name.toCharArray();
-
-        for (char c : chars) {
-            if (!Character.isLetter(c)){
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static boolean isValidEmailAddress(String email) {
-        boolean result = true;
-        try {
-            InternetAddress emailAddr = new InternetAddress(email);
-            emailAddr.validate();
-        } catch (AddressException ex) {
-            result = false;
-        }
-        return result;
-    }
 }
--- trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/Host.java
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/Host.java
@@ -6,12 +6,32 @@ package com.airbnb;
 public class Host extends User {
     private String city;
     enum ApartmentType {Place, Room, Apartment};
+    private ApartmentType apartmentType;
+
+    public Host(String name, String surname, String email, String city, ApartmentType apartmentType) throws Exception {
+        super(name, surname, email);
+        if (Host.validateName(city) == true) {
+            this.city = city;
+        } else throw new Exception();
+        this.apartmentType = apartmentType;
+    }
+
+//    public boolean validateCity(String city) {
+//        if (city == null || city.isEmpty() || isAlpha(city) == false){
+//            return false;
+//        }
+//        return true;
+//    }
+
+    public ApartmentType getApartmentType() {
+        return apartmentType;
+    }
 
     public String getCity() {
         return city;
     }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+//    public void setCity(String city) {
+//        this.city = city;
+//    }
 }
