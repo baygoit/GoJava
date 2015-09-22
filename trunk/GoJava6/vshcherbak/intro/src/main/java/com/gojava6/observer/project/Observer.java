2a8b5be0c6caf54686ff37fff107d6f5607c74f8
Slavik Shcherbak <slva@ukr.net>
2015-09-22 12:47:34 +0300

    del

--- trunk/GoJava6/vshcherbak/intro/src/main/java/com/gojava6/observer/project/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.gojava6.observer.project;
-
-/**
- * Created by slavik on 21.09.2015.
- */
-public interface Observer {
-    void update(String message);
-}
--- trunk/GoJava6/vshcherbak/intro/src/main/java/com/gojava6/observer/project/RentType.java
+++
@@ -1,8 +0,0 @@
-package com.gojava6.observer.project;
-
-/**
- * Created by slavik on 22.09.2015.
- */
-enum RentType {
-    PLACE, ROOM, APARTMENT
-}
