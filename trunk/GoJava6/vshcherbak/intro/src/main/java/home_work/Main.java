45035a7cf491dae7b030b6f664ff93e7e5b3c359
Apologetic <slva@ukr.net>
2015-09-23 13:09:29 +0300

    sort test

--- trunk/GoJava6/vshcherbak/intro/src/main/java/home_work/Main.java
+++ trunk/GoJava6/vshcherbak/intro/src/main/java/home_work/Main.java
@@ -1,6 +1,7 @@
 package home_work;
 
 import home_work.arraylist.MyList;
+import home_work.oneswipetest.OneSwipeArrayTest;
 
 /**
  * Created by slavik on 22.09.2015.
@@ -21,5 +22,23 @@ public class Main {
 
         list.remove(3);
         System.out.println("capacity= " + list.getCapacity() + " size= " + list.getSize() + '\n' + list);
+
+        int[] array = new int[] {2, 3, 4, 6, 7, 5, 8, 9};
+        OneSwipeArrayTest test = new OneSwipeArrayTest(array);
+        boolean rezult = test.test();
+        System.out.println(rezult);
+        array = new int[] {2, 3, 4, 6, 7, 5, 8, 9};
+        test.array = new int[] {2, 3, 4, 6, 7, 6, 8, 9};
+        rezult = test.test();
+        System.out.println(rezult);
+
+        test.array = new int[] {1, 2, 4, 3, 2};
+        rezult = test.test();
+        System.out.println(rezult);
+
+        test.array = new int[] {1, 2, 5, 7, 2};
+        rezult = test.test();
+        System.out.println(rezult);
     }
 }
+
