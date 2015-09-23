53c312b066b949ea3f3a8be0b6fb593f66543ef2
Denis Shatalin <shatalinden@gmail.com>
2015-09-23 22:58:29 +0300

    parser

---
+++ trunk/GoJava6/shatalinden/intro/src/main/java/com/gojava6/MyList/MyLinkedList.java
@@ -0,0 +1,17 @@
+package com.gojava6.MyList;
+
+import javax.xml.soap.Node;
+
+/**
+ * Created by shata on 23.09.2015.
+ */
+public class MyLinkedList {
+    Node previous;
+    Node next = previous;
+    Object obj;
+
+
+
+
+
+}
