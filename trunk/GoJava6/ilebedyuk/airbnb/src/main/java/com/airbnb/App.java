4c5ca53a9ae81c54f69b45da93f29044871a4f0e
ilebedyuk <ilebeduyk@gmail.com>
2015-09-29 22:50:00 +0300

    Observer+Decorator

--- trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/App.java
@@ -9,24 +9,138 @@ import java.util.List;
  * Created by ����� on 20.09.2015.
  */
 public class App {
-    private List<User> users = new ArrayList<User>();
+    private List<Client> clients = new ArrayList<Client>();
+    private List<Host> hosts = new ArrayList<Host>();
+    private List<Apartment> apartments = new ArrayList<Apartment>();
+    private List<Date> reservationDates = new ArrayList<Date>();
 
-    public List<User> getUsers() {
-        return users;
-    }
+//    public void registerClient(Client client) throws Exception {
+//
+//        clients.add(client);
+//        System.out.println("Hello! " + client.getName() + ", you've been registered successfully!");
+//    }
 
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    public void register(User user) throws Exception {
-        if(user.validateName(user.getName()) == true &&
-                user.validateEmail(user.getEmail()) == true &&
-                user.validateName(user.getSurname()) == true){
-            users.add(user);
-            System.out.println("Hello! " + user.getName() + ", you've been registered successfully!");
-        } else throw new Exception();
-
+    public void register(User user) {
+        if (user instanceof Host) {
+            hosts.add((Host) user);
+        }else{
+            clients.add((Client) user);
         }
+        System.out.println("Hello! " + user.getName() + ", you've been registered successfully!");
+    }
+
+//    public void registerHosts(Host host) throws Exception {
+//        hosts.add(host);
+//        System.out.println("Hello! " + host.getName() + ", you've been registered successfully!");
+//    }
+
+    public List<Client> getClients() {
+        for (Client client : clients) {
+            System.out.println(client.getName());
+        }
+        return clients;
+    }
+
+    public List<Host> getHosts() {
+        for (Host host :hosts) {
+            System.out.println(host.getName());
+        }
+        return hosts;
+    }
+
+    public List<Apartment> getApartments() {
+//        for (Apartment apartment : apartments) {
+//            System.out.println(apartment.getOwnerName());
+//        }
+        return apartments;
+    }
+
+    public List<Date> getReservationDates() {
+        for (Date date : reservationDates) {
+            date.getPeriod();
+        }
+
+        return reservationDates;
+    }
+
+    public Apartment createApartment(Apartment.ApartmentType apartmentType, String city, User user ) throws Exception {
+        if (hosts.contains(user) == false) {
+            throw new Exception();
+        }
+        Apartment apartment = new Apartment(apartmentType, city, user.getName());
+        apartments.add(apartment);
+        System.out.println(user.getName() + " is create apartament " + apartmentType);
+        return apartment;
+    }
+
+    public void makeReservation(User user, Apartment apartment, Date period) throws Exception {
+        if (clients.contains(user) == false) {
+            throw new Exception("You didn't registered");
+        }
+        System.out.println("Client " + user.getName() + " reserved apartment successfully! " + apartment.getApartmentType() + " on date: " + period.getDateBegin() + " to " + period.getDateEnd());
+        reservationDates.add(period);
+    }
+
+    public List<Apartment> searchByOwner(List<Apartment> apartments, User user) throws Exception {
+        if (hosts.contains(user) == false){
+            throw new Exception("This host doesn't exist!");
+        }
+        if (apartments.isEmpty()){
+            throw new NullPointerException ("No one apartments is available!");
+        }
+
+        List<Apartment> newListOfApartments = new ArrayList<Apartment>();
+        for (Apartment apartment : apartments) {
+            if (apartment.getOwnerName() == user.getName()){
+                newListOfApartments.add(apartment);
+                System.out.println(user.getName() + " has apartment: " + apartment.getApartmentType());
+                //apartments.remove(apartment);
+            }
+        }
+
+//        for (Apartment apartment : apartments) {
+//            System.out.println(user.getName() + " has apartment: " + apartment.getApartmentType());
+//        }
+        return newListOfApartments;
+    }
+
+    public List<Apartment> searchByCity(List<Apartment> apartments, String city) throws Exception {
+        if (apartments.isEmpty()){
+            throw new NullPointerException ("No one apartments is available!");
+        }
+        List<Apartment> newListOfApartments = new ArrayList<Apartment>();
+        for (Apartment apartment : apartments) {
+                if (apartment.getCity() == city){
+                    newListOfApartments.add(apartment);
+                    //apartments.remove(apartment);
+                    System.out.println("In city " + city + " next apartments: " + apartment.getApartmentType());
+                }
+            }
+            return newListOfApartments;
+        }
+
+    public boolean IsAvaible(Apartment apartament, Date period) {
+        for (Date dates : reservationDates) {
+            if (dates.getDateBegin() == period.getDateBegin() || dates.getDateEnd() == dates.getDateEnd()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Apartment> searchByDate(List<Apartment> apartments, Date period) throws Exception {
+        if (apartments.isEmpty()){
+            throw new NullPointerException ("No one apartments is available!");
+        }
+        List<Apartment> newListOfApartments = new ArrayList<Apartment>();
+            for (Apartment apartment : apartments) {
+                if (IsAvaible(apartment, period) == true){
+                    newListOfApartments.add(apartment);
+                    System.out.println("On that period " + period.getDateBegin() + " to " + period.getDateEnd() + " next apartaments available:" + apartment.getApartmentType());
+                    //apartments.remove(apartment);
+                }
+            }
+        return newListOfApartments;
+    }
 
 }
--- trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/Main.java
+++ trunk/GoJava6/ilebedyuk/airbnb/src/main/java/com/airbnb/Main.java
@@ -1,5 +1,8 @@
 package com.airbnb;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Игорь on 21.09.2015.
  */
@@ -7,22 +10,47 @@ public class Main {
     public static void main(String[] args) {
         try {
             User client1 = new Client("Vasil", "Boyko", "jhg@gmail.com");
-            User client2 = new Host("Petro", "Limar", "juy@gmail.com", "Kiev", Host.ApartmentType.Place);
+            User client2 = new Client("Igor", "Lebedyukk", "igor@gmail.com");
+            User host1 = new Host("Petro", "Limar", "juy@gmail.com", "Kiev");
+            User host2 = new Host("Dima", "Tarasenko", "dima@gmail.com", "Lviv");
 
             App airbnb = new App();
             airbnb.register(client1);
-            airbnb.register(client2);
+            //airbnb.register(client2);
+            airbnb.register(host1);
+            airbnb.register(host2);
 
-            airbnb.getUsers();
+            //airbnb.getClients();
+            //airbnb.getHosts();
 
-            NewsSubscribe newsSubscribe = new NewsSubscribe();
-            newsSubscribe.registerObserver(client1);
-            newsSubscribe.registerObserver(client2);
 
-            newsSubscribe.notifyObservers();
+            Apartment room1 = airbnb.createApartment(Apartment.ApartmentType.Room, "Kiev", host1);
+            Apartment place1 = airbnb.createApartment(Apartment.ApartmentType.Place, "Lviv", host1);
+            Apartment place2 = airbnb.createApartment(Apartment.ApartmentType.Place, "Lviv", host2);
 
-           newsSubscribe.removeObserver(client1);
-           newsSubscribe.notifyObservers();
+            //airbnb.getApartments();
+            Date period1 = new Date(11,17);
+            Date period2 = new Date(18,20);
+
+            //System.out.println(airbnb.IsAvaible(room1, period1));
+
+            airbnb.makeReservation(client1, room1, period1);
+
+            //System.out.println(airbnb.IsAvaible(room1, period1));
+
+            //List<Apartment> searchApartments = new ArrayList<Apartment>();
+
+            //airbnb.searchByOwner(airbnb.searchByCity(airbnb.getApartments(), "Lviv"), host1);
+             airbnb.searchByDate(airbnb.getApartments(), period2);
+
+//            NewsSubscribe newsSubscribe = new NewsSubscribe();
+//            newsSubscribe.registerObserver(client1);
+//            newsSubscribe.registerObserver(client2);
+//
+//            newsSubscribe.notifyObservers();
+//
+//           newsSubscribe.removeObserver(client1);
+//           newsSubscribe.notifyObservers();
 
         } catch (Exception e) {
             System.out.println("Uncorrect data");
