3436b984783940950aeb48e9ad783a090f2d0ea4
hunkyua <op.ua@yandex.ru>
2015-09-30 11:40:27 +0300

    test

--- trunk/GoJava6/vopanasyuk/intro/src/test/java/com/gojava6/IslandProblemResolvingTest.java
+++ trunk/GoJava6/vopanasyuk/intro/src/test/java/com/gojava6/IslandProblemResolvingTest.java
@@ -10,8 +10,8 @@ public class IslandProblemResolvingTest {
     private int[][] islandMap;
     private int count;
 
-    @Before
-    public void setUp() {
+    @Test
+    public void testCountIslandsWithFiveIslands() {
         islandMap = new int[][]{
                 {1, 1, 0, 0, 1},
                 {1, 0, 0, 0, 0},
@@ -19,10 +19,51 @@ public class IslandProblemResolvingTest {
                 {1, 0, 0, 1, 1}
         };
         count = 5;
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
     }
-
     @Test
-    public void testCountIslands() {
+    public void testCountIslandsWithOneIslands() {
+        islandMap = new int[][]{
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}
+        };
+        count = 1;
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
+    }
+    @Test
+    public void testCountIslandsWithFifteenIslands() {
+        islandMap = new int[][]{
+                {0, 0, 0, 1, 0, 1, 0},
+                {1, 1, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 1, 0, 0},
+                {1, 1, 0, 0, 0, 0, 1},
+                {0, 0, 0, 1, 0, 0, 0},
+                {1, 1, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 1},
+                {1, 0, 0, 1, 1, 0, 1}
+        };
+        count = 15;
+        IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
+        assertEquals(count, islandProblemResolving.countIslands());
+    }
+    @Test
+    public void testCountIslandsWithTenIslands() {
+        islandMap = new int[][]{
+                {1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1},
+                {1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1},
+                {1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1},
+                {1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1}
+        };
+        count = 10;
         IslandProblemResolving islandProblemResolving = new IslandProblemResolving(islandMap);
         assertEquals(count, islandProblemResolving.countIslands());
     }
