20f2bb5d540317f66fc0680e6488c627db9ff9ed
Olga Yurchuk <olga-mala@mail.ru>
2015-09-30 23:49:14 +0300

    Add AirBnB

---
+++ trunk/GoJava6/oyurchuk/AirBnB/src/main/java/obcerver/ApartmentType.java
@@ -0,0 +1,13 @@
+package obcerver;
+import java.lang.Enum;
+
+/**
+ * Created by macmini on 29.09.15.
+ */
+public enum ApartmentType {
+    PLACE,
+    ROOM,
+    APARTMENT;
+
+
+}
--- trunk/GoJava6/oyurchuk/AirBnB/src/main/java/obcerver/User.java
+++ trunk/GoJava6/oyurchuk/AirBnB/src/main/java/obcerver/User.java
@@ -1,42 +1,46 @@
 package obcerver;
+import java.util.Date;
 
 /**
  * Created by macmini on 20.09.15.
  */
-public class User implements Observer{
+public class User implements Observer {
 
     protected String name;
     protected String surname;
-    protected final String mail ;
-
+    protected final String mail;
+    private enum Status  {CLIENT, HOST};
+    private Status myStatus = Status.CLIENT;
 
     public User(String name, String surname, String mail) {
 
         this.mail = mail;
+        this.surname = surname;
+        this.name = name;
     }
 
 
-    public String getName (){
+    public String getName() {
 
         return name;
-        }
+    }
 
     public void setName(String name) {
 
         this.name = name;
     }
 
-    public String getSurname (){
+    public String getSurname() {
 
         return surname;
     }
 
-    public void setSurname (String surname) {
+    public void setSurname(String surname) {
 
         this.surname = surname;
     }
 
-    public String getMail(){
+    public String getMail() {
 
         return mail;
     }
@@ -46,5 +50,26 @@ public class User implements Observer{
         System.out.println("Hello!" + getName());
 
     }
+
+    public boolean createApartment(String city, ApartmentType apartType, String address) {
+
+        if (myStatus == Status.CLIENT){
+            return false;
+        }
+        Apartment apartment = new Apartment(city, apartType, address);
+        Validation v = new Validation();
+        if (v.validateApartment(apartment)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void reservApartment (Apartment apartment, Date start, Date end){
+
+        apartment.makeReservation(start,end);
+
+    }
 }
 
+// из клиента сделать хоста
