b4902c00a3ebe11a719e70f52239c473865c1dfa
Mazhara Roman <mazhararom@gmail.com>
2015-10-02 14:56:58 +0300

    Island Problem Solved

--- trunk/GoJava6/rmazhara/intro/src/main/java/airbnb/Client.java
+++ trunk/GoJava6/rmazhara/intro/src/main/java/airbnb/Client.java
@@ -2,6 +2,7 @@ package airbnb;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 /**
  * Created by mazha on 9/29/2015.
@@ -10,5 +11,15 @@ public class Client extends User implements Observer {
 
     public void update(Observable o, Object arg) {
 
+    }
+
+    @Override
+    public void registerNew(Scanner scanner) throws MyException {
+        System.out.println("Please enter your name:");
+        this.setName(scanner.next());
+        System.out.println("Please enter your surname:");
+        this.setSurname(scanner.next());
+        System.out.println("Please enter your email:");
+        this.setEmail(scanner.next());
     }
 }
