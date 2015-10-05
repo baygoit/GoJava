bc9d8a9b3a84a5115cf2e73e310518f42034f77b
RomaRedman <Roma.Redman@gmail.com>
2015-10-05 09:58:42 +0300

    sm

---
+++ trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/Apartment.java
@@ -0,0 +1,10 @@
+package com.Airbnb.app;
+
+/**
+ * Created by romanroma on 28.09.15.
+ */
+public class Apartment {
+    public Apartment(Host hos, String city, ApartType apartType, boolean active){
+
+    }
+}
