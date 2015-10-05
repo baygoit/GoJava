2333c08f74cb40fcd63c8ddec5115ae4b6faaf67
dtarasenko <dmitriy.tarasenko@live.co.uk>
2015-10-05 12:00:54 +0300

    HomeWork 6

--- trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/airbnb/Main.java
+++ trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/airbnb/Main.java
@@ -1,134 +1,83 @@
 package com.gojava6.airbnb;
 
 import com.gojava6.airbnb.apartment.Apartment;
-import com.gojava6.airbnb.apartment.ApartmentBuilder;
 import com.gojava6.airbnb.apartment.ApartmentType;
-import com.gojava6.airbnb.apartment.ReservationData;
 import com.gojava6.airbnb.application.Application;
 import com.gojava6.airbnb.application.LoyaltyProgram;
 import com.gojava6.airbnb.application.SearchResult;
-import com.gojava6.airbnb.users.Client;
-import com.gojava6.airbnb.users.Host;
+import com.gojava6.airbnb.users.User;
 
 import java.util.Date;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
 
+    public static void main(String[] args) {
 
-        //create application
-        Application app = new Application();
+        Application application = new Application();
 
-        //host creation and registration
-        Host host1 = new Host();
-        host1.setName("Nikolay");
-        host1.setSurName("Chayka");
-        host1.setEmail("nikolay.chayka@gmail.com");
-        app.registerUser(host1);
+        User user1 = new User();
+        user1.setName("Nikolay");
+        user1.setSurName("Chayka");
+        user1.setEmail("nikolay.chayka@gmail.com");
 
+        application.registerUser(user1);
 
+        User user2 = new User();
+        user2.setName("Roman");
+        user2.setSurName("Solomakha");
+        user2.setEmail("roman.solomakha@gmail.com");
 
+        application.registerUser(user2);
 
+        User user3 = new User();
+        user3.setName("Vladislav");
+        user3.setSurName("Shurubalko");
+        user3.setEmail("vladislav.shurubalko@gmail.com");
 
+        application.registerUser(user3);
 
-        //create client and registration
-        Client client1 = new Client();
-        client1.setName("Roman");
-        client1.setSurName("Solomakha");
-        client1.setEmail("roman.solomakha@gmail.com");
-        app.registerUser(client1);
+        user1.becomeHost();
+        Apartment apartment1 = new Apartment("Lviv", ApartmentType.ROOM);
+        user1.addApartment(apartment1);
 
-        Client client2 = new Client();
-        client2.setName("Vladislav");
-        client2.setSurName("Shurubalko");
-        client2.setEmail("vladislav.shurubalko@gmail.com");
-        app.registerUser(client2);
+        user2.becomeHost();
+        Apartment apartment2 = new Apartment("Lviv", ApartmentType.APARTMENT);
+        user2.addApartment(apartment2);
 
-
-
-
-
-
-
-
-
-        //apartment creation and adding to host
-        ApartmentBuilder apartBuilder1 = new ApartmentBuilder();
-        apartBuilder1.setUser(host1);
-        apartBuilder1.setCity("Kiev");
-        apartBuilder1.setApartmentType(ApartmentType.PLACE);
-        Apartment apartment1 = apartBuilder1.createApartment();
-        app.addApartment(apartment1);
-
-        ApartmentBuilder apartBuilder2 = new ApartmentBuilder();
-        apartBuilder2.setUser(host1);
-        apartBuilder2.setCity("Lviv");
-        apartBuilder2.setApartmentType(ApartmentType.ROOM);
-        Apartment apartment2 = apartBuilder2.createApartment();
-        app.addApartment(apartment2);
-
-        ApartmentBuilder apartBuilder3 = new ApartmentBuilder();
-        apartBuilder3.setUser(host1);
-        apartBuilder3.setCity("Odessa");
-        apartBuilder3.setApartmentType(ApartmentType.APARTMENT);
-        Apartment apartment3 = apartBuilder3.createApartment();
-        app.addApartment(apartment3);
-
-        ApartmentBuilder apartBuilder4 = new ApartmentBuilder();
-        apartBuilder4.setUser(host1);
-        apartBuilder4.setCity("Lviv");
-        apartBuilder4.setApartmentType(ApartmentType.APARTMENT);
-        Apartment apartment4 = apartBuilder4.createApartment();
-        app.addApartment(apartment4);
-
-        //searchResult apartment
-        SearchResult sr = new SearchResult(app);
-        sr.showSearchResults();
-        sr.filterByCity("Lviv");
-        sr.showSearchResults();
-        sr.filterByApartmentType("Apartment");
-        sr.showSearchResults();
-
-        //reserveApartment
-        app.reserveApartment(4, client1, new Date(115, 8, 10), new Date(115, 8, 15));
-        app.reserveApartment(4, client1, new Date(115, 8, 20), new Date(115, 8, 25));
-        app.reserveApartment(4, client1, new Date(115, 8, 22), new Date(115, 8, 27));
-
-
-        System.out.print("\nReservation list:");
-        for (ReservationData rd : app.getListOfReservedDates()) {
-            System.out.print("\nApartment ID: " + rd.getApartmentId());
-            System.out.print(", User ID: " + rd.getUserId() + ", ");
-            System.out.print(new Date(rd.getStart()) + ", ");
-            System.out.print(new Date(rd.getEnd()));
-        }
-        System.out.println();
-
-        //searchResult apartment
-        sr = new SearchResult(app);
-        sr.filterByCity("Lviv");
-        sr.filterByApartmentType("Apartment");
-        sr.filterByDates(new Date(115, 8, 15), new Date(115, 8, 17));
-        sr.showSearchResults();
+        user3.becomeHost();
+        Apartment apartment3 = new Apartment("Odessa", ApartmentType.PLACE);
+        user3.addApartment(apartment3);
 
 
 
 
 
+        apartment1.reserve(new Date(115, 8, 10), new Date(115, 8, 15), user2);
+        long start = new Date(115, 8, 14).getTime();
+        long end = new Date(115, 8, 17).getTime();
+        apartment1.isAvailable(start, end);
+        apartment1.reserve(new Date(115, 8, 18), new Date(115, 8, 20), user2);
+        apartment1.reserve(new Date(115, 8, 22), new Date(115, 8, 26), user2);
 
-        //create loyalty program
+
+
+        SearchResult searchResult1 = new SearchResult(application.getUserList());
+        searchResult1.showSearchResults();
+        searchResult1.filterByCity("Lviv");
+        searchResult1.showSearchResults();
+        searchResult1.filterByApartmentType(ApartmentType.ROOM);
+        searchResult1.showSearchResults();
+        searchResult1.filterByDates(new Date(115, 8, 21), new Date(115, 8, 25));
+        searchResult1.showSearchResults();
+
+
         LoyaltyProgram loyaltyProgram = new LoyaltyProgram();
         loyaltyProgram.setLoyaltyProgramName("New Year loyalty program");
         loyaltyProgram.setAvailable(false);
 
-        //Two clients signed for loyalty program notification
-        loyaltyProgram.registerObserver(client1);
-        loyaltyProgram.registerObserver(client2);
-        loyaltyProgram.setAvailable(true);
-
-        //1 of 2 clients decided that he or she doesn't want to receive notifications
-        loyaltyProgram.removeObserver(client1);
-        loyaltyProgram.setAvailable(true);
-
+        loyaltyProgram.registerObserver(user2);
+        loyaltyProgram.registerObserver(user3);
+        loyaltyProgram.removeObserver(user2);
+        loyaltyProgram.notifyObservers();
     }
 }
--- trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/airbnb/users/User.java
+++ trunk/GoJava6/dtarasenko/intro/src/main/java/com/gojava6/airbnb/users/User.java
@@ -1,16 +1,26 @@
 package com.gojava6.airbnb.users;
 
-public abstract class User {
+import com.gojava6.airbnb.apartment.Apartment;
+import com.gojava6.airbnb.observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User implements Observer {
 
     private static int userIdCounter;
     private int userId;
     private String name;
     private String surName;
     private String email;
+    private UserType userType;
+    private List<Apartment> apartmentList;
+
 
     public User() {
         userIdCounter += 1;
         this.userId =  userIdCounter;
+        this.userType = UserType.CLIENT;
     }
 
     public int getUserId() {
@@ -40,4 +50,29 @@ public abstract class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Apartment> getApartmentList() {
+        return apartmentList;
+    }
+
+    public void becomeHost() {
+        this.userType = UserType.HOST;
+        this.apartmentList = new ArrayList<Apartment>();
+        System.out.println(name + " became host");
+    }
+
+    public void addApartment(Apartment apartment) {
+        if (userType.equals(UserType.HOST)) {
+            apartmentList.add(apartment);
+            System.out.println(name + " added apartment");
+        } else {
+            System.out.println("Only hosts can add apartments");
+        }
+    }
+
+    @Override
+    public void update(String loyaltyProgramName) {
+        System.out.println(getName() + ", " + loyaltyProgramName + " is available for you!");
+    }
+
 }
