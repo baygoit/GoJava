79d35cb73f1ef2a7cd9817c4238f216b06328788
Slavik Shcherbak <slva@ukr.net>
2015-10-06 09:58:59 +0300

    parsing

--- trunk/GoJava6/vshcherbak/intro/src/main/java/home_work/parsing/Parsing.java
+++ trunk/GoJava6/vshcherbak/intro/src/main/java/home_work/parsing/Parsing.java
@@ -1,27 +1,35 @@
 package home_work.parsing;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.io.IOException;
 
-public class Parsing {
-    private static SortedSet<Employee> employes = new TreeSet<>();
+public class Parsing  {
+    private Set<Employee> employes = new TreeSet<>();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader fin = new BufferedReader( new InputStreamReader(new FileInputStream(
-                "C:\\Java\\GoJava\\trunk\\GoJava6\\vshcherbak\\intro\\src\\main\\java\\home_work\\parsing\\input.txt")));
+    public void parser(BufferedReader fin) {
         String input = "";
-        while ( (input = fin.readLine()) != null ){
-            String[] data = input.split(" ");
-            int managerID = Integer.parseInt(data[0]);
-            String name = data[1];
-            int ID = Integer.parseInt(data[2]);
-            //System.out.println(ID + " " + name + " " + managerID);
-            Employee employee = new Employee(ID, name, managerID);
-            employes.add(employee);
+        try {
+            while ((input = fin.readLine()) != null) {
+                String[] data = input.split(" ");
+                int managerID = Integer.parseInt(data[0]);
+                String name = data[1];
+                int ID = Integer.parseInt(data[2]);
+                Employee employee = new Employee(ID, name, managerID);
+                employes.add(employee);
+            }
+        } catch (IOException e) {
+            System.out.println(e);
         }
-        fin.close();
+    }
+
+    public String getEmployes() {
+        String out = "";
         for (Employee employee: employes) {
-            System.out.println(employee);
+            out += employee;
         }
+        return out;
     }
 }
