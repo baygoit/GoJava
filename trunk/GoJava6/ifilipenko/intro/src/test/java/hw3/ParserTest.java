ede46490070c2fa1801a213bb8384027f0268681
inna <inna.filipenko@gmail.com>
2015-10-09 17:19:59 +0300

    Parser: Added temp test to check sorting.

--- trunk/GoJava6/ifilipenko/intro/src/test/java/hw3/ParserTest.java
+++ trunk/GoJava6/ifilipenko/intro/src/test/java/hw3/ParserTest.java
@@ -10,9 +10,9 @@ import java.util.Queue;
 
 public class ParserTest {
 
-    String filePath1 = "C:\\Users\\ifilipenko\\Dropbox\\GitHub\\GoJava\\trunk\\GoJava6\\ifilipenko\\intro\\src\\main\\resources\\parserFile1";
-    String filePath2 = "C:\\Users\\ifilipenko\\Dropbox\\GitHub\\GoJava\\trunk\\GoJava6\\ifilipenko\\intro\\src\\main\\resources\\parserFile2";
-    String filePath3 = "C:\\Users\\ifilipenko\\Dropbox\\GitHub\\GoJava\\trunk\\GoJava6\\ifilipenko\\intro\\src\\main\\resources\\parserFile3";
+    String filePath1 = "/Users/inna/Dropbox/GitHub/GoJava/trunk/GoJava6/ifilipenko/intro/src/main/resources/parserFile1";
+    String filePath2 = "/Users/inna/Dropbox/GitHub/GoJava/trunk/GoJava6/ifilipenko/intro/src/main/resources/parserFile2";
+    String filePath3 = "/Users/inna/Dropbox/GitHub/GoJava/trunk/GoJava6/ifilipenko/intro/src/main/resources/parserFile3";
     Parser parser;
     Queue<Employee> actual;
 
@@ -61,6 +61,26 @@ public class ParserTest {
         expected.add(employee1);
 
         Assert.assertEquals(expected.toString(), actual.toString());
+    }
+
+    @Test
+    public void test(){
+        Queue<Employee> expected = new PriorityQueue<>();
+        Employee employee1 = new Employee(3, "Name4", 1);
+        Employee employee2 = new Employee(1, "Name3", 4);
+        Employee employee3 = new Employee(2, "Name2", 2);
+        Employee employee4 = new Employee(3, "Name1", 4);
+        expected.add(employee1);
+        expected.add(employee2);
+        expected.add(employee3);
+        expected.add(employee4);
+
+        for (Employee employee : expected) {
+            System.out.println(employee);
+        }
+
+
+
     }
 
 }
