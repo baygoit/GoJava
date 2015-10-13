027d23f4c41edf375af53ae42e0ef5c31a96e5c8
innafilipenko1 <inna.filipenko@gmail.com>
2015-10-13 12:50:26 +0300

    OneSwapArray: task is complete.

--- trunk/GoJava6/ifilipenko/intro/src/main/java/hw2/OneSwipeArray.java
+++ trunk/GoJava6/ifilipenko/intro/src/main/java/hw2/OneSwipeArray.java
@@ -1,7 +1,45 @@
 package hw2;
 
 public class OneSwipeArray {
+
     public boolean solution(int[] inputArray) {
+
+        if (inputArray == null || inputArray.length < 2) {
+            throw new IllegalArgumentException();
+        }
+
+        int max = inputArray[0];
+        int maxIndex = 0;
+
+        if(isSorted(inputArray)){
+            return false;
+        }
+
+        //find the maximum element in the array
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] > max) {
+                max = inputArray[i];
+                maxIndex = i;
+            }
+        }
+        //check what is next smallest element
+        for (int j = maxIndex + 1; j < inputArray.length - 1; j++) {
+            if (!(inputArray[j] < max && inputArray[j + 1] < inputArray[j]))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean arraySwaping(int[] inputArray) {
         if (inputArray == null || inputArray.length < 2) {
             throw new IllegalArgumentException();
         }
@@ -21,4 +59,6 @@ public class OneSwipeArray {
         array[i] = array[j];
         array[j] = temp;
     }
+
+
 }
--- trunk/GoJava6/ifilipenko/intro/src/test/java/hw2/OneSwipeArrayTest.java
+++ trunk/GoJava6/ifilipenko/intro/src/test/java/hw2/OneSwipeArrayTest.java
@@ -4,48 +4,43 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 public class OneSwipeArrayTest {
 
     private OneSwipeArray oneSwipeArray;
     private int[] falseArray;
     private int[] trueArray;
+    private int[] sortedArray;
 
     @Before
     public void setUp(){
         oneSwipeArray = new OneSwipeArray();
         trueArray = new int[]{1, 5, 3, 3, 7};
         falseArray = new int[]{1, 3, 5, 3, 4};
+        sortedArray = new int[]{1, 2, 3, 4, 5};
     }
 
     @Test
-    public void testSolution() {
+    public void verifyResultIsTrueIfOneSwapNeededToSortArray() {
         Assert.assertTrue(oneSwipeArray.solution(trueArray));
     }
 
     @Test
-    public void testSolutionTwo() {
+    public void verifyResultIsFalseIfAlreadySortedArray() {
+        Assert.assertFalse(oneSwipeArray.solution(sortedArray));
+    }
+
+    @Test
+    public void verifyResultIsFalseIfMoreThanOneSwapNeededToSortArray() {
         Assert.assertFalse(oneSwipeArray.solution(falseArray));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSolutionNull() {
+    public void verifyResultIsErrorIfInputIsNull() {
         oneSwipeArray.solution(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testSolutionLessThanTwo() {
+    public void verifyResultIsErrorIfInputArrayLengthIsLessThan2() {
         oneSwipeArray.solution(new int[1]);
     }
-
-    @Test
-    public void testInna() {
-        Arrays.sort(trueArray);
-        int[] expected = new int[]{1, 3, 3, 5, 7};
-        Assert.assertEquals(Arrays.toString(trueArray), Arrays.toString(expected));
-
-    }
-
-
 }
