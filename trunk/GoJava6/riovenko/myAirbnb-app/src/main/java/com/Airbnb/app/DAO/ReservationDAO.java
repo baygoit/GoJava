3c506c4a55becfc18414e697a43a0e0b260807b2
RomaRedman <Roma.Redman@gmail.com>
2015-10-17 18:55:01 +0300

    sm

--- trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/DAO/ReservationDAO.java
+++ trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/DAO/ReservationDAO.java
@@ -4,11 +4,12 @@ import com.Airbnb.app.model.ApartType;
 import com.Airbnb.app.model.Apartment;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by romanroma on 28.09.15.
  */
 public interface ReservationDAO {
-    public List<Apartment> searcApartements (ApartType apartType, String city, Date dateFrom, Date dateTo);
+    public List<Apartment> searchApartements (ApartType apartType, String city, Date dateFrom, Date dateTo);
     public void makeRedservation ();
 }
--- trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/Maps.java
+++ trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/Maps.java
@@ -15,6 +15,5 @@ public class Maps {
     public static Map<Integer, User> clients = new HashMap<Integer, User>();
     public static Map<Integer, User> hosts = new HashMap<Integer, User>();
     public static Map<Integer, Apartment> apartments = new HashMap<Integer, Apartment>();
-    public static Map<>
     public List<String> cities= new ArrayList<String>();
 }
