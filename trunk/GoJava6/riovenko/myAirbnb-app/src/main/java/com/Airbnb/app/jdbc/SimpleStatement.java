32cd697b28cfb973de1ea702810e2dfcd109f199
RomaRedman <Roma.Redman@gmail.com>
2015-10-18 19:15:06 +0300

    sm

--- trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/jdbc/SimpleStatement.java
+++ trunk/GoJava6/riovenko/myAirbnb-app/src/main/java/com/Airbnb/app/jdbc/SimpleStatement.java
@@ -8,7 +8,7 @@ import java.sql.*;
 public class SimpleStatement {
     public static void main(String[] args) throws SQLException {
         retrieve();
-        //update();
+        update();
     }
 
     private static void retrieve() throws SQLException {
