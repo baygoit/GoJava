39affbf44caa39bab174cbb299f8ece6f828d91b
TrentovskyjVova <trentovskyj@gmail.com>
2015-11-04 00:55:17 +0200

    refactoring code under code convention

--- trunk/GoJava7/trentovskyjvova/Division/src/ua/goit7/vovat/DivisionAlgorithm.java
+++ trunk/GoJava7/trentovskyjvova/Division/src/ua/goit7/vovat/DivisionAlgorithm.java
@@ -5,16 +5,10 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- *  The DivisionAlgorithm class provides static method for division an
- *  integer on integer.
- *  
- *  @author Trentovskyj
- */
 public class DivisionAlgorithm {
 
 	private static final int BASE_MULTIPLIER = 10;
-	private static final int DECIMAL_PLACES = 100;
+	private static final int DECIMAL_PLACES  = 100;
 
 	public static void main(String[] args) {
 
@@ -29,7 +23,9 @@ public class DivisionAlgorithm {
 
 		String strArray[] = operation.split("/");
 
-		ArrayList<String> log = makeDivision(Integer.parseInt(strArray[0]), Integer.parseInt(strArray[1]));
+		ArrayList<String> log = makeDivision(
+				Integer.parseInt(strArray[0]), 
+				Integer.parseInt(strArray[1]));
 
 		log.stream().forEach(n -> System.out.println(n));
 	}
@@ -37,6 +33,7 @@ public class DivisionAlgorithm {
 	private static ArrayList<String> makeDivision(int dividend, int divisor) {
 
 		ArrayList<String> result = new ArrayList<String>();
+		
 		result.add(" " + dividend + " |" + divisor);
 
 		QuotientBuilder quotientBuilder = new QuotientBuilder();
@@ -60,16 +57,8 @@ public class DivisionAlgorithm {
 		}
 	}
 
-	/**
-	 * Division dividend on divisor and return result.
-	 * 
-	 * @param dividend an integer           
-	 * @param divisor an integer         
-	 * @return a String
-	 */
 	public static String division(int dividend, int divisor) {
 		ArrayList<String> result = new ArrayList<String>();
-
 		QuotientBuilder quotientBuilder = new QuotientBuilder();
 
 		division(dividend, divisor, "", quotientBuilder, result);
@@ -77,10 +66,11 @@ public class DivisionAlgorithm {
 		return quotientBuilder.get();
 	}
 
-	private static void division(int dividend, int divisor, String leftMargin, QuotientBuilder quotientBuilder,
-			ArrayList<String> result) {
+	private static void division(int dividend, int divisor, String leftMargin,
+			QuotientBuilder quotientBuilder, ArrayList<String> result) {
 
-		int curentDividend = getCurentDividend(Integer.toString(dividend), Integer.toString(divisor), quotientBuilder);
+		int curentDividend = getCurentDividend(Integer.toString(dividend), 
+				Integer.toString(divisor), quotientBuilder);
 
 		result.add(leftMargin + " " + curentDividend);
 
@@ -91,32 +81,38 @@ public class DivisionAlgorithm {
 		leftMargin += " ";
 
 		if (checkEnd(dividend, quotientBuilder, curentDividend, product)) {
-
-			int difference = (int) Math.pow(BASE_MULTIPLIER,
-					Math.max(length(dividend) - Math.min(length(curentDividend), length(product)), 0));
+			
+			int degree = Math.max(length(dividend) 
+					- Math.min(length(curentDividend), length(product)), 0);
+			int difference = (int) Math.pow(BASE_MULTIPLIER, degree);
+			
 			if (dividend == (product * difference)) {
 				quotientBuilder.multiply(difference);
-			} else {
-				
-				int nextStepDividend = findNextStepDividend(dividend, divisor, quotientBuilder, curentDividend, product,
-						difference);
+			} else {			
+				int nextStepDividend = findNextStepDividend(dividend, divisor, 
+						quotientBuilder, curentDividend, product, difference);
 
-				division(nextStepDividend, divisor, leftMargin, quotientBuilder, result);
+				division(nextStepDividend, divisor, 
+						leftMargin, quotientBuilder, result);
 			}
 		}
 
 	}
 
-	private static boolean checkEnd(int dividend, QuotientBuilder quotientBuilder, int curentDividend, int product) {
+	private static boolean checkEnd(int dividend, 
+			QuotientBuilder quotientBuilder, int curentDividend, int product) {
+		
 		return (product % dividend != 0 || curentDividend % product != 0)
-				&& quotientBuilder.get().length() < DECIMAL_PLACES;
+				&& (quotientBuilder.get().length() < DECIMAL_PLACES);
 	}
 
-	private static int findNextStepDividend(int dividend, int divisor, QuotientBuilder quotientBuilder,
-			int curentDividend, int product, int difference) {
+	private static int findNextStepDividend(int dividend, int divisor, 
+			QuotientBuilder quotientBuilder, int curentDividend, 
+			int product, int difference) {
 		
 		int nextStepDividend = 0;
 		int minus = product * difference;
+		
 		if (curentDividend < minus) {
 			if (dividend > minus) {
 				nextStepDividend = dividend - minus;
@@ -125,7 +121,7 @@ public class DivisionAlgorithm {
 				} else {
 					int zerosToAdd = length(minus) - length(nextStepDividend) - length(product);
 					if (zerosToAdd > 0) {
-						quotientBuilder.multiply((int) Math.pow(BASE_MULTIPLIER, zerosToAdd));
+						quotientBuilder.multiply((int)Math.pow(BASE_MULTIPLIER, zerosToAdd));
 					}
 				}
 			} else {
@@ -140,23 +136,28 @@ public class DivisionAlgorithm {
 		return nextStepDividend;
 	}
 
-	private static int divide(int curentDividend, int divisor, QuotientBuilder quotientBuilder) {
+	private static int divide(int curentDividend, int divisor, 
+			QuotientBuilder quotientBuilder) {
+		
 		int c = curentDividend / divisor;
+		
 		quotientBuilder.add(c);
-		int product = c * divisor;
-		return product;
+		
+		return c * divisor;
 	}
 
-	private static Integer length(int in) {
-		return Integer.toString(in).length();
+	private static Integer length(int i) {
+		return Integer.toString(i).length();
 	}
 
-	private static Integer getCurentDividend(String dividend, String divisor, QuotientBuilder quotientBuilder) {
+	private static Integer getCurentDividend(String dividend, String divisor,
+			QuotientBuilder quotientBuilder) {
 
-		int a = Integer.parseInt(dividend.substring(0, Math.min(dividend.length(), divisor.length())));
-
+		int substring = Integer.parseInt(
+				dividend.substring(0, Math.min(dividend.length(), divisor.length())));
 		String newA = dividend;
-		while (a < Integer.parseInt(divisor)) {
+		
+		while (substring < Integer.parseInt(divisor)) {
 			if (dividend.length() > divisor.length()) {
 				newA = dividend.substring(0, divisor.length() + 1);
 			} else {
@@ -169,17 +170,17 @@ public class DivisionAlgorithm {
 					quotientBuilder.add("0");
 				}
 			}
-			a = Integer.parseInt(newA);
+			substring = Integer.parseInt(newA);
 		}
 		return Integer.parseInt(newA);
 	}
 
 	private static String scanUserInput() {
-
 		System.out.println("Enter the command like 24/48");
+		
 		Scanner scanner = new Scanner(System.in);
-
 		String input = "";
+		
 		if (scanner.hasNextLine()) {
 			input = scanner.nextLine();
 		}
