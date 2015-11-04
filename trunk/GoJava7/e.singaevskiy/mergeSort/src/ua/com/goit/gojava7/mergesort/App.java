c288d1a905abae2d41577ee6461a6e434c9e247d
Ujin <eugenicon@gmail.com>
2015-11-04 21:08:57 +0200

    refactored Console

--- trunk/GoJava7/e.singaevskiy/mergeSort/src/ua/com/goit/gojava7/mergesort/App.java
+++ trunk/GoJava7/e.singaevskiy/mergeSort/src/ua/com/goit/gojava7/mergesort/App.java
@@ -1,26 +1,37 @@
-// OLEG What is http:://sin.com? A porn side advertisement? Really?
-package com.sin.eugene;
+package ua.com.goit.gojava7.mergesort;
 
 import java.util.Arrays;
 
 public class App {
 
+	private static final String DELIMITER = " ";
+	private static final String WELCOME_MESSAGE = "Input array values divided by '" + DELIMITER + "' symbol";
+	private static final String ERROR_MESSAGE = "Illegal argument. " + WELCOME_MESSAGE;
+	
 	public static void main(String[] args) {
 
-		Console console = new Console();
-		// OLEG it looks like we have strong dependency between readUserInput() and parseUserInput(). We should remember about it and share this knowladge with other developers, or change something. May be merge both methods into one? Or return String in readUserInput()? I don't know
-		// OLEG UserInput. But we wait for array of numbers... I should be in context of the task to understand it.
-		console.readUserInput();
-		//console.setUserInput("2 0 3 6 8 7 1 9 4 5");
-		int[] numbers = console.parseUserInput();
-		// OLEG I have static methods... But ok, now it's not relevant.
-		SortMerge.sort(numbers);
-		System.out.println(Arrays.toString(numbers));
-		// OLEG what is we had any exception before? console can be not closed...
-		console.close();
+		start();
 
 	}
 	
-	
+	public static void start() {
 
+		Console console = new Console();
+		console.print(WELCOME_MESSAGE);
+		int[] numbers = null;
+		
+		while (numbers == null) {
+			try {
+				//numbers = console.parseIntArray("2 0 3 6 8 7 1 9 4 5", DELIMITER);
+				numbers = console.readIntArray(DELIMITER);
+			} catch (IllegalArgumentException e) {
+				console.print(ERROR_MESSAGE);
+			}
+		}
+		
+		SortMerge.sort(numbers);
+		console.print(Arrays.toString(numbers));
+
+	}
+	
 }
--- trunk/GoJava7/e.singaevskiy/mergeSort/src/ua/com/goit/gojava7/mergesort/Console.java
+++ trunk/GoJava7/e.singaevskiy/mergeSort/src/ua/com/goit/gojava7/mergesort/Console.java
@@ -1,60 +1,44 @@
-package com.sin.eugene;
-
+package ua.com.goit.gojava7.mergesort;
 
 import java.util.Scanner;
 
 public class Console {
 
-	private String userInput;
 	private Scanner inputReader;
+	
+	private Scanner getInputReader(){
+		if (this.inputReader == null) {
+			this.inputReader = new Scanner(System.in);			
+		}
+		return this.inputReader;
+	}
+	
+	private String readLine(){
+		if (getInputReader().hasNextLine()) {
+			return getInputReader().nextLine();
+		} else {
+			return "";
+		}
+	}
 
-	private static final String WELCOME_MESSAGE = "Input array values divided by spaces";
-	private static final String DELIMITER = " ";
+	public int[] readIntArray(String delimiter) throws IllegalArgumentException {
 
-	public Console() {
-		// OLEG do we need this emply line? This is our code convention? But in setUserInput() there is no empty line... Inconsistent
-
-		this.userInput = "";
-		this.inputReader = new Scanner(System.in);
+		return parseIntArray(readLine(), delimiter);
 
 	}
 
-	// OLEG it looks line this method do something more then just set...
-	public void setUserInput(String userInput) {
-		System.out.println(WELCOME_MESSAGE);
-		this.userInput = userInput;
-		System.out.println(this.userInput);
-	}
-
-	public void readUserInput() {
-
-		System.out.println(WELCOME_MESSAGE);
-		this.userInput = this.inputReader.nextLine();
-
-	}
-
-	public int[] parseUserInput() {
-
-		String[] numbers = this.userInput.split(DELIMITER);
-		// OLEG reSult!
-		int[] rezult = new int[numbers.length];
+	public int[] parseIntArray(String numbersInLine, String delimiter) throws IllegalArgumentException {
+		String[] numbers = numbersInLine.split(delimiter);
+		int[] result = new int[numbers.length];
 		
 		for (int i = 0; i < numbers.length; i++) {
-			// OLEG what about runtime exception? just exit application with default stacktrace?
-			rezult[i] = Integer.parseInt(numbers[i]);
+			result[i] = Integer.parseInt(numbers[i]);
 		}
-
-		return rezult;
-
+		return result;
 	}
 
-	public void close() {
-
-		this.inputReader.close();
-		// OLEG why? It clear now that Console is not just a class to work with STDIN/STDOUT, but hold some string and threat it as array of integer delimited by DELIMITER, and parse string to array of integers
-		// OLEG SRL?
-		this.userInput = "";
-
+	public void print(String message){
+		System.out.println(message);
 	}
 
 }
