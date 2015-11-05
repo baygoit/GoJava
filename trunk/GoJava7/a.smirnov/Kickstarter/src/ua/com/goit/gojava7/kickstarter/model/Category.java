724e9e8dfb927c324f4f0017371ba6ecb4f09f06
Anthon Smirnov(working account) <smirnov.a@novaposhta.ua>
2015-11-05 12:54:07 +0200

    Updated class Category.java

--- trunk/GoJava7/a.smirnov/Kickstarter/src/ua/com/goit/gojava7/kickstarter/model/Category.java
+++ trunk/GoJava7/a.smirnov/Kickstarter/src/ua/com/goit/gojava7/kickstarter/model/Category.java
@@ -6,10 +6,7 @@ import java.util.List;
 public class Category {
 	private String name;
 
-	public Category() {}
-
 	public Category(String name) {
-		super();
 		this.name = name;
 	}
 
@@ -23,16 +20,7 @@ public class Category {
 
 	@Override
 	public String toString() {
-		return "Category [name=" + name + "]";
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null || obj instanceof Category) {
-			Category category = (Category) obj;
-			return category.getName().equals(name);
-		}
-		return false;
+		return "Category: " + getName();
 	}
 
 }
