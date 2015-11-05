71d9f091511423622a6fb5fdecad70aa59245cc7
Anton Smirnov <smiranton2013@gmail.com>
2015-11-06 00:53:40 +0200

    Add view's classes

---
+++ trunk/GoJava7/a.smirnov/Kickstarter/src/ua/com/goit/gojava7/kickstarter/view/Main.java
@@ -0,0 +1,27 @@
+package ua.com.goit.gojava7.kickstarter.view;
+import java.util.List;
+
+import ua.com.goit.gojava7.kickstarter.dao.ProjectDAO;
+import ua.com.goit.gojava7.kickstarter.dao.QuoteDAO;
+import ua.com.goit.gojava7.kickstarter.model.Project;
+import ua.com.goit.gojava7.kickstarter.model.Quote;
+
+public class Main {
+	public static void main(String[] args) {
+//		ProjectDAO pr = new ProjectDAO();
+//		List<Project> projects = pr.getDataSource();
+//		
+//		
+//		for(Project project : projects) {
+//			System.out.println(project);
+//			System.out.println("===========================");
+//		}
+//		
+			PrinterCategory printerCategory = new PrinterCategory();
+			printerCategory.showMenu();
+
+		
+		
+	}
+
+}
