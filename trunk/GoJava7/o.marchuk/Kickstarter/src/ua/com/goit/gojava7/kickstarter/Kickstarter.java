d3aade5aede24eb3e40d600279a5b47f59264f7d
Oleg Marchuk <kingoleg@mail.ru>
2015-11-06 17:41:20 +0200

    Split Kickstarter to Kickstarter and KickstarterRunner

--- trunk/GoJava7/o.marchuk/Kickstarter/src/ua/com/goit/gojava7/kickstarter/Kickstarter.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/ua/com/goit/gojava7/kickstarter/Kickstarter.java
@@ -4,21 +4,22 @@ import java.util.List;
 
 import ua.com.goit.gojava7.kickstarter.console.ConsolePrinter;
 import ua.com.goit.gojava7.kickstarter.domain.Category;
-import ua.com.goit.gojava7.kickstarter.domain.Quote;
 import ua.com.goit.gojava7.kickstarter.storage.CategoryStorage;
 import ua.com.goit.gojava7.kickstarter.storage.QuoteStorage;
 
 public class Kickstarter {
+	private QuoteStorage quoteStorage;
+	private CategoryStorage categoryStorage;
+	private ConsolePrinter consolePrinter;
 
-	private static QuoteStorage quoteStorage;
-	private static CategoryStorage categoryStorage;
-	private static ConsolePrinter consolePrinter;
+	public Kickstarter(ConsolePrinter consolePrinter,
+			QuoteStorage quoteStorage, CategoryStorage categoryStorage) {
+		this.consolePrinter = consolePrinter;
+		this.quoteStorage = quoteStorage;
+		this.categoryStorage = categoryStorage;
+	}
 
-	public static void main(String[] args) {
-		quoteStorage = initQuotes();
-		categoryStorage = initCategories();
-		consolePrinter = new ConsolePrinter();
-
+	public void run() {
 		consolePrinter.print(quoteStorage.getRandomQuote());
 
 		List<Category> categories = categoryStorage.getAllCategories();
@@ -27,30 +28,7 @@ public class Kickstarter {
 		// ask use to select 1
 		// read user input
 		// show selected category
+
 	}
 
-	private static QuoteStorage initQuotes() {
-		QuoteStorage quoteStorage = new QuoteStorage();
-		quoteStorage
-				.add(new Quote(
-						"Your work is going to fill a large part of your life,"
-								+ " and the only way to be truly satisfied is to do what"
-								+ " you believe is great work. And the only way to do"
-								+ " great work is to love what you do. If you haven't"
-								+ " found it yet, keep looking. Don't settle. As with"
-								+ " all matters of the heart, you'll know when you"
-								+ " find it.", "Steve Jobs"));
-		quoteStorage.add(new Quote(
-				"Innovation distinguishes between a leader and a follower.",
-				"Steve Jobs"));
-		return quoteStorage;
-	}
-
-	private static CategoryStorage initCategories() {
-		CategoryStorage categoryStorage = new CategoryStorage();
-		categoryStorage.add(new Category("Movie"));
-		categoryStorage.add(new Category("Dances"));
-		categoryStorage.add(new Category("Food"));
-		return categoryStorage;
-	}
 }
