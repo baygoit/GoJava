044c571754341c92e663628326bc82a7f8fffa9c
Devian_ua <devian.ua@gmail.com>
2015-11-07 17:38:57 +0200

    Refactoring packages

---
+++ trunk/GoJava7/a.sukhenko/Kickstarter/src/ua/com/goit/gojava7/kickstarter/storage/QuoteStorage.java
@@ -0,0 +1,31 @@
+package ua.com.goit.gojava7.kickstarter.storage;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import ua.com.goit.gojava7.kickstarter.model.Quote;
+
+public class QuoteStorage {
+	private List<Quote> quotes = new ArrayList<>();
+	private static final Random rand = new Random();
+	
+	public Quote getRandomQuote(){
+		int random = rand.nextInt(quotes.size());
+		return quotes.get(random);
+		
+	}
+	public void addQuote(Quote q){
+		quotes.add(q);
+	}
+	
+	/**
+	 * @return Unmodifiable List of <b>Quote</b>s
+	 */
+	public List<Quote> getQuotes(){
+		return Collections.unmodifiableList(quotes);
+		
+	}
+	
+}
