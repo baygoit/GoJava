7292cc053f3117191392ce3b40dc8a75012a6e70
Ujin <eugenicon@gmail.com>
2015-11-08 22:38:48 +0200

    added hashcode() & equals()

--- trunk/GoJava7/e.singaevskiy/kickstarter/src/com/kickstarter/beans/Project.java
+++ trunk/GoJava7/e.singaevskiy/kickstarter/src/com/kickstarter/beans/Project.java
@@ -134,6 +134,49 @@ public class Project {
 		long ms = getEndDate().getTime() - System.currentTimeMillis();
 		return ms / (1000L*60L*60L*24L);
 	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (startDate == null) {
+			if (other.startDate != null)
+				return false;
+		} else if (!startDate.equals(other.startDate))
+			return false;
+		return true;
+	}
 
 	@Override
 	public String toString() {
