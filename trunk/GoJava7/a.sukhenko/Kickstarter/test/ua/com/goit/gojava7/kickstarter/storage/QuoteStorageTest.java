50da7f1afa413b957fddb96a856c0c97cf4f1bd8
Oleg <kingoleg@mail.ru>
2015-11-08 13:56:24 +0200

    Update QuoteStorageTest.java
    Code review

--- trunk/GoJava7/a.sukhenko/Kickstarter/test/ua/com/goit/gojava7/kickstarter/storage/QuoteStorageTest.java
+++ trunk/GoJava7/a.sukhenko/Kickstarter/test/ua/com/goit/gojava7/kickstarter/storage/QuoteStorageTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.CoreMatchers.is;
 import ua.com.goit.gojava7.kickstarter.model.Quote;
 
 public class QuoteStorageTest {
+	// OLEG it better to use a private access here
 	QuoteStorage quoteStorage = new QuoteStorage();
 	
 	@Before
@@ -17,10 +18,14 @@ public class QuoteStorageTest {
 		quoteStorage.addQuote(new Quote("Quote","Author"));
 	}
 	@Test
+	// OLEG what we are testing here? quoteStorage or Quote.getName? If 1st - use correct name for test. If 2 - move it to QuoteTest
 	public void testQuoteName() {
+		// OLEG it is better use just is("Quote") in "is" parameter
 		assertThat(quoteStorage.getQuotes().get(0).getQuoteName(), is(new Quote("Quote","Author").getQuoteName()));
 	}
 	@Test
+	// OLEG what we are testing here? I am sure I understand.
+	// OLEG will we use the simiplar code in the real application?
 	public void testRandomQuote(){
 		HashSet<Quote> quotesRandomHolder = new HashSet<>();
 		quoteStorage.addQuote(new Quote("Q1", "A1"));
@@ -31,6 +36,7 @@ public class QuoteStorageTest {
 			quotesRandomHolder.add(quoteStorage.getRandomQuote());
 		}
 		//System.out.println(quotesRandomHolder.size());
+		// OLEG I am not sure I see the meaning for this
 		assertTrue(quotesRandomHolder.size() > 4);
 		
 		
@@ -39,6 +45,7 @@ public class QuoteStorageTest {
 	@Test (expected = UnsupportedOperationException.class)
 	public void testUnmodifiableList(){
 		quoteStorage.getQuotes().add(new Quote("Some Quote", "Some Author"));
+		// OLEG unmodifieable list doesnot suuport size()? Hm
 		assertThat(quoteStorage.getQuotes().size(), is(1));
 	}
 }
