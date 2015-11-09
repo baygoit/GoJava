b3329c2631084bbd71a2b2c857f8ed7c1f5c1009
Marina Dobritsa <maramister@gmail.com>
2015-11-09 15:56:23 +0200

    added tested. will be changed

---
+++ trunk/GoJava7/dobritsa/KickstarterDobritsa/test/ua/com/goit/gojava7/kickstarter/domain/QouteTest.java
@@ -0,0 +1,21 @@
+package ua.com.goit.gojava7.kickstarter.domain;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class QouteTest {
+	private Quote quote = new Quote("TestText", "TestAuthor");
+
+	@Test
+	public void testGetText() {
+		assertThat(quote.getText(), is("TestText"));
+	}
+	
+	@Test
+	public void testGetAuthor() {
+		assertThat(quote.getAuthor(), is("TestAuthor"));
+	}	
+}
