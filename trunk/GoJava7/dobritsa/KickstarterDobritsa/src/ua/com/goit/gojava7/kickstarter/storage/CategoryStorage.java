ec1652fc0736e8db30d6ad7e984fa1a6ead88529
Marina Dobritsa <maramister@gmail.com>
2015-11-10 10:31:08 +0200

    added list of project to category class

--- trunk/GoJava7/dobritsa/KickstarterDobritsa/src/ua/com/goit/gojava7/kickstarter/storage/CategoryStorage.java
+++ trunk/GoJava7/dobritsa/KickstarterDobritsa/src/ua/com/goit/gojava7/kickstarter/storage/CategoryStorage.java
@@ -1,6 +1,5 @@
 package ua.com.goit.gojava7.kickstarter.storage;
 
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -10,20 +9,20 @@ import ua.com.goit.gojava7.kickstarter.domain.Category;
 public class CategoryStorage {
 
 	private List<Category> categories = new ArrayList<Category>();
-		
+
 	public void add(Category category) {
 		categories.add(category);
 	}
-	
+
 	public List<Category> get() {
-		return Collections.unmodifiableList(categories);		
+		return Collections.unmodifiableList(categories);
 	}
-	
+
 	public Category get(int index) {
-		return categories.get(index);		
+		return categories.get(index);
 	}
-	
+
 	public Integer size() {
-		return categories.size();		
+		return categories.size();
 	}
 }
