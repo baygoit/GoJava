69cdf8d1c3d6453c854065762c23b5fc437a2a07
Artur Sukhenko <artur.sukhenko@gmail.com>
2015-11-12 13:18:55 +0200

    Forgot to add ProjectManager and ProjectManagerTest ))

---
+++ trunk/GoJava7/a.sukhenko/Kickstarter/test/ua/com/goit/gojava7/kickstarter/storage/ProjectManagerTest.java
@@ -0,0 +1,28 @@
+package ua.com.goit.gojava7.kickstarter.storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import ua.com.goit.gojava7.kickstarter.model.Project;
+
+public class ProjectManagerTest {
+	ProjectManager projectManager = new ProjectManager();
+	
+	
+	@Test
+	public void testAddProject() {
+		projectManager.addProject(new Project());
+		assertThat(projectManager.getProjects().size(), is(1));
+	}
+	
+	@Test
+	public void testGetProjectByName(){
+		Project project = new Project();
+		String projectName = "ProjectName";
+		project.setProjectName(projectName);
+		projectManager.addProject(project);
+		assertThat(projectManager.getProjectByName(projectName),is(project));
+	}
+
+}
