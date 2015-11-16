d0321692995af9b51e6315f24da308a06c9fc7ee
Oleg Marchuk <kingoleg@mail.ru>
2015-11-16 16:27:05 +0200

    Added project actions menu

--- trunk/GoJava7/o.marchuk/Kickstarter/test/ua/com/goit/gojava7/kickstarter/KickstarterTest.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/test/ua/com/goit/gojava7/kickstarter/KickstarterTest.java
@@ -2,11 +2,14 @@ package ua.com.goit.gojava7.kickstarter;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.contains;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +20,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import ua.com.goit.gojava7.kickstarter.console.ConsolePrinter;
 import ua.com.goit.gojava7.kickstarter.console.ConsoleScanner;
 import ua.com.goit.gojava7.kickstarter.domain.Category;
+import ua.com.goit.gojava7.kickstarter.domain.Project;
 import ua.com.goit.gojava7.kickstarter.storage.CategoryStorage;
 import ua.com.goit.gojava7.kickstarter.storage.QuoteStorage;
 
@@ -79,6 +83,58 @@ public class KickstarterTest {
 
 		verify(consolePrinter).printCategoryWithProjects(any(Category.class));
 		verify(consolePrinter).print(contains("Bye"));
+	}
+
+	// cut
+	@Test
+	public void testShowProjectsMenuEntered0SaysBye() {
+		Category selectedCategory = new Category("category name");
+
+		kickstarter.showProjectsMenu(selectedCategory);
+
+		verify(consolePrinter).print(contains("Go up"));
+	}
+
+	@Test
+	public void testShowProjectsMenuEnter1NoProjectsAtAll() {
+		Category selectedCategory = new Category("category name");
+
+		when(consoleScanner.getInt()).thenReturn(1, 0);
+
+		kickstarter.showProjectsMenu(selectedCategory);
+
+		verify(consolePrinter).print(contains("Please, enter the number between"));
+		verify(consolePrinter).print(contains("Go up"));
+	}
+
+	@Test
+	public void testShowProjectsMenuEnter_1NoProjectsAtAll() {
+		Category selectedCategory = new Category("category name");
+
+		when(consoleScanner.getInt()).thenReturn(-1, 0);
+
+		kickstarter.showProjectsMenu(selectedCategory);
+
+		verify(consolePrinter).print(contains("Please, enter the number between"));
+		verify(consolePrinter).print(contains("Go up"));
+	}
+
+	@Test
+	public void testShowProjectsMenuEnter1Has1Project() {
+		Category selectedCategory = new Category("category name");
+
+		Set<Project> projects = new HashSet<Project>();
+		projects.add(new Project("project name", "short description", 50, 10));
+
+		selectedCategory.setProjects(projects);
+
+		when(consoleScanner.getInt()).thenReturn(1, 0);
+
+		kickstarter.showProjectsMenu(selectedCategory);
+
+		verify(consolePrinter, times(2)).printCategoryWithProjects(any(Category.class));
+		verify(consolePrinter).print(contains("You selected the project number"));
+		verify(consolePrinter, times(2)).print(contains("Go up"));
 	}
 
 }
