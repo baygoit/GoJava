7cc3c21b2949b79098bdaed73460b762b0944749
vladimirkomlev <vladimir.komlev00@gmail.com>
2015-11-16 02:03:27 +0200

    Update U.S. 5

--- trunk/GoJava7/v.komlev/Kikstarter/src/ua/com/goit/gojava7/kikstarter/Kikstarter.java
+++ trunk/GoJava7/v.komlev/Kikstarter/src/ua/com/goit/gojava7/kikstarter/Kikstarter.java
@@ -1,37 +1,57 @@
 package ua.com.goit.gojava7.kikstarter;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Kikstarter {
 
-	/**
-	 * @throws IOException
-	 * 
-	 */
+	private ConsoleReader consoleReader;
+	private ConsolePrinter consolePrinter;
+	private QuotesStorage quotesStorage;
+	private CategoryStorage categoryStorage;
 
-	public static void main(String[] args) throws IOException {
-		String strResult;
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				System.in));
-		Quote quote = new Quote();
-		Project project = new Project();
-		Category category = new Category();
-
-		// show quote random
-		System.out.println(quote.getQuoteRandom());
-		System.out.println("====================");
-
-		// show all categories
-		category.getMapCategory();
-
-		System.out.println("====================");
-		System.out.println("Selec one of the categories from list");
-		strResult = reader.readLine();
-		System.out.println("You selected category #" + strResult);
-
-		project.getProject(Integer.parseInt(strResult));
-
+	public Kikstarter(ConsoleReader consoleReader, ConsolePrinter consolePrinter,
+			QuotesStorage quotesStorage, CategoryStorage categoryStorage) {
+		this.consoleReader = consoleReader;
+		this.consolePrinter = consolePrinter;
+		this.quotesStorage = quotesStorage;
+		this.categoryStorage = categoryStorage;
 	}
+
+	public void startUp() throws IOException {
+		consolePrinter.printQuote(quotesStorage.getRandomQuote());
+		boolean stopWhile = true;
+
+		do {
+			consolePrinter.printAllCategories(categoryStorage);
+			consolePrinter.printString("Please choose the category from list");
+
+			int numberOfSelectedCategory = (consoleReader.getNumberFromConsoel()) - 1;
+
+			try {
+				consolePrinter.printCategory(categoryStorage.getCategory(numberOfSelectedCategory));
+			} catch (Exception e) {
+				System.out.println("there is no such category!");
+				continue;
+			}
+
+			do {
+				consolePrinter.printProjectsOfCurrentCategory(categoryStorage,
+						numberOfSelectedCategory);
+
+				consolePrinter.printString("Enter number project; Enter 0 ot see all categories");
+				int numberOfselectedProject = (consoleReader.getNumberFromConsoel()) - 1;
+				if (numberOfselectedProject == -1) {
+					break;
+				} else {
+					consolePrinter.printPoject(categoryStorage
+							.getCategory(numberOfSelectedCategory).getProject(
+									numberOfselectedProject));
+				}
+
+				consolePrinter.printString("Enter 0 to see all projects");
+			} while (stopWhile);
+
+		} while (stopWhile);
+	}
+
 }
