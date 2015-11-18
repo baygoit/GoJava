b1065414bef2cf8521d4ac50bce0ce40b4dba173
Marina Dobritsa <maramister@gmail.com>
2015-11-18 09:40:04 +0200

    Implemented reading projects from separated file

--- trunk/GoJava7/dobritsa/KickstarterDobritsa/src/ua/com/goit/gojava7/kickstarter/dao/FileCategoryReader.java
+++ trunk/GoJava7/dobritsa/KickstarterDobritsa/src/ua/com/goit/gojava7/kickstarter/dao/FileCategoryReader.java
@@ -2,11 +2,11 @@ package ua.com.goit.gojava7.kickstarter.dao;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.util.List;
 import ua.com.goit.gojava7.kickstarter.domain.Category;
 
 public class FileCategoryReader extends FileReader<Category> implements CategoryReader {
-	//private List<File> categoriesFile;
 
 	public FileCategoryReader(File file) {
 		super(file);
@@ -18,27 +18,10 @@ public class FileCategoryReader extends FileReader<Category> implements Category
 		String line;
 		while ((line = fileReader.readLine()) != null) {
 			Category category = new Category(line);
-			category.addPathFile(fileReader.readLine());			
-			
-			//BufferedReader fileReaderProject = null;
-			//InputStream projectsFileSteam = new FileInputStream(file1);
-			//fileReaderProject = new BufferedReader(new InputStreamReader(projectsFileSteam));
-			//while ((fileReaderProject.readLine()) != null) {
-			//	Project project = new Project();
-			//	project.setName(fileReaderProject.readLine());
-			//	project.setDescription(fileReaderProject.readLine());
-			//	project.setGoal(new Integer(fileReaderProject.readLine()));
-			//	project.setPledged(new Integer(fileReaderProject.readLine()));
-			//	project.setDaysToGo(new Integer(fileReaderProject.readLine()));
-			//	project.setHistory(fileReaderProject.readLine());
-			//	project.setLink(fileReaderProject.readLine());
-			///	project.setQuestionFile(fileReaderProject.readLine());
-			//	project.setRewardFile(fileReaderProject.readLine());
-			//	category.add(project);
-			//}
+			FileProjectReader fpr = new FileProjectReader(new File(fileReader.readLine()));
+			category.setAll(fpr.read());
 			data.add(category);
 		}
 		return data;
-
 	}
 }
---
+++ trunk/GoJava7/dobritsa/KickstarterDobritsa/src/ua/com/goit/gojava7/kickstarter/dao/FileProjectReader.java
@@ -0,0 +1,32 @@
+package ua.com.goit.gojava7.kickstarter.dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import ua.com.goit.gojava7.kickstarter.domain.Project;
+
+public class FileProjectReader extends FileReader<Project> {
+
+	public FileProjectReader(File file) {
+		super(file);
+	}
+
+	@Override
+	public List<Project> readIt() throws IOException {
+		while ((fileReader.readLine()) != null) {
+			Project project = new Project();
+			project.setName(fileReader.readLine());
+			project.setDescription(fileReader.readLine());
+			project.setGoal(new Integer(fileReader.readLine()));
+			project.setPledged(new Integer(fileReader.readLine()));
+			project.setDaysToGo(new Integer(fileReader.readLine()));
+			project.setHistory(fileReader.readLine());
+			project.setLink(fileReader.readLine());
+			project.setQuestionFile(fileReader.readLine());
+			project.setRewardFile(fileReader.readLine());
+			data.add(project);
+		}
+		return data;
+	}
+}
