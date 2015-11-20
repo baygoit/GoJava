8885ab7bb2ec64aa3e3f2eba284229c49d4c7920
Marina Dobritsa <maramister@gmail.com>
2015-11-20 13:32:41 +0200

    fixed bug, I mean

--- trunk/GoJava7/dobritsa/KickstarterDobritsa/test/ua/com/goit/gojava7/kickstarter/console/ConsoleScannerTest.java
+++ trunk/GoJava7/dobritsa/KickstarterDobritsa/test/ua/com/goit/gojava7/kickstarter/console/ConsoleScannerTest.java
@@ -17,81 +17,84 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 @RunWith(MockitoJUnitRunner.class)
-public class ConsoleScannerTest {
-	
+public class ConsoleScannerTest {	
+
 	BufferedReader bufferedReader = org.mockito.Mockito.mock(BufferedReader.class);
 
 	@Mock
 	private PrintStream printSteam;
-	
 
 	@InjectMocks
 	private ConsoleScanner consoleScanner = new ConsoleScanner();
-		
+
 	@Test
-	public void testGetIntEntered0() throws IOException {		
+	public void testGetIntEntered0() throws IOException {
 		when(bufferedReader.readLine()).thenReturn("0");
-		assertThat(consoleScanner.getInt(0, 3), is(0));	
+		assertThat(consoleScanner.getInt(0, 3), is(0));
 	}
-	
+
 	@Test
-	public void testGetIntEntered1() throws IOException {		
+	public void testGetIntEntered1() throws IOException {
 		when(bufferedReader.readLine()).thenReturn("1");
-		assertThat(consoleScanner.getInt(0, 3), is(1));	
+		assertThat(consoleScanner.getInt(0, 3), is(1));
 	}
-	
+
 	@Test
-	public void testGetIntEnteredWrongNumberThenEntered0() throws IOException {	
+	public void testGetIntEnteredWrongNumberThenEntered0() throws IOException {
 		PrintStream printSteam = mock(PrintStream.class);
 		System.setOut(printSteam);
 		when(bufferedReader.readLine()).thenReturn("10").thenReturn("0");
-		assertThat(consoleScanner.getInt(0, 3), is(0));	
+		assertThat(consoleScanner.getInt(0, 3), is(0));
 		verify(printSteam).println(contains("You should type a number FROM 0 TO 3"));
 	}
-	
+
 	@Test
-	public void testGetIntEnteredNotNumberThenEntered0() throws IOException {	
+	public void testGetIntEnteredNotNumberThenEntered0() throws IOException {
 		PrintStream printSteam = mock(PrintStream.class);
 		System.setOut(printSteam);
 		when(bufferedReader.readLine()).thenReturn("ssssss").thenReturn("0");
-		assertThat(consoleScanner.getInt(0, 3), is(0));	
+		assertThat(consoleScanner.getInt(0, 3), is(0));
 		verify(printSteam).println(contains("You should type a NUMBER from 0 to 3"));
 	}
 
-	
 	@Test
-	public void testGetOptionEnteredA() throws IOException {		
+	public void testGetOptionEnteredA() throws IOException {
 		when(bufferedReader.readLine()).thenReturn("a");
-		assertThat(consoleScanner.getOption(), is("a"));	
+		assertThat(consoleScanner.getOption(), is("a"));
 	}
 
 	@Test
-	public void testGetOptionEnteredB() throws IOException {	
+	public void testGetOptionEnteredB() throws IOException {
 		when(bufferedReader.readLine()).thenReturn("b");
-		assertThat(consoleScanner.getOption(), is("b"));	
+		assertThat(consoleScanner.getOption(), is("b"));
 	}
-	
+
 	@Test
 	public void testGetOptionEntered0() throws IOException {
 		PrintStream printSteam = mock(PrintStream.class);
 		System.setOut(printSteam);
 		when(bufferedReader.readLine()).thenReturn("0");
-		assertThat(consoleScanner.getOption(), is("0"));	
+		assertThat(consoleScanner.getOption(), is("0"));
 		verify(printSteam).println(contains("Type:"));
 	}
-	
+
 	@Test
 	public void testGetOptionEnteredWrongCharacterThan0() throws IOException {
 		PrintStream printSteam = mock(PrintStream.class);
 		System.setOut(printSteam);
 		when(bufferedReader.readLine()).thenReturn("5").thenReturn("0");
-		assertThat(consoleScanner.getOption(), is("0"));	
+		assertThat(consoleScanner.getOption(), is("0"));
 		verify(printSteam, times(2)).println(contains("Type:"));
 	}
-	
+
 	@Test
 	public void testGetString() throws IOException {
 		when(bufferedReader.readLine()).thenReturn("kdkadkw29w90");
-		assertThat(consoleScanner.getString(), is("kdkadkw29w90"));	
+		assertThat(consoleScanner.getString(), is("kdkadkw29w90"));
+	}
+	
+	@Test
+	public void testClose() {
+		
 	}
 }
