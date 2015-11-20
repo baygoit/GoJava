bfc2b1095f2d411032f0de9585e60c31d8e53148
Anthon Smirnov(working account) <smirnov.a@novaposhta.ua>
2015-11-20 17:16:37 +0200

    Refactored Kickstarter project and added console printer class testing and beans

--- trunk/GoJava7/a.smirnov/Mykickstarter/src/ua/com/goit/gojava7/kickstarter/control/KickstarterForFiles.java
+++ trunk/GoJava7/a.smirnov/Mykickstarter/src/ua/com/goit/gojava7/kickstarter/control/KickstarterForFiles.java
@@ -230,7 +230,7 @@ protected void selectCategory() {
 		} while (payment == null);
 	}
 	
-	protected void stop() {
+	public void stop() {
 		consoleScanner.close();
 	}
 }
--- trunk/GoJava7/a.smirnov/Mykickstarter/src/ua/com/goit/gojava7/kickstarter/runner/KickstarterRunner.java
+++ trunk/GoJava7/a.smirnov/Mykickstarter/src/ua/com/goit/gojava7/kickstarter/runner/KickstarterRunner.java
@@ -1,16 +1,22 @@
-package ua.com.goit.gojava7.kickstarter.control;
+package ua.com.goit.gojava7.kickstarter.runner;
+
+import ua.com.goit.gojava7.kickstarter.control.KickstarterForFiles;
+import ua.com.goit.gojava7.kickstarter.control.KickstarterForMemory;
 
 public class KickstarterRunner {
+	private static final String SEPARATOR = "**********************************************************************";
 
 	public static void main(String[] args) {
 
 		if (args.length == 0) {
+			System.out.println(SEPARATOR);
 			System.out.println("Starting program using files storages");
 			KickstarterForFiles kickstarterForFiles = new KickstarterForFiles();
 			kickstarterForFiles.start();
 			kickstarterForFiles.stop();
 
 		} else {
+			System.out.println(SEPARATOR);
 			System.out.println("Starting program using memory storages");
 			KickstarterForMemory kickstarterForMemory = new KickstarterForMemory();
 			kickstarterForMemory.start();
