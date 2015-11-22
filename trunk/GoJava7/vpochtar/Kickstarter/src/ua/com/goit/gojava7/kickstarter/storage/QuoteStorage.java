7a4e99ae459ddcae55523dffcd58e86c7ea5bfc8
vpochtar <v.pochtar@gmail.com>
2015-11-22 18:06:50 +0200

    USs 6 and 7

--- trunk/GoJava7/vpochtar/Kickstarter/src/ua/com/goit/gojava7/kickstarter/storage/QuoteStorage.java
+++ trunk/GoJava7/vpochtar/Kickstarter/src/ua/com/goit/gojava7/kickstarter/storage/QuoteStorage.java
@@ -1,13 +1,14 @@
 package ua.com.goit.gojava7.kickstarter.storage;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import ua.com.goit.gojava7.kickstarter.domain.Quote;
 
 public class QuoteStorage {
 	private Random random;
-	private ArrayList<Quote> quotes = new ArrayList<>();
+	private List<Quote> quotes = new ArrayList<>();
 	
 	public QuoteStorage(Random random) {
 		this.random = random;
