c263c7396ae816006fdad154aca79137a174c635
Dmytro Mitin <dmitin3@gmail.com>
2015-11-27 14:37:53 +0200

    fixed bug in FileCategoryStoragr for not sorted list of categories

--- trunk/GoJava7/dmytro.mitin/homework3_kickstarter/src/ua/com/goit/gojava7/kickstarter/model/storage/DatabaseQuoteStorage.java
+++ trunk/GoJava7/dmytro.mitin/homework3_kickstarter/src/ua/com/goit/gojava7/kickstarter/model/storage/DatabaseQuoteStorage.java
@@ -2,9 +2,25 @@ package ua.com.goit.gojava7.kickstarter.model.storage;
 
 import ua.com.goit.gojava7.kickstarter.model.Quote;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DatabaseQuoteStorage implements QuoteStorage {
+    public DatabaseQuoteStorage() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = null;
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/kickstarter","postgres", "password");
+            connection.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public List<Quote> getQuotes() {
         return null;
