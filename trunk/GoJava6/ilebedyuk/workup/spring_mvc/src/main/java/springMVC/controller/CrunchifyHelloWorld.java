fcd126aaa54cd327995dc0cb2760a870121b5e10
ilebedyuk <ilebeduyk@gmail.com>
2015-11-27 18:18:33 +0200

    spring MVC

---
+++ trunk/GoJava6/ilebedyuk/workup/spring_mvc/src/main/java/springMVC/controller/CrunchifyHelloWorld.java
@@ -0,0 +1,20 @@
+package springMVC.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Игорь on 27.11.2015.
+ */
+@Controller
+public class CrunchifyHelloWorld {
+
+    @RequestMapping("/welcome")
+    public ModelAndView hlloWorld() {
+
+        String message = "<br><div style='text-align:center;'>"
+                + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+        return new ModelAndView("welcome", "message", message);
+    }
+}
