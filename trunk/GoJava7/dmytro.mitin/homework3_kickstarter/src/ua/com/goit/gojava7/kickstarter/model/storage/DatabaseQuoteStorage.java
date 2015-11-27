d4aca5aeda6519d53defb7de48955fa7642e797e
Dmytro Mitin <dmitin3@gmail.com>
2015-11-27 16:07:55 +0200

    database storage for quotes implemented

--- trunk/GoJava7/dmytro.mitin/homework3_kickstarter/src/ua/com/goit/gojava7/kickstarter/model/storage/DatabaseQuoteStorage.java
+++ trunk/GoJava7/dmytro.mitin/homework3_kickstarter/src/ua/com/goit/gojava7/kickstarter/model/storage/DatabaseQuoteStorage.java
@@ -2,19 +2,19 @@ package ua.com.goit.gojava7.kickstarter.model.storage;
 
 import ua.com.goit.gojava7.kickstarter.model.Quote;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseQuoteStorage implements QuoteStorage {
+    private Connection connection = null;
+
     public DatabaseQuoteStorage() {
         try {
             Class.forName("org.postgresql.Driver");
-            Connection connection = null;
             connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/kickstarter","postgres", "password");
-            connection.close();
+                    "jdbc:postgresql://localhost:5432/kickstarter", "postgres", "password");
+
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
@@ -23,11 +23,42 @@ public class DatabaseQuoteStorage implements QuoteStorage {
 
     @Override
     public List<Quote> getQuotes() {
-        return null;
+        List<Quote> quotes = new ArrayList<>();
+
+        String query = "SELECT text, author FROM quotes";
+        try (PreparedStatement statement = connection.prepareStatement(query))
+        {
+            ResultSet result = statement.executeQuery();
+            while (result.next()) {
+                String text = result.getString("text");
+                String author = result.getString("author");
+                quotes.add(new Quote(text, author));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return quotes;
     }
 
     @Override
     public void add(Quote quote) {
+        String query = "INSERT INTO quotes (text, author) VALUES (?,?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, quote.getText());
+            statement.setString(2, quote.getAuthor());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void shutDown() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
