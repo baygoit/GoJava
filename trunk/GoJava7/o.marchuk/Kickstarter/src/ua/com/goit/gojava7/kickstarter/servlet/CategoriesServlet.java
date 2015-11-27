933efc8f6514d81365b304ef121ce3cf200c44ee
Oleg Marchuk <kingoleg@mail.ru>
2015-11-27 22:01:56 +0200

    US 1 in webapp

--- trunk/GoJava7/o.marchuk/Kickstarter/src/ua/com/goit/gojava7/kickstarter/servlet/CategoriesServlet.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/ua/com/goit/gojava7/kickstarter/servlet/CategoriesServlet.java
@@ -10,28 +10,40 @@ import javax.servlet.http.HttpServletResponse;
 
 import ua.com.goit.gojava7.kickstarter.config.DaoProvider;
 import ua.com.goit.gojava7.kickstarter.config.DataSource;
+import ua.com.goit.gojava7.kickstarter.dao.CategoryDao;
 import ua.com.goit.gojava7.kickstarter.dao.QuoteDao;
+import ua.com.goit.gojava7.kickstarter.domain.Category;
 import ua.com.goit.gojava7.kickstarter.domain.Quote;
 
-@WebServlet(urlPatterns = "/quote")
-public class QuoteServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/categories")
+public class CategoriesServlet extends HttpServlet {
 
 	private QuoteDao quoteDao;
 	private DaoProvider daoProvider;
+	private CategoryDao categoryDao;
 
 	@Override
 	public void init() throws ServletException {
-		System.out.println("myproperty" + System.getProperty("myproperty"));
-
 		daoProvider = new DaoProvider(DataSource.MYSQL);
 		daoProvider.open();
 		quoteDao = daoProvider.getQuoteDao();
+		categoryDao = daoProvider.getCategoryDao();
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Quote quote = quoteDao.getRandomQuote();
-		resp.getWriter().append("<html><head><title>Random quote</title></head><body>" + quote.getText() + "  (c) " + quote.getAuthor() + "</body></html>");
+		StringBuilder stringBuilder = new StringBuilder("<html><head><title>Categoriese</title></head><body>");
+		stringBuilder.append(quote.getText() + "  (c) " + quote.getAuthor());
+		stringBuilder.append("</br>");
+
+		for (Category category : categoryDao.getAll()) {
+			stringBuilder.append("<a href=\"category?id=" + category.getId() + "\">" + category.getName() + "</a><br/>");
+
+		}
+
+		stringBuilder.append("</body></html>");
+		resp.getWriter().append(stringBuilder);
 	}
 
 	@Override
