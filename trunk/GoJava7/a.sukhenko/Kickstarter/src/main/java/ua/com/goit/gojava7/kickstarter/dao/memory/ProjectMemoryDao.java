31a086efbad6f4fda314656d577fbc3b60a0c8cf
Artur Sukhenko <artur.sukhenko@gmail.com>
2015-12-02 17:38:17 +0200

    Dao

---
+++ trunk/GoJava7/a.sukhenko/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/memory/ProjectMemoryDao.java
@@ -0,0 +1,52 @@
+package ua.com.goit.gojava7.kickstarter.dao.memory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import ua.com.goit.gojava7.kickstarter.dao.MemoryDao;
+import ua.com.goit.gojava7.kickstarter.dao.storage.ProjectStorage;
+import ua.com.goit.gojava7.kickstarter.domain.Project;
+import ua.com.goit.gojava7.kickstarter.domain.User;
+
+public class ProjectMemoryDao extends MemoryDao<Project> implements ProjectStorage{
+
+	public ProjectMemoryDao(List<Project> data) {
+		super(data);
+	}
+
+	@Override
+	public Project getByNumber(int number) {
+		int index = number - 1;
+		return get(index);
+	}
+
+	@Override
+	public void updatePledged(Project project, int amount) {
+		project.updatePledged(amount);
+	}
+
+	@Override
+	public int getPledged(String projectName) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Project> getByCategory(String categoryName) {
+		return this.getAll().stream().filter(project -> project.getCategoryName().equals(categoryName))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public void userContributeToProject(User user, Double valueOf) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addProject(Project project) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ trunk/GoJava7/a.sukhenko/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/memory/QuoteMemoryDao.java
@@ -0,0 +1,24 @@
+package ua.com.goit.gojava7.kickstarter.dao.memory;
+
+import java.util.List;
+import java.util.Random;
+
+import ua.com.goit.gojava7.kickstarter.dao.MemoryDao;
+import ua.com.goit.gojava7.kickstarter.dao.storage.QuoteStorage;
+import ua.com.goit.gojava7.kickstarter.domain.Quote;
+
+public class QuoteMemoryDao extends MemoryDao<Quote> implements QuoteStorage{
+	private Random random;
+
+	public QuoteMemoryDao(List<Quote> data) {
+		super(data);
+	}
+
+	// TODO fix it
+	@Override
+	public Quote getRandomQuote() {
+		int randomNumber = random.nextInt(this.size());
+		return this.get(randomNumber);
+	}
+
+}
