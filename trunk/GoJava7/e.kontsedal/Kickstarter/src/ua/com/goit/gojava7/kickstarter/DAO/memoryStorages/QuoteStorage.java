aa0a2b2f899965f4fc0cba83b5f33cf942de6734
egor90wd <egor90wd@gmail.com>
2015-12-03 13:13:53 +0200

    dao

--- trunk/GoJava7/e.kontsedal/Kickstarter/src/ua/com/goit/gojava7/kickstarter/DAO/memoryStorages/QuoteStorage.java
+++ trunk/GoJava7/e.kontsedal/Kickstarter/src/ua/com/goit/gojava7/kickstarter/DAO/memoryStorages/QuoteStorage.java
@@ -1,13 +1,14 @@
-package ua.com.goit.gojava7.kickstarter.memoryStorages;
+package ua.com.goit.gojava7.kickstarter.DAO.memoryStorages;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import ua.com.goit.gojava7.kickstarter.DAO.AbstractQuoteStorage;
 import ua.com.goit.gojava7.kickstarter.model.Quote;
 
-public class QuoteStorage {
-
+public class QuoteStorage extends AbstractQuoteStorage{
+	private static int idGenerator = 0;
 	private final Random random;
 	private final List<Quote> quotes;
 	
@@ -16,16 +17,13 @@ public class QuoteStorage {
 		this.random = random;
 	}
 
-	public void addQuote(Quote quote) {
-		this.quotes.add(quote);
-	}
-	
-	public Quote takeRandomQuote() {
+	public Quote getRandomQuote() {
 		int randomNumber = random.nextInt(quotes.size());
 		return quotes.get(randomNumber);
 	}
 
-	public List<Quote> getQuotes() {
-		return quotes;
+	public void add(Quote quote) {
+		quote.setIdQuote(++idGenerator);
+		this.quotes.add(quote);
 	}
 }
--- trunk/GoJava7/e.kontsedal/Kickstarter/src/ua/com/goit/gojava7/kickstarter/model/Project.java
+++ trunk/GoJava7/e.kontsedal/Kickstarter/src/ua/com/goit/gojava7/kickstarter/model/Project.java
@@ -1,39 +1,27 @@
 package ua.com.goit.gojava7.kickstarter.model;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
 
 public class Project {
 	private String projectName;
 	private String projectShortDescription;
 	private String projectDescription;
 	private String videoUrl;
-	private HashMap<String, String> questionsAndAnswers;
 	private int projectCostNeed;
-	private int projectCostsCollected;
 	private Calendar deadline;
-	private List<Payment> payments;
-	private List<String> questions;
+	private int idProject;
+	private int idParentCategory;
 
 	public Project() {
-		// default constructor
-	}
-
-	public Project(String projectName, String projectShortDescription, int projectCostNeed, int projectDaysNeed) {
-		this.projectName = projectName;
-		this.questionsAndAnswers = new HashMap<>();
-		this.projectShortDescription = projectShortDescription;
-		this.projectCostNeed = projectCostNeed;
-		this.payments = new ArrayList<>();
-		this.questions = new ArrayList<>();
-		this.projectCostsCollected = 0;
-		deadline = new GregorianCalendar();
-		deadline.add(Calendar.DAY_OF_YEAR, projectDaysNeed);
-	}
-
+		this.projectName = "";
+		this.projectShortDescription = "";
+		this.projectDescription = "";
+		this.videoUrl = "";
+		this.projectCostNeed = 0;
+		this.deadline = new GregorianCalendar();
+	} 
+	
 	public String getProjectName() {
 		return this.projectName;
 	}
@@ -44,16 +32,11 @@ public class Project {
 
 	public int getProjectDaysLeft() {
 		Calendar today = new GregorianCalendar();
-		return (int) ((deadline.getTimeInMillis() - today.getTimeInMillis()) 
-				/ 1000 / 60 / 60 / 24);// OLEG
+		return (int) ((deadline.getTimeInMillis() - today.getTimeInMillis()) / 1000 / 60 / 60 / 24);// OLEG
 	}
 
 	public int getProjectCostNeed() {
 		return this.projectCostNeed;
-	}
-
-	public int getProjectCostCollected() {
-		return this.projectCostsCollected;
 	}
 
 	public Calendar getDeadline() {
@@ -64,44 +47,32 @@ public class Project {
 		return this.videoUrl;
 	}
 
-	public HashMap<String, String> getQuestionsAndAnswer() {
-		return this.questionsAndAnswers;
-	}
-
-	public int getProjectCostsCollected() {
-		return projectCostsCollected;
-	}
-
-	public List<Payment> getPayments() {
-		return payments;
-	}
-
 	public String getProjectDescription() {
 		return this.projectDescription;
 	}
-	
-	public List<String> getQuestionList(){
-		return this.questions;
+
+	public int getIdProject() {
+		return idProject;
 	}
 
-	public void setProjectCostCollected(int price) {
-		this.projectCostsCollected += price;
+	public int getIdParentCategory() {
+		return idParentCategory;
+	}
+
+	public void setIdProject(int idProject) {
+		this.idProject = idProject;
 	}
 
 	public void setVideoUrl(String url) {
 		this.videoUrl = url;
 	}
 
-	public void setQuestionsAndAnswer(String question, String answer) {
-		this.questionsAndAnswers.put(question, answer);
-	}
-
 	public void setProjectDescription(String description) {
 		this.projectDescription = description;
 	}
 
-	public void setDeadline(Calendar deadline) {
-		this.deadline = deadline;
+	public void setDeadline(int projectDaysNeed) {
+		this.deadline.add(Calendar.DAY_OF_YEAR, projectDaysNeed);
 	}
 
 	public void setProjectName(String projectName) {
@@ -116,12 +87,7 @@ public class Project {
 		this.projectCostNeed = projectCostNeed;
 	}
 
-	public void setPayment(String cardOwner, long cardNumber, int rechargeAmount) {
-		this.payments.add(new Payment(cardOwner, cardNumber, rechargeAmount));
-		this.projectCostsCollected += rechargeAmount;
+	public void setIdParentCategory(int idParentCategory) {
+		this.idParentCategory = idParentCategory;
 	}
-	public void addQuestion(String question){
-		this.questions.add(question);
-	}
-
 }
