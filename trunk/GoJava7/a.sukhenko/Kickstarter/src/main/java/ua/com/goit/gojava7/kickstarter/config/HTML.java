31ed568acae6cae3f37d6d61ce826eadc3f11525
Artur Sukhenko <artur.sukhenko@gmail.com>
2015-12-07 17:17:19 +0200

    Loading options from environmental variable

--- trunk/GoJava7/a.sukhenko/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/config/HTML.java
+++ trunk/GoJava7/a.sukhenko/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/config/HTML.java
@@ -5,4 +5,12 @@ public class HTML{
 	public static final String HTML_HEAD_TITLE = "<html><head><title>";
 	public static final String BODY_HTML_CLOSE = "</body></html>";
 	public static final String HOME_LINK  = "<a href=\"kickstarter\"\\> <strong>Kickstarter</strong> </a><br/>";
+	public static final String FORM = "<form action=\"payment.jsp\" method=\"post\">"+
+  "Card<br>"+
+  "<input type=\"text\" name=\"card\"> "+
+  "<br>"+
+  "Last name:<br> "+
+  "<input type=\"text\" name=\"lastname\">" +
+"</form>";
+			
 }
