dbe8451966e287a555350b77d0f666fa6928a65f
sergii4 <getman.sergei@gmail.com>
2015-12-07 21:13:12 +0200

    adapter

---
+++ trunk/GoJava6/sgetman/intro/src/main/java/com/gojava6/adapter/TestSolution.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2015 WorldTicket A/S
+ * All rights reserved.
+ */
+package com.gojava6.adapter;
+
+import java.util.List;
+
+/**
+ * @author Sergii Getman (GESE) / WorldTicket A/S
+ * @version 12/7/15
+ */
+public class TestSolution {
+    public List getSorted(List objects) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new TestSolution().getSorted(null);
+    }
+}
