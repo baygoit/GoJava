04cd6dd5646a76e343ff77a34051b48e7a18cadf
Anton Smirnov <smiranton2013@gmail.com>
2015-12-11 01:39:39 +0200

    Added SPRING implementation

--- trunk/GoJava7/a.smirnov/MyWebKickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/mysql/RewardDaoMysqlImpl.java
+++ trunk/GoJava7/a.smirnov/MyWebKickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/mysql/RewardDaoMysqlImpl.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ua.com.goit.gojava7.kickstarter.beans.Reward;
+import ua.com.goit.gojava7.kickstarter.config.ConnectionPoolSource;
 import ua.com.goit.gojava7.kickstarter.dao.RewardDao;
 
 public class RewardDaoMysqlImpl implements RewardDao {
@@ -16,15 +17,17 @@ public class RewardDaoMysqlImpl implements RewardDao {
 	private static final String DELETE_REWARD = "DELETE FROM rewards WHERE projectId = ?";
 	private static final String SELECT_PROJECTS_REWARDS = "SELECT id, projectId, donatingSum, description FROM rewards WHERE projectId = ?";
 
-	private Connection connection;
+	private ConnectionPoolSource dataSource;
 
-	public RewardDaoMysqlImpl(Connection connection) {
-		this.connection = connection;
+	public RewardDaoMysqlImpl(ConnectionPoolSource dataSource) {
+		this.dataSource = dataSource;
 	}
 
 	@Override
 	public void add(Reward reward) {
-		try (PreparedStatement statement = connection.prepareStatement(INSERT_REWARD)) {
+
+		try (Connection connection = dataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(INSERT_REWARD)) {
 
 			statement.setInt(1, reward.getProjectID());
 			statement.setInt(2, reward.getDonatingSum());
@@ -39,25 +42,29 @@ public class RewardDaoMysqlImpl implements RewardDao {
 
 	@Override
 	public void remove(Reward reward) {
-		try (PreparedStatement statement = connection.prepareStatement(DELETE_REWARD)) {
+
+		try (Connection connection = dataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(DELETE_REWARD)) {
 
 			statement.setInt(1, reward.getProjectID());
-			
+
 			statement.executeUpdate();
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public List<Reward> getProjectsRewards(int projectId) {
+
 		List<Reward> rewards = new ArrayList<>();
-		
-		try (PreparedStatement statement = connection.prepareStatement(SELECT_PROJECTS_REWARDS)) {
-			
+
+		try (Connection connection = dataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(SELECT_PROJECTS_REWARDS)) {
+
 			statement.setInt(1, projectId);
-			
+
 			ResultSet resultSet = statement.executeQuery();
 
 			while (resultSet.next()) {
@@ -66,10 +73,10 @@ public class RewardDaoMysqlImpl implements RewardDao {
 				reward.setProjectID(resultSet.getInt("projectId"));
 				reward.setDonatingSum(resultSet.getInt("donatingSum"));
 				reward.setDescription(resultSet.getString("description"));
-								
+
 				rewards.add(reward);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
