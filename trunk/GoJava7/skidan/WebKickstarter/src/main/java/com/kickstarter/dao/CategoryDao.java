ac57e13dd97c8eefe8ee2ffbff8610742ab33d46
Aleksey Skidan <aleksey06sk@gmail.com>
2015-12-10 11:10:18 +0200

    WebKickstarter  Fist servlet & connection using Spring.

---
+++ trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/dao/CategoryDao.java
@@ -0,0 +1,13 @@
+package com.kickstarter.dao;
+
+import com.kickstarter.dao.interfaces.CategoryDaoType;
+import com.kickstarter.dao.interfaces.DbCategoryDaoImpl;
+
+public class CategoryDao extends CategoryDaoType {
+
+	public CategoryDao() {
+
+		categoryDaoInterface = new DbCategoryDaoImpl();
+	}
+
+}
---
+++ trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/servlet/ProvidePaymentServlet.java
@@ -0,0 +1,44 @@
+package com.kickstarter.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kickstarter.dao.interfaces.DbProjectDaoImpl;
+import com.kickstarter.model.Project;
+
+@WebServlet("/ProvidePaymentServlet")
+public class ProvidePaymentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	DbProjectDaoImpl projectDao;
+
+	public void init() throws ServletException {
+		projectDao = new DbProjectDaoImpl();
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Integer paymentAmount = Integer.parseInt(request.getParameter("paymentAmount"));
+		int projectId = Integer.parseInt(request.getParameter("projectId"));
+
+		if(paymentAmount < 0){
+			response.sendRedirect("http://localhost:8080/WebKickstarter/SingleProjectServlet?projectId=" + projectId);
+
+		} else if(paymentAmount.equals(null)){
+			response.sendRedirect("http://localhost:8080/WebKickstarter/SingleProjectServlet?projectId=" + projectId);
+		}else{
+		Project project = projectDao.getOne(projectId);
+		int gainedSum = project.getGainedSum();
+		project.setGainedSum(gainedSum + paymentAmount);
+		projectDao.update(project);
+		response.sendRedirect("http://localhost:8080/WebKickstarter/SingleProjectServlet?projectId=" + projectId);
+	}}
+
+
+}
