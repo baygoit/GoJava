99cf702805f1040d8655b15397bf23088f2eef30
Oleg Marchuk <kingoleg@mail.ru>
2015-12-11 21:31:57 +0200

    property mapper

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/mysql/QuoteDaoMySqlImpl.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/mysql/QuoteDaoMySqlImpl.java
@@ -1,11 +1,8 @@
 package ua.com.goit.gojava7.kickstarter.dao.mysql;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import ua.com.goit.gojava7.kickstarter.dao.QuoteDao;
@@ -19,17 +16,9 @@ public class QuoteDaoMySqlImpl implements QuoteDao {
 
 	@Override
 	public Quote getRandomQuote() {
-		return jdbcTemplate.queryForObject("SELECT text, author FROM quote order by rand() limit 1 ", new RowMapper<Quote>() {
-
-			@Override
-			public Quote mapRow(ResultSet rs, int rowNum) throws SQLException {
-				Quote quote = new Quote();
-				quote.setText(rs.getString("text"));
-				quote.setAuthor(rs.getString("author"));
-				return quote;
-			}
-
-		});
+		return jdbcTemplate.queryForObject("SELECT text, author FROM quote order by rand() limit 1 ",
+				new BeanPropertyRowMapper<Quote>(
+				Quote.class));
 	}
 
 }
