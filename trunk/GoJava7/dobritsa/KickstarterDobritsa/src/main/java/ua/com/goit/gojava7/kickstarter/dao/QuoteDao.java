87a21280c4ab5ebdb8f8434939e60a12d52ceacd
Marina Dobritsa <maramister@gmail.com>
2015-12-12 11:31:40 +0200

    separated web and console

--- trunk/GoJava7/dobritsa/KickstarterDobritsa/src/main/java/ua/com/goit/gojava7/kickstarter/dao/QuoteDao.java
+++
@@ -1,9 +0,0 @@
-package ua.com.goit.gojava7.kickstarter.dao;
-
-import ua.com.goit.gojava7.kickstarter.domain.Quote;
-
-public interface QuoteDao extends Dao<Quote> {
-
-	Quote getRandomQuote();
-
-}
