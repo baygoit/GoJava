2c827044c42f18ae9910732d12c18975f53bad31
Oleg Marchuk <kingoleg@mail.ru>
2015-12-18 16:20:29 +0200

    Fixes BeanPropertyRowMapper

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/Quote.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/Quote.java
@@ -5,17 +5,16 @@ public class Quote {
 	private String text;
 	private String author;
 
-	public Quote() {
-		
-	}
-	
-	public Quote(String text, String author) {
-		this.text = text;
-		this.author = author;
-	}
-
 	public String getText() {
 		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
 	}
 
 	public String getAuthor() {
