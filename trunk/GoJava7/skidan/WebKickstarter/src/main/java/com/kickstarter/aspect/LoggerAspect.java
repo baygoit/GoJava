5f25a7614c11c62ca0406a965a3a506f6b021f77
Aleksey Skidan <aleksey06sk@gmail.com>
2015-12-24 18:01:15 +0200

    Payment model

--- trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/aspect/LoggerAspect.java
+++ trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/aspect/LoggerAspect.java
@@ -3,15 +3,22 @@ package com.kickstarter.aspect;
 import java.util.Arrays;
 import java.util.Calendar;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jdbc.core.JdbcTemplate;
 
 @Aspect
 public class LoggerAspect {
+
+//	@Autowired
+//	private JdbcTemplate jdbcTemplate;
 
 	private static final Logger log = LoggerFactory.getLogger(LoggerAspect.class);
 
@@ -19,23 +26,46 @@ public class LoggerAspect {
 	public Object testAspect(ProceedingJoinPoint joinPoint) {
 		Object object = null;
 		try {
-            log.info("Sterted method = {} with args ={} in class = {}", joinPoint.getSignature().getName(),Arrays.toString(joinPoint.getArgs()), joinPoint.getTarget().getClass().getName());
+			log.info("Started method = {} with args ={} in class = {}", joinPoint.getSignature().getName(),
+					Arrays.toString(joinPoint.getArgs()), joinPoint.getTarget().getClass().getName());
 			long millisStart = Calendar.getInstance().getTimeInMillis();
-			
-			 object = joinPoint.proceed();
-			
+			object = joinPoint.proceed();
 			long millisEnd = Calendar.getInstance().getTimeInMillis();
 			long millisFinal = millisEnd - millisStart;
-			log.info("Finished method = {} in {} millisecinds ", joinPoint.getSignature().getName(), millisFinal);
+			log.info("Finished method = {} in {} milliseconds ", joinPoint.getSignature().getName(), millisFinal);
 		} catch (Throwable e) {
-			log.error("Erroê in {}",joinPoint.getTarget().getClass());
+			log.error("Error in {}", joinPoint.getTarget().getClass());
 			e.printStackTrace();
 		}
 		return object;
 	}
-	
+
+	@Before("execution(* org.springframework.jdbc.core.JdbcTemplate.*(..))")
+	public void dbQueryAdd(JoinPoint joinPoint) throws Throwable {
+		Object[] objArr = joinPoint.getArgs();
+		String sql = (String)objArr[0];
+		System.out.println("I am an sql...........................:    " + sql);
+//		int queriesOfType = jdbcTemplate.queryForObject("select count(query) from queries",
+//			 Integer.class);
+//		System.out.println("I am the count..............: " + queriesOfType);
+//		return joinPoint.proceed();
+//		if (queriesOfType == 0) {
+//			jdbcTemplate.update("INSERT INTO queries (query) VALUES (?)", new Object[] { sql });
+//			return joinPoint.proceed();
+//		} else {
+//			return joinPoint.proceed();
+//		}
+		
+	}
+
 	@Pointcut("within(@(@org.springframework.stereotype.Component *) *)")
 	public void cutOfPoints() {
 
 	}
+
+//	@Pointcut("execution(* org.springframework.jdbc.core.JdbcTemplate.*(..))")
+//	public void getCutOfPoints() {
+//
+//	}
+
 }
--- trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/servlet/SelectedCategoryProjectsServlet.java
+++ trunk/GoJava7/skidan/WebKickstarter/src/main/java/com/kickstarter/servlet/SelectedCategoryProjectsServlet.java
@@ -12,36 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-
 import com.kickstarter.dao.interfaces.DbProjectDaoImpl;
+import com.kickstarter.dao.interfaces.PaymentDaoImpl;
 import com.kickstarter.model.Project;
-
 
 @WebServlet("/SelectedCategoryProjectsServlet")
 public class SelectedCategoryProjectsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Autowired
 	DbProjectDaoImpl projectDao;
-  
-	
+
+	@Autowired
+	PaymentDaoImpl paymentDao;
+
 	public void init() throws ServletException {
-		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
-			      getServletContext());
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, getServletContext());
 	}
-	
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String categoryTitlet = request.getParameter("categoryTitle");
 		List<Project> projectList = projectDao.getAll(categoryTitlet);
-		
-		
+		for (Project p : projectList) {
+			p.setGainedSum(paymentDao.getAll(p.getId()));
+		}
+
 		request.setAttribute("projectList", projectList);
 		request.getRequestDispatcher("/WEB-INF/SelectedCategoryProjects.jsp").forward(request, response);
 
 	}
-
-	
-	
 
 }
