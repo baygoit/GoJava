e4de24c859faa58a8d9f683c4f0eaa0da44a3075
eugene <eugenicon@gmail.com>
2015-12-26 19:50:21 +0200

    added listener servlet for hibernate session

---
+++ trunk/GoJava7/e.singaevskiy/kickstarter_web/src/main/java/ua/com/goit/gojava7/kickstarter/dao/jdbc/util/HibernateUtil.java
@@ -0,0 +1,20 @@
+package ua.com.goit.gojava7.kickstarter.dao.jdbc.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+	private static final SessionFactory sessionFactory;
+	static {
+		try {
+			sessionFactory = new Configuration().configure().buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
