fe2c6c87276dda7c4e52a57f41dabf3490384ee2
Oleg Marchuk <kingoleg@mail.ru>
2015-12-28 21:43:04 +0200

    Hibernate + Spring +bug with undeploy

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/db/QuoteDaoImpl.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/db/QuoteDaoImpl.java
@@ -2,19 +2,23 @@ package ua.com.goit.gojava7.kickstarter.dao.db;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import ua.com.goit.gojava7.kickstarter.dao.QuoteDao;
 import ua.com.goit.gojava7.kickstarter.domain.Quote;
-import ua.com.goit.gojava7.kickstarter.hibernate.HibernateUtil;
 
 @Repository
 public class QuoteDaoImpl implements QuoteDao {
 
+	@Autowired
+	private SessionFactory sessionFactory;
+
 	@Override
 	public Quote getRandomQuote() {
-		Session session = HibernateUtil.getSessionFactory().openSession();
+		Session session = sessionFactory.openSession();
 		
 		Criteria criteria = session.createCriteria(Quote.class);
 		criteria.add(Restrictions.sqlRestriction("1=1 order by rand()"));
