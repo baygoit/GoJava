597f4d58a5dd249163e3616c7149cb4473ca4591
vladimirkomlev <vladimir.komlev00@gmail.com>
2016-01-07 03:11:06 +0200

    Delete old DAO classes

--- trunk/GoJava7/v.komlev/WebKikstarter/src/main/java/ua/com/goit/gojava7/kikstarter/dao/database/ConfigDaoDb.java
+++
@@ -1,8 +0,0 @@
-package ua.com.goit.gojava7.kikstarter.dao.database;
-
-interface ConfigDaoDb {
-
-	static final String DATABASE_URL = "jdbc:oracle:thin:@localhost:1521:PROBA";
-	static final String USER_NAME = "MILLER";
-	static final String PASSWORD = "KOLOBOK";
-}
