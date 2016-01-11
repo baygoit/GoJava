57466a19590d793c39966dfab14de2ad2169e756
eugene <eugenicon@gmail.com>
2016-01-11 15:39:40 +0200

    fixed DAO tests

--- trunk/GoJava7/e.singaevskiy/kickstarter_web/src/test/java/ua/com/goit/gojava7/kickstarter/dao/jdbc/postgre/ProjectPostgreDAOTest.java
+++ trunk/GoJava7/e.singaevskiy/kickstarter_web/src/test/java/ua/com/goit/gojava7/kickstarter/dao/jdbc/postgre/ProjectPostgreDAOTest.java
@@ -6,53 +6,59 @@ import static org.junit.Assert.assertThat;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import ua.com.goit.gojava7.kickstarter.dao.jdbc.util.HibernateUtil;
 import ua.com.goit.gojava7.kickstarter.domain.Category;
 import ua.com.goit.gojava7.kickstarter.domain.Project;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:applicationContext*.xml")
 public class ProjectPostgreDAOTest {
 
     List<Project> list;
-    ProjectPostgreDAO dao;
+    
+    @Autowired
+    ProjectPostgreDAO projectPostgreDAO;
+    
+	@Autowired
+    CategoryPostgreDAO categoryPostgreDAO;
+
+	private List<Category> categories;
 
     @Before
     public void setUp() throws Exception {
-    	HibernateUtil.configure("hibernate.cfg.xml");
-    	dao = new ProjectPostgreDAO();
-
+    	categories = categoryPostgreDAO.getAll();
+    	
+    	projectPostgreDAO.clear();   	
         list = new ArrayList<>();
-        list.add(new Project(1, "p1", "a1", new Category(1, "cat1")));
-        list.add(new Project(2, "p2", "a2", new Category(1, "cat1")));
-        list.add(new Project(3, "p3", "a3", null));
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        dao.clear();
+        list.add(new Project(1, "p1", "a1", categories.get(0)));
+        list.add(new Project(2, "p2", "a2", categories.get(0)));
+        list.add(new Project(3, "p3", "a3", categories.get(1)));
+        projectPostgreDAO.addAll(list);
     }
 
     @Test
     public void testAddGetAll() {
-        dao.addAll(list);
-        assertThat(dao.getAll(), is(list));
+        assertThat(projectPostgreDAO.getAll(), is(list));
     }
 
     @Test
     public void testAddGet() {
         Project project = list.get(1);
-        list.forEach(dao::add);
-        assertThat(dao.get(project.getId()), is(project));
+        list.forEach(projectPostgreDAO::add);
+        assertThat(projectPostgreDAO.get(project.getId()), is(project));
     }
 
     @Test
     public void testGetByCategory() {
-        dao.addAll(list);
-        int catId = 1;
-        dao.getByCategory(1).forEach(p -> assertThat(p.getCategory().getId(), is(catId)));
+        projectPostgreDAO.addAll(list);
+        int catId = categories.get(0).getId();
+        projectPostgreDAO.getByCategory(catId).forEach(p -> assertThat(p.getCategory().getId(), is(catId)));
     }
 
 }
