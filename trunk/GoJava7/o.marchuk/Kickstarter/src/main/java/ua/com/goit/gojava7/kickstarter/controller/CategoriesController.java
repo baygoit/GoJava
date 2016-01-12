aff18b49ba3247d151794ef3301af4c4171b30d0
Oleg Marchuk <kingoleg@mail.ru>
2016-01-12 10:41:33 +0200

    @Transactional here and there

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/controller/CategoriesController.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/controller/CategoriesController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -31,10 +32,15 @@ public class CategoriesController {
 	}
 
 	@RequestMapping("/category")
+	@Transactional
 	public ModelAndView category(@RequestParam(name = "id") Long categoryId) {
 		ModelAndView modelAndView = new ModelAndView("category");
 		Category category = categoryDao.get(categoryId);
-		category.getProjects();
+
+		// TODO
+		if (category == null) {
+			// / 404
+		}
 		
 		log.debug("Category: {}", category);
 		log.debug("Category projects: {}", category.getProjects());
--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/Category.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/Category.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,7 +22,7 @@ public class Category {
 	private Long id;
 	@Column
 	private String name;
-	@OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+	@OneToMany(mappedBy = "category")
 	private Set<Project> projects = new HashSet<Project>();
 
 	public Category() {
