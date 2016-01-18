75da08bb84cddb3c0ba9a16b09333ee04fca324a
Oleg Marchuk <kingoleg@mail.ru>
2016-01-18 21:39:16 +0200

    Criteria (I don't like)

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/impl/CategoryDaoImpl.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/dao/impl/CategoryDaoImpl.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,9 +24,14 @@ public class CategoryDaoImpl implements CategoryDao {
 
 	@Override
 	public List<Category> getAll() {
-		Query query = em.createNamedQuery("Category.getAll");
+		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
 
-		List<Category> categories = (List<Category>) query.getResultList();
+		CriteriaQuery<Category> criteriaQuery = criteriaBuilder.createQuery(Category.class);
+		Root<Category> categoryRoot = criteriaQuery.from(Category.class);
+		criteriaQuery.select(categoryRoot);
+
+		TypedQuery<Category> query = em.createQuery(criteriaQuery);
+		List<Category> categories = query.getResultList();
 
 		return categories;
 	}
