ffd069cd407d6f010fc0f74f9f2444be5129dc32
eugene <eugenicon@gmail.com>
2016-01-20 13:11:36 +0200

    fixed native query for top donated categories

--- trunk/GoJava7/e.singaevskiy/kickstarter_web/src/main/java/ua/com/goit/gojava7/kickstarter/dao/CategoryDAO.java
+++ trunk/GoJava7/e.singaevskiy/kickstarter_web/src/main/java/ua/com/goit/gojava7/kickstarter/dao/CategoryDAO.java
@@ -1,12 +1,11 @@
 package ua.com.goit.gojava7.kickstarter.dao;
 
-import java.util.HashMap;
 import java.util.List;
 
 import ua.com.goit.gojava7.kickstarter.domain.Category;
 
 public interface CategoryDAO extends DataSource<Category>{
 
-	List<HashMap<String, Object>> getTopDonated(int limit);
+	List<ua.com.goit.gojava7.kickstarter.domain.CategoryStatistic> getTopDonated(int limit);
     
 }
--- trunk/GoJava7/e.singaevskiy/kickstarter_web/src/main/java/ua/com/goit/gojava7/kickstarter/dao/jdbc/postgre/CategoryPostgreDAO.java
+++ trunk/GoJava7/e.singaevskiy/kickstarter_web/src/main/java/ua/com/goit/gojava7/kickstarter/dao/jdbc/postgre/CategoryPostgreDAO.java
@@ -1,16 +1,17 @@
 package ua.com.goit.gojava7.kickstarter.dao.jdbc.postgre;
 
-import java.util.HashMap;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import ua.com.goit.gojava7.kickstarter.dao.CategoryDAO;
 import ua.com.goit.gojava7.kickstarter.domain.Category;
+import ua.com.goit.gojava7.kickstarter.domain.CategoryStatistic;
 
 @Repository
 public class CategoryPostgreDAO implements CategoryDAO {
@@ -46,17 +47,19 @@ public class CategoryPostgreDAO implements CategoryDAO {
 		return entityManager.createNamedQuery("Category.getAll", Category.class).getResultList();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public List<HashMap<String, Object>> getTopDonated(int limit) {
-/*		String query = "select category.id, category.name, " 
+	public List<CategoryStatistic> getTopDonated(int limit) {
+		String query = "select category.id, category.name, " 
 				+ "sum(coalesce(payment.sum,0)) as sum " 
 				+ "from category "
 				+ "left join project on category.id = project.category_id " 
 				+ "left join payment on project.id = payment.project_id "
-				+ "group by category.id, category.name " + "order by sum desc, category.name " 
-				+ "limit ?";
-		return hiberUtil.getForSQL(query, limit);*/
-		return null;
+				+ "group by category.id, category.name " 
+				+ "order by sum desc, category.name " 
+				+ "limit " + limit;
+		Query nativeQuery = entityManager.createNativeQuery(query, CategoryStatistic.class);
+		return nativeQuery.getResultList();
 	}
 
 }
