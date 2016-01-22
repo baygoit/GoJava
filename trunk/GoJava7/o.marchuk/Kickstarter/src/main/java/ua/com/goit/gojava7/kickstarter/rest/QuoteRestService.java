ab2e3aa28fc646a75900b4f23ec01f8119a10589
Oleg Marchuk <kingoleg@mail.ru>
2016-01-22 21:31:54 +0200

    rest!

---
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/rest/QuoteRestService.java
@@ -0,0 +1,22 @@
+package ua.com.goit.gojava7.kickstarter.rest;
+ 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+ 
+@Path("rest")
+public class QuoteRestService {
+ 
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
