1504e24cb50a2483e99ddb4fb4aedbb6942d37b1
TrentovskyjVova <trentovskyj@gmail.com>
2016-01-25 00:00:03 +0200

    done JPA mapping tests

--- trunk/GoJava7/trentovskyjvova/Kickstarter/src/test/java/ua/com/goit/gojava7/kickstarter/domain/RewardMappingTest.java
+++ trunk/GoJava7/trentovskyjvova/Kickstarter/src/test/java/ua/com/goit/gojava7/kickstarter/domain/RewardMappingTest.java
@@ -3,65 +3,40 @@ package ua.com.goit.gojava7.kickstarter.domain;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.junit.After;
-import org.junit.Before;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:H2/H2ApplicationContext*.xml")
 @Category(IntegrationTest.class)
+@Transactional
 public class RewardMappingTest {
-	private SessionFactory sessionFactory;
-	
-	@Before
-	public void setUp() throws Exception {
-		// A SessionFactory is set up once for an application!
-		// configures settings from hibernate.cfg.xml
-		final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-		try {
-			sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
-		} catch (Exception e) {
-			e.printStackTrace();
-			// The registry would be destroyed by the SessionFactory, but we had
-			// trouble building the SessionFactory
-			// so destroy it manually.
-			StandardServiceRegistryBuilder.destroy(registry);
-		}
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		if (sessionFactory != null) {
-			sessionFactory.close();
-		}
-	}
+	@PersistenceContext
+	private EntityManager em;
 	
 	@Test
 	public void testBasicUsage() {
-		// create a couple of events...
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
+				
 		Reward reward1 = new Reward();
 		reward1.setBenefit("Reward 1");
-
+		reward1.setProjectId(1);
+		
 		Reward reward2 = new Reward();
 		reward2.setBenefit("Reward 2");
-
-		session.save(reward1);
-		session.save(reward2);
-		session.getTransaction().commit();
-		session.close();
-
-		// now lets pull events from the database and list them
-		session = sessionFactory.openSession();
-		Reward reward = session.get(Reward.class, 1);
-		session.close();
+		reward2.setProjectId(1);
 		
-		assertThat(reward.getBenefit(), is(reward.getBenefit()));
+		em.persist(reward1);
+		em.persist(reward2);
+		
+		Reward reward = em.find(Reward.class, reward1.getId());
+		
+		assertThat(reward.getBenefit(), is(reward1.getBenefit()));
 	}
 }
