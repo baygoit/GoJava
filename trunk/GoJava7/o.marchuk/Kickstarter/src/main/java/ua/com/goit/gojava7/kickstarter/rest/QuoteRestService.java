ab96b8d30159f574c3b1d225d9a73dabbee260c7
Oleg Marchuk <kingoleg@mail.ru>
2016-01-25 18:38:13 +0200

    Spring rest service

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/rest/QuoteRestService.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/rest/QuoteRestService.java
@@ -1,19 +1,15 @@
 package ua.com.goit.gojava7.kickstarter.rest;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import ua.com.goit.gojava7.kickstarter.dao.QuoteDao;
 import ua.com.goit.gojava7.kickstarter.domain.Quote;
 
-@Produces(MediaType.APPLICATION_JSON)
-@Path("/quote")
-public class QuoteRestService extends SpringBeanAutowiringSupport {
+@RestController
+@RequestMapping("/rest")
+public class QuoteRestService {
 
 	@Autowired
 	private QuoteDao quoteDao;
@@ -24,7 +20,7 @@ public class QuoteRestService extends SpringBeanAutowiringSupport {
 	 *
 	 * @return String that will be returned as a text/plain response.
 	 */
-	@GET
+	@RequestMapping("/quote")
 	public Quote getQuote() {
 		Quote quote = quoteDao.getRandomQuote();
 		return quote;
