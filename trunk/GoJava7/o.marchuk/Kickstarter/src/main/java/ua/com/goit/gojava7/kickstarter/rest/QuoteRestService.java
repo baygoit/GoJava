bbee865176d8bd95f3cfa98dda0475389bab5af3
Oleg Marchuk <kingoleg@mail.ru>
2016-01-25 18:25:45 +0200

    Quote jersey rest service

--- trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/rest/QuoteRestService.java
+++ trunk/GoJava7/o.marchuk/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/rest/QuoteRestService.java
@@ -1,22 +1,32 @@
 package ua.com.goit.gojava7.kickstarter.rest;
- 
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
- 
-@Path("rest")
-public class QuoteRestService {
- 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Got it!";
-    }
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import ua.com.goit.gojava7.kickstarter.dao.QuoteDao;
+import ua.com.goit.gojava7.kickstarter.domain.Quote;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/quote")
+public class QuoteRestService extends SpringBeanAutowiringSupport {
+
+	@Autowired
+	private QuoteDao quoteDao;
+
+	/**
+	 * Method handling HTTP GET requests. The returned object will be sent to
+	 * the client as "text/plain" media type.
+	 *
+	 * @return String that will be returned as a text/plain response.
+	 */
+	@GET
+	public Quote getQuote() {
+		Quote quote = quoteDao.getRandomQuote();
+		return quote;
+	}
 }
