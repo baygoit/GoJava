fc5eb43acbd713b6ac6d9c287fa37295c22ef02b
TrentovskyjVova <trentovskyj@gmail.com>
2016-02-01 11:04:58 +0200

    deleted unused files

--- trunk/GoJava7/trentovskyjvova/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/User.java
+++ trunk/GoJava7/trentovskyjvova/Kickstarter/src/main/java/ua/com/goit/gojava7/kickstarter/domain/User.java
@@ -2,31 +2,31 @@ package ua.com.goit.gojava7.kickstarter.domain;
 
 public class User {
 
-	private String name = "UNKNOWN";
-	private String password = "UNKNOWN";
-
-	public User() {
-
-	}
-
-	public User(String name, String password) {
-		this.name = name;
-		this.password = password;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+	private String login;
+    private String password;
+ 
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+ 
+    public User() {
+ 
+    }
+ 
+    public String getLogin() {
+        return login;
+    }
+ 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+ 
+    public String getPassword() {
+        return password;
+    }
+ 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- trunk/GoJava7/trentovskyjvova/Kickstarter/src/main/webapp/WEB-INF/jsp/login.jsp
+++ trunk/GoJava7/trentovskyjvova/Kickstarter/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -1,21 +1,23 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <jsp:include page="header.jsp" />
 
-<form action="../login" method="post">
-	
-	<h1>
-		Please log in to continue
-	</h1>	
+<form action="/j_spring_security_check" method="post">
+
+	<h1>Please log in to continue</h1>
 	<p>
-		Name: <input type="text" name="name" />
+		Name: <input type="text" name="j_username" />
 	</p>
 	<p>
-		Password: <input type="password" name="password" />
+		Password: <input type="password" name="j_password" />
+	</p>
+	<p>
+		remember me : <input type="checkbox"
+			name="_spring_security_remember_me" />
 	</p>
 	<p>
 		<input type="submit" value="LogIn" />
 	</p>
-	
+
 </form>
 
 <jsp:include page="footer.jsp" />
