ec7b9d6d926f00443610501dc9bf49564bdcc204
Dmytro Mitin <dmitin3@gmail.com>
2016-02-01 17:07:50 +0200

    JSF hello world

---
+++ trunk/GoJava7/dmytro.mitin/homework11_jsf/src/main/java/ua/com/goit/gojava7/jsfdemo/HelloBean.java
@@ -0,0 +1,21 @@
+package ua.com.goit.gojava7.jsfdemo;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
