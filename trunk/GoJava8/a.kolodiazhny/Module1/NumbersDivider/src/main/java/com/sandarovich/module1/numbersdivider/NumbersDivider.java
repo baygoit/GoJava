5be412a79c0318ffcd89acc285e4c140255fb560
sa <sandarovich>
2016-02-21 17:28:21 +0200

    v.0.0.1 - Initial

---
+++ trunk/GoJava8/a.kolodiazhny/Module1/NumbersDivider/src/main/java/com/sandarovich/module1/numbersdivider/NumbersDivider.java
@@ -0,0 +1,10 @@
+package com.sandarovich.module1.numbersdivider;
+
+/**
+ * Hello world!
+ */
+public class NumbersDivider {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ trunk/GoJava8/a.kolodiazhny/Module1/NumbersDivider/src/test/java/com/sandarovich/module1/numbersdivider/NumbersDividerTest.java
@@ -0,0 +1,15 @@
+package com.sandarovich.module1.numbersdivider;
+
+import org.junit.Before;
+
+/**
+ * Unit test for simple NumbersDivider.
+ */
+
+public class NumbersDividerTest {
+    @Before
+    public void initialize() {
+
+    }
+
+}
