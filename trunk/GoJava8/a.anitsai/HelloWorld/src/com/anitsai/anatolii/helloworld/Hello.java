c9aa977de1669f86394248a85c269e14c683b8f1
Anatolii Anitsai <anatolii.anitsai@gmail.com>
2016-02-23 20:46:35 +0400

    hello

---
+++ trunk/GoJava8/a.anitsai/HelloWorld/src/com/anitsai/anatolii/helloworld/Hello.java
@@ -0,0 +1,9 @@
+package com.anitsai.anatolii.helloworld;
+
+public class Hello {
+
+	public void print_hello() {
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ trunk/GoJava8/a.anitsai/HelloWorld/src/com/anitsai/anatolii/helloworld/Main.java
@@ -0,0 +1,9 @@
+package com.anitsai.anatolii.helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.print_hello();
+	}
+}
