da2d5d2aca6234aea2beaa1e12808d93cf0e4ef1
dmitriyborisenko <t_a_k@ukr.net>
2016-02-29 11:53:08 +0200

    Project reuploaded

---
+++ trunk/GoJava8/d.borisenko/numbersdistance/src/ua/dborisenko/numbersdistance/AppStarter.java
@@ -0,0 +1,10 @@
+package ua.dborisenko.numbersdistance;
+
+public class AppStarter {
+
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.start();
+    }
+
+}
