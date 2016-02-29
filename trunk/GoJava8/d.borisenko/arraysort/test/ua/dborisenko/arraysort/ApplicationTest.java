07d0df3ad03df4b788f48f53231e16c0c0c38762
dmitriyborisenko <t_a_k@ukr.net>
2016-02-29 18:15:57 +0200

    Review-based partial rebuild.
    - Fixed pom.xml.
    - Added test folder.
    - Removed BeforeClass method.
    - Changed output stream handling
    - Removed redundant nullifications.
    - Changed logic of getRandomArrayTest method.
    - Changed some assert methods.

--- trunk/GoJava8/d.borisenko/arraysort/test/ua/dborisenko/arraysort/ApplicationTest.java
+++ trunk/GoJava8/d.borisenko/arraysort/test/ua/dborisenko/arraysort/ApplicationTest.java
@@ -1,6 +1,8 @@
 package ua.dborisenko.arraysort;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
@@ -11,66 +13,55 @@ import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ApplicationTest {
 
+    private static PrintStream realOutContent;
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private static Application application;
+    private Application application = new Application();
 
-    @BeforeClass
-    public static void initialize() {
-        application = new Application();
-    }
-
-    @Before
-    public void setUpStreams() {
+    @Before public void setUpStreams() {
+        realOutContent = System.out;
         System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
     }
 
     @After
     public void cleanUpStreams() {
-        System.setOut(null);
-        System.setErr(null);
+        System.setOut(realOutContent);
     }
 
     @Test
     public void getRandomArrayTest() {
-        int[] testArray1 = new int[20];
-        int[] testArray2 = new int[20];
-        assertTrue(Arrays.equals(testArray1, testArray2));
-        testArray1 = application.getRandomArray(20);
-        assertFalse(Arrays.equals(testArray1, testArray2));
-        testArray1 = null;
-        testArray2 = null;
-    }
+        int testLength = 200000;
+        int[] testArray1 = new int[testLength];
+        int[] testArray2 = new int[testLength];
+        testArray1 = this.application.getRandomArray(20);
+        Boolean arraysEqualFlag = true;
+        for (int i = 0; i < testLength; i++) {
+            if (testArray1[i] != testArray2[i]) {
+                arraysEqualFlag = false;
+                break;
+            }
+        }
+        assertFalse(arraysEqualFlag);
+     }
 
     @Test
     public void mergeSortedArraysTest() {
         int[] arrayA = { 1, 2, 5, 8, 10 };
         int[] arrayB = { 4, 4, 6, 8, 13 };
         int[] expectedArray = { 1, 2, 4, 4, 5, 6, 8, 8, 10, 13 };
-        int[] resultArray = application.mergeSortedArrays(arrayA, arrayB);
+        int[] resultArray = this.application.mergeSortedArrays(arrayA, arrayB);
         assertTrue(Arrays.equals(resultArray, expectedArray));
-        arrayA = null;
-        arrayB = null;
-        resultArray = null;
-        expectedArray = null;
     }
 
     @Test
     public void sortArrayByMergingTest() {
-
         int[] testArray = { 4, 1, 13, 2, 5, 4, 6, 8, 10, 8 };
         int[] expectedArray = { 1, 2, 4, 4, 5, 6, 8, 8, 10, 13 };
-        int[] resultArray = application.sortArrayByMerging(testArray);
+        int[] resultArray = this.application.sortArrayByMerging(testArray);
         assertTrue(Arrays.equals(resultArray, expectedArray));
-        testArray = null;
-        expectedArray = null;
-        resultArray = null;
     }
 
     @Test
@@ -79,6 +70,8 @@ public class ApplicationTest {
         Application mockApplication = spy(Application.class);
         when(mockApplication.getRandomArray(anyInt())).thenReturn(testArray);
         mockApplication.start();
-        assertEquals(outContent.toString(), "Given array:\r\n[4, 1, 13, 2, 5, 4, 6, 8, 10, 8]\r\nSorted array:\r\n[1, 2, 4, 4, 5, 6, 8, 8, 10, 13]\r\n");
+        assertEquals(
+                "Given array:\r\n[4, 1, 13, 2, 5, 4, 6, 8, 10, 8]\r\nSorted array:\r\n[1, 2, 4, 4, 5, 6, 8, 8, 10, 13]\r\n",
+                outContent.toString());
     }
 }
