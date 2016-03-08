ea84421d6a20c109b6de7b6230d90b427e50b774
AnteUlych <satoru@i.ua>
2016-03-08 17:52:19 +0200

    Kickstarter was created

---
+++ trunk/GoJava8/anton.ulytskyi/Kickstarter/src/site/CategoryPage.java
@@ -0,0 +1,70 @@
+package site;
+
+import categories.Book;
+
+public class CategoryPage extends Page {
+	CategoryPage(Book kickstarter) {
+		super(kickstarter);
+}
+
+	@Override
+	public void openPage() {
+		showVision();
+
+		String userChoice = console.read();
+		direction = new ListPage(kickstarter);
+		
+		if (userChoice.equals(FIRST_CHOICE)) {
+			console.write(PARADOX+":");
+			console.write(DECORATION);
+			console.write(kickstarter.openCatalog(PARADOX));
+			direction.openPage();
+			
+		} else if (userChoice.equals(SECOND_CHOICE)) {
+			console.write(SOPHISM+":");
+			console.write(DECORATION);
+			console.write((kickstarter.openCatalog(SOPHISM)));
+			direction.openPage();
+
+		} else if (userChoice.equals(THIRD_CHOICE)) {
+			console.write(APORIA+":");
+			console.write(DECORATION);
+			console.write((kickstarter.openCatalog(APORIA)));
+			direction.openPage();
+
+		} else if (userChoice.equals(PREVIOUS_PAGE)) {
+			openPreviousPage();
+
+		} else if (userChoice.equals(EXIT)) {
+			exit();
+
+		} else {
+			openPage();
+		}
+		
+	}
+
+	@Override
+	void openPreviousPage() {
+		direction = new StartPage(kickstarter);
+		direction.openPage();
+		
+	}
+
+	@Override
+	void showVision() {
+	
+		console.write(DECORATION);
+		console.write("Please, choose category:");
+		console.write(DECORATION);
+		console.write("1. Paradox is a statement that apparently contradicts itself and yet might be true.");
+		console.write("2. Sophism is a specious argument for displaying ingenuity in reasoning or for deceiving someone.;");
+		console.write("3. Aporia is a philosophical puzzle or a seemingly insoluble impasse in an inquiry;");
+		console.write(DECORATION);
+		console.write("p. Go to the previous page.");
+		console.write("0. Exit");
+		console.write(DECORATION);
+		
+	}
+	
+}
