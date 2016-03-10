f8926e1f31ee256f1e9a93b87362973683eb4e16
anmertrix <bioj@i.ua>
2016-03-10 03:38:34 +0200

    Add US7 and US8.

--- trunk/GoJava8/a.prokopchuk/Kickstarter/Kickstarter-Console/src/main/java/com/anmertrix/ProjectSource.java
+++ trunk/GoJava8/a.prokopchuk/Kickstarter/Kickstarter-Console/src/main/java/com/anmertrix/ProjectSource.java
@@ -8,38 +8,70 @@ public class ProjectSource {
 
 	public ProjectSource(CategorySource categorySource) {
 		this.categorySource = categorySource;
-		categorySource.getCategories().get(0).setProject(new Project("test1"));
-		categorySource.getCategories().get(0).setProject(new Project("test2"));
-		categorySource.getCategories().get(1).setProject(new Project("test3"));
-		categorySource.getCategories().get(1).setProject(new Project("test4"));
-		categorySource.getCategories().get(2).setProject(new Project("test5"));
-		categorySource.getCategories().get(2).setProject(new Project("test6"));
-		categorySource.getCategories().get(3).setProject(new Project("test7"));
-		categorySource.getCategories().get(3).setProject(new Project("test8"));
-		categorySource.getCategories().get(4).setProject(new Project("test9"));
-		categorySource.getCategories().get(4).setProject(new Project("test10"));
-		categorySource.getCategories().get(5).setProject(new Project("test11"));
-		categorySource.getCategories().get(5).setProject(new Project("test11"));
+		categorySource.getCategories().get(0).setProject(new Project("Cube soccer ball", "Test description", 5000, 1000, 12, "Test history"));
+		categorySource.getCategories().get(0).setProject(new Project("La Liga Weekly Podcast", "Test description", 8000, 300, 22, "Test history"));
+		categorySource.getCategories().get(1).setProject(new Project("Author’s vocabulary", "Test description", 7000, 200, 25, "Test history"));
+		categorySource.getCategories().get(1).setProject(new Project("Reporter camera", "Test description", 4000, 500, 5, "Test history"));
+		categorySource.getCategories().get(2).setProject(new Project("Yes Cart", "Test description", 6500, 10, 9, "Test history"));
+		categorySource.getCategories().get(2).setProject(new Project("ARoglyph", "Test description", 5600, 100, 13, "Test history"));
+		categorySource.getCategories().get(3).setProject(new Project("Start Control", "Test description", 8000, 300, 15, "Test history"));
+		categorySource.getCategories().get(3).setProject(new Project("OldStyleRacing", "Test description", 5000, 400, 11, "Test history"));
+		categorySource.getCategories().get(4).setProject(new Project("Poughkeepsie", "Test description", 5000, 400, 11, "Test history"));
+		categorySource.getCategories().get(4).setProject(new Project("photobook", "Test description", 5000, 400, 11, "Test history"));
+		categorySource.getCategories().get(5).setProject(new Project("Portraits & Stories", "Test description", 5000, 400, 11, "Test history"));
+		categorySource.getCategories().get(5).setProject(new Project("Alzheimer", "Test description", 5000, 400, 11, "Test history"));
 	}
 	
-	public String getProjectsMenu(int idCategory) {
-		StringBuffer result = new StringBuffer();
+	public String getProjectList(int idCategory) {
+		StringBuilder result = new StringBuilder();
 		result.append("Please, select project or enter 0 - to exit: \n");
 		Category category = categorySource.getCategory(idCategory);
 		List<Project> projects = category.getProjects();
 		
 		for (int i = 0; i < projects.size(); i++) {
-			result.append(i + 1).append(" - ").append(projects.get(i).getName()).append("    ");
+			Project project = projects.get(i);
+			result.append(i + 1)
+				.append(" - ")
+				.append(project.getName() + "\n")
+				.append("Description: " + project.getDescription() + "\n")
+				.append("Required budget: " + project.getRequiredBudget() + "\n")
+				.append("Gathered budget: " + project.getGatheredBudget() + "\n")
+				.append("Days left: " + project.getDaysLeft())
+				.append("\n" + "\n");
 		}
 		
 		return result.toString().trim();
 	}
 	
-	public String getNameSelectedProject(int idCategory, int idProject) {
+	public String getInfoSelectedProject(int idCategory, int idProject) {
 		Category category = categorySource.getCategory(idCategory);
 		List<Project> projects = category.getProjects();
+		Project project = projects.get(idProject);
 		
-		return projects.get(idProject).getName();
+		StringBuilder result = new StringBuilder();
+		result.append(project.getName() + "\n")
+			.append("Description: " + project.getDescription() + "\n")
+			.append("Required budget: " + project.getRequiredBudget() + "\n")
+			.append("Gathered budget: " + project.getGatheredBudget() + "\n")
+			.append("Days left: " + project.getDaysLeft() + "\n")
+			.append("History: " + project.getHistory() + "\n")
+			.append("Video URL: " + project.getURL() + "\n")
+			.append("Question and answer: \n" + project.getQuestionAnswer());
+		return result.toString();
 	}
+	
+	public String getProjectMenu() {
+		StringBuilder result = new StringBuilder();
+		
+		result.append("Please, select menu item or enter 0 - to exit: \n")
+		.append("1 - Ask a question")
+		.append("    ")
+		.append("2 - Invest project")
+		.append("    ")
+		.append("3 - Return to project list");
+	
+		return result.toString().trim();
+	}
+	
 	
 }
