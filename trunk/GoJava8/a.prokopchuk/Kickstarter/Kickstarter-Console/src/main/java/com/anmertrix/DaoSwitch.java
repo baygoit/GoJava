7be93c6510414d735a49a5f824b8c2628be21235
anmertrix <bioj@i.ua>
2016-03-24 11:32:47 +0200

    Add more info about supported modes

--- trunk/GoJava8/a.prokopchuk/Kickstarter/Kickstarter-Console/src/main/java/com/anmertrix/DaoSwitch.java
+++ trunk/GoJava8/a.prokopchuk/Kickstarter/Kickstarter-Console/src/main/java/com/anmertrix/DaoSwitch.java
@@ -1,31 +1,34 @@
 package com.anmertrix;
 
+import java.util.Arrays;
+
 class DaoSwitch {
 	
 	private static final String KICKSTARTER_MODE = "KICKSTARTER_MODE";
 
 	private static enum DataSourceTypes {
-		
+
 		MEMORY, FILE, SQL;
-		
+
 		private static String initMode() {
 			String mode = System.getenv(KICKSTARTER_MODE).toUpperCase();
-			// TODO Add more info about supported modes
 			// TODO Add more checks
 			if (mode == null || mode.isEmpty() || !contains(mode)) {
-				throw new IllegalStateException("Environment variable " + KICKSTARTER_MODE + " is not found or empty.");
+				throw new IllegalStateException("Environment variable "
+						+ KICKSTARTER_MODE + " is not found or empty. Please, establish any variable: " + Arrays.asList(DataSourceTypes.values()).toString().replace("[", "").replace("]", "") );
 			}
 			System.err.println("Mode is " + mode);
 			return mode;
 		}
-		
-		public static boolean contains(String mode)
-		  {
-		      for(DataSourceTypes choice:values())
-		           if (choice.name().equals(mode)) 
-		              return true;
-		      return false;
-		  }
+
+		public static boolean contains(String mode) {
+			for (DataSourceTypes choice : values()) {
+				if (choice.name().equals(mode)) {
+					return true;
+				}
+			}
+			return false;
+		}
 	}
 	
 	public static String getMode() {
